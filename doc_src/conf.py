254447039e7f58892e7060a7841386f02ddc6e7c
Pierre Raybaut <pierre.raybaut@gmail.com>
2009-08-10 22:29:02 +0200

    Updated documentation

--- doc_src/conf.py
+++ doc_src/conf.py
@@ -96,8 +96,8 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'sidebarbgcolor': '#227A2B',
-                      'sidebarlinkcolor': '#98ff99'}
+## html_theme_options = {'sidebarbgcolor': '#227A2B',
+##                       'sidebarlinkcolor': '#98ff99'}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
