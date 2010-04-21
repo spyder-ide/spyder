f1c39f74e09032261252a7141cead4734bf49171
Pierre Raybaut <pierre.raybaut@gmail.com>
2010-04-21 15:02:27 +0200

    Matplotlib figure "Options" button: added support for image parameters (interpolation, colormap, ...)

--- spyderlib/widgets/figureoptions.py
+++ spyderlib/widgets/figureoptions.py
@@ -9,6 +9,12 @@
 from spyderlib.widgets.formlayout import fedit
 from spyderlib.config import get_icon
 
+import matplotlib.cm, matplotlib.image
+
+
+COLORMAPS = matplotlib.cm.datad.keys()
+INTERPOLATIONS = matplotlib.image.AxesImage._interpd.keys()
+
 LINESTYLES = {
               '-': 'Solid',
               '--': 'Dashed',
@@ -46,6 +52,7 @@ MARKERS = {
 COLORS = {'b': '#0000ff', 'g': '#00ff00', 'r': '#ff0000', 'c': '#ff00ff',
           'm': '#ff00ff', 'y': '#ffff00', 'k': '#000000', 'w': '#ffffff'}
 
+
 def col2hex(color):
     """Convert matplotlib color to hex"""
     return COLORS.get(color, color)
@@ -55,6 +62,7 @@ def figure_edit(axes, parent=None):
     sep = (None, None) # separator
     
     has_curve = len(axes.get_lines()) > 0
+    has_image = len(axes.get_images()) > 0
     
     # Get / General
     xmin, xmax = axes.get_xlim()
@@ -102,14 +110,51 @@ def figure_edit(axes, parent=None):
                          ]
             curves.append([curvedata, label, ""])
         
+    if has_image:
+        # Get / Images
+        imagedict = {}
+        for image in axes.get_images():
+            label = image.get_label()
+            if label == '_nolegend_':
+                continue
+            imagedict[label] = image
+        images = []
+        interpolations = zip(INTERPOLATIONS, INTERPOLATIONS)
+        colormaps = zip(COLORMAPS, COLORMAPS)
+        imagelabels = sorted(imagedict.keys())
+        for label in imagelabels:
+            image = imagedict[label]
+            xmin, xmax, ymin, ymax = image.get_extent()
+            cmap_name = image.get_cmap().name
+            imagedata = [
+                         ('Label', label),
+                         sep,
+                         ('Colormap', [cmap_name] + colormaps),
+                         ('Alpha', image.get_alpha()),
+                         ('Interpolation',
+                          [image.get_interpolation()] + interpolations),
+                         ('Resize filter radius', image.get_filterrad()),
+                         sep,
+                         (None, '<b>Extent</b>'),
+                         ('xmin', xmin), ('xmax', xmax),
+                         ('ymin', ymin), ('ymax', ymax),
+                         ]
+            images.append([imagedata, label, ""])
+        
     datalist = [(general, "Axes", "")]
     if has_curve:
         datalist.append((curves, "Curves", ""))
+    if has_image:
+        datalist.append((images, "Images", ""))
         
     def apply_callback(data):
         """This function will be called to apply changes"""
-        if has_curve:
+        if has_curve and has_image:
+            general, curves, images = data
+        elif has_curve:
             general, curves = data
+        elif has_image:
+            general, images = data
         else:
             general, = data
             
@@ -125,10 +170,10 @@ def figure_edit(axes, parent=None):
         
         if has_curve:
             # Set / Curves
-            for index, curve in enumerate(curves):
+            for index, curveopts in enumerate(curves):
                 line = linedict[curvelabels[index]]
-                label, linestyle, linewidth, color, \
-                    marker, markersize, markerfacecolor, markeredgecolor = curve
+                label, linestyle, linewidth, color, marker, markersize, \
+                    markerfacecolor, markeredgecolor = curveopts
                 line.set_label(label)
                 line.set_linestyle(linestyle)
                 line.set_linewidth(linewidth)
@@ -138,6 +183,24 @@ def figure_edit(axes, parent=None):
                     line.set_markersize(markersize)
                     line.set_markerfacecolor(markerfacecolor)
                     line.set_markeredgecolor(markeredgecolor)
+        
+        if has_image:
+            # Set / Images
+            for index, imageopts in enumerate(images):
+                image = imagedict[imagelabels[index]]
+                
+                label, cmap_name, alpha, interpolation, \
+                    filterrad, xmin, xmax, ymin, ymax = imageopts
+                image.set_label(label)
+                image.set_cmap(matplotlib.cm.get_cmap(cmap_name))
+                if alpha >= 0 and alpha <= 1:
+                    # Ignoring invalid values
+                    image.set_alpha(alpha)
+                image.set_interpolation(interpolation)
+                if filterrad > 0:
+                    # Ignoring invalid values
+                    image.set_filterrad(filterrad)
+                image.set_extent((xmin, xmax, ymin, ymax))
             
         # Redraw
         if axes.get_legend() is not None:
