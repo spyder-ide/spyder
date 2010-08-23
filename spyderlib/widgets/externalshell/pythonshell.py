9a73fa6c3bd8157a9462fbf649edbbf4d24e2ec9
Pierre Raybaut <pierre.raybaut@gmail.com>
2010-08-23 10:17:53 +0200

    Fixed Issue 308: Unable to run Python scripts located on Windows network shares

--- spyderlib/widgets/externalshell/pythonshell.py
+++ spyderlib/widgets/externalshell/pythonshell.py
@@ -301,11 +301,11 @@ class ExternalPythonShell(ExternalShellBase):
             p_args.append('-i')
         if self.debug_action.isChecked():
             p_args.extend(['-m', 'pdb'])
-        if os.name == 'nt':
-            # When calling pdb on Windows, one has to double the backslashes 
-            # to help Python escaping these characters (otherwise, for example,
-            # '\t' will be interpreted as a tabulation):
-            p_args.append(self.fname.replace(os.sep, os.sep*2))
+        if os.name == 'nt' and self.debug_action.isChecked():
+            # When calling pdb on Windows, one has to replace backslashes by
+            # slashes to avoid confusion with escape characters (otherwise, 
+            # for example, '\t' will be interpreted as a tabulation):
+            p_args.append(osp.normpath(self.fname).replace(os.sep, '/'))
         else:
             p_args.append(self.fname)
         
