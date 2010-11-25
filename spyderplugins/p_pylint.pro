9269f5117b45e64ba90dd828872979375fecfcbb
Pierre Raybaut <pierre.raybaut@gmail.com>
2010-11-25 16:02:31 +0100

    Added missing pylint translations

--- spyderplugins/p_pylint.pro
+++ spyderplugins/p_pylint.pro
@@ -1,3 +1,4 @@
 SOURCES	     += p_pylint.py
+SOURCES	     += widgets/pylintgui.py
 
 TRANSLATIONS += p_pylint_fr.ts
--- spyderplugins/p_pylint_fr.ts
+++ spyderplugins/p_pylint_fr.ts
@@ -4,75 +4,180 @@
 <context>
     <name>Pylint</name>
     <message>
-        <location filename="p_pylint.py" line="73"/>
+        <location filename="widgets/pylintgui.py" line="70"/>
+        <source>Results for </source>
+        <translation>Résultats pour </translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="75"/>
+        <source>Convention</source>
+        <translation>Convention</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="77"/>
+        <source>Refactor</source>
+        <translation>Factorisation</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="79"/>
+        <source>Warning</source>
+        <translation>Avertissement</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="304"/>
+        <source>Error</source>
+        <translation>Erreur</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="160"/>
+        <source>Stop</source>
+        <translation>Arrêter</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="162"/>
+        <source>Stop current analysis</source>
+        <translation>Arrêter l&apos;analyse en cours</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="305"/>
+        <source>Process failed to start</source>
+        <translation>Le processus n&apos;a pas pu démarrer</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="405"/>
+        <source>Source code has not been rated yet.</source>
+        <translation>Le code source n&apos;a pas encore été analysé.</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="428"/>
+        <source>previous run:</source>
+        <translation>analyse précédente :</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="217"/>
+        <source>Please install &lt;b&gt;pylint&lt;/b&gt;:</source>
+        <translation>Merci d&apos;installer au préalable &lt;b&gt;pylint&lt;/b&gt; :</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="424"/>
+        <source>Global evaluation:</source>
+        <translation>Évaluation globale :</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="156"/>
+        <source>Analyze</source>
+        <translation>Analyser</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="157"/>
+        <source>Run analysis</source>
+        <translation>Analyser le code source</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="175"/>
+        <source>Output</source>
+        <translation>Sortie</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="178"/>
+        <source>Complete Pylint output</source>
+        <translation>Sortie complète de l&apos;analyse Pylint</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="278"/>
+        <source>Pylint output</source>
+        <translation>Analyse Pylint</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="412"/>
+        <source>Analysis did not succeed (see output for more details).</source>
+        <translation>L&apos;analyse n&apos;a pas réussi (voir la sortie pour plus de détails).</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="241"/>
+        <source>Select Python script</source>
+        <translation>Sélectionner un script Python</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="242"/>
+        <source>Python scripts</source>
+        <translation>Scripts Python</translation>
+    </message>
+    <message>
+        <location filename="widgets/pylintgui.py" line="214"/>
+        <source>Pylint script was not found. Please add &quot;%s&quot; to PATH.</source>
+        <translation>Le script pylint est introuvable. Merci d&apos;ajouter &quot;%s&quot; au PATH.</translation>
+    </message>
+    <message>
+        <location filename="p_pylint.py" line="67"/>
         <source>Pylint</source>
         <translation>Pylint</translation>
     </message>
     <message>
-        <location filename="p_pylint.py" line="87"/>
+        <location filename="p_pylint.py" line="81"/>
         <source>History...</source>
         <translation>Historique...</translation>
     </message>
     <message>
-        <location filename="p_pylint.py" line="89"/>
+        <location filename="p_pylint.py" line="83"/>
         <source>Set history maximum entries</source>
         <translation>Modifier le nombre d&apos;entrées maximum de l&apos;historique</translation>
     </message>
     <message>
         <location filename="p_pylint.py" line="91"/>
         <source>&amp;Font...</source>
-        <translation>&amp;Police...</translation>
+        <translation type="obsolete">&amp;Police...</translation>
     </message>
     <message>
         <location filename="p_pylint.py" line="92"/>
         <source>Set font style</source>
-        <translation>Changer la police d&apos;écriture</translation>
+        <translation type="obsolete">Changer la police d&apos;écriture</translation>
     </message>
     <message>
-        <location filename="p_pylint.py" line="105"/>
+        <location filename="p_pylint.py" line="96"/>
         <source>Run pylint code analysis</source>
         <translation>Analyser le code source avec pylint</translation>
     </message>
     <message>
-        <location filename="p_pylint.py" line="129"/>
+        <location filename="p_pylint.py" line="122"/>
         <source>History</source>
         <translation>Historique</translation>
     </message>
     <message>
-        <location filename="p_pylint.py" line="130"/>
+        <location filename="p_pylint.py" line="123"/>
         <source>Maximum entries</source>
         <translation>Nombre maximum d&apos;entrées</translation>
     </message>
     <message>
         <location filename="p_pylint.py" line="139"/>
         <source>Select a new font</source>
-        <translation>Sélectionner une police d&apos;écriture</translation>
+        <translation type="obsolete">Sélectionner une police d&apos;écriture</translation>
     </message>
 </context>
 <context>
     <name>PylintConfigPage</name>
     <message>
-        <location filename="p_pylint.py" line="41"/>
+        <location filename="p_pylint.py" line="38"/>
         <source>History: </source>
         <translation>Historique : </translation>
     </message>
     <message>
-        <location filename="p_pylint.py" line="42"/>
+        <location filename="p_pylint.py" line="39"/>
         <source> results</source>
         <translation> résultats</translation>
     </message>
     <message>
         <location filename="p_pylint.py" line="31"/>
         <source>Set text font style</source>
-        <translation>Changer la police d&apos;écriture</translation>
+        <translation type="obsolete">Changer la police d&apos;écriture</translation>
     </message>
     <message>
-        <location filename="p_pylint.py" line="34"/>
+        <location filename="p_pylint.py" line="31"/>
         <source>History</source>
         <translation>Historique</translation>
     </message>
     <message>
-        <location filename="p_pylint.py" line="38"/>
+        <location filename="p_pylint.py" line="35"/>
         <source>Pylint plugin results are stored here:
 %1
 
