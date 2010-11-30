64f1c725f875b023be9b4f847bf326d2b4157640
Pierre Raybaut <pierre.raybaut@gmail.com>
2010-11-30 14:15:23 +0100

    (Fixes Issue 475) Console/IPython colors: console is now using the official platform colors (Windows: official colors, MacOS: Terminal.app colors, UNIX: xterm colors)

--- spyderlib/widgets/shellhelpers.py
+++ spyderlib/widgets/shellhelpers.py
@@ -7,6 +7,9 @@
 """Shell helpers"""
 
 
+import os
+
+
 def get_error_match(text):
     """Return error match"""
     import re
@@ -15,16 +18,42 @@ def get_error_match(text):
 
 class ANSIEscapeCodeHandler(object):
     """ANSI Escape sequences handler"""
-    ANSI_COLORS = ( # Normal, Bright/Light
-                   ('#000000', '#808080'), # 0: black
-                   ('#ff0000', '#ff0000'), # 1: red
-                   ('#00ff00', '#4e8975'), # 2: green
-                   ('#800000', '#ffff00'), # 3: yellow
-                   ('#0000ff', '#0000FF'), # 4: blue
-                   ('#800080', '#ca226b'), # 5: magenta
-                   ('#00ffff', '#8f8fbd'), # 6: cyan
-                   ('#c0c0c0', '#ffff00'), # 7: white
-                   )
+    if os.name == 'nt':
+        # Windows terminal colors:
+        ANSI_COLORS = ( # Normal, Bright/Light
+                       ('#000000', '#808080'), # 0: black
+                       ('#800000', '#ff0000'), # 1: red
+                       ('#008000', '#00ff00'), # 2: green
+                       ('#808000', '#ffff00'), # 3: yellow
+                       ('#000080', '#0000ff'), # 4: blue
+                       ('#800080', '#ff00ff'), # 5: magenta
+                       ('#008080', '#00ffff'), # 6: cyan
+                       ('#c0c0c0', '#ffffff'), # 7: white
+                       )
+    elif os.name == 'mac':
+        # Terminal.app colors:
+        ANSI_COLORS = ( # Normal, Bright/Light
+                       ('#000000', '#818383'), # 0: black
+                       ('#C23621', '#FC391F'), # 1: red
+                       ('#25BC24', '#25BC24'), # 2: green
+                       ('#ADAD27', '#EAEC23'), # 3: yellow
+                       ('#492EE1', '#5833FF'), # 4: blue
+                       ('#D338D3', '#F935F8'), # 5: magenta
+                       ('#33BBC8', '#14F0F0'), # 6: cyan
+                       ('#CBCCCD', '#E9EBEB'), # 7: white
+                       )
+    else:
+        # xterm colors:
+        ANSI_COLORS = ( # Normal, Bright/Light
+                       ('#000000', '#7F7F7F'), # 0: black
+                       ('#CD0000', '#ff0000'), # 1: red
+                       ('#00CD00', '#00ff00'), # 2: green
+                       ('#CDCD00', '#ffff00'), # 3: yellow
+                       ('#0000EE', '#5C5CFF'), # 4: blue
+                       ('#CD00CD', '#ff00ff'), # 5: magenta
+                       ('#00CDCD', '#00ffff'), # 6: cyan
+                       ('#E5E5E5', '#ffffff'), # 7: white
+                       )
     def __init__(self):
         self.intensity = 0
         self.italic = None
@@ -43,7 +72,9 @@ class ANSIEscapeCodeHandler(object):
         elif code == 1:
             # Text color intensity
             self.intensity = 1
-            self.bold = True
+            # The following line is commented because most terminals won't 
+            # change the font weight, against ANSI standard recommendation:
+#            self.bold = True
         elif code == 3:
             # Italic on
             self.italic = True
