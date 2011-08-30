37d7c090624baecbb4ec9cf645b960c7ec218952
Pierre Raybaut <pierre.raybaut@gmail.com>
2011-08-30 10:40:59 +0200

    Spyder bootstrap script: added command line options --gui=pyqt, --gui=pyside, --debug

--- bootstrap.py
+++ bootstrap.py
@@ -14,6 +14,17 @@ http://code.google.com/p/spyderlib/issues/detail?id=741
 import os
 import subprocess
 import sys
+import optparse
+
+# Parsing command line options
+parser = optparse.OptionParser(usage="python bootstrap.py [options]")
+parser.add_option('--gui', dest="gui", default=None,
+                  help="GUI toolkit: pyqt (for PyQt4) or pyside (for PySide)")
+parser.add_option('-d', '--debug', dest="debug", action='store_true',
+                  default=False, help="Debug mode")
+options, _args = parser.parse_args()
+assert options.gui in (None, 'pyqt', 'pyside'),\
+       "Invalid GUI toolkit option '%s'" % options.gui
 
 print("Executing Spyder from source checkout")
 
@@ -32,19 +43,30 @@ print("01. Patched sys.path with %s" % DEVPATH)
 
 # Selecting the GUI toolkit: PySide if installed, otherwise PyQt4
 # (Note: PyQt4 is still the officially supported GUI toolkit for Spyder)
-try:
-    import PySide
-    print("02. PySide is detected, selecting (experimental)")
-    os.environ['QT_API'] = 'pyside'
-except:
-    print("02. No PySide detected, using PyQt4 if available")
+if options.gui is None:
+    try:
+        import PySide
+        print("02. PySide is detected, selecting (experimental)")
+        os.environ['QT_API'] = 'pyside'
+    except:
+        print("02. No PySide detected, using PyQt4 if available")
+else:
+    print ("02. Skipping GUI toolkit detection")
+    os.environ["QT_API"] = options.gui
 
 # Importing Spyder
 from spyderlib import spyder
+QT_API = spyder.qt._modname
+QT_LIB = {'pyqt': 'PyQt4', 'pyside': 'PySide'}[QT_API]
+if QT_API == 'pyqt':
+    import sip
+    try:
+        QT_LIB += (" (API v%d)" % sip.getapi('QString'))
+    except AttributeError:
+        pass
 print("03. Imported Spyder %s (Qt %s via %s %s)" % \
     (spyder.__version__, spyder.qt.QtCore.__version__,
-     {'pyside':'PySide', 'pyqt':'PyQt4'}[spyder.qt._modname],
-     spyder.qt.__version__))
+     QT_LIB, spyder.qt.__version__))
 
 # Executing Spyder
 print("04. Executing spyder.main()")
