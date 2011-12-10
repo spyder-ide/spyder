9f1a83a19091a9e9cfd2297bb71f165c66411981
anatoly techtonik <techtonik@gmail.com>
2011-12-10 09:41:03 +0200

    Add simple manual test for Outline Explorer comment regexps

--- spyderlib/widgets/sourcecode/syntaxhighlighters.py
+++ spyderlib/widgets/sourcecode/syntaxhighlighters.py
@@ -303,6 +303,9 @@ class PythonSH(BaseSH):
      INSIDE_SQSTRING, INSIDE_DQSTRING) = range(5)
     DEF_TYPES = {"def": OutlineExplorerData.FUNCTION,
                  "class": OutlineExplorerData.CLASS}
+    # Comments suitable for Outline Explorer
+    OECOMMENT = re.compile('^# ?--[-]+[\ -]*?[^- ]+')
+    
     def __init__(self, parent, font=None, color_scheme='Spyder'):
         BaseSH.__init__(self, parent, font, color_scheme)
         self.import_statements = {}
@@ -358,8 +361,7 @@ class PythonSH(BaseSH):
                     else:
                         self.setFormat(start, end-start, self.formats[key])
                         if key == "comment":
-                            if re.match(r'^# ?--[-]+[\ -]*?[^- ]+',
-                                        text.lstrip()):
+                            if self.OECOMMENT.match(text.lstrip()):
                                 oedata = OutlineExplorerData()
                                 oedata.text = unicode(text).strip()
                                 oedata.fold_level = start
@@ -754,3 +756,21 @@ class CssSH(BaseWebSH):
     """CSS Syntax Highlighter"""
     PROG = re.compile(make_css_patterns(), re.S)
 
+
+if __name__ == '__main__':
+    # Test Python Outline Explorer comment regexps
+    valid_comments = [
+      '# --- First variant',
+      '#------ 2nd variant',
+    #  '### 3rd variant'
+    ]
+    invalid_comments = [
+      '#---', '#--------', '#---   '
+    ]
+    for line in valid_comments:
+        if not PythonSH.OECOMMENT.match(line):
+            print "Error matching '%s' as outline comment" % line
+    for line in invalid_comments:
+        if PythonSH.OECOMMENT.match(line):
+            print "Error: '%s' is matched as outline comment" % line
+        
