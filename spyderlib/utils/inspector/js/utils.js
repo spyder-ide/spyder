5c358c53fec72384764c460d54c833db84f95884
Carlos Cordoba <ccordoba12@gmail.com>
2012-03-26 12:12:30 -0500

    Object Inspector: Change docstring headers from h1 to h2
    -. It can only be an h1 and that's now the page title
    -. Fix styles accordingly

--- spyderlib/utils/inspector/js/utils.js
+++ spyderlib/utils/inspector/js/utils.js
@@ -15,4 +15,11 @@ $(document).ready(function () {
     // Remove anchor header links.
     // They're used by Sphinx to create crossrefs, so we don't need them
     $('a.headerlink').remove();
+    
+    // Change docstring headers from h1 to h2
+    // It can only be an h1 and that's the page title
+    // Taken from http://forum.jquery.com/topic/how-to-replace-h1-h2
+    $(document).find('div.section h1').replaceWith(function () {
+        return '<h2>' + $(this).text() + '</h2>';
+    });
 });
