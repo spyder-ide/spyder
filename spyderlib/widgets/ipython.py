b208a180eca1181b1db7282b72a806c22d222d1d
Pierre Raybaut <pierre.raybaut@gmail.com>
2012-05-31 16:52:30 +0200

    IPython widget: simplified a bit the frontend widget creation related methods

--- spyderlib/widgets/ipython.py
+++ spyderlib/widgets/ipython.py
@@ -118,9 +118,9 @@ class IPythonApp(IPythonQtConsoleApp):
         BaseIPythonApplication.initialize(self, argv=argv)
         IPythonConsoleApp.initialize(self, argv=argv)
 
-    def create_kernel_manager(self):
+    def create_kernel_manager(self, connection_file=None):
         """Create a kernel manager"""
-        cf = find_connection_file(self.existing, profile='default')
+        cf = find_connection_file(connection_file, profile='default')
         kernel_manager = QtKernelManager(connection_file=cf,
                                          config=self.config)
         kernel_manager.load_connection_file()
@@ -130,11 +130,8 @@ class IPythonApp(IPythonQtConsoleApp):
     def create_new_client(self, connection_file=None):
         """Create and return new client (frontend)
         from connection file basename"""
-        if connection_file is not None:
-            self.parse_command_line(argv=['--existing']+[connection_file])
-        kernel_manager = self.create_kernel_manager()
-        self.widget_factory = SpyderIPythonWidget
-        widget = self.widget_factory(config=self.config, local_kernel=False)
+        kernel_manager = self.create_kernel_manager(connection_file)
+        widget = SpyderIPythonWidget(config=self.config, local_kernel=False)
         widget.kernel_manager = kernel_manager
         return widget
 #==============================================================================
