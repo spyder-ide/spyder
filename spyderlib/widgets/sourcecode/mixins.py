3e09137faf30028c29b28697ec51c94e0b52ef9d
Pierre Raybaut <pierre.raybaut@gmail.com>
2012-08-13 21:53:05 +0200

    Editor: fixed traceback when indenting text (missing method 'replace' in mixin)
    How to reproduce the bug: select the beginning of a line (spaces) and press Tab.

--- spyderlib/widgets/sourcecode/mixins.py
+++ spyderlib/widgets/sourcecode/mixins.py
@@ -373,6 +373,20 @@ class BaseEditMixin(object):
     def remove_selected_text(self):
         """Delete selected text"""
         self.textCursor().removeSelectedText()
+        
+    def replace(self, text, pattern=None):
+        """Replace selected text by *text*
+        If *pattern* is not None, replacing selected text using regular
+        expression text substitution"""
+        cursor = self.textCursor()
+        cursor.beginEditBlock()
+        if pattern is not None:
+            seltxt = unicode(cursor.selectedText())
+        cursor.removeSelectedText()
+        if pattern is not None:
+            text = re.sub(unicode(pattern), unicode(text), unicode(seltxt))
+        cursor.insertText(text)
+        cursor.endEditBlock()
 
 
     #------Find/replace
