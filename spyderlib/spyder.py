bfe0319492334dfc4e6d16393db818b89db1eb89
Pierre Raybaut <pierre.raybaut@gmail.com>
2012-08-21 22:33:33 +0200

    Windows: moved the functions hiding parent console to spyderlib/utils/windows.py

--- spyderlib/spyder.py
+++ spyderlib/spyder.py
@@ -51,23 +51,8 @@ requirements.check_qt()
 set_attached_console_visible = None
 is_attached_console_visible = None
 if os.name == 'nt':
-    try:
-        import win32gui, win32console, win32con
-        win32console.GetConsoleWindow() # do nothing, this is just a test
-        def set_attached_console_visible(state):
-            """Show/hide console window attached to current window
-            
-            This is for Windows platforms only. Requires pywin32 library."""
-            win32gui.ShowWindow(win32console.GetConsoleWindow(),
-                                win32con.SW_SHOW if state else win32con.SW_HIDE)
-        def is_attached_console_visible():
-            """Return True if attached console window is visible"""
-            return win32gui.IsWindowVisible(win32console.GetConsoleWindow())
-    except (ImportError, NotImplementedError):
-        # This is not a Windows platform (ImportError)
-        # or pywin32 is not installed (ImportError)
-        # or GetConsoleWindow is not implemented on current platform
-        pass
+    from spyderlib.utils.windows import (set_attached_console_visible,
+                                         is_attached_console_visible)
 
 # Workaround: importing rope.base.project here, otherwise this module can't
 # be imported if Spyder was executed from another folder than spyderlib
