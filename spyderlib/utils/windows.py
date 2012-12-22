5477a6d0c23e1ea4c4f48d044f4005c8f30ac1b4
Pierre Raybaut <pierre.raybaut@gmail.com>
2012-12-22 14:18:25 +0100

    (Fixes Issue 1199) Fixed spyderlib/utils/windows.py encoding issue with 2to3

--- spyderlib/utils/windows.py
+++ spyderlib/utils/windows.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2012 Pierre Raybaut
+# Copyright Â© 2012 Pierre Raybaut
 # Licensed under the terms of the MIT License
 # (see spyderlib/__init__.py for details)
 
