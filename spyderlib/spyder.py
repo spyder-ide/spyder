7466e9f25802f4c2f3346f37ccf47f19c71af58b
Pierre Raybaut <pierre.raybaut@gmail.com>
2013-01-01 14:44:54 +0100

    External tools submenu: added shortcuts to guidata and guiqwt examples

--- spyderlib/spyder.py
+++ spyderlib/spyder.py
@@ -645,15 +645,31 @@ class MainWindow(QMainWindow):
                 self.external_tools_menu_actions += additact
                 
             # Sift
-            if is_module_installed('guidata') \
-               and is_module_installed('guiqwt'):
+            gdgq_act = []
+            if is_module_installed('guidata'):
                 from guidata import configtools
-                from guiqwt import config  # (loading icons) analysis:ignore
-                sift_icon = configtools.get_icon('sift.svg')
-                sift_act = create_python_script_action(self, _("Sift"),
+                from guidata import config  # (loading icons) analysis:ignore
+                guidata_icon = configtools.get_icon('guidata.svg')
+                guidata_act = create_python_script_action(self,
+                               _("guidata examples"), guidata_icon, "guidata",
+                               osp.join("tests", "__init__"))
+                if guidata_act:
+                    gdgq_act += [guidata_act]
+                if is_module_installed('guiqwt'):
+                    from guiqwt import config  # analysis:ignore
+                    guiqwt_icon = configtools.get_icon('guiqwt.svg')
+                    guiqwt_act = create_python_script_action(self,
+                                   _("guiqwt examples"), guiqwt_icon, "guiqwt",
+                                   osp.join("tests", "__init__"))
+                    if guiqwt_act:
+                        gdgq_act += [guiqwt_act]
+                    sift_icon = configtools.get_icon('sift.svg')
+                    sift_act = create_python_script_action(self, _("Sift"),
                                sift_icon, "guiqwt", osp.join("tests", "sift"))
-                if sift_act:
-                    self.external_tools_menu_actions += [None, sift_act]
+                    if sift_act:
+                        gdgq_act += [sift_act]
+            if gdgq_act:
+                self.external_tools_menu_actions += [None] + gdgq_act
                 
             # ViTables
             vitables_act = create_program_action(self, _("ViTables"),
