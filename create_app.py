09b15f2237a7bb0eeeef40c046c59c65012550b9
Carlos Cordoba <ccordoba12@gmail.com>
2013-03-08 15:29:54 -0500

    Mac app: Add mpl_toolkits to its package list
    - It's needed to reproduce all matplotlib gallery examples

--- create_app.py
+++ create_app.py
@@ -76,7 +76,8 @@ DEPS = ['pylint', 'logilab_astng', 'logilab_common', 'pep8', 'setuptools']
 EXCLUDES = DEPS + ['mercurial', 'nose']
 PACKAGES = ['spyderlib', 'spyderplugins', 'sphinx', 'jinja2', 'docutils',
             'IPython', 'zmq', 'pygments', 'rope', 'distutils', 'PIL', 'PyQt4',
-            'sklearn', 'skimage', 'pandas', 'sympy', 'mpmath', 'statsmodels']
+            'sklearn', 'skimage', 'pandas', 'sympy', 'mpmath', 'statsmodels',
+            'mpl_toolkits']
 INCLUDES = get_stdlib_modules()
 
 OPTIONS = {
