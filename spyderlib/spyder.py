77c04137a3a0c6fc6c78f31dd396e2b9fcafb4a6
Carlos Cordoba <ccordoba12@gmail.com>
2013-04-29 10:39:22 -0500

    Main Toolbar: Only add "External tools" button to it if it has active buttons

--- spyderlib/spyder.py
+++ spyderlib/spyder.py
@@ -965,7 +965,12 @@ class MainWindow(QMainWindow):
                                                icon="ext_tools.png")
             external_tools_act.setMenu(self.external_tools_menu)
             self.tools_menu_actions.append(external_tools_act)
-            self.main_toolbar_actions.append(external_tools_act)
+            add_ext_tools = False
+            for et in self.external_tools_menu_actions:
+                if et and et.isEnabled():
+                    add_ext_tools = True
+            if add_ext_tools:
+                self.main_toolbar_actions.append(external_tools_act)
             
             # Filling out menu/toolbar entries:
             add_actions(self.file_menu, self.file_menu_actions)
