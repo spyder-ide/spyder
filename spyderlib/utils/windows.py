404677b1c7c02a612439085072e739b392106c70
anatoly techtonik <techtonik@gmail.com>
2013-06-25 14:18:00 +0300

    Remove pywin32 dependency for hiding Windows console window

--- spyderlib/utils/windows.py
+++ spyderlib/utils/windows.py
@@ -6,24 +6,36 @@
 
 """Windows-specific utilities"""
 
-set_attached_console_visible = None
-is_attached_console_visible = None
 
-try:
-    import win32gui, win32console, win32con
-    win32console.GetConsoleWindow() # do nothing, this is just a test
-    def set_attached_console_visible(state):
-        """Show/hide console window attached to current window
-        
-        This is for Windows platforms only. Requires pywin32 library."""
-        win32gui.ShowWindow(win32console.GetConsoleWindow(),
-                            win32con.SW_SHOW if state else win32con.SW_HIDE)
-    def is_attached_console_visible():
-        """Return True if attached console window is visible"""
-        return win32gui.IsWindowVisible(win32console.GetConsoleWindow())
-except (ImportError, NotImplementedError):
-    # This is not a Windows platform (ImportError)
-    # or pywin32 is not installed (ImportError)
-    # or GetConsoleWindow is not implemented on current platform
-    pass
+from ctypes import windll
 
+
+# --- Window control ---
+
+SW_SHOW = 5    # activate and display
+SW_SHOWNA = 8  # show without activation
+SW_HIDE = 0
+
+GetConsoleWindow = windll.kernel32.GetConsoleWindow
+ShowWindow = windll.user32.ShowWindow
+IsWindowVisible = windll.user32.IsWindowVisible
+
+# Handle to console window associated with current Python
+# interpreter procss, 0 if there is no window
+console_window_handle = GetConsoleWindow()
+
+def set_attached_console_visible(state):
+    """Show/hide system console window attached to current process.
+       Return it's previous state.
+
+       Availability: Windows"""
+    flag = {True: SW_SHOW, False: SW_HIDE}
+    return bool(ShowWindow(console_window_handle, flag[state]))
+
+def is_attached_console_visible():
+    """Return True if attached console window is visible"""
+    return IsWindowVisible(console_window_handle)
+
+
+# [ ] the console state asks for a storage container
+# [ ] reopen console on exit - better die open than become a zombie
