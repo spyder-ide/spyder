4a48402d2f3dbe840902fe4069cae7afa6200b69
Carlos Cordoba <ccordoba12@gmail.com>
2013-08-08 17:47:00 -0500

    Debug Toolbar: Improve tooltips of several buttons to make their actions easier to understand

--- spyderlib/plugins/editor.py
+++ spyderlib/plugins/editor.py
@@ -648,23 +648,26 @@ class Editor(SpyderPluginWidget):
                                      triggered=self.debug_file)
         self.register_shortcut(debug_action, context="Editor",
                                name="Debug", default="Ctrl+F5")
-        debug_next_action = create_action(self, _("Step Over"), 
-               icon='arrow-step-over.png', tip=_("Step Over"), 
+        debug_next_action = create_action(self, _("Step"), 
+               icon='arrow-step-over.png', tip=_("Run current line"), 
                triggered=lambda: self.debug_command("next")) 
         self.register_shortcut(debug_next_action, "_",
                    "Debug Step Over", "Ctrl+F10")
         debug_continue_action = create_action(self, _("Continue"),
-               icon='arrow-continue.png', tip=_("Continue"), 
+               icon='arrow-continue.png', tip=_("Continue execution until\n"
+                                                "the next breakpoint"), 
                triggered=lambda: self.debug_command("continue"))                                                 
         self.register_shortcut(debug_continue_action, "_",
                    "Debug Continue", "Ctrl+F12")
         debug_step_action = create_action(self, _("Step Into"), 
-               icon='arrow-step-in.png', tip=_("Step Into"), 
+               icon='arrow-step-in.png', tip=_("Step into function, method\n"
+                                               "or class of current line"), 
                triggered=lambda: self.debug_command("step"))                
         self.register_shortcut(debug_step_action, "_",
                    "Debug Step Into", "Ctrl+F11")                             
         debug_return_action = create_action(self, _("Step Return"), 
-               icon='arrow-step-out.png', tip=_("Step Return"), 
+               icon='arrow-step-out.png', tip=_("Run until current function\n"
+                                                "or method returns"), 
                triggered=lambda: self.debug_command("return"))               
         self.register_shortcut(debug_return_action, "_",
                    "Debug Step Return", "Ctrl+Shift+F11")
