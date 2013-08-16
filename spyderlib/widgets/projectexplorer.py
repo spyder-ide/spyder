d4cc9dbd15c5ed9f1436e2a61d4c99615f76422f
Jed Ludlow <jed.ludlow@gmail.com>
2013-08-15 21:53:38 -0600

    Python 3 compatibility: Fixed pickling problems with project workspace files.
    Workspace files should be compatible with both Python 2.7 and 3.3 now.
    
    Update issue 1414
    Status: Fixed
    I believe this should be fixed by this revision, but additional verification
    would be helpful. Tested on Ubuntu 13.04 with Python 2.7 and 3.3.

--- spyderlib/widgets/projectexplorer.py
+++ spyderlib/widgets/projectexplorer.py
@@ -123,7 +123,7 @@ class Project(object):
             # Old format (Spyder 2.0-2.1 for Python 2)
             with open(fname, 'U') as fdesc:
                 data = pickle.loads(fdesc.read())
-        except (pickle.PickleError, TypeError):
+        except (pickle.PickleError, TypeError, UnicodeDecodeError):
             try:
                 # New format (Spyder >=2.2 for Python 2 and Python 3)
                 with open(fname, 'rb') as fdesc:
@@ -295,7 +295,7 @@ class Workspace(object):
             # Old format (Spyder 2.0-2.1 for Python 2)
             with open(fname, 'U') as fdesc:
                 data = pickle.loads(fdesc.read())
-        except (pickle.PickleError, TypeError):
+        except (pickle.PickleError, TypeError, UnicodeDecodeError):
             try:
                 # New format (Spyder >=2.2 for Python 2 and Python 3)
                 with open(fname, 'rb') as fdesc:
