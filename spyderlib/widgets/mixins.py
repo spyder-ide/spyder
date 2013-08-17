afd3bce279981ab3e69eccf7e97b68db099c9573
Pierre Raybaut <pierre.raybaut@gmail.com>
2013-08-17 11:22:08 +0200

    Editor/Block separator: added support for '# %%' and '# <codecell>' separators. The first one is intended to support code edited with Eclipse and reopened with Spyder afterwards, the second one adds support for code exported from an IPython notebook.
    Update Issue 852
    Status: Fixed

--- spyderlib/widgets/mixins.py
+++ spyderlib/widgets/mixins.py
@@ -30,7 +30,7 @@ HISTORY_FILENAMES = []
 
 
 class BaseEditMixin(object):
-    BLOCK_SEPARATOR = '#%%'
+    BLOCK_SEPARATORS = ('#%%', '# %%', '# <codecell>')
 
     def __init__(self):
         self.eol_chars = None
@@ -184,7 +184,7 @@ class BaseEditMixin(object):
         cursor0 = QTextCursor(cursor)
         cursor0.select(QTextCursor.BlockUnderCursor)
         text = unicode(cursor0.selectedText())
-        return text.lstrip().startswith(self.BLOCK_SEPARATOR)
+        return text.lstrip().startswith(self.BLOCK_SEPARATORS)
 
     def select_current_block(self):
         """
