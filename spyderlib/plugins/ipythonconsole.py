bf663171e2553dd9c5465d33fe07e7c8837aad3e
Carlos Cordoba <ccordoba12@gmail.com>
2013-09-22 12:20:37 -0500

    IPython Console: Add (again) an option to change clients background color to be dark
    Update Issue 1520
    Status: Fixed
    
    - Thanks to phyo.arkarlwin for the help.
    - phyo suggested a much easier way to do it than what I did before (which
    required subclassing IPythonQtConsoleApp).

--- spyderlib/plugins/ipythonconsole.py
+++ spyderlib/plugins/ipythonconsole.py
@@ -101,6 +101,17 @@ class IPythonConsoleConfigPage(PluginConfigPage):
         interface_layout.addWidget(inspector_box)
         interface_group.setLayout(interface_layout)
 
+        # Background Color Group
+        bg_group = QGroupBox(_("Background color"))
+        light_radio = self.create_radiobutton(_("Light background"),
+                                              'light_color', True)
+        dark_radio = self.create_radiobutton(_("Dark background"),
+                                             'dark_color', False)
+        bg_layout = QVBoxLayout()
+        bg_layout.addWidget(light_radio)
+        bg_layout.addWidget(dark_radio)
+        bg_group.setLayout(bg_layout)
+
         # Source Code Group
         source_code_group = QGroupBox(_("Source code"))
         buffer_spin = self.create_spinbox(
@@ -354,7 +365,7 @@ class IPythonConsoleConfigPage(PluginConfigPage):
 
         # --- Tabs organization ---
         tabs = QTabWidget()
-        tabs.addTab(self.create_tab(font_group, interface_group, 
+        tabs.addTab(self.create_tab(font_group, interface_group, bg_group,
                                     source_code_group), _("Display"))
         tabs.addTab(self.create_tab(pylab_group, backend_group, inline_group),
                                     _("Graphics"))
