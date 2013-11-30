3777903343b5fb8c11e7643a1342d84aa3fd531f
stonebig <stonebig..>
2013-11-30 22:53:34 +0100

    Execution on Current spyder interpreter needs to read the source file as utf-8, not as default (legacy) pagecode of windows

--- spyderlib/widgets/externalshell/sitecustomize.py
+++ spyderlib/widgets/externalshell/sitecustomize.py
@@ -44,7 +44,11 @@ except ImportError:
     import builtins
     basestring = (str,)
     def execfile(filename, namespace):
-        exec(compile(open(filename).read(), filename, 'exec'), namespace)
+        if os.name == 'nt' and sys.version[0] == '3':
+        #python 3 under windows : a python source file is now supposed to be utf-8, not default system pagecode
+            exec(compile(open(filename, encoding="utf-8").read(), filename, 'exec'), namespace)
+        else:
+            exec(compile(open(filename).read(), filename, 'exec'), namespace)
 
 
 # Colorization of sys.stderr (standard Python interpreter)
