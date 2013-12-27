b415a3dbe69edc3c3b5eaaa8eb10334167f35b07
Carlos Cordoba <ccordoba12@gmail.com>
2013-12-27 11:30:07 -0500

    IPython Widget: Improve some docstrings

--- spyderlib/widgets/ipython.py
+++ spyderlib/widgets/ipython.py
@@ -223,8 +223,7 @@ class IPythonShellWidget(RichIPythonWidget):
     
     #---- Public API ----------------------------------------------------------
     def set_ipyclient(self, ipyclient):
-        """Bind this IPython widget to an IPython client widget
-        (see spyderlib/plugins/ipythonconsole.py)"""
+        """Bind this shell widget to an IPython client one"""
         self.ipyclient = ipyclient
         self.exit_requested.connect(ipyclient.exit_callback)
     
@@ -420,7 +419,7 @@ class IPythonClient(QWidget, SaveHistoryMixin):
         self.shellwidget.executed.connect(self.auto_refresh_namespacebrowser)
     
     def show_kernel_error(self, error):
-        """Show kernel initialization errors in the client info widget"""
+        """Show kernel initialization errors in infowidget"""
         # Remove explanation about how to kill the kernel (doesn't apply to us)
         error = error.split('issues/2049')[-1]
         # Remove unneeded blank lines at the beginning
