6ba47eb6aec202e86b8ea5992d4c6c539a37318c
Steven Silvester <steven.silvester@ieee.org>
2014-01-14 22:55:32 -0600

    Fix more regressions in editor.py

--- spyderlib/widgets/editor.py
+++ spyderlib/widgets/editor.py
@@ -380,9 +380,10 @@ class FileInfo(QObject):
     def find_nearest_function_call(self, position):
         """Find the nearest function call at or prior to current position"""
         source_code = self.get_source_code()
+        position = min(len(source_code) - 1, position)
         orig_pos = position
         # find the first preceding opening parens (keep track of closing parens)
-        if not position or not source_code[position - 1] == '(':
+        if not position or not source_code[position] == '(':
             close_parens = 0
             position -= 1
             while position and not (source_code[position] == '(' and close_parens == 0):
@@ -394,7 +395,7 @@ class FileInfo(QObject):
                 if source_code[position] in ['\n', '\r']:
                     position = orig_pos
                     break
-        if position and source_code[position - 1] == '(':
+        if position and source_code[position] == '(':
             position -= 1
                 
         return position
