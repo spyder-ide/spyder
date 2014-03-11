a14489e728d997b6bca19210374f0aa3df55ffd7
Roberto Rosati <robrosit>
2014-03-11 15:37:17 +0000

    Changed the windows_memory_usage() function to be compatible with the ctypes packages for Python 2 and Python 3, up to version 1.1.0

--- spyderlib/utils/system.py
+++ spyderlib/utils/system.py
@@ -16,24 +16,26 @@ from spyderlib.utils import programs
 def windows_memory_usage():
     """Return physical memory usage (float)
     Works on Windows platforms only"""
-    from ctypes import windll, wintypes
-    class MemoryStatus(wintypes.Structure):
-        _fields_ = [('dwLength', wintypes.DWORD),
-                    ('dwMemoryLoad', wintypes.DWORD),
-                    ('ullTotalPhys', wintypes.c_uint64),
-                    ('ullAvailPhys', wintypes.c_uint64),
-                    ('ullTotalPageFile', wintypes.c_uint64),
-                    ('ullAvailPageFile', wintypes.c_uint64),
-                    ('ullTotalVirtual', wintypes.c_uint64),
-                    ('ullAvailVirtual', wintypes.c_uint64),
-                    ('ullAvailExtendedVirtual', wintypes.c_uint64),]
+    from ctypes import windll, Structure, c_uint64, sizeof, byref
+    from ctypes.wintypes import DWORD
+    class MemoryStatus(Structure):
+        _fields_ = [('dwLength', DWORD),
+                    ('dwMemoryLoad',DWORD),
+                    ('ullTotalPhys', c_uint64),
+                    ('ullAvailPhys', c_uint64),
+                    ('ullTotalPageFile', c_uint64),
+                    ('ullAvailPageFile', c_uint64),
+                    ('ullTotalVirtual', c_uint64),
+                    ('ullAvailVirtual', c_uint64),
+                    ('ullAvailExtendedVirtual', c_uint64),]
     memorystatus = MemoryStatus()
     # MSDN documetation states that dwLength must be set to MemoryStatus
     # size before calling GlobalMemoryStatusEx
     # http://msdn.microsoft.com/en-us/library/aa366770(v=vs.85)
-    memorystatus.dwLength = wintypes.sizeof(memorystatus)
-    windll.kernel32.GlobalMemoryStatusEx(wintypes.byref(memorystatus))
+    memorystatus.dwLength = sizeof(memorystatus)
+    windll.kernel32.GlobalMemoryStatusEx(byref(memorystatus))
     return float(memorystatus.dwMemoryLoad)
+
 
 def psutil_phymem_usage():
     """Return physical memory usage (float)
@@ -41,6 +43,7 @@ def psutil_phymem_usage():
     (http://code.google.com/p/psutil/)"""
     import psutil
     return psutil.phymem_usage().percent
+
 
 if programs.is_module_installed('psutil', '>=0.3.0'):
     #  Function `psutil.phymem_usage` was introduced in psutil v0.3.0
@@ -56,6 +59,8 @@ if __name__ == '__main__':
     print("*"*80)
     print(memory_usage.__doc__)
     print(memory_usage())
-    print("*"*80)
-    print(windows_memory_usage.__doc__)
-    print(windows_memory_usage())
+    if os.name == 'nt':
+        #  windll can only be imported if os.name = 'nt' or 'ce'
+        print("*"*80)
+        print(windows_memory_usage.__doc__)
+        print(windows_memory_usage())
