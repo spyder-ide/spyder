ebe3e70239d0059e35b678d9351d4167e6d19ce9
Daniel Høegh <dhoegh91@gmail.com>
2014-03-25 11:25:12 +0100

    First working version of DataFrameEditor

--- spyderlib/widgets/dataframeeditor.py
+++ spyderlib/widgets/dataframeeditor.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar 23 15:16:04 2014
-
-@author: Daniel Høegh
+pandas DataFrame Editor Dialog based on Qt
 """
 
 # Or if you use PyQt4:
 from spyderlib.qt.QtCore import (QAbstractTableModel, Qt, QModelIndex, SIGNAL,
                                  SLOT)
-from spyderlib.qt.QtGui import (
-        QApplication, QDialog, QVBoxLayout, QTableView, QWidget, QColor, QGridLayout, QDialogButtonBox, QHBoxLayout)
+from spyderlib.qt.QtGui import (QDialog, QTableView, QColor, QGridLayout,
+                                QDialogButtonBox, QHBoxLayout)
 from spyderlib.qt.compat import to_qvariant, from_qvariant
 from spyderlib.utils.qthelpers import qapplication
 from spyderlib.widgets.dicteditorutils import get_color_name
 from numpy import int64
+
 
 class DataFrameModel(QAbstractTableModel):
     ''' data model for a DataFrame class '''
     def __init__(self, dataFrame):
         super(DataFrameModel, self).__init__()
         self.df = dataFrame
-        self.sat = .7 # Saturation
-        self.val = 1. # Value
-        self.alp = .3 # Alpha-channel
+        self.sat = .7  # Saturation
+        self.val = 1.  # Value
+        self.alp = .3  # Alpha-channel
         self.bgcolor_enabled = True
         self.signalUpdate()
         
@@ -37,7 +36,7 @@ class DataFrameModel(QAbstractTableModel):
             return to_qvariant()
 
         if orientation == Qt.Horizontal:
-            if section==0:
+            if section == 0:
                 return 'row'
             try:
                 return self.df.columns.tolist()[section-1]
@@ -45,45 +44,43 @@ class DataFrameModel(QAbstractTableModel):
                 return to_qvariant()
         else:
             return to_qvariant()
-        
         return to_qvariant()
+        
     def get_bgcolor(self, index):
         """Background color depending on value"""
-        value=self.df.ix[index.row(), index.column()-1]
-        if index.column()==0:
+        value = self.df.ix[index.row(), index.column()-1]
+        if index.column() == 0:
             color = QColor(Qt.lightGray)
             color.setAlphaF(.05)
         else:
             color = QColor(get_color_name(value))
             color.setAlphaF(self.alp)
         return color
-    def data(self, index, role=Qt.DisplayRole):
         
+    def data(self, index, role=Qt.DisplayRole):
         if not index.isValid():
             return to_qvariant()
         if role == Qt.DisplayRole:
-            if index.column()==0:
+            if index.column() == 0:
                 return to_qvariant(self.df.index.tolist()[index.row()])
             else:
-                return to_qvariant(str(self.df.ix[index.row(), index.column()-1]))
+                return to_qvariant(str(self.df.ix[index.row(),
+                                                  index.column()-1]))
         elif role == Qt.BackgroundColorRole:
             return to_qvariant(self.get_bgcolor(index))
-        
         return to_qvariant()
-            
 
     def flags(self, index):
             flags = super(DataFrameModel, self).flags(index)
-            if index.column()!=0:
+            if index.column() != 0:
                 flags |= Qt.ItemIsEditable
-            
             return flags
 
     def setData(self, index, value, role):
-    
         #row = self.df.index[index.row()]
         value = from_qvariant(value, str)
-        if isinstance(self.df.ix[index.row(), index.column()-1],(long, int, int64, float,unicode)):
+        if isinstance(self.df.ix[index.row(), index.column()-1],
+                      (long, int, int64, float, unicode)):
                 try:
                     value = float(value)
                 except ValueError:
@@ -94,6 +91,7 @@ class DataFrameModel(QAbstractTableModel):
         else:
             return
         return True
+
     def get_data(self):
         """Return data"""
         return self.df
@@ -109,7 +107,6 @@ class DataFrameEditor(QDialog):
     ''' a simple widget for using DataFrames in a gui '''
     def __init__(self, parent=None):
         QDialog.__init__(self, parent)
-        
         # Destroying the C++ object right after closing the dialog box,
         # otherwise it may be garbage-collected in another QThread
         # (e.g. the editor's analysis thread in Spyder), thus leading to
@@ -122,12 +119,11 @@ class DataFrameEditor(QDialog):
         self.layout = None
     
     def setup_and_check(self, dataFrame, title=''):
-        
         self.layout = QGridLayout()
         self.setLayout(self.layout)
-        #self.setWindowIcon(get_icon('arredit.png'))
+        # self.setWindowIcon(get_icon('arredit.png'))
         if title:
-            title = unicode(title) # in case title is not a string
+            title = unicode(title)  # in case title is not a string
         else:
             title = "Data Frame editor"
         self.setWindowTitle(title)
@@ -152,7 +148,6 @@ class DataFrameEditor(QDialog):
         
         btn_layout.addWidget(bbox)
         self.layout.addLayout(btn_layout, 2, 0)
-        
         return True
         
     def get_value(self):
@@ -160,7 +155,7 @@ class DataFrameEditor(QDialog):
         # It is import to avoid accessing Qt C++ object as it has probably
         # already been destroyed, due to the Qt.WA_DeleteOnClose attribute
         return self.dataModel.get_data()
-        
+
 
 def test_edit(data, title="", parent=None):
     """Test subroutine"""
@@ -171,16 +166,16 @@ def test_edit(data, title="", parent=None):
         import sys
         sys.exit()
 
+
 def test():
     from pandas import DataFrame
     from numpy import nan
-    df1 = DataFrame([[1,'test'],[1,'test'],[1,'test'],[1,'test']],index=['a','b',nan,nan ],columns=['a','b'])
-    out=test_edit(df1)
+    df1 = DataFrame([[1, 'test'], [1, 'test'], [1, 'test'], [1, 'test']],
+                    index=['a', 'b', nan, nan], columns=['a', 'b'])
+    out = test_edit(df1)
     print ("out:", out)
     return out
     
 if __name__ == '__main__':
     _app = qapplication()
-    df=test()
-    
-    
+    df = test()
