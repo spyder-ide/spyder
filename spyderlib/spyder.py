1b7665b9bb83fd8727b53bad88cecb39ca08e008
contrebasse <devnull@localhost>
2014-05-24 21:52:30 +0200

    Height, width, x and y are methods and not attributes...

--- spyderlib/spyder.py
+++ spyderlib/spyder.py
@@ -1229,15 +1229,15 @@ class MainWindow(QMainWindow):
     def get_window_settings(self):
         """Return current window settings
         Symetric to the 'set_window_settings' setter"""
-        window_size = (self.window_size.width, self.window_size.height)
+        window_size = (self.window_size.width(), self.window_size.height())
         is_fullscreen = self.isFullScreen()
         if is_fullscreen:
             is_maximized = self.maximized_flag
         else:
             is_maximized = self.isMaximized()
-        pos = (self.window_position.x, self.window_position.y)
-        prefs_dialog_size = (self.prefs_dialog_size.width,
-                             self.prefs_dialog_size.height)
+        pos = (self.window_position.x(), self.window_position.y())
+        prefs_dialog_size = (self.prefs_dialog_size.width(),
+                             self.prefs_dialog_size.height())
         hexstate = qbytearray_to_str(self.saveState())
         return (hexstate, window_size, prefs_dialog_size, pos, is_maximized,
                 is_fullscreen)
