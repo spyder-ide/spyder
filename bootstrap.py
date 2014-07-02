37a0ebe164e80dcd6be8dda8a7db788c1e624379
anatoly techtonik <techtonik@gmail.com>
2014-07-02 07:32:28 +0300

    bootstrap.py: Remove --versions argument (revert a62939c9cd2f207a)
    Last three optimizations make --versions unnecessary. It saves
    only about 0.1s, which is barely noticeable, but adds inconvenience
    that important versions details are missing from console on crash

--- bootstrap.py
+++ bootstrap.py
@@ -35,9 +35,6 @@ parser.add_option('--gui', default=None,
                   help="GUI toolkit: pyqt (for PyQt4) or pyside (for PySide)")
 parser.add_option('--hide-console', action='store_true',
                   default=False, help="Hide parent console window (Windows only)")
-parser.add_option('--versions', dest="versions", action='store_true',
-                  default=False,
-                  help="Show Qt and Python versions used by Spyder")
 parser.add_option('--test', dest="test", action='store_true', default=False,
                   help="Test Spyder with a clean settings dir")
 parser.add_option('--no-apport', action='store_true',
@@ -131,15 +128,14 @@ if options.debug:
     # if operation is successful
 
 
-if options.versions:
-    # Importing spyder (among other things, this has the effect of setting the 
-    # QT_API environment variable if this has not yet been done just above)
-    from spyderlib import get_versions
-    versions = get_versions(reporev=False)
-    print("03. Imported Spyder %s" % versions['spyder'])
-    print("    [Python %s %dbits, Qt %s, %s %s on %s]" % \
-          (versions['python'], versions['bitness'], versions['qt'],
-           versions['qt_api'], versions['qt_api_ver'], versions['system']))
+# Checking versions (among other things, this has the effect of setting the
+# QT_API environment variable if this has not yet been done just above)
+from spyderlib import get_versions
+versions = get_versions(reporev=False)
+print("03. Imported Spyder %s" % versions['spyder'])
+print("    [Python %s %dbits, Qt %s, %s %s on %s]" % \
+      (versions['python'], versions['bitness'], versions['qt'],
+       versions['qt_api'], versions['qt_api_ver'], versions['system']))
 
 
 # --- Executing Spyder
