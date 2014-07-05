396ae82ac28fed212a2604e157db5954605a4225
Carlos Cordoba <ccordoba12@gmail.com>
2014-07-04 19:06:23 -0500

    Make bootstrap work again with Python 3
    Fixes issue 1881

--- spyderlib/__init__.py
+++ spyderlib/__init__.py
@@ -56,15 +56,18 @@ def add_to_distribution(dist):
                                     '.qm', '.py',),
                                    copy_to_root=False)
 
+
 def get_versions(reporev=True):
     """Get version information for components used by Spyder"""
     import sys
     import platform
+    # Hack to let IPython set QT_API, in case it's installed
     try:
         from IPython.external import qt
     except ImportError:
-        from spyderlib import qt
-    import qt.QtCore
+        pass
+    import spyderlib.qt
+    import spyderlib.qt.QtCore
 
     revision = None
     if reporev:
@@ -77,13 +80,14 @@ def get_versions(reporev=True):
         system = platform.system()
     else:
         system = 'Darwin'
+    
     return {
         'spyder': __version__,
         'python': platform.python_version(),  # "2.7.3"
         'bitness': 64 if sys.maxsize > 2**32 else 32,
-        'qt': qt.QtCore.__version__,
-        'qt_api': qt.API_NAME,      # PySide or PyQt4
-        'qt_api_ver': qt.__version__,
+        'qt': spyderlib.qt.QtCore.__version__,
+        'qt_api': spyderlib.qt.API_NAME,      # PySide or PyQt4
+        'qt_api_ver': spyderlib.qt.__version__,
         'system': system,   # Linux, Windows, ...
         'revision': revision,  # '9fdf926eccce+:2430+'
     }
