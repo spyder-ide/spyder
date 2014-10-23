e4a89bce7c86c46ba138fc61f3a52b60a8404da1
Steven Silvester <steven.silvester@ieee.org>
2014-10-22 21:39:03 -0500

    Reinstate jedi

--- spyderlib/utils/introspection/plugin_manager.py
+++ spyderlib/utils/introspection/plugin_manager.py
@@ -17,7 +17,7 @@ from spyderlib.qt.QtGui import QApplication
 from spyderlib.qt.QtCore import pyqtSignal, QThread, QObject, QTimer
 
 
-PLUGINS = ['rope', 'fallback']
+PLUGINS = ['jedi', 'rope', 'fallback']
 
 LOG_FILENAME = get_conf_path('introspection.log')
 DEBUG_EDITOR = DEBUG >= 3
