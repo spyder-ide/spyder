726b0fa2795ec5d613b440d40b5a3f7033a9f6a6
Carlos Cordoba <ccordoba12@gmail.com>
2014-11-01 20:30:06 -0500

    Variable Explorer: Show contents of lists, dicts, sets and tuples by default

--- spyderlib/config.py
+++ spyderlib/config.py
@@ -305,7 +305,7 @@ DEFAULTS = [
               'inplace': False,
               'truncate': True,
               'minmax': False,
-              'collvalue': False,
+              'collvalue': True,
               'remote_editing': False,
               }),
             ('editor',
@@ -715,7 +715,7 @@ DEFAULTS = [
 # 2. If you want to *remove* options that are no longer needed in our codebase,
 #    you need to do a MAJOR update in version, e.g. from 3.0.0 to 4.0.0
 # 3. You don't need to touch this value if you're just adding a new option
-CONF_VERSION = '9.1.0'
+CONF_VERSION = '9.2.0'
 
 # XXX: Previously we had load=(not DEV) here but DEV was set to *False*.
 # Check if it *really* needs to be updated or not
