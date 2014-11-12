d07982fc0aa05002975559138a0b06aa322718e6
Carlos Cordoba <ccordoba12@gmail.com>
2014-11-12 14:33:48 -0500

    Rename UMD to UMR (User Module Reloader) everywhere
    Fixes issue 1975
    
    - Also rewrite the message printed in the consoles when it takes effect

--- spyderlib/widgets/externalshell/baseshell.py
+++ spyderlib/widgets/externalshell/baseshell.py
@@ -303,10 +303,10 @@ def test():
                                 arguments="-q4thread -pylab -colors LightBG",
                                 light_background=False)
 #    shell = ExternalPythonShell(wdir=osp.dirname(spyderlib.__file__),
-#                                interact=True, umd_enabled=True,
+#                                interact=True, umr_enabled=True,
 #                                stand_alone=settings,
-#                                umd_namelist=['guidata', 'guiqwt'],
-#                                umd_verbose=True, light_background=False)
+#                                umr_namelist=['guidata', 'guiqwt'],
+#                                umr_verbose=True, light_background=False)
 #    shell = ExternalSystemShell(wdir=osp.dirname(spyderlib.__file__),
 #                                light_background=False)
     shell.shell.toggle_wrap_mode(True)
