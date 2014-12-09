1d47da2e17667b1788493d7901039c534acafbeb
Carlos Cordoba <ccordoba12@gmail.com>
2014-12-08 20:54:00 -0500

    Revert revision 5d2fde6fe338
    - We don't support PyQt versions lower than 4.6 already (since 2.3.0) so it
    really doesn't make sense to try to support API #1

--- spyderlib/qt/__init__.py
+++ spyderlib/qt/__init__.py
@@ -10,40 +10,36 @@
 import os
 
 os.environ.setdefault('QT_API', 'pyqt')
-assert os.environ['QT_API'] in ('pyqt', 'pyqtv1', 'pyside')
+assert os.environ['QT_API'] in ('pyqt', 'pyside')
 
 API = os.environ['QT_API']
-API_NAME = None
+API_NAME = {'pyqt': 'PyQt4', 'pyside': 'PySide'}[API]
 
 if API == 'pyqt':
     # Spyder 2.3 is compatible with both #1 and #2 PyQt API,
     # but to avoid issues with IPython and other Qt plugins
     # we choose to support only API #2 for 2.4+
+    import sip
     try:
-        import sip
         sip.setapi('QString', 2)
         sip.setapi('QVariant', 2)
-    except ImportError:
-        print('qt: PyQt4 is not found. Fallback to PySide API')
-        API = os.environ['QT_API'] = 'pyside'
     except AttributeError:
-        # PyQt < 4.6. Fallback to API #1
-        print('qt: Fallback to PyQt4 API #1')
-        API = os.environ['QT_API'] = 'pyqtv1'
+        # PyQt < v4.6. The actual check is done by requirements.check_qt()
+        # call from spyder.py
+        pass
 
-if API in ('pyqtv1', 'pyqt'):
     try:
         from PyQt4.QtCore import PYQT_VERSION_STR as __version__
     except ImportError:
-        # No PyQt4. Fallback to PySide
-        print('qt: Fallback to PySide API')
+        # Switching to PySide
         API = os.environ['QT_API'] = 'pyside'
+        API_NAME = 'PySide'
     else:
         is_old_pyqt = __version__.startswith(('4.4', '4.5', '4.6', '4.7'))
         is_pyqt46 = __version__.startswith('4.6')
         import sip
         try:
-            API_NAME = ("PyQt4 (API v%d)" % sip.getapi('QString'))
+            API_NAME += (" (API v%d)" % sip.getapi('QString'))
         except AttributeError:
             pass
 
@@ -54,4 +50,3 @@ if API == 'pyside':
         raise ImportError("Spyder requires PySide or PyQt to be installed")
     else:
         is_old_pyqt = is_pyqt46 = False
-    API_NAME = 'PySide'
