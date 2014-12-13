b5be92cc21d9128999123ec6e6b8dd0e36e7c74f
Carlos Cordoba <ccordoba12@gmail.com>
2014-12-13 02:05:23 -0500

    External Console: edit_goto is really an overloaded signal (it was defined incorrectly)
    - This fixes wrong behavior while debugging

--- spyderlib/plugins/externalconsole.py
+++ spyderlib/plugins/externalconsole.py
@@ -444,8 +444,8 @@ class ExternalConsole(SpyderPluginWidget):
     CONF_SECTION = 'console'
     CONFIGWIDGET_CLASS = ExternalConsoleConfigPage
     DISABLE_ACTIONS_WHEN_HIDDEN = False
-    
-    edit_goto = Signal(str, int, str, bool)
+
+    edit_goto = Signal((str, int, str), (str, int, str, bool))
     focus_changed = Signal()
     redirect_stdio = Signal(bool)
     
@@ -693,7 +693,7 @@ class ExternalConsole(SpyderPluginWidget):
         # This is a unique form of the edit_goto signal that is intended to 
         # prevent keyboard input from accidentally entering the editor
         # during repeated, rapid entry of debugging commands.    
-        self.edit_goto.emit(fname, lineno, '', False)
+        self.edit_goto[str, int, str, bool].emit(fname, lineno, '', False)
         if shellwidget.is_ipykernel:
             # Focus client widget, not kernel
             ipw = self.main.ipyconsole.get_focus_widget()
@@ -1112,7 +1112,7 @@ class ExternalConsole(SpyderPluginWidget):
                 self.inspector.set_external_console(self)
             self.historylog = self.main.historylog
             self.edit_goto.connect(self.main.editor.load)
-            self.edit_goto.connect(
+            self.edit_goto[str, int, str, bool].connect(
                          lambda fname, lineno, word, processevents:
                          self.main.editor.load(fname, lineno, word,
                                                processevents=processevents))
