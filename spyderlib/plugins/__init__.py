7adf7ce26597ef1e0bc9073762cc226f8dff882f
Carlos Cordoba <ccordoba12@gmail.com>
2014-12-21 22:28:20 -0500

    Main Window: Correctly show shortcuts to move to each plugin in View/Panes menu
    - Before I tried to do the same but failed to give the right context to these
    shortcuts

--- spyderlib/plugins/__init__.py
+++ spyderlib/plugins/__init__.py
@@ -157,6 +157,16 @@ class SpyderPluginMixin(object):
         self.mainwindow = None
         self.ismaximized = False
         self.isvisible = False
+
+        # NOTE: Don't use the default option of CONF.get to assign a
+        # None shortcut to plugins that don't have one. That will mess
+        # the creation of our Keyboard Shortcuts prefs page
+        try:
+            self.shortcut = CONF.get('shortcuts', '_/switch to %s' % \
+                                     self.CONF_SECTION)
+        except configparser.NoOptionError:
+            self.shortcut = None
+
         # We decided to create our own toggle action instead of using
         # the one that comes with dockwidget because it's not possible
         # to raise and focus the plugin with it.
@@ -236,15 +246,10 @@ class SpyderPluginMixin(object):
         self.connect(dock, SIGNAL('plugin_closed()'),
                      self.plugin_closed)
         self.dockwidget = dock
-        try:
-            short = CONF.get('shortcuts', '_/switch to %s' % self.CONF_SECTION)
-        except configparser.NoOptionError:
-            short = None
-        if short is not None:
-            shortcut = QShortcut(QKeySequence(short),
-                                 self.main, self.switch_to_plugin)
-            self.register_shortcut(shortcut, "_",
-                                   "Switch to %s" % self.CONF_SECTION)
+        if self.shortcut is not None:
+            sc = QShortcut(QKeySequence(self.shortcut), self.main,
+                            self.switch_to_plugin)
+            self.register_shortcut(sc, "_", "Switch to %s" % self.CONF_SECTION)
         return (dock, self.LOCATION)
     
     def create_mainwindow(self):
@@ -372,7 +377,12 @@ class SpyderPluginMixin(object):
         title = self.get_plugin_title()
         if self.CONF_SECTION == 'editor':
             title = _('Editor')
-        action = create_action(self, title, toggled=self.toggle_view)
+        if self.shortcut is not None:
+            action = create_action(self, title, toggled=self.toggle_view,
+                                   shortcut=QKeySequence(self.shortcut))
+            action.setShortcutContext(Qt.WidgetWithChildrenShortcut)
+        else:
+            action = create_action(self, title, toggled=self.toggle_view)
         self.toggle_view_action = action
     
     def toggle_view(self, checked):
