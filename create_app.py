a0100edd96df0c163c01a3d1b354cc3c2ebedd3a
Carlos Cordoba <ccordoba12@gmail.com>
2015-03-03 17:01:33 -0500

    Mac app: Add seaborn to the app

--- create_app.py
+++ create_app.py
@@ -73,7 +73,7 @@ EXCLUDES = DEPS + ['mercurial']
 PACKAGES = ['spyderlib', 'spyderplugins', 'sphinx', 'jinja2', 'docutils',
             'IPython', 'zmq', 'pygments', 'rope', 'distutils', 'PIL', 'PyQt4',
             'sklearn', 'skimage', 'pandas', 'sympy', 'pyflakes', 'psutil',
-            'mpl_toolkits', 'nose', 'patsy','statsmodels']
+            'mpl_toolkits', 'nose', 'patsy','statsmodels', 'seaborn']
 
 INCLUDES = get_stdlib_modules()
 EDIT_EXT = [ext[1:] for ext in EDIT_EXT]
