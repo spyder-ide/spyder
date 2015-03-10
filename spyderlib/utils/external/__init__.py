7b13893bc527db266f3d2059aa6f9e629b0ece09
Carlos Cordoba <ccordoba12@gmail.com>
2015-03-10 15:43:47 -0500

    Remove utils.external from sys.path
    Fixes #1952, fixes #1380
    
    - This was added by Pierre a long time ago to use an internal version of
    pyflakes but never removed.

--- spyderlib/utils/external/__init__.py
+++ spyderlib/utils/external/__init__.py
@@ -11,7 +11,3 @@ spyderlib.utils.external
 External libraries needed for Spyder to work.
 Put here only untouched libraries, else put them in utils.
 """
-
-import sys
-from spyderlib.baseconfig import get_module_source_path
-sys.path.insert(0, get_module_source_path(__name__))
