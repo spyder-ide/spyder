6c20fe9f201bec0f1bb208c73a7745761ffde2c4
Steven Silvester <steven.silvester@ieee.org>
2015-03-13 15:41:29 -0500

    Allow for localization

--- spyderlib/utils/introspection/plugin_manager.py
+++ spyderlib/utils/introspection/plugin_manager.py
@@ -7,7 +7,7 @@ import os
 import imp
 import time
 
-from spyderlib.baseconfig import DEBUG, get_conf_path, debug_print
+from spyderlib.baseconfig import _, DEBUG, get_conf_path, debug_print
 from spyderlib.utils.introspection.module_completion import (
     get_preferred_submodules)
 from spyderlib.utils import sourcecode
@@ -444,7 +444,7 @@ class PluginManager(QObject):
 
         if resp['name']:
             if not resp['docstring']:
-                resp['docstring'] = 'No documentation available.'
+                resp['docstring'] = _('No documentation available.')
 
             self.send_to_inspector.emit(
                 resp['name'], resp['argspec'],
