0dd8ed07c23fa553fe2fd91a1b94cb5fa78e2aa1
Carlos Cordoba <ccordoba12@gmail.com>
2015-03-31 21:55:15 -0500

    userconfig: Remove license text
    - This was added because (I think) Pierre designed it at the beginning
    to be used outside Spyder, but now this is simply not possible.

--- spyderlib/userconfig.py
+++ spyderlib/userconfig.py
@@ -1,37 +1,14 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-#    userconfig License Agreement (MIT License)
-#    ------------------------------------------
-#    
-#    Copyright © 2009-2012 Pierre Raybaut
-#    Copyright © 2014 The Spyder Development Team
-#    
-#    Permission is hereby granted, free of charge, to any person
-#    obtaining a copy of this software and associated documentation
-#    files (the "Software"), to deal in the Software without
-#    restriction, including without limitation the rights to use,
-#    copy, modify, merge, publish, distribute, sublicense, and/or sell
-#    copies of the Software, and to permit persons to whom the
-#    Software is furnished to do so, subject to the following
-#    conditions:
-#    
-#    The above copyright notice and this permission notice shall be
-#    included in all copies or substantial portions of the Software.
-#    
-#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-#    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-#    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-#    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-#    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-#    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-#    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-#    OTHER DEALINGS IN THE SOFTWARE.
-
+#
+# Copyright © 2009-2013 Pierre Raybaut
+# Copyright © 2014-2015 The Spyder Development Team
+# Licensed under the terms of the MIT License
+# (see spyderlib/__init__.py for details)    
 
 """
-This module provides user configuration file (.ini file) management features
-based on ``ConfigParser`` (present in the standard library).
+This module provides user configuration file management features for Spyder
+
+It's based on the ConfigParser module (present in the standard library).
 """
 
 from __future__ import print_function
