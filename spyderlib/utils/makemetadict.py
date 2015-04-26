36fd80a990a2e9b100aa9b51bfc80e01ffd4f2ea
Daniel Manson <d.manson.11@ucl.ac.uk>
2015-04-26 14:06:16 +0100

    1d and 2d plotting in makemetadict

--- spyderlib/utils/makemetadict.py
+++ spyderlib/utils/makemetadict.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# TODO: ideally this file should be user-editable like the startup.py file
-
 from contextlib import contextmanager
 from collections import OrderedDict
 
 def make_meta_dict(value):
     """
     This wraps the user's code, giving a stacktrace on errors, but still
-    returning a valid output.
+    returning a valid output.  The idea is that the make_meta_dict_user
+    is actually located in a separate file that can be modified by the
+    user (like the startup.py scripts).
     """
     try:
         return make_meta_dict_user(value)
@@ -31,10 +31,26 @@ def make_meta_dict_user(value):
     variable in question.  
     
     The function should return a dictionary-like object with "simple" meta data
-    to be rendered as a list of key-values.
+    to be rendered as a list of key-values.  Note we suggest using an OrderedDict
+    in order to control the order in which the keys are displayed.
     
     If an "html" key is present in the dictionary, that data wil be interpreted
-    as raw html and rendered after the simple meta data.    
+    as raw html and rendered after the simple meta data.  
+    Search for "richtext-html-subset qt" to find out exactly what html is
+    supported.
+    
+    Note that ``value`` may be a copy or may be the original value, so
+    so do not modify it inplace, but conversely do not rely on "is"
+    comparisons for caching or other comparisons.
+            
+    You can display multiple images, but here we try to guess what kind
+    of graphical representation would best suit the given data.  If you 
+    don't like that you can change it!
+    
+    Although this function may be long each bit of it should be kept 
+    fairly independent...you  may want to split it into lots of little
+    functions and then call then explictly call each in turn, wrapped
+    in the lazy ignore(Exception).
     """
     meta = OrderedDict()
     class DudObject():
@@ -73,19 +89,70 @@ def make_meta_dict_user(value):
                 meta['memory'] = str(round(n_bytes/(1024.0**2), 1)) + 'MB' 
 
         with ignore(Exception):
-            if value.ndim == 2 and value.shape[0] > 6 and value.shape[1] > 6:
-                # we have a 2D array which is bigger than 6x6, so make a thumbnail
-                from io import BytesIO
-                from PIL import Image
-                from base64 import b64encode
+            can_make_images = False
+            from io import BytesIO
+            from PIL import Image, ImageDraw
+            from base64 import b64encode
+            def img_to_html(img, alt_text='data img'):
+                b = BytesIO()  
+                img.save(b, format='png')
+                return '<img alt="' + alt_text +'"'\
+                            + ' src="data:image/png;base64,' \
+                            +  b64encode(b.getvalue()) + '" />'
+            can_make_images = True
+        image_list = []
+        
+        # If we have a 2D array which is bigger than 6x6, make a thumbnail                   
+        with ignore(Exception):
+            if can_make_images and value.ndim == 2 and value.shape[0] > 6 and value.shape[1] > 6:
                 from matplotlib import cm
                 from matplotlib.colors import Normalize
-                b = BytesIO()  
                 if not ma.isMaskedArray(value):
                     value = ma.array(value, mask=isnan(value))  
                 img = Image.fromarray(cm.jet(Normalize()(value), bytes=True))
                 img.thumbnail((128,128))
-                img.save(b, format='png')
-                meta['html'] = '<img alt="2d array" src="data:image/png;base64,' + \
-                                b64encode(b.getvalue()) + '" />'       
+                image_list.append(img_to_html(img, "matshow image"))
+                
+        # If we have a long 1d array, then show a simple line plot 
+        # unless the line plot fills more than half the axes area
+        with ignore(Exception):
+            if value.squeeze().ndim == 1 and value.size > 15:
+                from numpy import (linspace, max as np_max, min as np_min, 
+                                   corrcoef, arange)
+                img = Image.new("RGBA", (128,128))
+                draw = ImageDraw.Draw(img)
+                value = value.squeeze()
+                value = value - np_min(value)
+                fill_color = (255,0,0,255)
+                draw.line(zip(linspace(0,127,value.size),
+                         127-value * 127./np_max(value)),
+                        fill_color)   
+                filled_count = next(count for count, color in img.getcolors()\
+                                            if color == fill_color)
+                if filled_count < 0.5*img.size[0]*img.size[1]:
+                    image_list.append(img_to_html(img,"1d line plot"))
+                    meta['corrcoef'] = corrcoef(arange(len(value)),value)[0,1]
+                    
+        # If we have a 2xn or nx2 with large n, then do a scatter plot
+        with ignore(Exception):
+            if value.squeeze().ndim == 2 and min(value.shape) == 2 and \
+                    max(value.shape) > 15:
+                from numpy import (max as np_max, min as np_min, corrcoef)
+                img = Image.new("RGBA", (128,128))
+                draw = ImageDraw.Draw(img)
+                value = value.squeeze()
+                value = value if value.shape[0] == 2 else value.T
+                value = value - np_min(value, axis=1, keepdims=True)
+                fill_color = (0,0,255,255)
+                draw.point(zip(value[0]*127./np_max(value[0]),
+                         127-value[1]*127./np_max(value[1])),
+                         fill_color)   
+                filled_count = next(count for count, color in img.getcolors()\
+                                            if color == fill_color)
+                if filled_count < 0.5*img.size[0]*img.size[1]:
+                    image_list.append(img_to_html(img,"xy scatter plot"))
+                    meta['corrcoef'] = corrcoef(value[0],value[1])[0,1]
+            
+        if len(image_list) > 0:
+            meta['html'] = ' '.join(image_list)
     return meta
