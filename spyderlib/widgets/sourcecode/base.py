3eb1bd36ed29c8a0dc10c4340b8ecd36160410d9
Steven Silvester <steven.silvester@ieee.org>
2015-05-02 15:20:57 -0500

    Cleanup

--- spyderlib/widgets/sourcecode/base.py
+++ spyderlib/widgets/sourcecode/base.py
@@ -900,20 +900,19 @@ class TextEditBaseWidget(QPlainTextEdit, BaseEditMixin):
     def show_completion_list(self, completions, completion_text="",
                              automatic=True):
         """Display the possible completions"""
-        c = completions
-        if len(c) == 0:
+        if not completions:
             return
-        if not isinstance(c[0], tuple):
-            c = [(c, '') for c in completions]
-        if len(c) == 1 and c[0][0] == completion_text:
+        if not isinstance(completions[0], tuple):
+            completions = [(c, '') for c in completions]
+        if len(completions) == 1 and completions[0][0] == completion_text:
             return
         self.completion_text = completion_text
         # Sorting completion list (entries starting with underscore are 
         # put at the end of the list):
-        underscore = set([(comp, t) for (comp, t) in c
+        underscore = set([(comp, t) for (comp, t) in completions
                           if comp.startswith('_')])
 
-        completions = sorted(set(c) - underscore,
+        completions = sorted(set(completions) - underscore,
                              key=lambda x: str_lower(x[0]))
         completions += sorted(underscore, key=lambda x: str_lower(x[0]))
         self.show_completion_widget(completions, automatic=automatic)
