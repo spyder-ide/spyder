5a9179f497e4aa525220073bbe91e3ae9ff89f95
Carlos Cordoba <ccordoba12@gmail.com>
2015-06-08 18:01:19 -0500

    Try to fix startup crashes when creating spyder.lock
    Fixes #2363
    
    - This also tries to remove stale spyder.lock files left from previous
    attempts

--- spyderlib/start_app.py
+++ spyderlib/start_app.py
@@ -73,24 +73,54 @@ def main():
         # instances started at the same time step in their
         # own foots while trying to create the lock file
         time.sleep(random.randrange(1000, 2000, 90)/10000.)
-        
+
         # Lock file creation
-        lockf = get_conf_path('spyder.lock')
-        lock = lockfile.FilesystemLock(lockf)
-        
-        # lock.lock() tries to lock spyder.lock. If it fails,
-        # it returns False and so we try to start the client
-        if not lock.lock():
+        lock_file = get_conf_path('spyder.lock')
+        lock = lockfile.FilesystemLock(lock_file)
+
+        # Try to lock spyder.lock. If it's *possible* to do it, then
+        # there is no previous instance running and we can start a
+        # new one. If *not*, then there is an instance already
+        # running, which is locking that file
+        try:
+            lock_created = lock.lock()
+        except:
+            # If locking fails because of errors in the lockfile
+            # module, try to remove a possibly stale spyder.lock.
+            # This is reported to solve all problems with
+            # lockfile (See issue 2363)
+            try:
+                if os.name == 'nt':
+                    if osp.isdir(lock_file):
+                        import shutil
+                        shutil.rmtree(lock_file, ignore_errors=True)
+                else:
+                    if osp.islink(lock_file):
+                        os.unlink(lock_file)
+            except:
+                pass
+
+            # Then start Spyder as usual and *don't* continue
+            # executing this script because it doesn't make
+            # sense
+            from spyderlib import spyder
+            spyder.main()
+            return
+
+        if lock_created:
+            # Start a new instance
+            if TEST is None:
+                atexit.register(lock.unlock)
+            from spyderlib import spyder
+            spyder.main()
+        else:
+            # Pass args to Spyder or print an informative
+            # message
             if args:
                 send_args_to_spyder(args)
             else:
                 print("Spyder is already running. If you want to open a new \n"
                       "instance, please pass to it the --new-instance option")
-        else:
-            if TEST is None:
-                atexit.register(lock.unlock)
-            from spyderlib import spyder
-            spyder.main()
     else:
         from spyderlib import spyder
         spyder.main()
