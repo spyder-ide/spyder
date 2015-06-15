b5db0504336a1ee3d78e2ace91c9d11549e10301
Daniel Manson <d.manson.11@ucl.ac.uk>
2015-06-15 23:09:17 +0100

    move helperwidget styling to class def

--- spyderlib/widgets/helperwidgets.py
+++ spyderlib/widgets/helperwidgets.py
@@ -7,10 +7,26 @@
 
 from spyderlib.qt.QtCore import QPoint
 from spyderlib.qt.QtGui import QToolButton, QToolTip
+from spyderlib.utils.qthelpers import get_std_icon
+
 
 class HelperToolButton(QToolButton):
     """Subclasses QToolButton, to provide a simple tooltip on mousedown.
     """
+    def __init__(self):
+        QToolButton.__init__(self)
+        self.setIcon(get_std_icon('MessageBoxInformation'))
+        style = """
+            QToolButton {
+              border: 1px solid grey;
+              padding:0px;
+              border-radius: 2px;
+              background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
+                  stop: 0 #f6f7fa, stop: 1 #dadbde);
+            }
+            """
+        self.setStyleSheet(style)
+
     def setToolTip(self, text):
         self._tip_text = text
 
