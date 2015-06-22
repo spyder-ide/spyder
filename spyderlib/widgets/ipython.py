2c4e0d4dc0a990b122bcb55ed24eab07efd9b223
Carlos Cordoba <ccordoba12@gmail.com>
2015-06-21 22:20:26 -0500

    IPython Console: Improve showing signatures

--- spyderlib/widgets/ipython.py
+++ spyderlib/widgets/ipython.py
@@ -13,6 +13,7 @@ from __future__ import absolute_import
 # Stdlib imports
 import os
 import os.path as osp
+import re
 from string import Template
 import sys
 import time
@@ -48,6 +49,7 @@ from spyderlib.widgets.browser import WebView
 from spyderlib.widgets.calltip import CallTipWidget
 from spyderlib.widgets.mixins import (BaseEditMixin, InspectObjectMixin,
                                       SaveHistoryMixin, TracebackLinksMixin)
+from spyderlib.py3compat import PY3
 
 
 #-----------------------------------------------------------------------------
@@ -252,16 +254,32 @@ These commands were executed:
 
         return [inspect, clear_console]
 
+    def clean_invalid_var_chars(self, var):
+        """
+        Replace invalid variable chars in a string by underscores
+
+        Taken from http://stackoverflow.com/a/3305731/438386
+        """
+        if PY3:
+            return re.sub('\W|^(?=\d)', '_', var, re.UNICODE)
+        else:
+            return re.sub('\W|^(?=\d)', '_', var)
+
     def get_signature(self, content):
         """Get signature from inspect reply content"""
-        self._control.current_prompt_pos = self._prompt_pos
         data = content.get('data', {})
         text = data.get('text/plain', '')
         if text:
             text = ANSI_OR_SPECIAL_PATTERN.sub('', text)
+            self._control.current_prompt_pos = self._prompt_pos
             line = self._control.get_current_line_to_cursor()
             name = line[:-1].split('(')[-1]   # Take last token after a (
             name = name.split('.')[-1]        # Then take last token after a .
+            # Clean name from invalid chars
+            try:
+                name = self.clean_invalid_var_chars(name).split('_')[-1]
+            except:
+                pass
             argspec = getargspecfromtext(text)
             if argspec:
                 # This covers cases like np.abs, whose docstring is
