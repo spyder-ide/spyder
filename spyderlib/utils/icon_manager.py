f8e3ae798013fc7d88d50f229ccdd003c7001052
Sylvain Corlay <sylvain.corlay@gmail.com>
2015-07-10 12:31:28 -0400

    Carlos icons

--- spyderlib/utils/icon_manager.py
+++ spyderlib/utils/icon_manager.py
@@ -42,7 +42,7 @@ _qtaargs = {
     'arrow-step-out':          [('spyder.step-out',), {'color': '#3775a9'}],
     'stop_debug':              [('fa.stop',), {'color': '#3775a9'}],
     'run':                     [('fa.play',), {'color': 'green'}],
-    'run_settings':            [('fa.wrench', 'fa.play'), {'options': [{'offset':(0.0, -0.1)}, {'offset': (0.1, 0.125), 'color': 'green', 'scale_factor': 0.8}]}],
+    'run_settings':            [('fa.wrench', 'fa.play'), {'options': [{'offset':(0.0, -0.1)}, {'offset': (0.2, 0.125), 'color': 'green', 'scale_factor': 0.8}]}],
     'run_again':               [('fa.repeat', 'fa.play'), {'options': [{'offset':(0.0, -0.1)}, {'offset': (0.2, 0.125), 'color': 'green', 'scale_factor': 0.8}]}],
     'run_selection':           [('spyder.run-selection',), {}],
     'run_cell':                [('spyder.cell-page', 'spyder.cell-page-shadow', 'spyder.cell-code', 'spyder.cell-border', 'spyder.cell-play'),
