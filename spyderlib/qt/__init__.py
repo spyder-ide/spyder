c5b65be48df6c8abef4b777b0f254ab44ee5e609
stonebig <stonebig34@gmail.com>
2015-07-15 18:46:21 +0200

    be more PyQt5 friendly, when PyQt5 is here

--- spyderlib/qt/__init__.py
+++ spyderlib/qt/__init__.py
@@ -8,8 +8,15 @@
 """Transitional package (PyQt4 --> PySide)"""
 
 import os
+import imp
 
-os.environ.setdefault('QT_API', 'pyqt')
+try:
+    #be friendly with Pyqt5
+    are_you_here = __import__('PyQt5')
+    os.environ.setdefault('QT_API','pyqt5')
+except:
+    os.environ.setdefault('QT_API', 'pyqt')
+
 assert os.environ['QT_API'] in ('pyqt5', 'pyqt', 'pyside')
 
 API = os.environ['QT_API']
