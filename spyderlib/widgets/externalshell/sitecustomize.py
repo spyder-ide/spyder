c3d08dcb808c3da3affa71aef92671cca3b1a118
Carlos Cordoba <ccordoba12@gmail.com>
2015-07-21 18:42:41 -0500

    Define qt_nt_inputhook only for Windows
    - Also extracts the Qt imports that I added inside it before because it
    makes no sense to do those imports every time the inputhook is called.

--- spyderlib/widgets/externalshell/sitecustomize.py
+++ spyderlib/widgets/externalshell/sitecustomize.py
@@ -204,8 +204,9 @@ try:
 except ImportError:
     matplotlib = None   # analysis:ignore
 
+
 #==============================================================================
-# Communication between Spyder and the remote process
+# Monitor-based functionality
 #==============================================================================
 if os.environ.get('SPYDER_SHELL_ID') is None:
     monitor = None
@@ -245,13 +246,8 @@ else:
             _print("Can't open file %s" % source, file=sys.stderr)
     builtins.open_in_spyder = open_in_spyder
 
-    def qt_nt_inputhook():
-        """Qt input hook for Windows
-        
-        This input hook wait for available stdin data (notified by
-        ExternalPythonShell through the monitor's inputhook_flag
-        attribute), and in the meantime it processes Qt events.
-        """
+    # Our own input hook, monitor based and for Windows only
+    if os.name == 'nt':
         # Qt imports
         if os.environ["QT_API"] == 'pyqt5':
             from PyQt5 import QtCore
@@ -260,43 +256,50 @@ else:
         elif os.environ["QT_API"] == 'pyside':
             from PySide import QtCore          # analysis:ignore
 
-        # Refreshing variable explorer, except on first input hook call:
-        # (otherwise, on slow machines, this may freeze Spyder)
-        monitor.refresh_from_inputhook()
-        try:
-            # This call fails for Python without readline support
-            # (or on Windows platforms) when PyOS_InputHook is called
-            # for the second consecutive time, because the 100-bytes
-            # stdin buffer is full.
-            # For more details, see the `PyOS_StdioReadline` function
-            # in Python source code (Parser/myreadline.c)
-            sys.stdin.tell()
-        except IOError:
+        def qt_nt_inputhook():
+            """Qt input hook for Windows
+            
+            This input hook wait for available stdin data (notified by
+            ExternalPythonShell through the monitor's inputhook_flag
+            attribute), and in the meantime it processes Qt events.
+            """
+            # Refreshing variable explorer, except on first input hook call:
+            # (otherwise, on slow machines, this may freeze Spyder)
+            monitor.refresh_from_inputhook()
+            try:
+                # This call fails for Python without readline support
+                # (or on Windows platforms) when PyOS_InputHook is called
+                # for the second consecutive time, because the 100-bytes
+                # stdin buffer is full.
+                # For more details, see the `PyOS_StdioReadline` function
+                # in Python source code (Parser/myreadline.c)
+                sys.stdin.tell()
+            except IOError:
+                return 0
+    
+            # Input hook
+            app = QtCore.QCoreApplication.instance()
+            if app and app.thread() is QtCore.QThread.currentThread():
+                timer = QtCore.QTimer()
+                timer.timeout.connect(app.quit)
+                monitor.toggle_inputhook_flag(False)
+                while not monitor.inputhook_flag:
+                    timer.start(50)
+                    QtCore.QCoreApplication.exec_()
+                    timer.stop()
+    
+                # Socket-based alternative:
+                #socket = QtNetwork.QLocalSocket()
+                #socket.connectToServer(os.environ['SPYDER_SHELL_ID'])
+                #socket.waitForConnected(-1)
+                #while not socket.waitForReadyRead(10):
+                #    timer.start(50)
+                #    QtCore.QCoreApplication.exec_()
+                #    timer.stop()
+                #socket.read(3)
+                #socket.disconnectFromServer()
             return 0
 
-        # Input hook
-        app = QtCore.QCoreApplication.instance()
-        if app and app.thread() is QtCore.QThread.currentThread():
-            timer = QtCore.QTimer()
-            timer.timeout.connect(app.quit)
-            monitor.toggle_inputhook_flag(False)
-            while not monitor.inputhook_flag:
-                timer.start(50)
-                QtCore.QCoreApplication.exec_()
-                timer.stop()
-
-            # Socket-based alternative:
-            #socket = QtNetwork.QLocalSocket()
-            #socket.connectToServer(os.environ['SPYDER_SHELL_ID'])
-            #socket.waitForConnected(-1)
-            #while not socket.waitForReadyRead(10):
-            #    timer.start(50)
-            #    QtCore.QCoreApplication.exec_()
-            #    timer.stop()
-            #socket.read(3)
-            #socket.disconnectFromServer()
-        return 0
-    
 
 #==============================================================================
 # Matplotlib settings
