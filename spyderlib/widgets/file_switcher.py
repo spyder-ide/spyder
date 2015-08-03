0a644b2fa7f7ce14c66e3a5aa423e27dde7b6e17
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2015-08-03 02:18:36 -0500

    Add fuzzy ordered search and size autoadjust

--- spyderlib/widgets/file_switcher.py
+++ spyderlib/widgets/file_switcher.py
@@ -8,7 +8,6 @@
 from __future__ import print_function
 import os
 import os.path as osp
-import re
 
 from spyderlib.qt.QtCore import Signal, Qt, QObject, QSize, QEvent
 from spyderlib.qt.QtGui import (QVBoxLayout, QHBoxLayout,
@@ -19,6 +18,7 @@ from spyderlib.qt.QtGui import (QVBoxLayout, QHBoxLayout,
 from spyderlib.baseconfig import _
 from spyderlib.guiconfig import new_shortcut
 from spyderlib.py3compat import to_text_string
+from spyderlib.utils.stringmatching import get_search_scores
 from spyderlib.widgets.helperwidgets import HelperToolButton, HTMLDelegate
 
 
@@ -48,7 +48,7 @@ def shorten_paths(path_list, is_unsaved):
             path_list[ii] = [part for part in path.split(osp.sep) if part]
 
     def recurse_level(level_idx):
-        # If toks are all empty we need not have reucrsed here
+        # If toks are all empty we need not have recursed here
         if not any(level_idx.values()):
             return
 
@@ -117,25 +117,6 @@ def shorten_paths(path_list, is_unsaved):
     return [path.rstrip(os.sep) for path in new_path_list]
 
 
-def BuildFuzzyFormatter(needle):
-    """
-    Matches on the first occurance of the letters in needle in order, wrapping
-    those letters with <b></b> tags. If no match is found None is returned.
-    """
-    re_exons = re.compile(''.join("([^" + re.escape(c) + "]*)" + re.escape(c)
-                                  for c in needle) + '(.*)')
-
-    def func(text):
-        exons = re_exons.match(text)
-        if not exons:
-            return
-        exons = exons.groups()
-        ret = ''.join(e + i for e, i in
-                      zip(exons, ['<b>' + c + '</b>'for c in needle]))
-        return ret + exons[-1]
-    return func
-
-
 class UpDownFilter(QObject):
     """
     Use with `installEventFilter` to get up/down arrow key press signal.
@@ -159,45 +140,36 @@ class FileSwitcher(QDialog):
 
     def __init__(self, parent, tabs, tab_data):
         QDialog.__init__(self, parent)
-        self.setWindowFlags(Qt.Popup | Qt.FramelessWindowHint)
-        self.setWindowOpacity(0.9)
+
+        # Variables
         self.tabs = tabs
         self.tab_data = tab_data
+
         self.filtered_index_to_path = None
         self.full_index_to_path = None
         self.path_to_line_count = None
-
-        edit_layout = QHBoxLayout()
-        self.edit = QLineEdit(self)
-        self.up_down_filter = UpDownFilter()
-        self.up_down_filter.sig_up_down.connect(self.handle_sig_up_down)
-        self.edit.installEventFilter(self.up_down_filter)
-        self.edit.returnPressed.connect(self.handle_sig_edit_file)
-        self.edit.textChanged.connect(lambda text: self.synchronize(None))
-
-        edit_layout.addWidget(self.edit)
-        self.button_help = HelperToolButton()
-        help_str = _("""
-            Press <b>Enter</b> to switch files or <b>Esc</b> to cancel.<br><br>
-            Type to filter filenames.<br><br>
-            Use <b>:number</b> to go to a line, e.g. 'main:42'.<br><br>
-            Press <b>Ctrl+W</b> to close current tab.
-            """)
-        self.button_help.setToolTip(help_str)
-        edit_layout.addWidget(self.button_help)
-
-        self.listwidget = QListWidget(self)
-        self.listwidget.setItemDelegate(HTMLDelegate(self))
-        self.listwidget.itemSelectionChanged.connect(
-            self.item_selection_changed)
-        self.listwidget.itemActivated.connect(self.handle_sig_edit_file)
         self.original_path = None
         self.original_line_num = None
         self.line_num = -1
         self.line_num_modified_for_path = None
+        help_str = _("Press <b>Enter</b> to switch files or <b>Esc</b> to"
+                     "cancel.<br><br>Type to filter filenames.<br><br>"
+                     "Use <b>:number</b> to go to a line, e.g. 'main:42'."
+                     "<br><br> Press <b>Ctrl+W</b> to close current tab.")
 
-        self.sig_edit_line.connect(self.handle_sig_edit_line)
-        self.rejected.connect(self.handle_rejection)
+        # Widgets
+        self.edit = QLineEdit(self)
+        self.button_help = HelperToolButton()
+        self.listwidget = QListWidget(self)
+        self.up_down_filter = UpDownFilter()
+
+        # Widgets setup
+        self.setWindowFlags(Qt.Popup | Qt.FramelessWindowHint)
+        self.setWindowOpacity(0.9)
+
+        self.edit.installEventFilter(self.up_down_filter)
+        self.button_help.setToolTip(help_str)
+        self.listwidget.setItemDelegate(HTMLDelegate(self))
 
         def close_tab():
             self.sig_close_file.emit(
@@ -205,10 +177,25 @@ class FileSwitcher(QDialog):
                      self.listwidget.currentRow()))
         new_shortcut("Ctrl+W", self, close_tab)
 
+        # Layout
+        edit_layout = QHBoxLayout()
+        edit_layout.addWidget(self.edit)
+        edit_layout.addWidget(self.button_help)
+
         vlayout = QVBoxLayout()
         vlayout.addLayout(edit_layout)
         vlayout.addWidget(self.listwidget)
         self.setLayout(vlayout)
+
+        # Signals
+        self.edit.returnPressed.connect(self.handle_sig_edit_file)
+        self.edit.textChanged.connect(lambda text: self.synchronize(None))
+        self.sig_edit_line.connect(self.handle_sig_edit_line)
+        self.listwidget.itemSelectionChanged.connect(
+            self.item_selection_changed)
+        self.listwidget.itemActivated.connect(self.handle_sig_edit_file)
+        self.up_down_filter.sig_up_down.connect(self.handle_sig_up_down)
+        self.rejected.connect(self.handle_rejection)
 
     def show(self, stack_index):
         QDialog.show(self)
@@ -219,8 +206,7 @@ class FileSwitcher(QDialog):
         self.edit.selectAll()
         self.edit.setFocus()
         geo = parent.geometry()
-        width = min(300, 0.8*geo.width())
-        self.listwidget.setMinimumWidth(width)
+        width = self.listwidget.width()  # This has been set in synchronize
 
         left = parent.geometry().width()/2 - width/2
         top = 0
@@ -229,8 +215,7 @@ class FileSwitcher(QDialog):
             top += geo.top()
             left += geo.left()
             parent = parent.parent()
-        self.move(left,
-                  top + self.tabs.tabBar().geometry().height() + 1)
+        self.move(left, top + self.tabs.tabBar().geometry().height() + 1)
         # Note: the +1px on the top makes it look a little better
 
     def filtered_index_to_full(self, idx):
@@ -282,7 +267,7 @@ class FileSwitcher(QDialog):
             self.line_num_modified_for_path = None
             self.original_line_num = None
 
-        self.line_num = line_num  # record it for use when switching items
+        self.line_num = line_num  # Record it for use when switching items
 
         # Apply the line num to the current file, recording the original
         # location if need be
@@ -324,11 +309,11 @@ class FileSwitcher(QDialog):
             self.full_index_to_short_path = \
                 shorten_paths(self.full_index_to_path,
                               full_index_to_is_unsaved)
-            self.path_to_line_count = None  # we only get this on demand
+            self.path_to_line_count = None  # We only get this on demand
         else:
-            current_path = self.current_path()  # could be None
+            current_path = self.current_path()  # Could be None
 
-        # get filter text and optional line number
+        # Get filter text and optional line number
         filter_text = to_text_string(self.edit.text()).lower()
         trying_for_line_num = (':' in filter_text)
         filter_text, line_num = (filter_text.split(':', 1)
@@ -346,25 +331,38 @@ class FileSwitcher(QDialog):
 
         self.listwidget.clear()
         self.filtered_index_to_path = []
-        if filter_text:
-            filter_formatter = BuildFuzzyFormatter(filter_text)
+
+        # Get all available filenames and get the scores for "fuzzy" matching        
+        choices = []
         for index, path in enumerate(self.full_index_to_path):
-            text = to_text_string(self.tabs.tabText(index))
-            if filter_text:
-                text = filter_formatter(text)
-            if not filter_text or text:
+            choices.append(to_text_string(self.tabs.tabText(index)))
+        scores = get_search_scores(filter_text, choices, template="<b>{0}</b>")
+
+        # Build the text that will appear on the list widget
+        results = []
+        for index, score in enumerate(scores):
+            text = score[0]
+            score_value = score[-1]
+            if score_value != -1:
                 if trying_for_line_num:
                     text += " [{0:} {1:}]".format(
                         self.path_to_line_count[path], _("lines"))
                 text = '<big>' + text + '</big>'
                 text += "<br><i>{0:}</i>".format(
                     self.full_index_to_short_path[index])
-                item = QListWidgetItem(self.tabs.tabIcon(index),
-                                       text, self.listwidget)
-                item.setToolTip(path)
-                item.setSizeHint(QSize(0, 25))
-                self.listwidget.addItem(item)
-                self.filtered_index_to_path.append(path)
+
+                results.append((score_value, index, text))
+
+        # Sort the obtained scores and populate the list widget
+        for result in sorted(results):
+            index = result[1]
+            text = result[-1]
+            path = self.full_index_to_path[index]
+            item = QListWidgetItem(self.tabs.tabIcon(index), text)
+            item.setToolTip(path)
+            item.setSizeHint(QSize(0, 25))
+            self.listwidget.addItem(item)
+            self.filtered_index_to_path.append(path)
 
         if (current_path is not None and
                 current_path in self.filtered_index_to_path):
@@ -373,3 +371,9 @@ class FileSwitcher(QDialog):
         elif self.filtered_index_to_path:
             self.listwidget.setCurrentRow(0)
         self.sig_edit_line.emit(line_num)  # Note that line_num may =-1
+
+        # Update size of dialog based on longest shortened path
+        fm = self.listwidget.fontMetrics()
+        max_width = max([fm.width(l) for l in self.full_index_to_short_path])
+        print(max_width)
+        self.listwidget.setMinimumWidth(max_width + 50)
