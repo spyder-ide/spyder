7ea6912fcd4e72ae524a26362c97269ffe346db3
Carlos Cordoba <ccordoba12@gmail.com>
2015-08-15 11:13:37 -0500

    IPython Console: Check if qtconsole module is installed for IPython 4.0+
    This will avoid a crash if only IPython is installed, but not qtconsole

--- spyderlib/ipythonconfig.py
+++ spyderlib/ipythonconfig.py
@@ -14,11 +14,20 @@ from spyderlib.utils import programs
 def is_qtconsole_installed():
     pyzmq_installed = programs.is_module_installed('zmq')
     pygments_installed = programs.is_module_installed('pygments')
-    if programs.is_module_installed('IPython.qt') and pyzmq_installed \
-      and pygments_installed:
-        return True
+    ipyqt_installed = programs.is_module_installed('IPython.qt')
+    ipy4_installed = programs.is_module_installed('IPython', '>=4.0')
+
+    if ipyqt_installed and pyzmq_installed and pygments_installed:
+        if ipy4_installed:
+            if programs.is_module_installed('qtconsole'):
+                return True
+            else:
+                return False
+        else:
+            return True
     else:
         return False
+
 
 SUPPORTED_IPYTHON = '>=1.0'
 IPYTHON_QT_INSTALLED = is_qtconsole_installed()
