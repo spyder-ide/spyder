0fadd4575a57b1af79c5a03aeb556e82b03b8f3a
Daniel Manson <d.manson.11@ucl.ac.uk>
2015-09-21 17:39:12 +0100

    outline widget auto-build toolbar from context menu

--- spyderlib/widgets/editortools.py
+++ spyderlib/widgets/editortools.py
@@ -534,18 +534,25 @@ class OutlineExplorerWidget(QWidget):
                 self.outlineexplorer_is_visible.emit()
         
     def setup_buttons(self):
-        fromcursor_btn = create_toolbutton(self,
-                             icon=ima.icon('fromcursor'),
-                             tip=_('Go to cursor position'),
-                             triggered=self.treewidget.go_to_cursor_position)
-        collapse_btn = create_toolbutton(self)
-        collapse_btn.setDefaultAction(self.treewidget.collapse_selection_action)
-        expand_btn = create_toolbutton(self)
-        expand_btn.setDefaultAction(self.treewidget.expand_selection_action)
-        restore_btn = create_toolbutton(self)
-        restore_btn.setDefaultAction(self.treewidget.restore_action)
-        return (fromcursor_btn, collapse_btn, expand_btn, restore_btn)
-        
+        """Reuses the actions from the context menu of the treewidget as
+        buttons for a toolbar.
+        """
+        self.treewidget.update_menu()
+        actions = (a for a in self.treewidget.menu.actions()
+                   if not a.isSeparator())
+        buttons = []
+        non_icon_buttons = []
+        for action in actions:                    
+            if action.icon().isNull():
+                non_icon_buttons.append(action)
+            else:
+                new_button = create_toolbutton(self)
+                new_button.setDefaultAction(action)
+                buttons.append(new_button) 
+        if non_icon_buttons:
+            pass # TODO: buttons should get a continuation dropdown for these
+        return buttons
+                
     def set_current_editor(self, editor, fname, update, clear):
         if clear:
             self.remove_editor(editor)
