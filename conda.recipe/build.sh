a60a090c0fbe0aa91771cc4d9511eda28c1bac23
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-04 11:53:06 -0500

    Fix conda recipe for Linux and Python 3

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -3,5 +3,10 @@
 $PYTHON setup.py install
 
 rm -rf $PREFIX/man
-rm -rf $PREFIX/share
 rm -f $PREFIX/bin/spyder_win_post_install.py
+rm -rf $SP_DIR/Sphinx-*
+
+if [[ ($PY3K == 1) && (`uname` == Linux) ]]; then
+    BIN=$PREFIX/bin
+    mv $BIN/spyder3 $BIN/spyder
+fi
