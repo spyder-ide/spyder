2ef9f9dce250c79e7b139d7a2622f3f672af51be
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-08 14:26:00 -0500

    Testing: Rename 4 spaces indentation in travis_script

--- continuous_integration/travis_script.sh
+++ continuous_integration/travis_script.sh
@@ -3,49 +3,49 @@
 set -ex
 
 for f in spyderlib/*.py; do
-  if [[ $f == "spyderlib/restart_app.py" ]]; then
-    continue
-  fi
-  if [[ $f == "spyderlib/spyder.py" ]]; then
-    continue
-  fi
-  if [[ $f == "spyderlib/tour.py" ]]; then
-    continue
-  fi
-  if [[ $f == "spyderlib/start_app.py" ]]; then
-    continue
-  fi
-  if [[ $f == "spyderlib/pil_patch.py" ]]; then
-    continue
-  fi
-  python "$f"
-  if [ $? -ne 0 ]; then
-    exit 1
-  fi
+    if [[ $f == "spyderlib/restart_app.py" ]]; then
+        continue
+    fi
+    if [[ $f == "spyderlib/spyder.py" ]]; then
+        continue
+    fi
+    if [[ $f == "spyderlib/tour.py" ]]; then
+        continue
+    fi
+    if [[ $f == "spyderlib/start_app.py" ]]; then
+        continue
+    fi
+    if [[ $f == "spyderlib/pil_patch.py" ]]; then
+        continue
+    fi
+    python "$f"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
 done
 
 
 for f in spyderlib/*/*.py; do
-  if [[ $f == spyderlib/plugins/*.py ]]; then
-    continue
-  fi
-  if [[ $f == spyderlib/qt/*.py ]]; then
-    continue
-  fi
-  if [[ $f == spyderlib/utils/environ.py ]]; then
-    continue
-  fi
-  if [[ $f == spyderlib/utils/qthelpers.py ]]; then
-    continue
-  fi
-  if [[ $f == spyderlib/utils/windows.py ]]; then
-    continue
-  fi
-  if [[ $f == spyderlib/widgets/*.py ]]; then
-    continue
-  fi
-  python "$f"
-  if [ $? -ne 0 ]; then
-    exit 1
-  fi
+    if [[ $f == spyderlib/plugins/*.py ]]; then
+        continue
+    fi
+    if [[ $f == spyderlib/qt/*.py ]]; then
+        continue
+    fi
+    if [[ $f == spyderlib/utils/environ.py ]]; then
+        continue
+    fi
+    if [[ $f == spyderlib/utils/qthelpers.py ]]; then
+        continue
+    fi
+    if [[ $f == spyderlib/utils/windows.py ]]; then
+        continue
+    fi
+    if [[ $f == spyderlib/widgets/*.py ]]; then
+        continue
+    fi
+    python "$f"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
 done
