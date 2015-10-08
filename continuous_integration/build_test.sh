a99d7eca88cd7a411c62537cc132ea8e4ab245a5
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-07 22:01:44 -0500

    Yet another try at building the current PR with our conda recipe in Travis

--- continuous_integration/build_test.sh
+++ continuous_integration/build_test.sh
@@ -3,5 +3,7 @@
 set -ex
 
 if [ "$USE_CONDA" = true ] ; then
+    wget https://github.com/spyder-ide/spyder/pull/$TRAVIS_PULL_REQUEST.patch
+    mv $TRAVIS_PULL_REQUEST.patch conda.recipe
     conda build conda.recipe
 fi
