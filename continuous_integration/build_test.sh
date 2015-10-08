e4670c8a3fe6af61166da367e474f8893c3848a3
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-07 23:17:39 -0500

    Testing: Use the PR diff instead of its patch to build the conda package in Travis

--- continuous_integration/build_test.sh
+++ continuous_integration/build_test.sh
@@ -4,13 +4,13 @@ set -ex
 
 if [ "$USE_CONDA" = true ] ; then
     # Downloading PR patch
-    wget --no-check-certificate https://github.com/spyder-ide/spyder/pull/$TRAVIS_PULL_REQUEST.patch
-    mv $TRAVIS_PULL_REQUEST.patch conda.recipe
+    wget --no-check-certificate https://github.com/spyder-ide/spyder/pull/$TRAVIS_PULL_REQUEST.diff
+    mv $TRAVIS_PULL_REQUEST.diff conda.recipe
 
     # Making a level 0 patch so it can be applied by conda-build
     cd conda.recipe
-    sed -i s/'--- a\/'/'--- '/g $TRAVIS_PULL_REQUEST.patch
-    sed -i s/'+++ b\/'/'+++ '/g $TRAVIS_PULL_REQUEST.patch
+    sed -i s/'--- a\/'/'--- '/g $TRAVIS_PULL_REQUEST.diff
+    sed -i s/'+++ b\/'/'+++ '/g $TRAVIS_PULL_REQUEST.diff
     cd ..
 
     # Building the recipe
