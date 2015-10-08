443fafe990e2f6768ffa9a0c2700a69b1d5eb953
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-07 23:03:56 -0500

    Testing: Make PR patch a level 0 one so it can be applied by conda-build

--- continuous_integration/build_test.sh
+++ continuous_integration/build_test.sh
@@ -3,7 +3,16 @@
 set -ex
 
 if [ "$USE_CONDA" = true ] ; then
+    # Downloading PR patch
     wget --no-check-certificate https://github.com/spyder-ide/spyder/pull/$TRAVIS_PULL_REQUEST.patch
     mv $TRAVIS_PULL_REQUEST.patch conda.recipe
+
+    # Making a level 0 patch so it can be applied by conda-build
+    cd conda.recipe
+    sed -i s/'--- a\/'/'--- '/g $TRAVIS_PULL_REQUEST.patch
+    sed -i s/'+++ b\/'/'+++ '/g $TRAVIS_PULL_REQUEST.patch
+    cd ..
+
+    # Building the recipe
     conda build conda.recipe
 fi
