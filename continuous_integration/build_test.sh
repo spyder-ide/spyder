00e0d4bc468b63ca8816d33e42767342fbe70f92
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-07 23:52:00 -0500

    Testing: Move conda.recipe to continuous_integration dir
    The recipe only works in Travis now

--- continuous_integration/build_test.sh
+++ continuous_integration/build_test.sh
@@ -5,10 +5,10 @@ set -ex
 if [ "$USE_CONDA" = true ] ; then
     # Downloading PR patch
     wget --no-check-certificate https://github.com/spyder-ide/spyder/pull/$TRAVIS_PULL_REQUEST.diff
-    mv $TRAVIS_PULL_REQUEST.diff conda.recipe
+    mv $TRAVIS_PULL_REQUEST.diff continuous_integration/conda.recipe
 
     # Making a level 0 patch so it can be applied by conda-build
-    cd conda.recipe
+    cd continuous_integration/conda.recipe
     sed -i s/'--- a\/'/'--- '/g $TRAVIS_PULL_REQUEST.diff
     sed -i s/'+++ b\/'/'+++ '/g $TRAVIS_PULL_REQUEST.diff
     cd ..
