1136a1e08e74abb65d77dbea452479c02aba454f
David Woods <n/a>
2015-10-08 21:01:32 +0100

    Expand selection intellegently in arrayeditor when more data is loaded
    If the selection includes complete rows or columns, these are
    expanded to reach the new ends

--- spyderlib/widgets/varexp/arrayeditor.py
+++ spyderlib/widgets/varexp/arrayeditor.py
@@ -21,7 +21,8 @@ from spyderlib.qt.QtGui import (QHBoxLayout, QColor, QTableView, QItemDelegate,
                                 QMessageBox, QPushButton, QInputDialog, QMenu,
                                 QApplication, QKeySequence, QLabel, QComboBox,
                                 QSpinBox, QStackedWidget, QWidget, QVBoxLayout,
-                                QAbstractItemDelegate)
+                                QAbstractItemDelegate, QItemSelection,
+                                QItemSelectionRange)
 from spyderlib.qt.QtCore import (Qt, QModelIndex, QAbstractTableModel, Slot)
 
 from spyderlib.qt.compat import to_qvariant, from_qvariant
@@ -403,10 +404,39 @@ class ArrayView(QTableView):
                                lambda val: self.load_more_data(val, rows=True))
     
     def load_more_data(self, value, rows=False, columns=False):
+        old_selection = self.selectionModel().selection()
+        old_rows_loaded = old_cols_loaded = None
+        
         if rows and value == self.verticalScrollBar().maximum():
+            old_rows_loaded = self.model().rows_loaded
             self.model().fetch_more(rows=rows)
+            
         if columns and value == self.horizontalScrollBar().maximum():
+            old_cols_loaded = self.model().cols_loaded
             self.model().fetch_more(columns=columns)
+            
+        if old_rows_loaded is not None or old_cols_loaded is not None:
+            # if we've changed anything, update selection
+            new_selection = QItemSelection()
+            for part in old_selection:
+                top = part.top()
+                bottom = part.bottom()
+                if (old_rows_loaded is not None and 
+                    top == 0 and bottom == (old_rows_loaded-1)):
+                    # complete column selected (so expand it to match updated range)
+                    bottom = self.model().rows_loaded-1
+                left = part.left()
+                right = part.right()
+                if (old_cols_loaded is not None
+                    and left == 0 and right == (old_cols_loaded-1)):
+                    # compete row selected (so expand it to match updated range)
+                    right = self.model().cols_loaded-1
+                top_left = self.model().index(top, left)
+                bottom_right = self.model().index(bottom, right)
+                part = QItemSelectionRange(top_left, bottom_right)
+                new_selection.append(part)
+            self.selectionModel().select(new_selection, self.selectionModel().ClearAndSelect)
+                    
 
     def resize_to_contents(self):
         """Resize cells to contents"""
