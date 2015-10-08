e11cab8261f664df94f96e79cfbb1b5afff015c3
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-07 23:41:16 -0500

    Testing: Add some comments to run_test.sh

--- continuous_integration/run_test.sh
+++ continuous_integration/run_test.sh
@@ -3,8 +3,11 @@
 set -ex
 
 if [ "$USE_CONDA" = true ] ; then
+    # Move to a tmp dir
     mkdir ~/tmp
     cd ~/tmp
+
+    # Install and run the package
     conda install ~/miniconda/conda-bld/linux-64/spyder-*.tar.bz2
     spyder
 fi
