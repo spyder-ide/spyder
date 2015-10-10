5b3f1eecfecec690f5d210f5a1f70bd7e8c6b88c
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-10 14:27:09 -0500

    Testing: Add files at depth 3 from the root dir to our tests

--- continuous_integration/modules_test.sh
+++ continuous_integration/modules_test.sh
@@ -5,6 +5,7 @@ set -ex
 # Tell Spyder we're testing our widgets in Travis
 export TEST_TRAVIS_WIDGETS=True
 
+# Depth 1
 for f in spyderlib/*.py; do
     if [[ $f == spyderlib/restart_app.py ]]; then
         continue
@@ -28,6 +29,7 @@ for f in spyderlib/*.py; do
 done
 
 
+# Depth 2
 for f in spyderlib/*/*.py; do
     if [[ $f == spyderlib/plugins/*.py ]]; then
         continue
@@ -41,7 +43,33 @@ for f in spyderlib/*/*.py; do
     if [[ $f == spyderlib/utils/windows.py ]]; then
         continue
     fi
+    # TODO: Understand why formlayout is failing in Travis!!
     if [[ $f == spyderlib/widgets/formlayout.py ]]; then
+        continue
+    fi
+    python "$f"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+done
+
+
+# Depth 3
+for f in spyderlib/*/*/*.py; do
+    if [[ $f == spyderlib/external/*/*.py ]]; then
+        continue
+    fi
+    if [[ $f == spyderlib//utils/external/*.py ]]; then
+        continue
+    fi
+    if [[ $f == spyderlib/utils/inspector/*.py ]]; then
+        continue
+    fi
+    if [[ $f == spyderlib/utils/introspection/__init__.py ]]; then
+        continue
+    fi
+    # TODO: fallback_plugin tests are failing!!
+    if [[ $f == spyderlib/utils/introspection/fallback_plugin.py ]]; then
         continue
     fi
     if [[ $f == spyderlib/widgets/externalshell/inputhooks.py ]]; then
