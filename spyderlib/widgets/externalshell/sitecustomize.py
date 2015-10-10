8201cfdd02c0491eecf10a60d13cf33bd476bb2a
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-10 11:15:52 -0500

    Add missing copyright

--- spyderlib/widgets/externalshell/sitecustomize.py
+++ spyderlib/widgets/externalshell/sitecustomize.py
@@ -1,4 +1,8 @@
 #
+# Copyright (c) 2009- The Spyder Development Team)
+# Licensed under the terms of the MIT License)
+# (see spyderlib/__init__.py for details)
+#
 # IMPORTANT NOTE: Don't add a coding line here! It's not necessary for
 # site files
 #
