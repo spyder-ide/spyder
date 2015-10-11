3703a392ce896c4054e38109ca9cbe1419288f93
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-11 12:55:49 -0500

    Testing: Test widgets present in spyderplugins

--- continuous_integration/modules_test.sh
+++ continuous_integration/modules_test.sh
@@ -86,3 +86,12 @@ for f in spyderlib/*/*/*.py; do
         exit 1
     fi
 done
+
+
+# Spyderplugins
+for f in spyderplugins/widgets/*.py; do
+    python "$f"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+done
