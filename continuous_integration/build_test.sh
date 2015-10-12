352da2b7fe3debe073b89c4315ecdc519940ab21
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-12 15:56:22 -0500

    Testing: Report Python, pip and conda versions in build_test

--- continuous_integration/build_test.sh
+++ continuous_integration/build_test.sh
@@ -5,6 +5,12 @@ set -ex
 if [ "$USE_CONDA" = true ] ; then
     cd continuous_integration
 
+    echo "----------- TESTING VERSIONS -----------"
+    python --version
+    pip --version
+    conda --version
+    echo "----------------------------------------"
+
     # Use --python only for 3.5 to avoid building for old Pythons
     # on the other versions
     if [ "$TRAVIS_PYTHON_VERSION" = "3.5" ]; then
