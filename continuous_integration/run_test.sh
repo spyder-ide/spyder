678edc1dc1a35bca65b9ee82335d1f34fde3fbb4
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-17 17:53:05 -0500

    Testing: Don't install Spyder wheel with -U switch to avoid upgrading pyzmq

--- continuous_integration/run_test.sh
+++ continuous_integration/run_test.sh
@@ -5,16 +5,19 @@ set -ex
 # Tell Spyder we're testing the app in Travis
 export TEST_TRAVIS_APP=True
 
+
 # Extra packages to install besides Spyder regular dependencies
 # We install them here and not in travis_install.sh to see if
 # Spyder is correctly pulling its deps (some of them are shared
 # with mpl)
 export EXTRA_PACKAGES="pandas sympy pillow"
 
+
 # Don't install mpl for PyQt5 because it pulls PyQt4
 if [ "$USE_QT_API" = "PyQt4" ]; then
     EXTRA_PACKAGES+=" matplotlib"
 fi
+
 
 # Install our builds of Spyder
 if [ "$USE_CONDA" = true ] ; then
@@ -33,8 +36,9 @@ if [ "$USE_CONDA" = true ] ; then
         pip install jedi==0.8.1
     fi
 else
-    pip install -U dist/spyder-*.whl
+    pip install dist/spyder-*.whl
 fi
+
 
 # Testing that the app starts and runs
 spyder
