f53524c2a1a103216942eaa2d60656c6cab95ab3
Carlos Cordoba <ccordoba12@gmail.com>
2015-10-21 21:41:02 -0500

    Testing: Add a run_test.bat script for Appveyor

---
+++ continuous_integration/appveyor/run_test.bat
@@ -0,0 +1,23 @@
+REM Tell Spyder we're testing the app
+set TEST_TRAVIS_APP=True
+
+REM Set extra packages
+set EXTRA_PACKAGES=pandas sympy pillow
+
+if %USE_QT_API%==PyQt4 (
+    set EXTRA_PACKAGES=%EXTRA_PACKAGES% matplotlib
+)
+
+REM Move to a tmp dir before doing the installation
+mkdir C:\projects\tmp
+cd C:\projects\tmp
+
+REM Install the package we created
+conda install -q -y --use-local spyder==3.0.0b1
+
+REM Install extra packages
+conda install -q -y %EXTRA_PACKAGES%
+
+REM Test that the app starts correctly
+spyder
+if errorlevel 1 exit 1
