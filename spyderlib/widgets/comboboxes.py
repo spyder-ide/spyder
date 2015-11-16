6f40b9d88151fe0a2cc58846f021cdb810c1572f
Carlos Cordoba <ccordoba12@gmail.com>
2015-11-16 16:57:20 -0500

    Fix copyright in widgets/comboboxes

--- spyderlib/widgets/comboboxes.py
+++ spyderlib/widgets/comboboxes.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2009-2010 Pierre Raybaut
-# Copyright © 2010-2015 The Spyder Development Team
+# Copyright © 2009- The Spyder Development Team
 # Licensed under the terms of the MIT License
 # (see spyderlib/__init__.py for details)
 
