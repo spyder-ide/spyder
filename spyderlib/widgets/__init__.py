1449b1f708bd01c3fe831b01f1c9e7f5c62831d4
Carlos Cordoba <ccordoba12@gmail.com>
2015-11-22 19:33:40 -0500

    Remove imports from widgets/__init__ because it was making Spyder crash
    - Crashes happened when pandas or numpy were not installed because
    dataframeeditor and arrayeditor import them directly.
    - It seems we can't improve further this situation, but at least API
    changes are documented.

--- spyderlib/widgets/__init__.py
+++ spyderlib/widgets/__init__.py
@@ -13,12 +13,3 @@ Widgets defined in this module may be used in any other Qt-based application
 They are also used in Spyder through the Plugin interface
 (see spyderlib.plugins)
 """
-
-from spyderlib.widgets.variableexplorer import arrayeditor
-from spyderlib.widgets.variableexplorer import collectionseditor
-from spyderlib.widgets.variableexplorer import dataframeeditor
-from spyderlib.widgets.variableexplorer import objecteditor
-from spyderlib.widgets.variableexplorer import texteditor
-
-# For compatibility with Spyder 2
-dicteditor = collectionseditor
