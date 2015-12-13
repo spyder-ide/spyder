c18413b2dfae0c8a54093fd11dcfa3d7633f9954
Carlos Cordoba <ccordoba12@gmail.com>
2015-12-13 12:57:33 -0500

    Update version

--- spyderlib/__init__.py
+++ spyderlib/__init__.py
@@ -28,7 +28,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = '3.0.0b2'
+__version__ = '3.0.0dev'
 __license__ = __doc__
 __project_url__ = 'https://github.com/spyder-ide/spyder'
 __forum_url__   = 'http://groups.google.com/group/spyderlib'
