7542cc1e7b9d089b5d33d479cc8f357ac823bf4c
Pierre Raybaut <pierre.raybaut@gmail.com>
2016-01-04 09:09:51 +0100

    Major update of CONF_VERSION (up to 24.0.0)
    Some options have been renamed: this requires a major update in configuration version.

--- spyderlib/config/main.py
+++ spyderlib/config/main.py
@@ -743,9 +743,10 @@ DEFAULTS = [
 # 1. If you want to *change* the default value of a current option, you need to
 #    do a MINOR update in config version, e.g. from 3.0.0 to 3.1.0
 # 2. If you want to *remove* options that are no longer needed in our codebase,
-#    you need to do a MAJOR update in version, e.g. from 3.0.0 to 4.0.0
+#    or if you want to *rename* options, then you need to do a MAJOR update in 
+#    version, e.g. from 3.0.0 to 4.0.0
 # 3. You don't need to touch this value if you're just adding a new option
-CONF_VERSION = '23.0.0'
+CONF_VERSION = '24.0.0'
 
 # XXX: Previously we had load=(not DEV) here but DEV was set to *False*.
 # Check if it *really* needs to be updated or not
