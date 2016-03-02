5724dbd32fc79e5ba2271558f5dfc9dd4e2a9f13
Pieter Eendebak <pieter.eendebak@gmail.com>
2016-03-02 17:50:07 +0100

    rename variable c

--- spyderlib/utils/encoding.py
+++ spyderlib/utils/encoding.py
@@ -106,10 +106,10 @@ def get_coding(text):
     for line in text.splitlines()[:2]:
         result = CODING_RE.search(to_text_string(line))
         if result:
-            c = result.group(1)
+            codec = result.group(1)
             # sometimes we find a false encoding that can result in errors
-            if c in CODECS:
-                return c
+            if codec in CODECS:
+                return codec
             else:
                 return None
     return None
