0418cf5945efaa0894f773b453fe9f8964676432
Steven Silvester <steven.silvester@ieee.org>
2016-03-07 08:47:00 -0600

    Make generic async server/client using zmq

--- spyderlib/utils/introspection/plugin_server.py
+++ spyderlib/utils/introspection/plugin_server.py
@@ -4,101 +4,76 @@
 # Licensed under the terms of the MIT License
 # (see spyderlib/__init__.py for details)
 
-import threading
-import socket
-import errno
-import os
 import sys
-import time
-import atexit
+import traceback
 
-# Local imports
-from spyderlib.utils.introspection.utils import connect_to_port
-from spyderlib.py3compat import Queue
-from spyderlib.utils.bsdsocket import read_packet, write_packet
+import zmq
 
 
-# Timeout in seconds
-TIMEOUT = 60
+# Timeout in milliseconds
+TIMEOUT = 10000
 
 
-class PluginServer(object):
+class AsyncServer(object):
+
+    """
+    Introspection server, provides a separate process
+    for interacting with an object.
+    """
+
+    def __init__(self, port, *args):
+        self.port = port
+        self.object = self.initialize(*args)
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PAIR)
+        self.socket.connect("tcp://localhost:%s" % port)
+        self.socket.send_pyobj(port)
+
+    def initialize(self, plugin_name):
+        """Initialize the object and return it.
+        """
+        return object()
+
+    def run(self):
+        """Handle requests from the client.
+        """
+        while 1:
+            events = self.socket.poll(TIMEOUT)
+            if events == 0:
+                print('Timed out')
+                return
+            request = self.socket.recv_pyobj()
+            if request['func_name'] == 'server_quit':
+                print('Quitting')
+                sys.stdout.flush()
+                return
+            try:
+                func = getattr(self.object, request['func_name'])
+                args = request.get('args', [])
+                kwargs = request.get('kwargs', {})
+                request['result'] = func(*args, **kwargs)
+            except Exception:
+                request['error'] = traceback.format_exc()
+            self.socket.send_pyobj(request)
+
+
+class PluginServer(AsyncServer):
 
     """
     Introspection plugin server, provides a separate process
-    for interacting with the plugin.
+    for interacting with a plugin.
     """
 
-    def __init__(self, client_port, plugin_name):
+    def initialize(self, plugin_name):
+        """Initialize the object and return it.
+        """
         mod_name = plugin_name + '_plugin'
         mod = __import__('spyderlib.utils.introspection.' + mod_name,
                          fromlist=[mod_name])
         cls = getattr(mod, '%sPlugin' % plugin_name.capitalize())
         plugin = cls()
         plugin.load_plugin()
-        self.tlast = time.time()
-        self.plugin = plugin
-
-        self._client_port = int(client_port)
-        sock, self.server_port = connect_to_port()
-        sock.listen(2)
-        atexit.register(sock.close)
-        self._server_sock = sock
-
-        self.queue = Queue.Queue()
-        self._listener = threading.Thread(target=self.listen)
-        self._listener.setDaemon(True)
-        self._listener.start()
-
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(("127.0.0.1", self._client_port))
-        write_packet(sock, self.server_port)
-        sock.close()
-
-    def listen(self):
-        """Listen for requests"""
-        while True:
-            try:
-                conn, _addr = self._server_sock.accept()
-            except socket.error as e:
-                badfd = errno.WSAEBADF if os.name == 'nt' else errno.EBADF
-                extra = errno.WSAENOTSOCK if os.name == 'nt' else badfd
-                if e.args[0] in [errno.ECONNABORTED, badfd, extra]:
-                    return
-                # See Issue 1275 for details on why errno EINTR is
-                # silently ignored here.
-                eintr = errno.WSAEINTR if os.name == 'nt' else errno.EINTR
-                if e.args[0] == eintr:
-                    continue
-                raise
-            self.queue.put(read_packet(conn))
-
-    def run(self):
-        """Handle requests"""
-        while 1:
-            # Get most recent request
-            request = None
-            while 1:
-                try:
-                    request = self.queue.get(True, 0.01)
-                except Queue.Empty:
-                    break
-            if request is None:
-                if time.time() - self.tlast > TIMEOUT:
-                    sys.exit('Program timed out')
-                continue
-            self.tlast = time.time()
-            try:
-                method = getattr(self.plugin, request['method'])
-                args = request.get('args', [])
-                kwargs = request.get('kwargs', {})
-                request['result'] = method(*args, **kwargs)
-            except Exception as e:
-                request['error'] = str(e)
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect(("127.0.0.1", self._client_port))
-            write_packet(sock, request)
-            sock.close()
+        return plugin
 
 
 if __name__ == '__main__':
