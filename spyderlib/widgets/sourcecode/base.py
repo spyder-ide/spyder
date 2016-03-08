f423e4e4e8196947c246e885ca62e53c9a8853c2
Stephen Shank <sshank314@gmail.com>
2016-03-08 16:52:46 -0500

    Copying when nothing is selected no longer affects the clipboard.

--- spyderlib/widgets/sourcecode/base.py
+++ spyderlib/widgets/sourcecode/base.py
@@ -492,7 +492,8 @@ class TextEditBaseWidget(QPlainTextEdit, BaseEditMixin):
         Reimplement Qt method
         Copy text to clipboard with correct EOL chars
         """
-        QApplication.clipboard().setText(self.get_selected_text())
+        if len(self.get_selected_text()) > 0:
+            QApplication.clipboard().setText(self.get_selected_text())
     
     def toPlainText(self):
         """
