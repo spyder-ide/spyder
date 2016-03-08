690f6d11fee032ff3bca9ed1451041092265ae14
Carlos Cordoba <ccordoba12@gmail.com>
2016-03-08 17:00:35 -0500

    Config: Update version because of change in names option of color_schemes section

--- spyderlib/config/main.py
+++ spyderlib/config/main.py
@@ -587,7 +587,7 @@ DEFAULTS = [
 #    or if you want to *rename* options, then you need to do a MAJOR update in
 #    version, e.g. from 3.0.0 to 4.0.0
 # 3. You don't need to touch this value if you're just adding a new option
-CONF_VERSION = '26.0.0'
+CONF_VERSION = '26.1.0'
 
 
 # XXX: Previously we had load=(not DEV) here but DEV was set to *False*.
