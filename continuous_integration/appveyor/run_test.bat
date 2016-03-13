9d2e9cf1222a8a8c3963dc663b80860ad164c5be
Carlos Cordoba <ccordoba12@gmail.com>
2016-03-13 11:18:48 -0500

    Testing: Change some REM comments to use the :: syntax in batch scripts

--- continuous_integration/appveyor/run_test.bat
+++ continuous_integration/appveyor/run_test.bat
@@ -1,28 +1,28 @@
-REM Tell Spyder we're testing the app
+:: Tell Spyder we're testing the app
 set TEST_CI_APP=True
 
-REM Set extra packages
+:: Set extra packages
 set EXTRA_PACKAGES=pandas sympy pillow
 
 if %USE_QT_API%==PyQt4 (
     set EXTRA_PACKAGES=%EXTRA_PACKAGES% matplotlib
 )
 
-REM Move to a tmp dir before doing the installation
+:: Move to a tmp dir before doing the installation
 mkdir C:\projects\tmp
 cd C:\projects\tmp
 
-REM Install the package we created
+:: Install the package we created
 conda install -q -y --use-local spyder==3.0.0b2
 
-REM Install extra packages
+:: Install extra packages
 conda install -q -y %EXTRA_PACKAGES%
 
-REM Test that the app starts correctly
+:: Test that the app starts correctly
 echo ------- Testing the app ---------
 echo.
 echo %time%
-REM skipping spyder || exit 1
+:: skipping spyder || exit 1
 echo Success!
 echo %time%
 echo.
