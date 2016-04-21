b22762a64b27f932be7c5cedf9f1c49e4da5f75f
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2016-04-21 15:06:15 -0500

    Add arg to pip

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -87,7 +87,7 @@ install_pip()
         install_pyside;
     fi
 
-    pip install --no-index --trusted-host $WHEELHOUSE_URI --find-links=http://$WHEELHOUSE_URI/ $EXTRA_PACKAGES;
+    pip install --no-index --trusted-host $WHEELHOUSE_URI --allow-unverified --find-links=http://$WHEELHOUSE_URI/ $EXTRA_PACKAGES;
 }
 
 
