f601b43bf0d572035ff6259f37830391bbb977d7
Carlos Cordoba <ccordoba12@gmail.com>
2016-06-01 20:40:03 -0500

    Monkey patching sys.excepthook to prevent crashes in PyQt 5.5+

--- spyderlib/app/spyder.py
+++ spyderlib/app/spyder.py
@@ -2973,6 +2973,12 @@ def initialize():
         pass
     sys.exit = fake_sys_exit
 
+    #----Monkey patching sys.excepthook to avoid crashes in PyQt 5.5+
+    if PYQT5:
+        def spy_excepthook(type_, value, tback):
+            sys.__excepthook__(type_, value, tback) 
+        sys.excepthook = spy_excepthook
+
     # Removing arguments from sys.argv as in standard Python interpreter
     sys.argv = ['']
 
