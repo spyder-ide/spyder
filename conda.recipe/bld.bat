58156c41bccf11393595dc4b39f1db594b7f66fc
Morten Enemark Lund <melund@gmail.com>
2016-06-08 23:02:57 +0200

    Fix icon name in conda windows build script.

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -6,7 +6,7 @@ IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 
 copy %SRC_DIR%\img_src\spyder.ico %MENU_DIR%\
 if errorlevel 1 exit 1
-copy %SRC_DIR%\img_src\spyder_light.ico %MENU_DIR%\
+copy %SRC_DIR%\img_src\spyder_reset.ico %MENU_DIR%\
 if errorlevel 1 exit 1
 copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\spyder_shortcut.json
 if errorlevel 1 exit 1
