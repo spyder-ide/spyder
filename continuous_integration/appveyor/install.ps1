d8f47e5a9d4445345f00d5600d6135c9b8e4c72b
Carlos Cordoba <ccordoba12@gmail.com>
2016-06-23 15:50:34 -0500

    Testing: Pin conda-build to 1.21.0 because 1.21.1 is giving us errors

--- continuous_integration/appveyor/install.ps1
+++ continuous_integration/appveyor/install.ps1
@@ -95,7 +95,7 @@ function UpdateConda ($python_home) {
 function main () {
     InstallMiniconda $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
     UpdateConda $env:PYTHON
-    InstallCondaPackages $env:PYTHON "conda-build pytest pytest-cov pytest-qt"
+    InstallCondaPackages $env:PYTHON "conda-build==1.21.0 pytest pytest-cov pytest-qt"
 }
 
 
