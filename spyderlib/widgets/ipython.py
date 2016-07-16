b40419cfc0be0ca46d382fd6345b62243dbce0a4
Carlos Cordoba <ccordoba12@gmail.com>
2016-07-15 19:40:59 -0500

    Calltip: Don't hide it with a timer by default

--- spyderlib/widgets/ipython.py
+++ spyderlib/widgets/ipython.py
@@ -82,7 +82,7 @@ class IPythonControlWidget(TracebackLinksMixin, GetHelpMixin, QTextEdit,
         TracebackLinksMixin.__init__(self)
         GetHelpMixin.__init__(self)
 
-        self.calltip_widget = CallTipWidget(self, hide_timer_on=True)
+        self.calltip_widget = CallTipWidget(self, hide_timer_on=False)
         self.found_results = []
 
         # To not use Spyder calltips obtained through the monitor
