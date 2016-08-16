7def6b0e372918125edfb91a55acdf237062c919
Carlos Cordoba <ccordoba12@gmail.com>
2016-08-16 17:32:24 -0500

    Shortcuts: Fix the use of html delegate on it

--- spyderlib/widgets/helperwidgets.py
+++ spyderlib/widgets/helperwidgets.py
@@ -122,16 +122,21 @@ class HTMLDelegate(QStyledItemDelegate):
 
         textRect = style.subElementRect(QStyle.SE_ItemViewItemText, options)
         painter.save()
-        if style.objectName() in ['oxygen', 'qtcurve', 'breeze']:
-            if options.widget.files_list:
-                painter.translate(textRect.topLeft() + QPoint(4, -9))
+
+        # Adjustments for the file switcher
+        if hasattr(options.widget, 'files_list'):
+            if style.objectName() in ['oxygen', 'qtcurve', 'breeze']:
+                if options.widget.files_list:
+                    painter.translate(textRect.topLeft() + QPoint(4, -9))
+                else:
+                    painter.translate(textRect.topLeft())
             else:
-                painter.translate(textRect.topLeft())
+                if options.widget.files_list:
+                    painter.translate(textRect.topLeft() + QPoint(4, 4))
+                else:
+                    painter.translate(textRect.topLeft() + QPoint(2, 4))
         else:
-            if options.widget.files_list:
-                painter.translate(textRect.topLeft() + QPoint(4, 4))
-            else:
-                painter.translate(textRect.topLeft() + QPoint(2, 4))
+            painter.translate(textRect.topLeft() + QPoint(0, -3))
         doc.documentLayout().draw(painter, ctx)
         painter.restore()
 
