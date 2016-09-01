4f6cd9c9f07b9c8a9774f2186aad474d9b9a6497
Jitse Niesen <j.niesen@leeds.ac.uk>
2016-09-01 20:40:10 +0100

    Config: Avoid eval() when reading config file
    Using eval() has unintended side-effects. Instead, do nothing if we expect a string,
    and use ast.literal_eval() for other types (list, tuple, etc.)
    
    Fixes #3354.

--- spyder/config/tests/test_user.py
+++ spyder/config/tests/test_user.py
@@ -31,6 +31,11 @@ def userconfig(tmpdir, monkeypatch):
 def test_userconfig_get_string_from_inifile(userconfig):
     assert userconfig.get('section', 'option') == 'value'
 
+def test_userconfig_get_does_not_eval_functions(userconfig):
+    # regression test for issue #3354
+    userconfig.set('section', 'option', 'print("foo")')
+    assert userconfig.get('section', 'option') == 'print("foo")'
+
 def test_userconfig_set_with_string(userconfig):
     userconfig.set('section', 'option', 'new value')
     with open(userconfig.filename()) as inifile:
