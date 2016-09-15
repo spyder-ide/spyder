a11c97e27f2f2c7854a67f3913f70c9f0bbcf2a5
Carlos Cordoba <ccordoba12@gmail.com>
2016-09-15 08:16:44 -0500

    IPython Console: Don't run _post_exec_input if we're not in pdb mode

--- spyder/widgets/ipythonconsole/debugging.py
+++ spyder/widgets/ipythonconsole/debugging.py
@@ -75,14 +75,17 @@ class DebuggingWidget(RichJupyterWidget):
     # ---- Private API (defined by us) -------------------------------
     def _post_exec_input(self, line):
         """Commands to be run after writing to stdin"""
-        pdb_commands = ['next', 'continue', 'step', 'return']
-        if any([x == line for x in pdb_commands]):
-            # To open the file where the current pdb frame points to
-            self.silent_exec_input("!get_ipython().kernel.get_pdb_step()")
+        if self._reading:
+            pdb_commands = ['next', 'continue', 'step', 'return']
+            if any([x == line for x in pdb_commands]):
+                # To open the file where the current pdb frame points to
+                self.silent_exec_input("!get_ipython().kernel.get_pdb_step()")
 
-            # To refresh the Variable Explorer
-            self.silent_exec_input("!get_ipython().kernel.get_namespace_view()")
-            self.silent_exec_input("!get_ipython().kernel.get_var_properties()")
+                # To refresh the Variable Explorer
+                self.silent_exec_input(
+                    "!get_ipython().kernel.get_namespace_view()")
+                self.silent_exec_input(
+                    "!get_ipython().kernel.get_var_properties()")
 
     # ---- Private API (overrode by us) -------------------------------
     def _handle_input_request(self, msg):
