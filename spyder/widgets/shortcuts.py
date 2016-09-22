60a8f9889a756f03205877e6b44bc4c9bfb543e6
Rafael Laverde <leafar91@gmail.com>
2016-09-22 09:45:52 -0500

    Shortcuts Summary: Calculate font_size and amount of elements per column using screen size

--- spyder/widgets/shortcuts.py
+++ spyder/widgets/shortcuts.py
@@ -17,22 +17,27 @@ from itertools import groupby
 from qtpy.QtCore import Qt
 from qtpy.QtGui import QFont
 from qtpy.QtWidgets import (QDialog, QLabel, QGridLayout, QGroupBox,
-                            QVBoxLayout, QHBoxLayout)
+                            QVBoxLayout, QHBoxLayout, QDesktopWidget)
 
 # Local imports
 from spyder.config.base import _
 from spyder.config.gui import iter_shortcuts
 
 # Constants
-SHORTCUT_SHOURTCUTS_SUMMARY = "Ctrl+H"
-MAX_SHOURCUTS_COLUMN = 28
-
+MAX_FONT_SIZE = 16
+MIN_FONT_SIZE = 8
 
 class ShortCutsSummaryDialog(QDialog):
     def __init__(self, parent=None, offset=0, force_float=False):
         QDialog.__init__(self, parent=parent)
 
         self._shortcuts_summary_title = _("Spyder Keyboard ShortCuts")
+
+        # Calculate font and amount of elements in each column according screen size
+        width, height = self.get_screen_resolution()
+        font_size = height / 80
+        font_size = max(min(font_size, MAX_FONT_SIZE), MIN_FONT_SIZE)
+        shortcuts_column = (height - 8 * font_size) / (font_size +16)
 
         # Widgets
         style = """
@@ -43,14 +48,20 @@ class ShortCutsSummaryDialog(QDialog):
             }"""
         self.setStyleSheet(style)
 
-        groupbox_style = "QGroupBox { font-weight: bold;}"
+        font_names = QFont()
+        font_names.setPointSize(font_size)
+        font_names.setBold(True)
 
-        font_titles = QFont()
-        font_titles.setBold(True)
+        font_keystr = QFont()
+        font_keystr.setPointSize(font_size)
+
+        font_title = QFont()
+        font_title.setPointSize(font_size+2)
+        font_title.setBold(True)
 
         title_label = QLabel(self._shortcuts_summary_title)
         title_label.setAlignment(Qt.AlignCenter)
-        title_label.setFont(font_titles)
+        title_label.setFont(font_title)
 
         # iter over shortcuts and create GroupBox for each context
         # with shortcuts in a grid
@@ -75,22 +86,23 @@ class ShortCutsSummaryDialog(QDialog):
                     if context == '_': context = 'Global'
 
                     group = QGroupBox(context.capitalize())
-                    group.setStyleSheet(groupbox_style)
+                    group.setFont(font_names)
 
                     group_layout = QGridLayout()
                     group.setLayout(group_layout)
 
                 # Widgets
                 label_name = QLabel(name.capitalize().replace('_', ' '))
-                label_name.setFont(font_titles)
+                label_name.setFont(font_names)
                 label_keystr = QLabel(keystr)
+                label_keystr.setFont(font_keystr)
 
                 group_layout.addWidget(label_name, i, 0)
                 group_layout.addWidget(label_keystr, i, 1)
 
                 added_shortcuts += 1
 
-                if added_shortcuts >= MAX_SHOURCUTS_COLUMN:
+                if added_shortcuts >= shortcuts_column:
                     column_layout.addWidget(group)
                     columns_layout.addLayout(column_layout)
                     added_shortcuts = 0
@@ -108,6 +120,12 @@ class ShortCutsSummaryDialog(QDialog):
         self._layout.addWidget(title_label)
         self._layout.addLayout(columns_layout)
         self.setLayout(self._layout)
+
+    def get_screen_resolution(self):
+        """Return the screen resolution of the primary screen."""
+        widget = QDesktopWidget()
+        geometry = widget.availableGeometry(widget.primaryScreen())
+        return geometry.width(), geometry.height()
 
 
 def test():  # pragma: no cover
