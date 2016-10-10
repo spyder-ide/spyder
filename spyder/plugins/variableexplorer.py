01654a8f1e1903ad8bcc474e1015387c5de7b432
Jitse Niesen <j.niesen@leeds.ac.uk>
2016-10-10 21:28:01 +0100

    Variable explorer: Strip leading % from dataframe format before saving
    - In VariableExplorer.add_shellwidget(), connect sig_option_changed from the
      newly created NamespaceBrowser to a new .change_option() member function.
    - Add VariableExplorer.change_option() to check whether the option being changed
      is the dataframe format, in which case the leading '%' character from the format
      is stripped. Then sig_option_changed is emitted from the plugin, which leads
      to the new config setting being saved.

--- spyder/plugins/variableexplorer.py
+++ spyder/plugins/variableexplorer.py
@@ -7,7 +7,7 @@
 """Variable Explorer Plugin"""
 
 # Third party imports
-from qtpy.QtCore import Signal
+from qtpy.QtCore import Signal, Slot
 from qtpy.QtWidgets import QGroupBox, QStackedWidget, QVBoxLayout, QWidget
 
 # Local imports
@@ -123,6 +123,22 @@ class VariableExplorer(QWidget, SpyderPluginMixin):
         settings[name] = '%' + CONF.get(VariableExplorer.CONF_SECTION, name)
         return settings
 
+    @Slot(str, object)
+    def change_option(self, option_name, new_value):
+        """
+        Change a config option
+
+        This function is called if sig_option_changed is received. If the
+        option changed is the dataframe format, then the leading '%' character
+        is stripped (because it can't be stored in the user config). Then,
+        the signal is emitted again, so that the new value is saved in the
+        user config.
+        """
+        if option_name == 'dataframe_format':
+            assert new_value.startswith('%')
+            new_value = new_value[1:]
+        self.sig_option_changed.emit(option_name, new_value)
+
     # ----- Stack accesors ----------------------------------------------------
     def set_current_widget(self, nsb):
         self.stack.setCurrentWidget(nsb)
@@ -157,7 +173,7 @@ class VariableExplorer(QWidget, SpyderPluginMixin):
             nsb = NamespaceBrowser(self)
             nsb.set_shellwidget(shellwidget)
             nsb.setup(**VariableExplorer.get_settings())
-            nsb.sig_option_changed.connect(self.sig_option_changed.emit)
+            nsb.sig_option_changed.connect(self.change_option)
             self.add_widget(nsb)
             self.shellwidgets[shellwidget_id] = nsb
             self.set_shellwidget_from_id(shellwidget_id)
