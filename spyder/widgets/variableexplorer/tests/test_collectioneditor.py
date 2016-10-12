4c1d59bd93e1c76b8ff0eb1e85446c002e7d34e0
Jitse Niesen <j.niesen@leeds.ac.uk>
2016-10-12 14:58:46 +0100

    Variable explorer: Accept sig_option_changed in collection editor
    - When creating a DataFrameEditor in CollectionsDelegate.createEditor(),
      connect its sig_option_changed signal to the new .change_option() function.
    - Add CollectionsDelegate.change_option(), to call
      BaseTableView.set_dataframe_format() on the table view of the collection editor.
    - Add a test.

--- spyder/widgets/variableexplorer/tests/test_collectioneditor.py
+++ spyder/widgets/variableexplorer/tests/test_collectioneditor.py
@@ -35,6 +35,16 @@ def test_create_dataframeeditor_with_correct_format(qtbot, monkeypatch):
     editor.delegate.createEditor(None, None, editor.model.createIndex(0, 3))
     mockDataFrameEditor_instance.dataModel.set_format.assert_called_once_with('%10d')
 
+def test_accept_sig_option_changed_from_dataframeeditor(qtbot, monkeypatch):
+    df = pandas.DataFrame(['foo', 'bar'])
+    editor = CollectionsEditorTableView(None, {'df': df})
+    editor.set_dataframe_format('%10d')
+    assert editor.model.dataframe_format == '%10d'
+    editor.delegate.createEditor(None, None, editor.model.createIndex(0, 3))
+    dataframe_editor = next(iter(editor.delegate._editors.values()))['editor']
+    dataframe_editor.sig_option_changed.emit('dataframe_format', '%5f')
+    assert editor.model.dataframe_format == '%5f'
+
 
 if __name__ == "__main__":
     pytest.main()
