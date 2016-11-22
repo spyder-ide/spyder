e5ac3d76738c26365ebb64dcfee7dd0da89fae8b
Wallas Linux Deepin  15.03 Machine <malliwi88@hotmail.com>
2016-11-22 17:38:07 -0500

    Update python example test

--- spyder/utils/tests/data/example.py
+++ spyder/utils/tests/data/example.py
@@ -7,7 +7,14 @@
 
     *******************************************************************
 """
-# ahora
-hola-yo
-ahora-yo
-ahora
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) / 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+quicksort([3, 6, 8, 10, 1, 2, 1])
