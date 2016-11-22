a9924010970834d1bd3903f69fef3e78c7661ad2
Wallas Linux Deepin  15.03 Machine <malliwi88@hotmail.com>
2016-11-22 17:49:36 -0500

    add test for python files

--- spyder/utils/tests/test_get_words.py
+++ spyder/utils/tests/test_get_words.py
@@ -19,3 +19,21 @@ def test_get_words_html():
             'here', 'title', 'head', 'charset', 'Jamie',
             'World', 'lang', 'html', 'DOCTYPE', 'utf']
     assert sorted(words) == sorted(real)
+
+def test_get_words_python():
+    """Test for get word from html file syntax"""
+
+    path = dirname(dirname(abspath(__file__)))
+    f_in = path+"/tests/data/example.py"
+    words = get_words_file(f_in)
+    print(words)
+    real = ['a', 'sure', 'np', 'Z_', 'Simple', 'R', 'utn', 'values',
+            'is', 'N_', 'zeros', 'i', 'rules', 'jmg', 'Manuel', 'new',
+            'numpy', 'neighbours', 'shape', 'survive', 'int', 'gmail',
+            'home', 'birth', 'range', 'Z', '-', 'author', 'implemented',
+            'array', 'null', 'return', 'Qt', 'import', 'and', 'stay',
+            'VerySimpleWebBrowser', 'N', 'com', 'Juan', 'borders', 'Make',
+            'iterate_', 'as', 'ravel', 'in', 'Browser', 'def', 'over', 'Set',
+            'print', 'This', 'argwhere', 'for', 'Very', 'Web', 'Garcia',
+            'Apply', 'QtWebKit', 'Count']
+    assert sorted(words) == sorted(real)
