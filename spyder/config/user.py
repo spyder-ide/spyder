7ee59b9119f212a7d115a3c058dd93696fd613f3
Rafael Laverde <leafar91@gmail.com>
2016-12-06 14:22:05 -0500

    Change UserConfig get method to avoid to convert str to tuples or list

--- spyder/config/user.py
+++ spyder/config/user.py
@@ -386,12 +386,13 @@ class UserConfig(DefaultsConfig):
             value = float(value)
         elif isinstance(default_value, int):
             value = int(value)
-        else:
-            if PY2 and is_text_string(default_value):
+        elif is_text_string(default_value):
+            if PY2:
                 try:
                     value = value.decode('utf-8')
                 except (UnicodeEncodeError, UnicodeDecodeError):
                     pass
+        else:
             try:
                 # lists, tuples, ...
                 value = ast.literal_eval(value)
