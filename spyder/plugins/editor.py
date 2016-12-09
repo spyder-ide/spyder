ba33b486dcc83d5fb383710b1aafcfd8cbd3183b
dalthviz <d.althviz10@uniandes.edu.co>
2016-12-08 20:31:22 -0500

    Fixed failures of code style.

--- spyder/plugins/editor.py
+++ spyder/plugins/editor.py
@@ -1352,30 +1352,26 @@ class Editor(SpyderPluginWidget):
     def setup_other_windows(self):
         """Setup toolbars and menus for 'New window' instances"""
        
-        self.toolbar_list = (
-                             (_("File toolbar"), "file_toolbar", 
-                                self.main.file_toolbar_actions),
+        self.toolbar_list = ((_("File toolbar"), "file_toolbar", 
+                             self.main.file_toolbar_actions),
                              (_("Search toolbar"), "search_toolbar", 
-                                self.main.search_menu_actions),
+                             self.main.search_menu_actions),
                              (_("Source toolbar"), "source_toolbar", 
-                                self.main.source_toolbar_actions),
+                             self.main.source_toolbar_actions),
                              (_("Run toolbar"), "run_toolbar", 
-                                self.main.run_toolbar_actions),
+                             self.main.run_toolbar_actions),
                              (_("Debug toolbar"), "debug_toolbar",
-                                self.main.debug_toolbar_actions),
+                             self.main.debug_toolbar_actions),
                              (_("Edit toolbar"), "edit_toolbar", 
-                                self.main.edit_toolbar_actions)
-                            )
-        self.menu_list = (
-                          (_("&File"), self.main.file_menu_actions),
+                             self.main.edit_toolbar_actions))
+        self.menu_list = ((_("&File"), self.main.file_menu_actions),
                           (_("&Edit"), self.main.edit_menu_actions),
                           (_("&Search"), self.main.search_menu_actions),
                           (_("Sour&ce"), self.main.source_menu_actions),
                           (_("&Run"), self.main.run_menu_actions),
                           (_("&Tools"), self.main.tools_menu_actions),
                           (_("&View"), []),
-                          (_("&Help"), self.main.help_menu_actions),
-                         )
+                          (_("&Help"), self.main.help_menu_actions))
         # Create pending new windows:
         for layout_settings in self.editorwindows_to_be_created:
             win = self.create_new_window()
