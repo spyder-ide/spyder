dc0e2738dd994d459311bbdc2197a1526a536573
dalthviz <d.althviz10@uniandes.edu.co>
2016-12-09 00:26:17 -0500

    Fixed encoding issue when changing cwd.

--- spyder/widgets/ipythonconsole/shell.py
+++ spyder/widgets/ipythonconsole/shell.py
@@ -83,7 +83,7 @@ class ShellWidget(NamepaceBrowserWidget, HelpWidget, DebuggingWidget):
     def set_cwd(self, dirname):
         """Set shell current working directory."""
         return self.silent_execute(
-                         "get_ipython().kernel.set_cwd(r'{}')".format(dirname))
+                         u"get_ipython().kernel.set_cwd(r'{}')".format(dirname))
 
     # --- To handle the banner
     def long_banner(self):
