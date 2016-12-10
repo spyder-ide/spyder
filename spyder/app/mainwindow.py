1665e1001a7c70fbcb67fcdc1bd4fa68b2bdd592
Patrick Woo-Sam <pwoosam5@icloud.com>
2016-12-10 07:15:53 -0800

    fixes issue #3818
    Removed shortcut for opening spyder documentation from
    /app/mainwindow.py
    Registered shortcut for opening spyder documentation in
    /app/mainwindow.py
    Created default configuration for opening spyder documentaion in
    /config/main.py

--- spyder/app/mainwindow.py
+++ spyder/app/mainwindow.py
@@ -915,9 +915,11 @@ class MainWindow(QMainWindow):
             spyder_doc = 'http://pythonhosted.org/spyder'
         else:
             spyder_doc = file_uri(spyder_doc)
-        doc_action = create_action( self, _("Spyder documentation"), shortcut="F1", 
+        doc_action = create_action( self, _("Spyder documentation"),
                                     icon=ima.icon('DialogHelpButton'),
                                     triggered=lambda : programs.start_file(spyder_doc))
+        self.register_shortcut(doc_action, "_",
+                               "spyder documentation")
 
         if self.help is not None:
             tut_action = create_action(self, _("Spyder tutorial"),
