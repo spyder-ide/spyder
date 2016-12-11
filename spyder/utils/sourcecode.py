7cbbdd1a6ce2edcb56a7ba58fd61eff8986b9a52
dalthviz <d.althviz10@uniandes.edu.co>
2016-12-10 20:29:48 -0500

    Added case for comparison between file in root directory and one in a subdirectory.

--- spyder/utils/sourcecode.py
+++ spyder/utils/sourcecode.py
@@ -153,17 +153,22 @@ def differentiate_prefix(path_components0, path_components1):
     """
     longest_prefix = []
     root_comparison = False
+    common_elmt = None
     for index, (elmt0, elmt1) in enumerate(zip(path_components0, path_components1)):
         if elmt0 != elmt1:
             if index == 2:
                 root_comparison = True
             break
+        else:
+            common_elmt = elmt0
         longest_prefix.append(elmt0)
     file_name_length = len(path_components0[len(path_components0) - 1])
     path_0 = os.path.join(*path_components0)[:-file_name_length - 1]
     if len(longest_prefix) > 0:
         longest_path_prefix = os.path.join(*longest_prefix)
         longest_prefix_length = len(longest_path_prefix) + 1
-        if(path_0[longest_prefix_length:] != "" and not root_comparison):
+        if path_0[longest_prefix_length:] != "" and not root_comparison:
             path_0 = path_0[longest_prefix_length:]
+        elif not root_comparison:
+            path_0 = common_elmt
     return path_0
