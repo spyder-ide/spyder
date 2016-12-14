71ec0aa1c0323ad77e7f0dd7a53e97518a4e75cb
dalthviz <d.althviz10@uniandes.edu.co>
2016-12-14 02:37:14 -0500

    Fixed code style problem.

--- spyder/widgets/editor.py
+++ spyder/widgets/editor.py
@@ -1260,9 +1260,11 @@ class EditorStack(QWidget):
     #------ Hor/Ver splitting
     def __get_split_actions(self):
         # Undock Editor window
-        self.undock_action = create_action(self, _("Undock Editor window"),
-                icon=ima.icon('newwindow'), tip=_("Undock the editor window"),
-                triggered=lambda: self.undock_window.emit())
+        self.undock_action = create_action(self,_("Undock Editor window"),
+                                           icon=ima.icon('newwindow'),
+                                           tip=_("Undock the editor window"),
+                                           triggered=lambda:
+                                               self.undock_window.emit())
         # Splitting
         self.versplit_action = create_action(self, _("Split vertically"),
                 icon=ima.icon('versplit'),
