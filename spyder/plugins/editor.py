adfa08429c2061c320f4d15306314f04db6eca17
mariacamilaremolinagutierrez <mariacamilaremolinagutierrez@gmail.com>
2016-12-20 22:18:08 -0500

    redicing repeated code

--- spyder/plugins/editor.py
+++ spyder/plugins/editor.py
@@ -1845,68 +1845,39 @@ class Editor(SpyderPluginWidget):
         for index, filename in enumerate(filenames):
             # -- Do not open an already opened file
             if index == 0:  # this is the last file focused in previous session
-                current_editor = self.set_current_filename(filename,
-                                                           editorwindow,
-                                                           focus=True)
-                if current_editor is None:
-                    # -- Not a valid filename:
-                    if not osp.isfile(filename):
-                        continue
-                    # --
-                    current_es = self.get_current_editorstack(editorwindow)
-
-                    # Creating the editor widget in the first editorstack
-                    # (the one that can't be destroyed), then cloning this
-                    # editor widget in all other editorstacks:
-                    finfo = self.editorstacks[0].load(filename,
-                                                      set_current=False)
-                    finfo.path = self.main.get_spyder_pythonpath()
-                    self._clone_file_everywhere(finfo)
-                    current_editor = current_es.set_current_filename(filename)
-                    current_editor.set_breakpoints(load_breakpoints(filename))
-                    self.register_widget_shortcuts(current_editor)
-                    current_es.analyze_script()
-                    self.__add_recent_file(filename)
-                if goto is not None:  # 'word' is assumed to be None as well
-                    current_editor.go_to_line(goto[index], word=word)
-                    position = current_editor.get_position('cursor')
-                    self.cursor_moved(filename0, position0, filename, position)
-                current_editor.clearFocus()
-                current_editor.setFocus()
-                current_editor.window().raise_()
-                if processevents:
-                    QApplication.processEvents()
+                foc = True
             else:
-                current_editor = self.set_current_filename(filename,
-                                                           editorwindow,
-                                                           focus=False)
-                if current_editor is None:
-                    # -- Not a valid filename:
-                    if not osp.isfile(filename):
-                        continue
-                    # --
-                    current_es = self.get_current_editorstack(editorwindow)
-                    # Creating the editor widget in the first editorstack 
-                    # (the one that can't be destroyed), then cloning this 
-                    # editor widget in all other editorstacks:
-                    finfo = self.editorstacks[0].load(filename, set_current=False)
-                    finfo.path = self.main.get_spyder_pythonpath()
-                    self._clone_file_everywhere(finfo)
-                    current_editor = current_es.set_current_filename(filename,
-                                                                     focus=False)
-                    current_editor.set_breakpoints(load_breakpoints(filename))
-                    self.register_widget_shortcuts(current_editor)
-                    current_es.analyze_script()
-                    self.__add_recent_file(filename)
-                if goto is not None: # 'word' is assumed to be None as well
-                    current_editor.go_to_line(goto[index], word=word)
-                    position = current_editor.get_position('cursor')
-                    self.cursor_moved(filename0, position0, filename, position)
-                current_editor.clearFocus()
-                current_editor.setFocus()
-                current_editor.window().raise_()
-                if processevents:
-                    QApplication.processEvents()
+                foc = False
+            current_editor = self.set_current_filename(filename,
+                                                       editorwindow,
+                                                       focus=foc)
+            if current_editor is None:
+                # -- Not a valid filename:
+                if not osp.isfile(filename):
+                    continue
+                # --
+                current_es = self.get_current_editorstack(editorwindow)
+                # Creating the editor widget in the first editorstack 
+                # (the one that can't be destroyed), then cloning this 
+                # editor widget in all other editorstacks:
+                finfo = self.editorstacks[0].load(filename, set_current=False)
+                finfo.path = self.main.get_spyder_pythonpath()
+                self._clone_file_everywhere(finfo)
+                current_editor = current_es.set_current_filename(filename,
+                                                                 focus=foc)
+                current_editor.set_breakpoints(load_breakpoints(filename))
+                self.register_widget_shortcuts(current_editor)
+                current_es.analyze_script()
+                self.__add_recent_file(filename)
+            if goto is not None: # 'word' is assumed to be None as well
+                current_editor.go_to_line(goto[index], word=word)
+                position = current_editor.get_position('cursor')
+                self.cursor_moved(filename0, position0, filename, position)
+            current_editor.clearFocus()
+            current_editor.setFocus()
+            current_editor.window().raise_()
+            if processevents:
+                QApplication.processEvents()
 
     @Slot()
     def print_file(self):
