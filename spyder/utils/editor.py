bd8b9aa0c315ee6f72ce137312d78d9ae5f53c27
Rafael Laverde <leafar91@gmail.com>
2017-01-03 10:12:44 -0500

    Delete unused memoized function copied from pyqode

--- spyder/utils/editor.py
+++ spyder/utils/editor.py
@@ -19,36 +19,6 @@ from qtpy.QtGui import QColor, QTextCursor, QTextBlock, QTextDocument, QCursor
 from qtpy.QtWidgets import QApplication
 
 
-class memoized(object):
-    """
-    Decorator. Caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned
-    (not reevaluated).
-    """
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args):
-        try:
-            if args in self.cache:
-                return self.cache[args]
-            else:
-                value = self.func(*args)
-                self.cache[args] = value
-                return value
-        except TypeError:
-            return self.func(*args)
-
-    def __repr__(self):
-        """Return the function's docstring."""
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        """Support instance methods."""
-        return functools.partial(self.__call__, obj)
-
-
 def drift_color(base_color, factor=110):
     """
     Return color that is lighter or darker than the base color.
