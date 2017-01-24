7881e89be1173092bc67d829b31ed2254d425d9b
dalthviz <d.althviz10@uniandes.edu.co>
2017-01-23 21:39:00 -0500

    Adds a QApplication instance before the import of qtawesome.

--- spyder_profiler/__init__.py
+++ spyder_profiler/__init__.py
@@ -3,4 +3,8 @@
 #==============================================================================
 # The following statement is required to register this 3rd party plugin:
 #==============================================================================
+from spyder.utils.qthelpers import qapplication
+# The line below is needed in order to prevent a segmentation fault.
+# See issue #4014
+app = qapplication()
 from .profiler import Profiler as PLUGIN_CLASS
