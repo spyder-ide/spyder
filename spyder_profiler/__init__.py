6bd4a480e736706a60bacf286b14be9969fef89e
dalthviz <d.althviz10@uniandes.edu.co>
2017-01-23 22:40:54 -0500

    Create a QApplication only when needed.

--- spyder_profiler/__init__.py
+++ spyder_profiler/__init__.py
@@ -3,8 +3,10 @@
 #==============================================================================
 # The following statement is required to register this 3rd party plugin:
 #==============================================================================
-from spyder.utils.qthelpers import qapplication
-# The line below is needed in order to prevent a segmentation fault.
+from pyqt.QtWidgets import QApplication
+# The lines below are needed in order to prevent a segmentation fault.
 # See issue #4014
-app = qapplication()
+app = None
+if not QApplication.instance():
+   app = QApplication()
 from .profiler import Profiler as PLUGIN_CLASS
