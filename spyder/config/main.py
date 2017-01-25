c40b0f9fd1b254690a27c0b77ac9bfb0e521883a
Rafael Laverde <leafar91@gmail.com>
2017-01-24 21:02:47 -0500

    Change default value of show_internal_console_if_traceback to False.

--- spyder/config/main.py
+++ spyder/config/main.py
@@ -93,7 +93,7 @@ DEFAULTS = [
               'cpu_usage/timeout': 2000,
               'use_custom_margin': True,
               'custom_margin': 0,
-              'show_internal_console_if_traceback': True,
+              'show_internal_console_if_traceback': False,
               'check_updates_on_startup': True,
               'toolbars_visible': True,
               # Global Spyder fonts
@@ -655,7 +655,7 @@ DEFAULTS = [
 #    or if you want to *rename* options, then you need to do a MAJOR update in
 #    version, e.g. from 3.0.0 to 4.0.0
 # 3. You don't need to touch this value if you're just adding a new option
-CONF_VERSION = '37.1.0'
+CONF_VERSION = '37.2.0'
 
 # Main configuration instance
 try:
