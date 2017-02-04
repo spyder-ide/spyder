0a2701be1008ab244a74a7668374a4251a0ecb1c
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-02-03 23:09:47 -0500

    Added MATLAB importation tests

---
+++ spyder/utils/tests/test_iofuncs.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © Spyder Project Contributors
+# Licensed under the terms of the MIT License
+#
+
+"""
+Tests for iofuncs.py
+"""
+
+import pytest
+import numpy as np
+import spyder.utils.iofuncs as io
+
+@pytest.fixture
+def real_values():
+    file_s = np.load('./numpy_data.npz')
+    A = file_s['A'].item()
+    B = file_s['B']
+    C = file_s['C']
+    D = file_s['D'].item()
+    E = file_s['E']
+    return {'A':A, 'B':B, 'C':C, 'D':D, 'E':E}
+
+def test_matlab_import(real_values):
+    inf = io.load_matlab('./data.mat')[0]
+    valid = True
+    for var in sorted(real_values.keys()):
+        valid = valid and np.sum(real_values[var] == inf[var])
+    assert valid
