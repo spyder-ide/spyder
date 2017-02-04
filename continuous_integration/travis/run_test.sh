4fd37ba3c2d3b5cac2dd011f34c812fedb51a932
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-02-04 11:38:35 -0500

    Added scipy dependencies to Travis and AppVeyor CI

--- continuous_integration/travis/run_test.sh
+++ continuous_integration/travis/run_test.sh
@@ -10,7 +10,7 @@ export TEST_CI_APP=True
 # We install them here and not in travis_install.sh to see if
 # Spyder is correctly pulling its deps (some of them are shared
 # with mpl)
-export EXTRA_PACKAGES="nomkl pandas sympy pillow"
+export EXTRA_PACKAGES="nomkl pandas sympy pillow scipy"
 
 # Install our builds of Spyder
 if [ "$USE_CONDA" = true ] ; then
