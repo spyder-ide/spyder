1ded9305eff2a2b82af729cf156f57f7e4805bc7
Carlos Cordoba <ccordoba12@gmail.com>
2017-02-11 21:26:41 -0500

    Testing: Test that a new IPython client is created using create_client_for_kernel
    This is a better test for KernelConnectionDialog

--- spyder/plugins/tests/test_ipythonconsole.py
+++ spyder/plugins/tests/test_ipythonconsole.py
@@ -4,73 +4,76 @@
 # Licensed under the terms of the MIT License
 #
 
-import os
-import os.path as osp
-
-import mock
 import pytest
-from pytestqt import qtbot
 from qtpy.QtCore import Qt, QTimer
-from spyder.py3compat import to_text_string
-from spyder.plugins.ipythonconsole import (IPythonConsole,
-                                           KernelConnectionDialog)
-from qtpy.QtWidgets import QDialogButtonBox
-from jupyter_core.paths import jupyter_config_dir, jupyter_runtime_dir
+from qtpy.QtWidgets import QApplication, QDialog
 
+from spyder.plugins.ipythonconsole import IPythonConsole
+
+
+#==============================================================================
+# Constants
+#==============================================================================
+SHELL_TIMEOUT = 30000
+
+
+#==============================================================================
 # Utillity Functions
-#--------------------------------
-def dialog_interaction(kernelconn, _id, qtbot):
-    kernelconn.cf.setText(_id)
-    qtbot.keyPress(kernelconn, Qt.Key_Return)
+#==============================================================================
+def open_client_from_connection_file(connection_file, qtbot):
+    top_level_widgets = QApplication.topLevelWidgets()
+    for w in top_level_widgets:
+        if isinstance(w, QDialog):
+            w.cf.setText(connection_file)
+            qtbot.keyClick(w, Qt.Key_Enter)
 
+
+#==============================================================================
 # Qt Test Fixtures
-#--------------------------------
+#==============================================================================
 @pytest.fixture
 def ipyconsole():
     widget = IPythonConsole(None, testing=True)
     widget.create_new_client()
+    widget.show()
     return widget
 
-@pytest.fixture
-def kernelconn(qtbot):
-    widget = KernelConnectionDialog(None)
-    widget.hide()
-    qtbot.addWidget(widget)
-    return widget
 
+#==============================================================================
 # Tests
-#-------------------------------
-@pytest.mark.skipif(os.name == 'nt', reason="It's timing out on Windows")
-def test_sys_argv_clear(ipyconsole, qtbot):
+#==============================================================================
+def test_load_kernel_file(ipyconsole, qtbot):
+    """
+    Test that a new client is created using the connection file
+    of an existing client
+    """
     shell = ipyconsole.get_current_shellwidget()
     client = ipyconsole.get_current_client()
+    qtbot.waitUntil(lambda: shell._prompt_html is not None, timeout=SHELL_TIMEOUT)
 
-    qtbot.waitUntil(lambda: shell._prompt_html is not None, timeout=6000)
+    QTimer.singleShot(2000, lambda: open_client_from_connection_file(
+                                        client.connection_file,
+                                        qtbot))
+    ipyconsole.create_client_for_kernel()
+
+    new_client = ipyconsole.get_clients()[1]
+    new_shell = new_client.shellwidget
+    qtbot.waitUntil(lambda: new_shell._prompt_html is not None, timeout=SHELL_TIMEOUT)
+    new_shell.execute('a = 10')
+
+    assert new_client.name == '1/B'
+    assert shell.get_value('a') == new_shell.get_value('a')
+
+    ipyconsole.close()
+
+
+def test_sys_argv_clear(ipyconsole, qtbot):
+    """Test that sys.argv is cleared up correctly"""
+    shell = ipyconsole.get_current_shellwidget()
+    qtbot.waitUntil(lambda: shell._prompt_html is not None, timeout=SHELL_TIMEOUT)
+
     shell.execute('import sys; A = sys.argv')
     argv = shell.get_value("A")
     assert argv == ['']
 
-@pytest.mark.skipif(os.name == 'nt', reason="It's timing out on Windows")
-def test_load_kernel_id(kernelconn, qtbot):
-    dest_path = jupyter_runtime_dir()
-    _id = "testkernel"
-
-    QTimer.singleShot(2000, lambda: dialog_interaction(kernelconn, _id, qtbot))
-
-    result = KernelConnectionDialog.get_connection_parameters(
-                    dialog=kernelconn
-             )
-    path, _, _, _, _ = result
-    path_dir, filename = osp.dirname(path), osp.basename(path)
-    assert path_dir == dest_path and filename == 'kernel-%s.json' % (_id)
-
-@pytest.mark.skipif(os.name == 'nt', reason="It's timing out on Windows")
-def test_load_kernel_file(kernelconn, qtbot):
-    path = jupyter_runtime_dir()
-    file = osp.join(path, 'some_id.json')
-    QTimer.singleShot(2000, lambda: dialog_interaction(kernelconn, file, qtbot))
-    result = KernelConnectionDialog.get_connection_parameters(
-                    dialog=kernelconn
-             )
-    location, _, _, _, _ = result
-    assert location == file
+    ipyconsole.close()
