spyder-ide/spyder

commit 37a4af6a1cdbad2d81afe5dbf92d6a8b905db787

Change usage of isinstance(*,str) to py3commpat.is_text_string().

spyder/widgets/panels/manager.py
@@ -14,7 +14,7 @@ Adapted from https://github.com/pyQode/pyqode.core/blob/master/pyqode/core/manag
 from spyder.api.manager import Manager
 from spyder.api.panel import Panel
 from spyder.config.base import debug_print
-
+from spyder.py3compat import is_text_string
 
 class PanelsManager(Manager):
     """
@@ -94,7 +94,7 @@ class PanelsManager(Manager):
         :param name_or_klass: Name or class of the panel to retrieve.
         :return: The specified panel instance.
         """
-        if not isinstance(name_or_klass, str):
+        if not is_text_string(name_or_klass, str):
             name_or_klass = name_or_klass.__name__
         for zone in range(4):
             try:

spyder/workers/updates.py
@@ -14,7 +14,7 @@ from qtpy.QtCore import QObject, Signal
 # Local imports
 from spyder import __version__
 from spyder.config.base import _
-from spyder.py3compat import PY3
+from spyder.py3compat import PY3, is_text_string
 from spyder.utils.programs import check_version, is_stable_version
 
 
@@ -76,7 +76,7 @@ class WorkerUpdates(QObject):
                 data = page.read()
 
                 # Needed step for python3 compatibility
-                if not isinstance(data, str):
+                if not is_text_string(data, str):
                     data = data.decode()
 
                 data = json.loads(data)

commit 051e757cfdfe7d65159356eaf06179bf391015b2

Fix error introduced by 37a4af6a

spyder/widgets/panels/manager.py
@@ -94,7 +94,7 @@ class PanelsManager(Manager):
         :param name_or_klass: Name or class of the panel to retrieve.
         :return: The specified panel instance.
         """
-        if not is_text_string(name_or_klass, str):
+        if not is_text_string(name_or_klass):
             name_or_klass = name_or_klass.__name__
         for zone in range(4):
             try:

spyder/workers/updates.py
@@ -76,7 +76,7 @@ class WorkerUpdates(QObject):
                 data = page.read()
 
                 # Needed step for python3 compatibility
-                if not is_text_string(data, str):
+                if not is_text_string(data):
                     data = data.decode()
 
                 data = json.loads(data)