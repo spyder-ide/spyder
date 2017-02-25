05664d29c8636a6ec044bef71b7912f7057edbcf
Carlos Cordoba <ccordoba12@gmail.com>
2017-02-25 15:44:23 -0500

    IPython console: Make remove_value and copy_value work while debugging

--- spyder/widgets/ipythonconsole/namespacebrowser.py
+++ spyder/widgets/ipythonconsole/namespacebrowser.py
@@ -112,12 +112,20 @@ class NamepaceBrowserWidget(RichJupyterWidget):
 
     def remove_value(self, name):
         """Remove a variable"""
-        self.silent_execute("get_ipython().kernel.remove_value('%s')" % name)
+        code = u"get_ipython().kernel.remove_value('%s')" % name
+        if self._reading:
+            self.kernel_client.input(u'!' + code)
+        else:
+            self.silent_execute(code)
 
     def copy_value(self, orig_name, new_name):
         """Copy a variable"""
-        self.silent_execute("get_ipython().kernel.copy_value('%s', '%s')" %
-                            (orig_name, new_name))
+        code = u"get_ipython().kernel.copy_value('%s', '%s')" % (orig_name,
+                                                                 new_name)
+        if self._reading:
+            self.kernel_client.input(u'!' + code)
+        else:
+            self.silent_execute(code)
 
     def load_data(self, filename, ext):
         # Wait until the kernel tries to load the file
