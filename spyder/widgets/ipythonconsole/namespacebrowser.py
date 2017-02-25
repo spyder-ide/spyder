bbbc4618e88e7048a74839a77cd3c2e09b442045
Carlos Cordoba <ccordoba12@gmail.com>
2017-02-25 15:03:08 -0500

    IPython console: Simplify again getting values while debugging

--- spyder/widgets/ipythonconsole/namespacebrowser.py
+++ spyder/widgets/ipythonconsole/namespacebrowser.py
@@ -75,20 +75,22 @@ class NamepaceBrowserWidget(RichJupyterWidget):
 
     def get_value(self, name):
         """Ask kernel for a value"""
-        # Wait until the kernel returns the value
+        code = u"get_ipython().kernel.get_value('%s')" % name
         if self._reading:
-            self.silent_exec_input(u"!get_ipython().kernel.get_value('%s')" %
-                                   name)
+            method = self.kernel_client.input
+            code = u'!' + code
         else:
-            wait_loop = QEventLoop()
-            self.sig_got_reply.connect(wait_loop.quit)
-            self.silent_execute(u"get_ipython().kernel.get_value('%s')" %
-                                name)
-            wait_loop.exec_()
+            method = self.silent_execute
 
-            # Remove loop connection and loop
-            self.sig_got_reply.disconnect(wait_loop.quit)
-            wait_loop = None
+        # Wait until the kernel returns the value
+        wait_loop = QEventLoop()
+        self.sig_got_reply.connect(wait_loop.quit)
+        method(code)
+        wait_loop.exec_()
+
+        # Remove loop connection and loop
+        self.sig_got_reply.disconnect(wait_loop.quit)
+        wait_loop = None
 
         # Handle exceptions
         if self._kernel_value is None:
@@ -168,10 +170,7 @@ class NamepaceBrowserWidget(RichJupyterWidget):
             if isinstance(value, CannedObject):
                 value = value.get_object()
             self._kernel_value = value
-            if self._reading:
-                self.sig_input_reply.emit()
-            else:
-                self.sig_got_reply.emit()
+            self.sig_got_reply.emit()
 
     # ---- Private API (overrode by us) ----------------------------
     def _handle_execute_reply(self, msg):
