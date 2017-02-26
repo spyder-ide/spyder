ef71c3241b415aecf41fabadd87654ff5ee93db8
Carlos Cordoba <ccordoba12@gmail.com>
2017-02-25 19:07:00 -0500

    IPython console: Don't block the console trying to run cells or lines while debugging

--- spyder/plugins/ipythonconsole.py
+++ spyder/plugins/ipythonconsole.py
@@ -869,9 +869,12 @@ class IPythonConsole(SpyderPluginWidget):
         """Execute code instructions."""
         sw = self.get_current_shellwidget()
         if sw is not None:
-            if clear_variables:
-                sw.reset_namespace(force=True)
-            sw.execute(to_text_string(lines))
+            if sw._reading:
+                pass
+            else:
+                if clear_variables:
+                    sw.reset_namespace(force=True)
+                sw.execute(to_text_string(to_text_string(lines)))
             self.activateWindow()
             self.get_current_client().get_control().setFocus()
 
