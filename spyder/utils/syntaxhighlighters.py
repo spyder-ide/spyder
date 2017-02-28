ee18550c54d1614c85dc5d4cfec5a1094f317d42
Carlos Cordoba <ccordoba12@gmail.com>
2017-02-28 13:06:40 -0500

    Some fixes for the Cython highlighter

--- spyder/utils/syntaxhighlighters.py
+++ spyder/utils/syntaxhighlighters.py
@@ -509,17 +509,17 @@ class PythonSH(BaseSH):
 #==============================================================================
 # Cython syntax highlighter
 #==============================================================================
-C_TYPES = 'bool char double enum float int long mutable short signed struct unsigned void NULL'
+C_TYPES = 'bool char double enum float int long mutable short signed struct unsigned void'
 
 class CythonSH(PythonSH):
     """Cython Syntax Highlighter"""
     ADDITIONAL_KEYWORDS = [
         "cdef", "ctypedef", "cpdef", "inline", "cimport", "extern",
 	"include", "begin", "end", "by", "gil", "nogil", "const", "public",
-	"readonly", "fused ", "static", "api", "DEF", "IF", "ELIF", "ELSE"]
+	"readonly", "fused", "static", "api", "DEF", "IF", "ELIF", "ELSE"]
 	
     ADDITIONAL_BUILTINS = C_TYPES.split() + [
-        "array", "bint", "Py_ssize_t", "intern", "reload", "sizeof"]
+        "array", "bint", "Py_ssize_t", "intern", "reload", "sizeof", "NULL"]
     PROG = re.compile(make_python_patterns(ADDITIONAL_KEYWORDS,
                                            ADDITIONAL_BUILTINS), re.S)
     IDPROG = re.compile(r"\s+([\w\.]+)", re.S)
