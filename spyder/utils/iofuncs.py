e85b493a19c5d510b0547a9d44154150875dceae
Rafael Laverde <leafar91@gmail.com>
2017-03-03 18:18:22 -0500

    Remove comment out python2 prints.

--- spyder/utils/iofuncs.py
+++ spyder/utils/iofuncs.py
@@ -495,8 +495,6 @@ if __name__ == "__main__":
     t0 = time.time()
     example2, ok = load_dictionary("test.spydata")
     print("Data loaded in %.3f seconds" % (time.time()-t0))  # spyder: test-skip
-#    for key in example:
-#        print key, ":", example[key] == example2[key]
 
     a = MatlabStruct()
     a.b = 'spam'
