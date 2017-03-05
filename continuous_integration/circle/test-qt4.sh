062ca468106ca4b0996fc727bf1e9336226558a6
Carlos Cordoba <ccordoba12@gmail.com>
2017-03-05 14:18:01 -0500

    Testing: Run our tests in Python 3.6

--- continuous_integration/circle/test-qt4.sh
+++ continuous_integration/circle/test-qt4.sh
@@ -4,6 +4,13 @@ export PATH="$HOME/miniconda/bin:$PATH"
 
 source activate test
 
-conda install -q qt=4.* pyqt=4.*
+# Only run tests for PyQt4 in Python 2.7 and 3.5
+export PY_VERSIONS=($PY_VERSIONS)
+export PY_VERSION=${PY_VERSIONS[$CIRCLE_NODE_INDEX]}
 
-python runtests.py
+if [ "$PY_VERSION" = "2.7" ] || [ "$PY_VERSION" = "3.5" ]; then
+    conda install -q qt=4.* pyqt=4.*
+    python runtests.py
+else
+    exit 0
+fi
