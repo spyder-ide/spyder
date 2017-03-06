608f51510f474ab9a6f4ca18b69d4e2d8d0c8f05
dalthviz <d.althviz10@uniandes.edu.co>
2017-03-05 23:41:03 -0500

    Changes in test_system.py

--- spyder/utils/tests/test_system.py
+++ spyder/utils/tests/test_system.py
@@ -23,12 +23,7 @@ def test_system():
         #  windll can only be imported if os.name = 'nt' or 'ce'
         assert windows_memory_usage() > 0
     else:
-        # Based on http://stackoverflow.com/a/42275253 RAM usage with only stdlib
-        tot_m, used_m, free_m = map(int,
-                                os.popen(
-                                'free -t -m'
-                                ).readlines()[-1].split()[1:])
-        assert memory_usage() == round(used_m / tot_m, 2) * 100
+        assert memory_usage() > 0
     
 
 
