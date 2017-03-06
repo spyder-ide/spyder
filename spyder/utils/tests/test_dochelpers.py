c416c7ddcec3cb2ae6724f290734f95436284347
dalthviz <d.althviz10@uniandes.edu.co>
2017-03-05 23:19:03 -0500

    Take into account python version for test_dochelpers.py

--- spyder/utils/tests/test_dochelpers.py
+++ spyder/utils/tests/test_dochelpers.py
@@ -13,6 +13,7 @@ import pytest
 
 # Local imports
 from spyder.utils.dochelpers import getargtxt, getdoc, getobj, isdefined
+from spyder.py3compat import PY2
 
 def test_dochelpers():
     """Test dochelpers."""
@@ -20,7 +21,31 @@ def test_dochelpers():
         def method(self, x, y=2):
             pass
     assert getargtxt(Test.__init__) == None
-    assert getargtxt(Test.method) == ['x,', 'y=2']
+    if PY2:                    
+        assert getargtxt(Test.method) == ['x, ', 'y=2']
+        assert getdoc(sorted) == {'note': 'Function of __builtin__ module',
+                                  'argspec': u'(iterable, cmp=None, key=None, '
+                                              'reverse=False)',
+                                  'docstring': u'sorted(iterable, cmp=None, '
+                                                'key=None, reverse=False) --> '
+                                                'new sorted list',
+                                  'name': 'sorted'}
+        assert getargtxt(sorted) == ['iterable, ', ' cmp=None, ',
+                                     ' key=None, ', ' reverse=False']
+    else:
+        assert getargtxt(Test.method) == None
+        assert getdoc(sorted) == {'note': 'Function of builtins module',
+                                  'argspec': '(...)',
+                                  'docstring': 'Return a new list containing '
+                                               'all items from the iterable in'
+                                               ' ascending order.\n\nA custom '
+                                               'key function can be supplied '
+                                               'to customise the sort order, '
+                                               'and the\nreverse flag can be '
+                                               'set to request the result '
+                                               'in descending order.',
+                                               'name': 'sorted'}
+        assert getargtxt(sorted) == None
     assert isdefined('numpy.take', force_import=True) == True
     assert isdefined('__import__') == True
     assert isdefined('.keys', force_import=True) == False
@@ -28,19 +53,6 @@ def test_dochelpers():
     assert getobj('globals().keys') == None
     assert getobj('+scipy.signal.') == 'scipy.signal'
     assert getobj('4.') == '4'
-    assert getdoc(sorted) == {'note': 'Function of builtins module',
-                              'argspec': '(...)',
-                              'docstring': 'Return a new list containing all '
-                                           'items from the iterable in '
-                                           'ascending order.\n\nA custom '
-                                           'key function can be supplied to '
-                                           'customise the sort order, and '
-                                           'the\nreverse flag can be set to '
-                                           'request the result in descending '
-                                           'order.',
-                              'name': 'sorted'}
-    assert getargtxt(sorted) == None
-
 
 
 if __name__ == "__main__":
