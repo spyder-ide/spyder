9f6cea4997e047c32d3e447a502476e0ca6667e7
Carlos Cordoba <ccordoba12@gmail.com>
2017-03-14 13:05:15 -0500

    Testing: Backport a fix for test_dochelpers

--- spyder/utils/tests/test_dochelpers.py
+++ spyder/utils/tests/test_dochelpers.py
@@ -9,6 +9,7 @@ Tests for dochelpers.py
 """
 # Standard library imports
 import os
+import sys
 
 # Test library imports
 import pytest
@@ -21,6 +22,8 @@ class Test(object):
     def method(self, x, y=2):
         pass
 
+@pytest.mark.skipif(not 'Continuum' in sys.version,
+                    reason="It fails when not run in Anaconda")
 def test_dochelpers():
     """Test dochelpers."""
     assert not getargtxt(Test.__init__)
@@ -51,7 +54,7 @@ def test_dochelpers():
                                                    'the\nreverse flag can be '
                                                    'set to request the result '
                                                    'in descending order.',
-                                                   'name': 'sorted'}
+                                      'name': 'sorted'}
         else:
             assert getdoc(sorted) == {'note': 'Function of builtins module',
                                       'argspec': '(...)',
@@ -66,7 +69,7 @@ def test_dochelpers():
                                                    'the\nreverse flag can be '
                                                    'set to request the result '
                                                    'in descending order.',
-                                                   'name': 'sorted'}          
+                                      'name': 'sorted'}
         assert not getargtxt(sorted)
     assert isdefined('numpy.take', force_import=True)
     assert isdefined('__import__')
