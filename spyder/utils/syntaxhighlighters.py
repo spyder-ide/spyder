fc794475dad26e05e783e1c3de4837c05533070b
Rafael Laverde <leafar91@gmail.com>
2017-05-15 12:39:46 -0500

    Correct wrong docstring.

--- spyder/utils/syntaxhighlighters.py
+++ spyder/utils/syntaxhighlighters.py
@@ -218,7 +218,7 @@ class BaseSH(QSyntaxHighlighter):
         """
         Highlights a block of text. Please do not override, this method.
         Instead you should implement
-        :func:`pyqode.core.api.SyntaxHighlighter.highlight_block`.
+        :func:`spyder.utils.syntaxhighplighters.SyntaxHighlighter.highlight_block`.
 
         :param text: text to highlight.
         """
