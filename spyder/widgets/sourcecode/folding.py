c110eb3744a5a739c6a4c1213d52c64df0218833
Rafael Laverde <leafar91@gmail.com>
2017-05-22 13:07:46 -0500

    Folding: Make IndentFoldDetector less dependant of editor.indent_chars.

--- spyder/widgets/sourcecode/folding.py
+++ spyder/widgets/sourcecode/folding.py
@@ -212,9 +212,17 @@ class IndentFoldDetector(FoldDetector):
         :param block: current block to highlight
         """
         text = block.text()
+        prev_lvl = TextBlockHelper().get_fold_lvl(prev_block)
         # round down to previous indentation guide to ensure contiguous block
         # fold level evolution.
-        return (len(text) - len(text.lstrip())) // len(self.editor.indent_chars)
+
+        if prev_lvl and prev_block is not None:
+            prev_text = prev_block.text()
+            indent_len = (len(prev_text) - len(prev_text.lstrip())) // prev_lvl
+        else:
+            indent_len = len(self.editor.indent_chars)
+
+        return (len(text) - len(text.lstrip())) // indent_len
 
 
 class CharBasedFoldDetector(FoldDetector):
