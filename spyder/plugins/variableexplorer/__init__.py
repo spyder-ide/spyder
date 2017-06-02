a53fabe31dcf6eada18493d712231674a71413c2
Rafael Laverde <leafar91@gmail.com>
2017-06-02 17:38:49 -0500

    Add variable explorer __init__,py

---
+++ spyder/plugins/variableexplorer/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © Spyder Project Contributors
+# Licensed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+
+"""
+spyder.plugins.variableexplorer
+===========
+
+Variable Explorer plugin used to instrospect variables in the namespace.
+"""
+
+from spyder.plugins.variableexplorer.plugin import VariableExplorer
