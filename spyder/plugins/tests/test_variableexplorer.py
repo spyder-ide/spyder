75809a70bac2290286b9fb3ccd3343f3068ce5ba
Rafael Laverde <leafar91@gmail.com>
2017-06-05 11:40:10 -0500

    Fix broken reference to variable explorer plugin.

--- spyder/plugins/tests/test_variableexplorer.py
+++ spyder/plugins/tests/test_variableexplorer.py
@@ -19,7 +19,7 @@ def test_get_settings(monkeypatch):
         if option == 'dataframe_format': return '3d'
         
     monkeypatch.setattr(VariableExplorer, 'CONF_SECTION', 'sect')
-    monkeypatch.setattr('spyder.plugins.variableexplorer.REMOTE_SETTINGS', 
+    monkeypatch.setattr('spyder.plugins.variableexplorer.plugin.REMOTE_SETTINGS', 
                         ['remote1', 'remote2'])
     monkeypatch.setattr(VariableExplorer, 'get_option', mock_get_option)
 
