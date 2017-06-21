06f7bd953c87a30417c1b4300d723a99d67abbb1
Carlos Cordoba <ccordoba12@gmail.com>
2017-06-21 11:45:19 -0500

    Kernel spec: Catch UnicodeDecodeError's when converting env vars to bytes in Python 2

--- spyder/utils/ipython/kernelspec.py
+++ spyder/utils/ipython/kernelspec.py
@@ -15,6 +15,7 @@ from jupyter_client.kernelspec import KernelSpec
 
 from spyder.config.base import get_module_source_path
 from spyder.config.main import CONF
+from spyder.utils.encoding import to_unicode_from_fs
 from spyder.py3compat import PY2, iteritems, to_text_string, to_binary_string
 from spyder.utils.misc import (add_pathlist_to_PYTHONPATH,
                                get_python_executable)
@@ -107,11 +108,27 @@ class SpyderKernelSpec(KernelSpec):
         env_vars.update(pypath)
 
         # Making all env_vars strings
-        for k,v in iteritems(env_vars):
+        for key,var in iteritems(env_vars):
             if PY2:
-                uv = to_text_string(v)
-                env_vars[k] = to_binary_string(uv, encoding='utf-8')
+                # Try to convert vars first to utf-8.
+                try:
+                    unicode_var = to_text_string(var)
+                except UnicodeDecodeError:
+                    # If that fails, try to use the file system
+                    # encoding because one of our vars is our
+                    # PYTHONPATH, and that contains file system
+                    # directories
+                    try:
+                        unicode_var = to_unicode_from_fs(var)
+                    except:
+                        # If that also fails, make the var empty
+                        # to be able to start Spyder.
+                        # See https://stackoverflow.com/q/44506900/438386
+                        # for details.
+                        unicode_var = ''
+                env_vars[key] = to_binary_string(unicode_var,
+                                                 encoding='utf-8')
             else:
-                env_vars[k] = to_text_string(v)
+                env_vars[key] = to_text_string(var)
 
         return env_vars
