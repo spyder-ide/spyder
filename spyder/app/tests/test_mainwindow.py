b027cc9b513ff6b735e071e378e9799536e253c5
Carlos Cordoba <ccordoba12@gmail.com>
2017-07-11 11:49:45 -0500

    Testing: Add a test for dedicated consoles

--- spyder/app/tests/test_mainwindow.py
+++ spyder/app/tests/test_mainwindow.py
@@ -25,6 +25,8 @@ from qtpy.QtWidgets import QApplication, QFileDialog, QLineEdit
 
 from spyder.app.cli_options import get_options
 from spyder.app.mainwindow import initialize, run_spyder
+from spyder.config.main import CONF
+from spyder.plugins.runconfig import RunConfiguration
 from spyder.py3compat import PY2
 from spyder.utils.ipython.kernelspec import SpyderKernelSpec
 from spyder.utils.programs import is_module_installed
@@ -160,6 +162,46 @@ def test_calltip(main_window, qtbot):
 
     QTimer.singleShot(1000, lambda: close_save_message_box(qtbot))
     main_window.editor.close_file()
+
+
+@flaky(max_runs=3)
+def test_dedicated_consoles(main_window, qtbot):
+    """Test running code in dedicated consoles."""
+    # ---- Load test file ----
+    test_file = osp.join(LOCATION, 'script.py')
+    main_window.editor.load(test_file)
+    code_editor = main_window.editor.get_focus_widget()
+
+    # --- Set run options for this file ---
+    rc = RunConfiguration().get()
+    # A dedicated console is used when these two options are False
+    rc['current'] = rc['systerm'] = False
+    config_entry = (test_file, rc)
+    CONF.set('run', 'configurations', [config_entry])
+
+    # --- Run test file and assert that we get a dedicated console ---
+    qtbot.keyClick(code_editor, Qt.Key_F5)
+    qtbot.wait(500)
+    shell = main_window.ipyconsole.get_current_shellwidget()
+    qtbot.waitUntil(lambda: shell._prompt_html is not None, timeout=SHELL_TIMEOUT)
+    nsb = main_window.variableexplorer.get_focus_widget()
+
+    assert len(main_window.ipyconsole.get_clients()) == 2
+    assert main_window.ipyconsole.filenames == ['', test_file]
+    assert main_window.ipyconsole.tabwidget.tabText(1) == 'script.py/A'
+    qtbot.wait(500)
+    assert nsb.editor.model.rowCount() == 3
+
+    # --- Clean namespace after re-execution ---
+    with qtbot.waitSignal(shell.executed):
+        shell.execute('zz = -1')
+    qtbot.keyClick(code_editor, Qt.Key_F5)
+    qtbot.wait(500)
+    assert not shell.is_defined('zz')
+
+    # ---- Closing test file and resetting config ----
+    main_window.editor.close_file()
+    CONF.set('run', 'configurations', [])
 
 
 @flaky(max_runs=3)
