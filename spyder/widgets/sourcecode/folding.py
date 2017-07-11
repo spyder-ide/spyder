4d0e18954a5747c4ea9d7719e3fe36828cd1507a
Rafael Laverde <leafar91@gmail.com>
2017-07-11 10:26:23 -0500

    Improve IndentFold detector, avoid zero division errors.

--- spyder/widgets/sourcecode/folding.py
+++ spyder/widgets/sourcecode/folding.py
@@ -215,11 +215,11 @@ class IndentFoldDetector(FoldDetector):
         prev_lvl = TextBlockHelper().get_fold_lvl(prev_block)
         # round down to previous indentation guide to ensure contiguous block
         # fold level evolution.
-
+        indent_len = 0
         if prev_lvl and prev_block is not None:
             prev_text = prev_block.text()
             indent_len = (len(prev_text) - len(prev_text.lstrip())) // prev_lvl
-        else:
+        if indent_len == 0:
             indent_len = len(self.editor.indent_chars)
 
         return (len(text) - len(text.lstrip())) // indent_len
