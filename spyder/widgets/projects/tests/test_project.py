7a09f9c901961b18cf52ef50f46f58d0c356c011
Rafael Laverde <leafar91@gmail.com>
2017-08-02 11:26:11 -0500

    Add a test for set/get recent project files.

--- spyder/widgets/projects/tests/test_project.py
+++ spyder/widgets/projects/tests/test_project.py
@@ -26,15 +26,14 @@ def project_test(tmpdir_factory):
     Returns:
         str: Path of temporary project dir.
     """
-    p = tmpdir_factory.mktemp("test_project")
-    path = str(p)
-    project = EmptyProject(path)
-    return path, project
+    project_dir = tmpdir_factory.mktemp("test_project")
+    project = EmptyProject(str(project_dir))
+    return project_dir, project
 
 
 def test_empty_project(project_test):
-    path, project = project_test
-    assert project.root_path == path
+    project_dir, project = project_test
+    assert project.root_path == str(project_dir)
 
     # Assert Project onfigs
     conf_files = project.get_conf_files()
@@ -44,6 +43,28 @@ def test_empty_project(project_test):
 
         # assert configurations files
         assert osp.exists(project_config.filename())
+
+
+def test_set_load_recent_files(project_test):
+    project_dir, project = project_test
+
+    # Create some files for testing
+    files_paths = []
+    for f in ['a.py', 'b.py', 'c.py']:
+        file_ = project_dir.join(f)
+        file_.write('# Some dummy content')
+        files_paths.append(str(file_))
+
+    # setting and loading
+    project.set_recent_files(files_paths[:])
+    assert project.get_recent_files() == files_paths
+
+    # adding a duplicate elemnent
+    files_paths_duplicate = files_paths + [files_paths[0]]
+    assert len(files_paths_duplicate) == len(files_paths) + 1
+
+    project.set_recent_files(files_paths_duplicate[:])
+    assert project.get_recent_files() == files_paths
 
 
 if __name__ == "__main__":
