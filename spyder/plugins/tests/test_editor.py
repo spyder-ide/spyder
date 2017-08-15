82e997e8f6deea2f8d0d60bc731205b11c729f7f
Rafael Laverde <leafar91@gmail.com>
2017-08-15 18:57:32 -0500

    Add test for editor reorder_filenames.

--- spyder/plugins/tests/test_editor.py
+++ spyder/plugins/tests/test_editor.py
@@ -45,3 +45,36 @@ def test_basic_initialization(setup_editor):
 
     # Assert that editor exists
     assert editor is not None
+
+
+@pytest.mark.parametrize(
+    'filenames, expected_filenames, splitsettings, expected_splitsettings', [
+        (['/file1.py', '/file2.py', '/file3.py', '/file4.py'],
+         ['/file1.py', '/file2.py', '/file3.py', '/file4.py'],
+         [(False, '/other_file.py', [2, 4, 8, 12])],
+         [(False, '/file1.py', [2, 4, 8, 12])]),
+        (['/file1.py', '/file2.py', '/file3.py', '/file4.py'],
+         ['/file2.py', '/file1.py', '/file3.py', '/file4.py'],
+         [(False, '/file2.py', [2, 4, 8, 12])],
+         [(False, '/file2.py', [2, 2, 8, 12])]),
+        (['/file1.py', '/file2.py', '/file3.py', '/file4.py'],
+         ['/file1.py', '/file2.py', '/file3.py', '/file4.py'],
+         None,
+         None, )
+    ])
+def test_reorder_filenames(setup_editor, expected_filenames, filenames,
+                           splitsettings, expected_splitsettings):
+    editor, qtbot = setup_editor
+
+    def get_option(*args):
+        if splitsettings is None:
+            return None
+        return {'splitsettings': splitsettings}
+
+    def set_option(name, layout_settings):
+        assert layout_settings['splitsettings'] == expected_splitsettings
+
+    editor.get_option = get_option
+    editor.set_option = set_option
+
+    assert expected_filenames == editor.reorder_filenames(filenames)
