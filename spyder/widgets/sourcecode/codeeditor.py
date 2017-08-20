0bbbb890176bb7ca295044a8230d13b30b866e3d
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2017-08-21 01:49:52 +0200

    fix unblockcomment (#4313)

--- spyder/widgets/sourcecode/codeeditor.py
+++ spyder/widgets/sourcecode/codeeditor.py
@@ -2366,7 +2366,11 @@ class CodeEditor(TextEditBaseWidget):
         cursor3.setPosition(cursor1.position())
         cursor3.movePosition(QTextCursor.NextBlock)
         while cursor3.position() < cursor2.position():
-            cursor3.setPosition(cursor3.position()+2, QTextCursor.KeepAnchor)
+            cursor3.movePosition(QTextCursor.NextCharacter, QTextCursor.KeepAnchor)
+            if not cursor3.atBlockEnd():
+                # standard commenting inserts '# ' but a trailing space on an
+                # empty line might be stripped.
+                cursor3.movePosition(QTextCursor.NextCharacter, QTextCursor.KeepAnchor)
             cursor3.removeSelectedText()
             cursor3.movePosition(QTextCursor.NextBlock)
         for cursor in (cursor2, cursor1):
