40ea3e3785dbf63fc6c0f0b51d4085d55cebaec3
Rafael Laverde <leafar91@gmail.com>
2017-08-22 16:15:07 -0500

    Test close quotes.

--- spyder/widgets/sourcecode/editorextensions/tests/test_closequotes.py
+++ spyder/widgets/sourcecode/editorextensions/tests/test_closequotes.py
@@ -13,6 +13,7 @@ import pytest
 # Local imports
 from spyder.utils.qthelpers import qapplication
 from spyder.widgets.sourcecode.codeeditor import CodeEditor
+from spyder.utils.editor import TextHelper
 
 
 # --- Fixtures
@@ -31,12 +32,29 @@ def editor_close_quotes():
 # -----------------------------------------------------------------------------
 
 
-def test_close_quotes(qtbot, editor_close_quotes):
+@pytest.mark.parametrize(
+    'text, expected_text, cursor_column',
+    [
+        ('"', '""', 1),  # Complete single quotes
+        ("'", "''", 1),
+        ('#"', '#"', 2),  # In comment, dont add extra quote
+        ("#'", "#'", 2),
+        ('"""', '"""', 3),  # Three quotes, dont add extra quotes
+        ("'''", "'''", 3),
+        ('""""', '""""""', 3),  # Four, complete docstring quotes
+        ("''''", "''''''", 3),
+        ('"some_string"', '"some_string"', 13),  # Write a string
+        ("'some_string'", "'some_string'", 13),
+    ])
+def test_close_quotes(qtbot, editor_close_quotes, text, expected_text,
+                      cursor_column):
     """"""
     editor = editor_close_quotes
 
-    qtbot.keyClicks(editor, '"')
-    assert editor.toPlainText() == '""'
+    qtbot.keyClicks(editor, text)
+    assert editor.toPlainText() == expected_text
+
+    assert cursor_column == TextHelper(editor).current_column_nbr()
 
 
 if __name__ == '__main__':
