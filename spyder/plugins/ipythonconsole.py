3350d3e4b7b2ad27db741b9dbc8d27bbe7b1c967
Rafael Laverde <leafar91@gmail.com>
2017-09-04 13:51:39 -0500

    Only wait to console to be ready when running in a dedicated console.

--- spyder/plugins/ipythonconsole.py
+++ spyder/plugins/ipythonconsole.py
@@ -940,10 +940,10 @@ class IPythonConsole(SpyderPluginWidget):
                     sw.silent_execute(
                         'get_ipython().kernel.close_all_mpl_figures()')
                     sw.reset_namespace(force=True)
+                    # wait until console it's ready before executing code
+                    self.wait_shell_is_ready(sw)
                 elif current_client and clear_variables:
                     sw.reset_namespace(force=True)
-                # wait until console it's ready before executing code
-                self.wait_shell_is_ready(sw)
                 sw.execute(to_text_string(to_text_string(lines)))
             self.activateWindow()
             self.get_current_client().get_control().setFocus()
