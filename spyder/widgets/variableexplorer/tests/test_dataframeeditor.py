214cdb31e2bf7c490e5c3371f6ad8361589d2401
Prikers <melaine.euzenat@gmail.com>
2017-09-06 20:55:00 +0200

    Tests for multiple types of pandas indexes

--- spyder/widgets/variableexplorer/tests/test_dataframeeditor.py
+++ spyder/widgets/variableexplorer/tests/test_dataframeeditor.py
@@ -17,6 +17,7 @@ except ImportError:
 import os
 
 # Third party imports
+import pandas
 from pandas import DataFrame, date_range, read_csv
 from qtpy.QtGui import QColor
 from qtpy.QtCore import Qt
@@ -45,6 +46,20 @@ def data(dfm, i, j):
 
 def bgcolor(dfm, i, j):
     return dfm.get_bgcolor(dfm.createIndex(i, j))
+
+def generate_pandas_indexes():
+    """ Creates a dictionnary of many possible pandas indexes """
+    return {
+        'Index': pandas.Index(list('ABCDEFGHIJKLMNOPQRST')),
+        'RangeIndex': pandas.RangeIndex(0, 20),
+        'Float64Index': pandas.Index([i/10 for i in range(20)]),
+        'DatetimeIndex': pandas.DatetimeIndex(start='2017-01-01', periods=20,
+                                               freq='D'),
+        'MultiIndex': pandas.MultiIndex.from_product(
+            [list('ABCDEFGHIJ'), ('foo', 'bar')], names=['first', 'second']),
+        'CategoricalIndex': pandas.CategoricalIndex(
+            list('abcaadaccbbacabacccb'), categories=['a','b','c']),
+        }
 
 # --- Tests
 # -----------------------------------------------------------------------------
@@ -216,17 +231,41 @@ def test_header_encoding():
     assert model.headerData(5, orientation=Qt.Horizontal) == "c"
     assert model.headerData(6, orientation=Qt.Horizontal) == "d"
 
-def test_dataframeeditor_with_datetimeindex():
-    rng = date_range('20150101', periods=3)
-    editor = DataFrameEditor(None)
-    editor.setup_and_check(rng)
-    dfm = editor.dataModel
-    assert dfm.rowCount() == 3
-    assert dfm.columnCount() == 2
-    assert data(dfm, 0, 1) == '2015-01-01 00:00:00'
-    assert data(dfm, 1, 1) == '2015-01-02 00:00:00'
-    assert data(dfm, 2, 1) == '2015-01-03 00:00:00'
-
+def test_dataframeeditor_with_various_indexes():
+    for rng_name, rng in generate_pandas_indexes().items():
+        rng = rng[:3]
+        editor = DataFrameEditor(None)
+        editor.setup_and_check(rng)
+        dfm = editor.dataModel
+        assert dfm.rowCount() == 3
+        assert dfm.columnCount() == 2
+        assert data(dfm, 0, 0) == '0'
+        assert data(dfm, 1, 0) == '1'
+        assert data(dfm, 2, 0) == '2'
+        if rng_name == "Index":
+            assert data(dfm, 0, 1) == 'A'
+            assert data(dfm, 1, 1) == 'B'
+            assert data(dfm, 2, 1) == 'C'
+        elif rng_name == "RangeIndex":
+            assert data(dfm, 0, 1) == '0'
+            assert data(dfm, 1, 1) == '1'
+            assert data(dfm, 2, 1) == '2'
+        elif rng_name == "Float64Index":
+            assert data(dfm, 0, 1) == '0'
+            assert data(dfm, 1, 1) == '0.1'
+            assert data(dfm, 2, 1) == '0.2'
+        elif rng_name == "DatetimeIndex":
+            assert data(dfm, 0, 1) == '2017-01-01 00:00:00'
+            assert data(dfm, 1, 1) == '2017-01-02 00:00:00'
+            assert data(dfm, 2, 1) == '2017-01-03 00:00:00'
+        elif rng_name == "MultiIndex":
+            assert data(dfm, 0, 1) == "('A', 'foo')"
+            assert data(dfm, 1, 1) == "('A', 'bar')"
+            assert data(dfm, 2, 1) == "('B', 'foo')"
+        elif rng_name == "CategoricalIndex":
+            assert data(dfm, 0, 1) == 'a'
+            assert data(dfm, 1, 1) == 'b'
+            assert data(dfm, 2, 1) == 'c'
 
 if __name__ == "__main__":
     pytest.main()
