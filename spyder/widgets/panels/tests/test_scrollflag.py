2c7a8fc3e0e39e2350f7301f1cb5ac37cb4407d6
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-09-21 09:37:07 -0400

    Assert painting of error flags and more commenting.

--- spyder/widgets/panels/tests/test_scrollflag.py
+++ spyder/widgets/panels/tests/test_scrollflag.py
@@ -31,40 +31,41 @@ def editor_bot(qtbot):
                         font=QFont("Courier New", 10),
                         color_scheme='Zenburn',
                         language='Python')
-    qtbot.addWidget(widget)
+    # qtbot.addWidget(widget)
     return qtbot, widget
 
 
 # ---------------------------------------------------------------------------
 # Examples to Test Against
 # ---------------------------------------------------------------------------
-long_code = """Line 1
-Line 2
-Line 3
-Line 4
-Line 5
-Line 6
-Line 7
-Line 8
-Line 9
-Line 10
-Line 11
-Line 12
-Line 13
-Line 14
-Line 15
-Line 16
-Line 17
-Line 18
-Line 19
-Line 20
+long_code = """Line1
+Line2
+Line3
+Line4
+Line5
+Line6
+Line7
+Line8
+Line9
+Line10
+Line11
+Line12
+Line13
+Line14
+Line15
+Line16
+Line17
+Line18
+Line19
+Line20
 """
 
 short_code = """line1: Occurences
 line2: Breakpoints
 line3: TODOs
-line4: Code Analysis
-line5: Found Results
+line4: Code Analysis: warning
+line5: Code Analysis: error
+line6: Found Results
 """
 
 
@@ -73,6 +74,8 @@ line5: Found Results
 # ---------------------------------------------------------------------------
 
 def test_enabled(editor_bot):
+    """"Test that enabling and disabling the srollflagarea panel make
+    it visible or invisible depending on the case."""
     qtbot, editor = editor_bot
     sfa = editor.scrollflagarea
     editor.show()
@@ -84,36 +87,40 @@ def test_enabled(editor_bot):
 
 
 def test_flag_painting(editor_bot):
+    """"Test that there is no error when painting all flag types on the
+    scrollbar area when the editor vertical scrollbar is visible and not
+    visible. There is seven different flags: breakpoints, todos, warnings,
+    errors, found_results, and occurences"""
     qtbot, editor = editor_bot
     sfa = editor.scrollflagarea
 
     editor.resize(450, 300)
     editor.show()
 
-    # Check that the slider is not visible.
+    # Set a short text in the editor and assert that the slider is not visible.
     editor.set_text(short_code)
     qtbot.waitUntil(lambda: not sfa.slider)
 
     # Trigger the painting of all flag types.
     editor.add_remove_breakpoint(line_number=2)
     editor.process_todo([[True, 3]])
-    editor.process_code_analysis([['dummy text', 4]])
-    editor.highlight_found_results('line5')
+    editor.process_code_analysis([['E227 warning', 4], ['syntax error', 5]])
+    editor.highlight_found_results('line6')
     with qtbot.waitSignal(editor.sig_flags_changed, raising=True,
                           timeout=5000):
         cursor = editor.textCursor()
         cursor.setPosition(2)
         editor.setTextCursor(cursor)
 
-    # Check that the slider is not visible.
+    # Set a long text in the editor and assert that the slider is visible.
     editor.set_text(long_code)
     qtbot.waitUntil(lambda: sfa.slider)
 
     # Trigger the painting of all flag types.
     editor.add_remove_breakpoint(line_number=2)
     editor.process_todo([[True, 3]])
-    editor.process_code_analysis([['dummy text', 4]])
-    editor.highlight_found_results('line5')
+    editor.process_code_analysis([['E227 warning', 4], ['syntax error', 5]])
+    editor.highlight_found_results('line6')
     with qtbot.waitSignal(editor.sig_flags_changed, raising=True,
                           timeout=5000):
         cursor = editor.textCursor()
@@ -122,6 +129,10 @@ def test_flag_painting(editor_bot):
 
 
 def test_range_indicator_visible_on_hover_only(editor_bot):
+    """Test that the slider range indicator is visible only when hovering
+    over the scrollflag area when the editor vertical scrollbar is visible.
+    The scrollflag area should remain hidden at all times when the editor
+    vertical scrollbar is not visible."""
     qtbot, editor = editor_bot
     sfa = editor.scrollflagarea
 
@@ -165,40 +176,58 @@ def test_range_indicator_visible_on_hover_only(editor_bot):
 
 
 def test_range_indicator_alt_modifier_response(editor_bot):
+    """Test that the slider range indicator is visible while the alt key is
+    held down while the cursor is over the editor, but outside of the
+    scrollflag area. In addition, while the alt key is held down, mouse
+    click events in the editor should be forwarded to the scrollfag area and
+    should set the value of the editor vertical scrollbar."""
     qtbot, editor = editor_bot
     sfa = editor.scrollflagarea
     sfa._unit_testing = True
     vsb = editor.verticalScrollBar()
 
-    editor.show()
-    editor.set_text(long_code)
-
-    # Resize the editor and check that the slider is visible.
     editor.resize(600, 300)
+    editor.show()
+
+    # Set a long text in the editor and assert that the slider is visible.
+    editor.set_text(long_code)
     qtbot.waitUntil(lambda: sfa.slider)
 
+    # Set the cursor position to the center of the editor.
     w = editor.width()
     h = editor.height()
     qtbot.mousePress(editor, Qt.LeftButton, pos=QPoint(w/2, h/2))
 
+    # Hold the alt key and assert that the slider range indicator is visible.
     qtbot.keyPress(editor, Qt.Key_Alt)
     qtbot.waitUntil(lambda: sfa._range_indicator_is_visible)
 
+    # While the alt key is pressed, click with the mouse in the middle of the
+    # editor's height and assert that the editor vertical scrollbar has moved
+    # to its middle range position.
     with qtbot.waitSignal(editor.sig_alt_left_mouse_pressed, raising=True):
         qtbot.mousePress(editor.viewport(), Qt.LeftButton,
                          pos=QPoint(w/2, h/2), modifier=Qt.AltModifier)
     assert vsb.value() == (vsb.minimum()+vsb.maximum())//2
 
+    # While the alt key is pressed, click with the mouse at the top of the
+    # editor's height and assert that the editor vertical scrollbar has moved
+    # to its minimum position.
     with qtbot.waitSignal(editor.sig_alt_left_mouse_pressed, raising=True):
         qtbot.mousePress(editor.viewport(), Qt.LeftButton,
                          pos=QPoint(w/2, 1), modifier=Qt.AltModifier)
     assert vsb.value() == vsb.minimum()
 
+    # While the alt key is pressed, click with the mouse at the bottom of the
+    # editor's height and assert that the editor vertical scrollbar has moved
+    # to its maximum position.
     with qtbot.waitSignal(editor.sig_alt_left_mouse_pressed, raising=True):
         qtbot.mousePress(editor.viewport(), Qt.LeftButton,
                          pos=QPoint(w/2, h-1), modifier=Qt.AltModifier)
     assert vsb.value() == vsb.maximum()
 
+    # Release the alt key and assert that the slider range indicator is
+    # not visible.
     qtbot.keyRelease(editor, Qt.Key_Alt)
     qtbot.waitUntil(lambda: not sfa._range_indicator_is_visible, timeout=3000)
 
