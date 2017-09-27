f11906fb983e96f6714ca05ca5fbac8bdce283fe
dalthviz <d.althviz10@uniandes.edu.co>
2017-09-26 20:44:21 -0500

    Adds handling for FileNotFoundError when changing directories.

--- spyder/plugins/workingdirectory.py
+++ spyder/plugins/workingdirectory.py
@@ -283,11 +283,20 @@ class WorkingDirectory(QToolBar, SpyderPluginMixin):
             self.histindex = len(self.history)-1
         
         # Changing working directory
-        os.chdir(directory)
+        try:
+            FileNotFoundError
+        except NameError:
+            if os.name == 'nt':
+                FileNotFoundError = WindowsError
+            else:
+                FileNotFoundError = IOError
+        try:
+            os.chdir(directory)
+            if refresh_explorer:
+                self.set_explorer_cwd.emit(directory)
+            if refresh_console:
+                self.set_current_console_wd.emit(directory)
+            self.refresh_findinfiles.emit()
+        except FileNotFoundError:
+            self.history.pop(self.histindex)
         self.refresh_plugin()
-
-        if refresh_explorer:
-            self.set_explorer_cwd.emit(directory)
-        if refresh_console:
-            self.set_current_console_wd.emit(directory)
-        self.refresh_findinfiles.emit()
