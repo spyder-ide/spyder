fd9d4781a5b6217c45e19c2c8b61ce144fb47a1e
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2017-11-26 21:47:58 +0100

    update tour buttons with icons

--- spyder/utils/icon_manager.py
+++ spyder/utils/icon_manager.py
@@ -204,6 +204,12 @@ _qtaargs = {
     'module':                  [('spyder.circle-letter-m',), {'color': '#daa520'}],
     'no_match':                [('fa.circle',), {'color': 'gray'}],
     'no_match':                [('fa.circle',), {'color': 'gray'}],
+    # --- Spyder Tour --------------------------------------------------------
+    'tour.close':              [('fa.close',), {}],
+    'tour.home':               [('fa.fast-backward',), {}],
+    'tour.previous':           [('fa.backward',), {}],
+    'tour.next':               [('fa.forward',), {}],
+    'tour.end':                [('fa.fast-forward',), {}],
     # --- Third party plugins ------------------------------------------------
     'profiler':                [('fa.clock-o',), {}],
     'pylint':                  [('fa.search', 'fa.check'), {'options': [{}, {'offset': (0.125, 0.125), 'color': 'orange'}]}],
