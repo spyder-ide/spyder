7fdc9b91e12c5d624d0efdcde8a329b1b55b5080
C.A.M. Gerlach <widenetservices@gmail.com>
2017-12-17 06:43:02 -0500

    Display date/times in human readible format in Variable Explorer

--- spyder/widgets/variableexplorer/utils.py
+++ spyder/widgets/variableexplorer/utils.py
@@ -421,9 +421,12 @@ def value_to_display(value, minmax=False, level=0):
             display = value
             if level > 0:
                 display = u"'" + display + u"'"
-        elif (isinstance(value, NUMERIC_TYPES) or isinstance(value, bool) or
-              isinstance(value, datetime.date) or
-              isinstance(value, datetime.timedelta) or
+        # TODO: Implement full timedelta support in variable explorer
+        elif (isinstance(value, datetime.date) or 
+              isinstance(value, datetime.date)):
+            display = str(value)
+        elif (isinstance(value, NUMERIC_TYPES) or 
+              isinstance(value, bool) or
               isinstance(value, numeric_numpy_types)):
             display = repr(value)
         else:
