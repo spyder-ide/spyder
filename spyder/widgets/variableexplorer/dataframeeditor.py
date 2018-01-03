b24288108ba94ad134a6eef9743213e42a821d80
C.A.M. Gerlach <widenetservices@gmail.com>
2018-01-03 04:36:02 -0600

    Minor code cleanup to array and df setData functions

--- spyder/widgets/variableexplorer/dataframeeditor.py
+++ spyder/widgets/variableexplorer/dataframeeditor.py
@@ -362,7 +362,7 @@ class DataFrameModel(QAbstractTableModel):
             if isinstance(current_value, bool):
                 val = bool_false_check(val)
             supported_types = (bool,) + REAL_NUMBER_TYPES + COMPLEX_NUMBER_TYPES
-            if (isinstance(current_value, supported_types) or 
+            if (isinstance(current_value, supported_types) or
                     is_text_string(current_value)):
                 try:
                     self.df.iloc[row, column-1] = current_value.__class__(val)
