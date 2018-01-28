19c1dd0d18d39494781564bd7ee7c759cd26afa5
Carlos Cordoba <ccordoba12@gmail.com>
2018-01-28 13:25:10 -0500

    Help: Simplify call to Editor plugin

--- spyder/plugins/help.py
+++ spyder/plugins/help.py
@@ -902,7 +902,7 @@ class Help(SpyderPluginWidget):
     def render_sphinx_doc(self, doc, context=None):
         """Transform doc string dictionary to HTML and show it"""
         # Math rendering option could have changed
-        fname = self.parent().parent().editor.get_current_filename()
+        fname = self.main.editor.get_current_filename()
         dname = osp.dirname(fname)
         self._sphinx_thread.render(doc, context, self.get_option('math'),
                                    dname)
