53b90953fa0a6ae2990d18f98c119caf566cd5ca
Carlos Cordoba <ccordoba12@gmail.com>
2018-01-28 16:21:26 -0500

    Help: Improve readability of long conditions

--- spyder/plugins/help.py
+++ spyder/plugins/help.py
@@ -812,11 +812,11 @@ class Help(SpyderPluginWidget):
         index = self.source_combo.currentIndex()
         if hasattr(self.main, 'tabifiedDockWidgets'):
             # 'QMainWindow.tabifiedDockWidgets' was introduced in PyQt 4.5
-            if self.dockwidget and (force or self.dockwidget.isVisible()) \
-               and not self.ismaximized \
-               and (force or text != self._last_texts[index]):
+            if (self.dockwidget and (force or self.dockwidget.isVisible()) and
+                    not self.ismaximized and
+                    (force or text != self._last_texts[index])):
                 dockwidgets = self.main.tabifiedDockWidgets(self.dockwidget)
-                if (self.console.dockwidget not in dockwidgets and \
+                if (self.console.dockwidget not in dockwidgets and
                         self.ipyconsole is not None and
                         self.ipyconsole.dockwidget not in dockwidgets):
                     self.dockwidget.show()
