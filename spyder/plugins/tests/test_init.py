19f10ae1572a6ddf7c8d0f83fd6d63cd64cfd96b
C.A.M. Gerlach <widenetservices@gmail.com>
2018-01-29 23:34:26 -0600

    Create test file for base plugin classes, __init__.py

---
+++ spyder/plugins/tests/test_init.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © Spyder Project Contributors
+#
+# Licensed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+# -----------------------------------------------------------------------------
+
+"""Tests for the base plugin classes ('__init__.py')."""
+
+# Standard library imports
+import os
+try:
+    from unittest.mock import Mock
+except ImportError:
+    from mock import Mock  # Python 2
+
+# 3rd party imports
+import pytest
+from flaky import flaky
+
+
+if __name__ == "__main__":
+    pytest.main()
