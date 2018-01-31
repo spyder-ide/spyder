1942621ca24315443e2c60cc7f69a614f5fb0b21
C.A.M. Gerlach <widenetservices@gmail.com>
2018-01-30 18:26:19 -0600

    Increase timeouts and use flaky to ensure help tests pass reliably

--- spyder/plugins/tests/test_help.py
+++ spyder/plugins/tests/test_help.py
@@ -12,6 +12,7 @@ Tests for the Spyder `help` plugn, `help.py`.
 # Third party imports
 from qtpy.QtWebEngineWidgets import WEBENGINE
 import pytest
+from flaky import flaky
 
 # Local imports
 from spyder.plugins.help import Help
@@ -54,6 +55,7 @@ def check_text(widget, text):
         return text in widget.toHtml()
 
 
+@flaky(max_runs=3)
 def test_no_docs_message(help_plugin, qtbot):
     """
     Test that no docs message is shown when instrospection plugins
@@ -62,9 +64,10 @@ def test_no_docs_message(help_plugin, qtbot):
     help_plugin.render_sphinx_doc(default_info_response())
     qtbot.waitUntil(lambda: check_text(help_plugin._webpage,
                                        "No documentation available"),
-                    timeout=2000)
+                    timeout=4000)
 
 
+@flaky(max_runs=3)
 def test_no_further_docs_message(help_plugin, qtbot):
     """
     Test that no further docs message is shown when instrospection
@@ -77,7 +80,7 @@ def test_no_further_docs_message(help_plugin, qtbot):
     help_plugin.render_sphinx_doc(info)
     qtbot.waitUntil(lambda: check_text(help_plugin._webpage,
                                        "No further documentation available"),
-                    timeout=2000)
+                    timeout=3000)
 
 
 if __name__ == "__main__":
