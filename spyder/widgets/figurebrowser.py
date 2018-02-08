e708f58259d7c7c6ed957499435ce995566299e4
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2018-02-08 17:56:53 -0500

    Implemetation of the FigureCanvas
    This is the most basic class of the figure explorer.

--- spyder/widgets/figurebrowser.py
+++ spyder/widgets/figurebrowser.py
@@ -37,3 +37,82 @@ from spyder.utils import icon_manager as ima
 from spyder.utils.qthelpers import (add_actions, create_action,
                                     create_toolbutton, create_plugin_layout)
 
+
+class FigureCanvas(QFrame):
+    """
+    A basic widget on which can be painted a custom png, jpg, or svg image.
+    """
+
+    def __init__(self, parent=None):
+        super(FigureCanvas, self).__init__(parent)
+        self.setLineWidth(2)
+        self.setMidLineWidth(1)
+        self.setStyleSheet("background-color: white")
+
+        self.fig = None
+        self.fmt = None
+        self.qpix = None
+        self._qpix_store = []
+        self.fwidth, self.fheight = 200, 200
+
+    def clear_canvas(self):
+        """Clear the figure that was painted on the widget."""
+        self.fig = None
+        self.fmt = None
+        self.qpix = None
+        self._qpix_store = []
+        self.repaint()
+
+    def load_figure(self, fig, fmt):
+        """
+        Loads the figure from a png, jpg, or svg image, converts it in
+        a QPixmap, and forces a repaint of the widget.
+        """
+        self.fig = fig
+        self.fmt = fmt
+
+        if fmt in ['image/png', 'image/jpeg']:
+            qimg = QImage()
+            qimg.loadFromData(fig, fmt.upper())
+        elif fmt == 'image/svg+xml':
+            raise NotImplementedError
+
+        self.qpix = QPixmap(qimg)
+        self.fwidth = self.qpix.width()
+        self.fheight = self.qpix.height()
+        self._qpix_store = []
+        self.repaint()
+
+    def save_figure_tofile(self, filename):
+        raise NotImplementedError
+
+    def paintEvent(self, event):
+        """Qt method override to paint a custom image on the Widget."""
+        super(FigureCanvas, self).paintEvent(event)
+        # Prepare the rect on which the image is going to be painted :
+
+        fw = self.frameWidth()
+        rect = QRect(0 + fw, 0 + fw,
+                     self.size().width() - 2 * fw,
+                     self.size().height() - 2 * fw)
+
+        # Check/update the image buffer :
+
+        qpix2paint = None
+        for qpix in self._qpix_store:
+            if qpix.size().width() == rect.width():
+                qpix2paint = qpix
+                break
+        else:
+            if self.qpix is not None:
+                qpix2paint = self.qpix.scaledToWidth(
+                    rect.width(), mode=Qt.SmoothTransformation)
+                self._qpix_store.append(qpix2paint)
+
+        if qpix2paint is not None:
+            # Paint the image on the widget :
+            qp = QPainter()
+            qp.begin(self)
+            qp.setRenderHint(QPainter.Antialiasing, True)
+            qp.drawPixmap(rect, qpix2paint)
+            qp.end()
