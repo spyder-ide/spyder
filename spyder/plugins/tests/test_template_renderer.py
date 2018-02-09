8c764c729ba951f191cfc6fa87cf1498a0cb70c7
chengtian5huang <34744187+chengtian5huang@users.noreply.github.com>
2018-02-09 15:16:55 +0800

    add tests to code for fix#6379

---
+++ spyder/plugins/tests/test_template_renderer.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+"""
+test for PR: #6380
+"""
+import pytest
+class TestClass():
+    TEXT_ALL_GOOD = '''
+    # -*- coding: utf-8 -*-
+    """
+    Created on %(date-spyder)s
+
+    Creator: %(username-spyder)s
+    """
+    ### requirements
+    ### TODO
+
+
+    if __name__ == '__main__':
+
+    '''
+    TEXT_BAD_IN_TRIQUOTE = '''
+    # -*- coding: utf-8 -*-
+    """
+    Created on %(date-spyder)s
+
+    Creator: %(its_bad)s
+    """
+    ### requirements
+    ### TODO
+
+
+    if __name__ == '__main__':
+
+    '''
+    TEXT_BAD_OUTSIDE = '''
+    # -*- coding: utf-8 -*-
+    """
+    Created on %(date-spyder)s
+
+    Creator: %(username-spyder)s
+    """
+    import logzero,logging
+    f = '%(color)s[%(levelname)s][%(funcName)s|%(lineno)s] -> %(message)s%(end_color)s'
+    cts_msg = logzero.setup_logger(level=logging.INFO,# change level here
+                                   formatter=logzero.LogFormatter(fmt=f))
+    LD = cts_msg.debug
+    LI = cts_msg.info
+    LE = cts_msg.error
+    ### requirements
+    ### TODO
+
+
+    if __name__ == '__main__':
+
+    '''
+    TEXT_BAD_EVERYWHERE = '''
+    # -*- coding: utf-8 -*-
+    """
+    Created on %(date-spyder)s
+
+    Creator: %(its_bad)s
+    """
+    import logzero,logging
+    f = '%(color)s[%(levelname)s][%(funcName)s|%(lineno)s] -> %(message)s%(end_color)s'
+    cts_msg = logzero.setup_logger(level=logging.INFO,# change level here
+                                   formatter=logzero.LogFormatter(fmt=f))
+    LD = cts_msg.debug
+    LI = cts_msg.info
+    LE = cts_msg.error
+    ### requirements
+    ### TODO
+
+
+    if __name__ == '__main__':
+
+    '''
+
+    def _render_linebyline(self, t, _VARS=None):
+        t = t.split('\n')
+        for i, line in enumerate(t):
+            try:
+                t[i] = line % _VARS
+            except Exception:
+                pass
+        return '\n'.join(t)
+
+    def new(self, text):
+        import time, os
+
+        VARS = {
+            'date-spyder': time.strftime("%Y-%m-%d"),
+            'username-spyder': os.getenv('USERNAME'),
+        }
+
+        try:
+            text = text % VARS
+        except (KeyError, TypeError):
+            text = self._render_linebyline(text, VARS)
+
+        return text
+
+    def test_all_good(self):
+        """all placeholders are vaild."""
+        """expecting full-replaced."""
+        result = self.new(self.TEXT_ALL_GOOD)
+        expected = '''
+    # -*- coding: utf-8 -*-
+    """
+    Created on 2018-02-09
+
+    Creator: cts
+    """
+    ### requirements
+    ### TODO
+
+
+    if __name__ == '__main__':
+
+    '''
+        assert result == expected
+
+    def test_bad_in_triquote(self):
+        """some placeholders in triquote are invaild."""
+        """expecting good ones been replaced, bad ones get ignored"""
+        result = self.new(self.TEXT_BAD_IN_TRIQUOTE)
+        expected = '''
+    # -*- coding: utf-8 -*-
+    """
+    Created on 2018-02-09
+
+    Creator: %(its_bad)s
+    """
+    ### requirements
+    ### TODO
+
+
+    if __name__ == '__main__':
+
+    '''
+        assert result == expected
+
+    def test_bad_outside(self):
+        """some placeholders outside triquote section are invaild."""
+        """expecting good ones been replaced, bad ones get ignored"""
+        result = self.new(self.TEXT_BAD_OUTSIDE)
+        expected = '''
+    # -*- coding: utf-8 -*-
+    """
+    Created on 2018-02-09
+
+    Creator: cts
+    """
+    import logzero,logging
+    f = '%(color)s[%(levelname)s][%(funcName)s|%(lineno)s] -> %(message)s%(end_color)s'
+    cts_msg = logzero.setup_logger(level=logging.INFO,# change level here
+                                   formatter=logzero.LogFormatter(fmt=f))
+    LD = cts_msg.debug
+    LI = cts_msg.info
+    LE = cts_msg.error
+    ### requirements
+    ### TODO
+
+
+    if __name__ == '__main__':
+
+    '''
+        assert result == expected
+
+    def test_bad_everywhere(self):
+        """invaild placeholders are in both inside and outside of triquote section."""
+        """expecting good ones been replaced, bad ones get ignored"""
+        result = self.new(self.TEXT_BAD_EVERYWHERE)
+        expected = '''
+    # -*- coding: utf-8 -*-
+    """
+    Created on 2018-02-09
+
+    Creator: %(its_bad)s
+    """
+    import logzero,logging
+    f = '%(color)s[%(levelname)s][%(funcName)s|%(lineno)s] -> %(message)s%(end_color)s'
+    cts_msg = logzero.setup_logger(level=logging.INFO,# change level here
+                                   formatter=logzero.LogFormatter(fmt=f))
+    LD = cts_msg.debug
+    LI = cts_msg.info
+    LE = cts_msg.error
+    ### requirements
+    ### TODO
+
+
+    if __name__ == '__main__':
+
+    '''
+        assert result == expected
+if __name__ == '__main__':
+    pytest.main()
