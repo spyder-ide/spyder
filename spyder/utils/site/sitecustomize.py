6e5d7d856bf264cea330ba26493f8e83361c7e9b
Carlos Cordoba <ccordoba12@gmail.com>
2018-03-05 11:11:37 -0500

    IPython console: Control if Cython features are turned on from our kernel spec

--- spyder/utils/site/sitecustomize.py
+++ spyder/utils/site/sitecustomize.py
@@ -167,33 +167,32 @@ if sys.platform == 'darwin':
 
 
 #==============================================================================
-# Add Cython files import and runfile support
+# Cython support
 #==============================================================================
-try:
-    # Import pyximport for enable Cython files support for import statement
-    import pyximport
-    HAS_PYXIMPORT = True
-    pyx_setup_args = {}
-except:
-    HAS_PYXIMPORT = False
+RUN_CYTHON = os.environ.get("SPY_RUN_CYTHON") == "True"
 
-if HAS_PYXIMPORT:
-    # Add Numpy include dir to pyximport/distutils
+if RUN_CYTHON:
     try:
-        import numpy
-        pyx_setup_args['include_dirs'] = numpy.get_include()
-    except:
-        pass
+        __import__('Cython')
+        HAS_CYTHON = True
+    except Exception:
+        HAS_CYTHON = False
 
-    # Setup pyximport and enable Cython files reload
-    pyximport.install(setup_args=pyx_setup_args, reload_support=True)
-    
-try:
-    # Import cython_inline for runfile function
-    from Cython.Build.Inline import cython_inline
-    HAS_CYTHON = True
-except:
-    HAS_CYTHON = False
+    if HAS_CYTHON:
+        # Import pyximport to enable Cython files support for
+        # import statement
+        import pyximport
+        pyx_setup_args = {}
+
+        # Add Numpy include dir to pyximport/distutils
+        try:
+            import numpy
+            pyx_setup_args['include_dirs'] = numpy.get_include()
+        except Exception:
+            pass
+
+        # Setup pyximport and enable Cython files reload
+        pyximport.install(setup_args=pyx_setup_args, reload_support=True)
 
 
 #==============================================================================
@@ -695,7 +694,7 @@ def runfile(filename, args=None, wdir=None, namespace=None, post_mortem=False):
         os.chdir(wdir)
     if post_mortem:
         set_post_mortem()
-    if HAS_CYTHON and os.path.splitext(filename)[1].lower() == '.pyx':
+    if HAS_CYTHON:
         # Cython files
         with io.open(filename, encoding='utf-8') as f:
             from IPython.core.getipython import get_ipython
