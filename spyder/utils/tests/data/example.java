8225a8e2628b5f576be049bf71575085de1f997d
Cheryl Sabella <cheryl.sabella@gmail.com>
2018-03-07 16:59:58 -0500

    issue 6664: Fix typo in get_words

--- spyder/utils/tests/data/example.java
+++ spyder/utils/tests/data/example.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
+        // test_underscore
         System.out.println("Hello, World");
     }
 
