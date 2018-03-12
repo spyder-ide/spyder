c2c669deadde6df468261717ef5b1210a4e46fa6
C.A.M. Gerlach <widenetservices@gmail.com>
2018-03-11 23:44:09 -0500

    Fix #5642 by catching AttributeError on some special types like xml

--- spyder/widgets/variableexplorer/collectionseditor.py
+++ spyder/widgets/variableexplorer/collectionseditor.py
@@ -77,12 +77,14 @@ class ProxyObject(object):
         return len(get_object_attrs(self.__obj__))
 
     def __getitem__(self, key):
-        """Get the attribute corresponding to given key."""
-        # Fix #6284 where pandas MultiIndex returns NotImplementedError
-        # Due to NA checking not being supported on a multiindex.
+        """Get the attribute corresponding to the given key."""
+        # Catch NotImplementedError to fix #6284 in pandas MultiIndex
+        # due to NA checking not being supported on a multiindex.
+        # Catch AttributeError to fix #5642 in certain special classes like xml
+        # when this method is called on certain attributes
         try:
             attribute_toreturn = getattr(self.__obj__, key)
-        except NotImplementedError:
+        except (NotImplementedError, AttributeError):
             attribute_toreturn = None
         return attribute_toreturn
 
