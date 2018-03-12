4a48d6cbf39809f02f91928d84f4743a5b76affe
C.A.M. Gerlach <widenetservices@gmail.com>
2018-03-12 03:08:42 -0500

    Catch TypeError to fix #6727 crash modifying Period _typ in ColEdt

--- spyder/widgets/variableexplorer/collectionseditor.py
+++ spyder/widgets/variableexplorer/collectionseditor.py
@@ -81,10 +81,12 @@ class ProxyObject(object):
         # Catch NotImplementedError to fix #6284 in pandas MultiIndex
         # due to NA checking not being supported on a multiindex.
         # Catch AttributeError to fix #5642 in certain special classes like xml
-        # when this method is called on certain attributes
+        # when this method is called on certain attributes.
+        # Catch TypeError to prevent fatal Python crash to desktop after
+        # modifying certain pandas objects ( Issue #6727 ).
         try:
             attribute_toreturn = getattr(self.__obj__, key)
-        except (NotImplementedError, AttributeError):
+        except (NotImplementedError, AttributeError, TypeError):
             attribute_toreturn = None
         return attribute_toreturn
 
