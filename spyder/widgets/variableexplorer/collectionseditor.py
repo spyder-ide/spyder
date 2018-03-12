02e01872bda1c093f7b650353cd4c9ac68a8ca01
C.A.M. Gerlach <widenetservices@gmail.com>
2018-03-12 03:16:16 -0500

    Catch AttributeError to fix #6728 when setting attribs in ColEdit

--- spyder/widgets/variableexplorer/collectionseditor.py
+++ spyder/widgets/variableexplorer/collectionseditor.py
@@ -94,9 +94,12 @@ class ProxyObject(object):
 
     def __setitem__(self, key, value):
         """Set attribute corresponding to key with value."""
+        # Catch AttributeError to gracefully handle inability to set an
+        # attribute due to it not being writeable or set-table.
+        # Fix issue #6728 . Also, catch NotImplementedError for safety.
         try:
             setattr(self.__obj__, key, value)
-        except TypeError:
+        except (TypeError, AttributeError, NotImplementedError):
             pass
 
 
