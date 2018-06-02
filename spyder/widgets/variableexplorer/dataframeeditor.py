928ac38cee21e84878d3443841280e066ef63123
Carlos Cordoba <ccordoba12@gmail.com>
2018-06-02 16:33:16 -0500

    Variable Explorer: Remove unused import

--- spyder/widgets/variableexplorer/dataframeeditor.py
+++ spyder/widgets/variableexplorer/dataframeeditor.py
@@ -34,8 +34,7 @@ from spyder.config.base import _
 from spyder.config.fonts import DEFAULT_SMALL_DELTA
 from spyder.config.gui import get_font, config_shortcut
 from spyder.py3compat import (io, is_text_string, is_type_text_string, PY2,
-                              to_text_string, TEXT_TYPES)
-from spyder.utils import encoding
+                              to_text_string)
 from spyder.utils import icon_manager as ima
 from spyder.utils.qthelpers import (add_actions, create_action,
                                     keybinding, qapplication)
