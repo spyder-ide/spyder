5d7945eed6c6bce41e10ed9a01abcef4bc825a4c
dalthviz <d.althviz10@uniandes.edu.co>
2018-06-05 00:15:34 -0500

    Add test for latin1 enconding.

--- spyder/utils/tests/test_codeanalysis.py
+++ spyder/utils/tests/test_codeanalysis.py
@@ -20,6 +20,21 @@ from spyder.utils.codeanalysis import (check_with_pep8, check_with_pyflakes,
 from spyder.py3compat import PY2
 
 TEST_FILE = os.path.join(os.path.dirname(__file__), 'data/example.py')
+TEST_FILE_LATIN = os.path.join(os.path.dirname(__file__),
+                               'data/example_latin1.py')
+
+
+def test_codeanalysis_latin():
+    """Test codeanalysis with pyflakes and pep8."""
+    code = open(TEST_FILE_LATIN).read()
+    check_results = (check_with_pyflakes(code, TEST_FILE)
+                     + check_with_pep8(code, TEST_FILE) + find_tasks(code))
+    if PY2:
+        num_results = 89
+    else:
+        num_results = 90
+    assert len(check_results) == num_results
+
 
 def test_codeanalysis():
     """Test codeanalysis with pyflakes and pep8."""
