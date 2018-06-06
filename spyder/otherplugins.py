68276186011d6623adb3c00b1b061ad80d4632c3
Carlos Cordoba <ccordoba12@gmail.com>
2018-06-06 17:19:30 -0500

    Catch any error when trying to load third-party plugins

--- spyder/otherplugins.py
+++ spyder/otherplugins.py
@@ -103,27 +103,20 @@ def _import_module_from_path(module_name, plugin_path):
     Return None if no module is found.
     """
     module = None
-    if PY2:
-        info = imp.find_module(module_name, [plugin_path])
-        if info:
-            module = imp.load_module(module_name, *info)
-    elif sys.version_info[0:2] <= (3, 3):
-        loader = importlib.machinery.PathFinder.find_module(
-            module_name,
-            [plugin_path])
-        if loader:
-            module = loader.load_module(module_name)
-    else:  # Python 3.4+
-        spec = importlib.machinery.PathFinder.find_spec(
-            module_name,
-            [plugin_path])
-        if spec:
-            # Needed to prevent an error when 'spec.loader' is None
-            # See issue 6518
-            try:
+    try:
+        if PY2:
+            info = imp.find_module(module_name, [plugin_path])
+            if info:
+                module = imp.load_module(module_name, *info)
+        else:  # Python 3.4+
+            spec = importlib.machinery.PathFinder.find_spec(
+                module_name,
+                [plugin_path])
+            if spec:
                 module = spec.loader.load_module(module_name)
-            except AttributeError:
-                module = None
+    except Exception:
+        pass
+
     return module
 
 
