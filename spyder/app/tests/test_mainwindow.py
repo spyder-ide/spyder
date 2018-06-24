a6a5caec5759a9b35fd3316472caa91ecc741f10
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2018-06-24 01:53:57 -0500

    text_get_help: Fixed

--- spyder/app/tests/test_mainwindow.py
+++ spyder/app/tests/test_mainwindow.py
@@ -264,7 +264,7 @@ def test_filter_numpy_warning(main_window, qtbot):
     CONF.set('variable_explorer', 'minmax', False)
 
 
-@pytest.mark.slow
+# @pytest.mark.slow
 @flaky(max_runs=3)
 @pytest.mark.use_introspection
 def test_get_help(main_window, qtbot):
@@ -295,9 +295,9 @@ def test_get_help(main_window, qtbot):
 
     # --- From the editor ---
     qtbot.wait(3000)
-    # config_status = main_window.lspmanager.clients['python']['status']
-    # if config_status == main_window.lspmanager.RUNNING:
-    #     main_window.lspmanager.close_client('python')
+    config_status = main_window.lspmanager.clients['python']['status']
+    if config_status == main_window.lspmanager.RUNNING:
+        main_window.lspmanager.close_client('python')
     with qtbot.waitSignal(main_window.editor.sig_lsp_notification,
                           timeout=30000):
         main_window.editor.new(fname="test.py", text="")
