dd1802248b525c9b993a3d7c8a3b50c2d0b9f043
Carlos Cordoba <ccordoba12@gmail.com>
2018-07-25 09:47:32 -0500

    IPython console: Don't save pdb commands to history by default

--- spyder/widgets/ipythonconsole/debugging.py
+++ spyder/widgets/ipythonconsole/debugging.py
@@ -76,13 +76,9 @@ class DebuggingWidget(RichJupyterWidget):
             # Save history to browse it later
             if not (len(self._control.history) > 0
                     and self._control.history[-1] == line):
-                ignore_pdb_cmd_setting = True
-                if ignore_pdb_cmd_setting:
-                    # do not save pdb commands
-                    cmd = line.split(" ")[0]
-                    if "do_" + cmd not in dir(pdb.Pdb):
-                        self._control.history.append(line)
-                else:
+                # do not save pdb commands
+                cmd = line.split(" ")[0]
+                if "do_" + cmd not in dir(pdb.Pdb):
                     self._control.history.append(line)
 
             # This is the Spyder addition: add a %plot magic to display
