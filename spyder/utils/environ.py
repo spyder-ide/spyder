0d7a369746ca8faa52562b035010b33234bafd88
Carlos Cordoba <ccordoba12@gmail.com>
2018-08-23 19:53:32 -0500

    Fix style issue

--- spyder/utils/environ.py
+++ spyder/utils/environ.py
@@ -43,16 +43,22 @@ class RemoteEnvDialog(CollectionsEditor):
     def __init__(self, environ, parent=None):
         super(RemoteEnvDialog, self).__init__(parent)
         try:
-            self.setup(envdict2listdict(environ),
-                    title=_("Environment variables"),
-                    width=700,
-                    readonly=True,
-                    icon=ima.icon('environ'))
+            self.setup(
+                envdict2listdict(environ),
+                title=_("Environment variables"),
+                width=700,
+                readonly=True,
+                icon=ima.icon('environ')
+            )
         except Exception as e:
-            QMessageBox.warning(parent, _("Warning"),
-                    _("An error occurred while trying to show your "
-                      "environment variables. The error was<br><br>"
-                      "<tt>{0}</tt>").format(e), QMessageBox.Ok)
+            QMessageBox.warning(
+                parent,
+                _("Warning"),
+                _("An error occurred while trying to show your "
+                  "environment variables. The error was<br><br>"
+                  "<tt>{0}</tt>").format(e),
+                QMessageBox.Ok
+            )
 
 
 class EnvDialog(RemoteEnvDialog):
