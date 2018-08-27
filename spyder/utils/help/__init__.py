d79df69d30e5c6ce77fb98fa2a7d73b4019625db
C.A.M. Gerlach <widenetservices@gmail.com>
2018-08-26 23:14:58 -0500

    Add LICENSE.txt and __init__ headers in Help utils directory

--- spyder/utils/help/__init__.py
+++ spyder/utils/help/__init__.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2009- Spyder Project Contributors and others (see LICENSE.txt)
 #
-# Copyright © Spyder Project Contributors
-# Licensed under the terms of the MIT or BSD Licenses
-# (See every file for its license)
+# Licensed under the terms of the MIT and other licenses where noted
+# (see LICENSE.txt in this directory and NOTICE.txt in the root for details)
+# -----------------------------------------------------------------------------
 
 """
 spyder.utils.help
 =================
 
-Configuration files for the Help plugin rich text mode
+Configuration files for the Help plugin rich text mode.
+
+See their headers, LICENSE.txt in this directory or NOTICE.txt for licenses.
 """
 
 import sys
