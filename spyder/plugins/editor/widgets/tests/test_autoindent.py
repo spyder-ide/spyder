f4ddc0b11db57b68ce42cf60d1960ba6933659b3
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-08-30 11:33:22 -0400

    Test @pytest.mark.parametrize with loops instead
    This removes about 110 unnecessary entries in our test logs, while
    providing the same coverage and info when a test fails.

--- spyder/plugins/editor/widgets/tests/test_autoindent.py
+++ spyder/plugins/editor/widgets/tests/test_autoindent.py
@@ -149,69 +149,77 @@ def test_first_line():
     assert text == "import numpy", repr(text)
 
 
-@pytest.mark.parametrize(
-    "text_input, expected, test_text",
-    [
+def test_indentation_with_spaces():
+    text_input_expected_comment = [
         ("tags = ['(a)', '(b)', '(c)']\n", "tags = ['(a)', '(b)', '(c)']\n",
          "test_commented_brackets"),
-        ("s = a[(a['a'] == l) & (a['a'] == 1)]['a']\n", "s = a[(a['a'] == l) & (a['a'] == 1)]['a']\n",
+        ("s = a[(a['a'] == l) & (a['a'] == 1)]['a']\n",
+         "s = a[(a['a'] == l) & (a['a'] == 1)]['a']\n",
          "test_balanced_brackets"),
-        ("a = (a  #  some comment\n", "a = (a  #  some comment\n     ", "test_inline_comment"),
-        ("len(a) == 1\n", "len(a) == 1\n", "test_balanced_brackets_not_ending_in_bracket"),
-        ("x = f(\n", "x = f(\n      ", "test_short_open_bracket_not_hanging_indent"),
-        ("def some_func():\n    return 10\n", "def some_func():\n    return 10\n",
+        ("a = (a  #  some comment\n", "a = (a  #  some comment\n     ",
+         "test_inline_comment"),
+        ("len(a) == 1\n", "len(a) == 1\n",
+         "test_balanced_brackets_not_ending_in_bracket"),
+        ("x = f(\n", "x = f(\n      ",
+         "test_short_open_bracket_not_hanging_indent"),
+        ("def some_func():\n    return 10\n",
+         "def some_func():\n    return 10\n",
          "test_return"),
-        ("def some_func():\n    returns = 10\n", "def some_func():\n    returns = 10\n    ",
+        ("def some_func():\n    returns = 10\n",
+         "def some_func():\n    returns = 10\n    ",
          "test_return_not_keyword"),
         ("foo = 1  # Comment open parenthesis (\n",
-             "foo = 1  # Comment open parenthesis (\n",
-             "test_comment_with parenthesis"),
-    ])
-def test_indentation_with_spaces(text_input, expected, test_text):
-    text = get_indent_fix(text_input)
-    assert text == expected, test_text
+         "foo = 1  # Comment open parenthesis (\n",
+         "test_comment_with parenthesis")
+        ]
+    for text_input, expected, comment in text_input_expected_comment:
+        text = get_indent_fix(text_input)
+        assert text == expected, comment
+
 
 def test_def_with_unindented_comment():
     text = get_indent_fix("def function():\n# Comment\n")
     assert text == "def function():\n# Comment\n    ", repr(text)
 
 
-@pytest.mark.parametrize("tab_stop_width_spaces", [1,2,3,4,5,6,7,8])
-@pytest.mark.parametrize(
-    "text_input, expected, test_text",
-    [
 # ---- Tabs tests
+def test_indentation_with_tabs():
+    text_input_expected_comment = [
         ("this_tuple = (1, 2)\n", "this_tuple = (1, 2)\n", "simple tuple"),
         ("\ndef function():\n", "\ndef function():\n\t", "def with new line"),
         ("def function():\n\t# Comment\n", "def function():\n\t# Comment\n\t",
          "test with indented comment"),
         ("def function():\n\tprint []\n", "def function():\n\tprint []\n\t",
          "test brackets alone"),
-        ("\nsome_long_name = {\n", "\nsome_long_name = {\n\t\t", "indentation after opening bracket"),
+        ("\nsome_long_name = {\n", "\nsome_long_name = {\n\t\t",
+         "indentation after opening bracket"),
         ("def function():\n", "def function():\n\t", "test simple def"),
         ("open_parenthesis(\n", "open_parenthesis(\n\t\t",
          "open parenthesis"),
         ("tags = ['(a)', '(b)', '(c)']\n", "tags = ['(a)', '(b)', '(c)']\n",
          "test_commented_brackets"),
-        ("s = a[(a['a'] == l) & (a['a'] == 1)]['a']\n", "s = a[(a['a'] == l) & (a['a'] == 1)]['a']\n",
+        ("s = a[(a['a'] == l) & (a['a'] == 1)]['a']\n",
+         "s = a[(a['a'] == l) & (a['a'] == 1)]['a']\n",
          "test_balanced_brackets"),
         ("def some_func():\n\treturn 10\n", "def some_func():\n\treturn 10\n",
          "test_return"),
-        ("def some_func():\n\treturns = 10\n", "def some_func():\n\treturns = 10\n\t",
+        ("def some_func():\n\treturns = 10\n",
+         "def some_func():\n\treturns = 10\n\t",
          "test_return_not_keyword"),
         ("def function():\n# Comment\n", "def function():\n# Comment\n\t",
-             "test_def_with_unindented_comment"),
-    ])
-def test_indentation_with_tabs(text_input, expected, test_text,
-                               tab_stop_width_spaces):
-    text = get_indent_fix(text_input, indent_chars="\t",
-                          tab_stop_width_spaces=tab_stop_width_spaces)
-    assert text == expected, test_text
+         "test_def_with_unindented_comment")
+        ]
+
+    for text_input, expected, comment in text_input_expected_comment:
+        for tab_stop_width_spaces in [1, 2, 3, 4, 5, 6, 7, 8]:
+            text = get_indent_fix(text_input, indent_chars="\t",
+                                  tab_stop_width_spaces=tab_stop_width_spaces)
+            assert text == expected, comment
 
 
-@pytest.mark.parametrize(
-    "text_input, expected, tab_stop_width_spaces",
-    [
+def test_indentation_with_tabs_parenthesis():
+    """Simple parenthesis indentation test with different tab stop widths."""
+    text_input_expected_tab = [
         ("print(\n)", "print(\n\t\t)", 1),
         ("print(\n)", "print(\n\t\t)", 2),
         ("print(\n)", "print(\n\t\t)", 3),
@@ -220,45 +228,36 @@ def test_indentation_with_tabs(text_input, expected, test_text,
         ("print(\n)", "print(\n\t)", 6),
         ("print(\n)", "print(\n      )", 7),
         ("print(\n)", "print(\n      )", 8),
-        ("a = (a  #  some comment\n", "a = (a  #  some comment\n\t ", 4),
-    ])
-def test_indentation_with_tabs_parenthesis(text_input, expected,
-                                           tab_stop_width_spaces):
-    """Simple parenthesis indentation test with different tab stop widths."""
-    text = get_indent_fix(text_input, indent_chars="\t",
-                          tab_stop_width_spaces=tab_stop_width_spaces)
-    assert text == expected, tab_stop_width_spaces
+        ("a = (a  #  some comment\n", "a = (a  #  some comment\n\t ", 4)
+        ]
+    for text_input, expected, tab_stop_width_spaces in text_input_expected_tab:
+        text = get_indent_fix(text_input, indent_chars="\t",
+                              tab_stop_width_spaces=tab_stop_width_spaces)
+        assert text == expected, tab_stop_width_spaces
 
 
-@pytest.mark.parametrize("tab_stop_width_spaces", [1,2,3,4,5,6,7,8])
-@pytest.mark.parametrize(
-    "text_input, expected, test_text",
-    [
-        ("\tx = 1", "x = 1", "simple test"),
-    ])
-def test_unindentation_with_tabs(text_input, expected, test_text,
-                               tab_stop_width_spaces):
-    text = get_indent_fix(text_input, indent_chars="\t",
-                          tab_stop_width_spaces=tab_stop_width_spaces, 
-                          forward=False)
-    assert text == expected, test_text
+def test_unindentation_with_tabs():
+    text_input = "\tx = 1"
+    expected_text = "x = 1"
+    for tab_stop_width_spaces in [1, 2, 3, 4, 5, 6, 7, 8]:
+        text = get_indent_fix(text_input, indent_chars="\t",
+                              tab_stop_width_spaces=tab_stop_width_spaces,
+                              forward=False)
+        assert text == expected_text
 
 
-
-@pytest.mark.parametrize(
-    "text_input, expected, test_text",
-    [
 # ---- Simple indentation tests
+def test_simple_indentation():
+    # language None deactivate smart indentation
+    text_input_expected_comment = [
         ("hola\n", "hola\n", "witout indentation"),
         ("  hola\n", "  hola\n  ", "some indentation"),
         ("\thola\n", "\thola\n\t", "tab indentation"),
-        ("  hola(\n", "  hola(\n  ", "line with parenthesis"),
-    ])
-def test_simple_indentation(text_input, expected, test_text,):
-    # language None deactivate smart indentation
-    text = get_indent_fix(text_input, language=None)
-    assert text == expected, test_text
-
+        ("  hola(\n", "  hola(\n  ", "line with parenthesis")
+        ]
+    for text_input, text_expected, comment in text_input_expected_comment:
+        text = get_indent_fix(text_input, language=None)
+        assert text == text_expected, comment
 
 
 if __name__ == "__main__":
