13d849ad56edd55e8f303b30c9ebdf660acf894e
Carlos Cordoba <ccordoba12@gmail.com>
2018-09-09 13:32:11 -0500

    Find in Files: Use new default options for the main widget

--- spyder/widgets/findinfiles.py
+++ spyder/widgets/findinfiles.py
@@ -934,13 +934,13 @@ class FindInFilesWidget(QWidget):
 
     def __init__(self, parent,
                  search_text=r"# ?TODO|# ?FIXME|# ?XXX",
-                 search_text_regexp=True,
+                 search_text_regexp=False,
                  exclude="*.json, *.csv, *.js",
                  exclude_idx=None,
-                 exclude_regexp=True,
+                 exclude_regexp=False,
                  supported_encodings=("utf-8", "iso-8859-1", "cp1252"),
                  more_options=False,
-                 case_sensitive=True,
+                 case_sensitive=False,
                  external_path_history=[]):
         QWidget.__init__(self, parent)
 
