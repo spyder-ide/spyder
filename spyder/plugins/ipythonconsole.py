bfa4893dcb5e0d2af64ddaae3ee017171c9b19a6
Carlos Cordoba <ccordoba12@gmail.com>
2018-09-12 10:31:11 -0500

    IPython console: Remove unnecessary code to restart kernels

--- spyder/plugins/ipythonconsole.py
+++ spyder/plugins/ipythonconsole.py
@@ -1588,18 +1588,9 @@ class IPythonConsole(SpyderPluginWidget):
     def restart_kernel(self):
         """Restart kernel of current client."""
         client = self.get_current_client()
-        if self.test_no_stderr:
-            stderr_file = None
-        else:
-            stderr_file = client.stderr_file
 
         if client is not None:
-            stderr = self.get_stderr_file_handle(stderr_file)
-            try:
-                client.restart_kernel(stderr=stderr)
-            finally:
-                if stderr is not None:
-                    stderr.close()
+            client.restart_kernel()
 
     #------ Public API (for tabs) ---------------------------------------------
     def add_tab(self, widget, name, filename=''):
