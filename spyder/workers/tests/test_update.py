3df8861665ea07318b2f4ae7d9f305eb74c086a3
Carlos Cordoba <ccordoba12@gmail.com>
2018-09-16 13:56:53 -0500

    Testing: Add tests for a couple of prerelease cases

--- spyder/workers/tests/test_update.py
+++ spyder/workers/tests/test_update.py
@@ -6,35 +6,45 @@
 
 import pytest
 
-from spyder.utils.programs import check_version
 from spyder.workers.updates import WorkerUpdates
-
-# Max time to wait for the update check
-# (in miliseconds)
-UPDATE_TIMEOUT = 20000
 
 
 def test_update(qtbot):
-    """Test the update checking for a version that needs an update."""
+    """Test we offer updates for lower versions."""
     worker = WorkerUpdates(None, False, version="1.0.0")
     worker.start()
     assert worker.update_available
-    assert check_version("1.0.0", worker.latest_release, '<')
 
 
 def test_no_update(qtbot):
-    """Test the update checking for a version that don't needs an update."""
+    """Test we don't offer updates for very high versions."""
     worker = WorkerUpdates(None, False, version="1000.0.0")
     worker.start()
     assert not worker.update_available
-    assert not check_version("1000.0.0", worker.latest_release, '<')
 
 
 def test_no_update_development(qtbot):
     """Test we don't offer updates for development versions."""
-    worker = WorkerUpdates(None, False, version="3.3.2.dev0")
+    worker = WorkerUpdates(None, False, version="3.3.2.dev0",
+                           releases=['3.3.1'])
     worker.start()
     assert not worker.update_available
+
+
+def test_update_pre_to_pre(qtbot):
+    """Test we offer updates between prereleases."""
+    worker = WorkerUpdates(None, False, version="4.0.0a1",
+                           releases=['4.0.0b5'])
+    worker.start()
+    assert worker.update_available
+
+
+def test_update_pre_to_final(qtbot):
+    """Test we offer updates from prereleases to the final versions."""
+    worker = WorkerUpdates(None, False, version="4.0.0b3",
+                           releases=['4.0.0'])
+    worker.start()
+    assert worker.update_available
 
 
 if __name__ == "__main__":
