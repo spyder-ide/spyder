f290be6834e75a4f98a8198347f46b19e8a0b1f6
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-09-28 16:24:25 -0400

    When show_all_files, don't auto expand root

--- spyder/plugins/outlineexplorer/widgets.py
+++ spyder/plugins/outlineexplorer/widgets.py
@@ -475,13 +475,14 @@ class OutlineExplorerTreeWidget(OneColumnTree):
 
     def root_item_selected(self, item):
         """Root item has been selected: expanding it and collapsing others"""
-        for index in range(self.topLevelItemCount()):
-            root_item = self.topLevelItem(index)
+        if self.show_all_files:
+            return
+        for root_item in self.get_top_level_items():
             if root_item is item:
                 self.expandItem(root_item)
             else:
                 self.collapseItem(root_item)
-                
+
     def restore(self):
         """Reimplemented OneColumnTree method"""
         if self.current_editor is not None:
