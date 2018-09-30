6c09eb17ce5a6189381e41bcf14c4206ce028cdf
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2018-09-29 23:48:31 -0400

    Do not reorder file during setup

--- spyder/plugins/editor/plugin.py
+++ spyder/plugins/editor/plugin.py
@@ -2943,7 +2943,6 @@ class Editor(SpyderPluginWidget):
         self.close_all_files()
 
         if filenames and any([osp.isfile(f) for f in filenames]):
-            filenames = self.reorder_filenames(filenames)
             layout = self.get_option('layout_settings', None)
             is_vertical, cfname, clines = layout.get('splitsettings')[0]
             self.load(filenames, goto=clines)
@@ -2958,39 +2957,6 @@ class Editor(SpyderPluginWidget):
         else:
             self.__load_temp_file()
         self.set_create_new_file_if_empty(True)
-
-    def reorder_filenames(self, filenames):
-        """Take the last session filenames and put the last open on first.
-
-        It takes a list of filenames and using the current filename from the
-        layout settings, sets the one that had focused last in the position 0.
-        It also reorders the current lines for each file (supposing that they
-        are in the same order as the filenames) and sets them back in the
-        layout settings.
-        """
-        layout = self.get_option('layout_settings', None)
-        if layout is None:
-            return filenames
-        splitsettings = layout.get('splitsettings')
-        index_first_file = 0
-        reordered_splitsettings = []
-        for index, (is_vertical, cfname, clines) in enumerate(splitsettings):
-            #the first element of filenames is now the one that last had focus
-            if index == 0:
-                if cfname in filenames:
-                    index_first_file = filenames.index(cfname)
-                    filenames.pop(index_first_file)
-                    filenames.insert(0, cfname)
-                    clines_first_file = clines[index_first_file]
-                    clines.pop(index_first_file)
-                    clines.insert(0, clines_first_file)
-                else:
-                    cfname = filenames[0]
-                    index_first_file = 0
-            reordered_splitsettings.append((is_vertical, cfname, clines))
-        layout['splitsettings'] = reordered_splitsettings
-        self.set_option('layout_settings', layout)
-        return filenames
 
     def save_open_files(self):
         """Save the list of open files"""
