d8c4c7170c7a5d02586c13f94ec35ec73125fed8
Carlos Cordoba <ccordoba12@gmail.com>
2018-10-10 22:04:23 -0500

    Testing: Skip test in macOS and Python 3 because it's too slow

--- spyder/utils/introspection/tests/test_modulecompletion.py
+++ spyder/utils/introspection/tests/test_modulecompletion.py
@@ -8,13 +8,19 @@
 Tests for module_completion.py
 """
 
+# Stdlib imports
+import sys
+
 # Test library imports
 import pytest
 
 # Local imports
+from spyder.py3compat import PY3
 from spyder.utils.introspection.module_completion import get_preferred_submodules
 
 
+@pytest.mark.skipif(sys.platform == 'darwin' and PY3,
+                    reason="It's very slow in this combo")
 def test_module_completion():
     """Test module_completion."""
     assert 'numpy.linalg' in get_preferred_submodules()
