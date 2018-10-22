776337244a5805fa03d9911f4a043ab2fa573448
Vitoria Camilo <vitoriahmc@al.insper.edu.br>
2018-10-22 08:06:38 -0300

    switching import places for organization reasons

--- spyder/plugins/projects/widgets/tests/test_projectdialog.py
+++ spyder/plugins/projects/widgets/tests/test_projectdialog.py
@@ -9,14 +9,14 @@ Tests for projectdialog.py
 """
 
 # Standard library imports
-import pytest
+import os
 try:
     from unittest.mock import Mock
 except ImportError:
     from mock import Mock  # Python 2
 
 # Third party imports
-import os
+import pytest
 
 # Local imports
 from spyder.plugins.projects.widgets.projectdialog import ProjectDialog
