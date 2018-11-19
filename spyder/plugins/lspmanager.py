b0b4c046afcc62a1e6946cce7efbdf8f015b8f96
Randy Eckman <randy.eckman@nasa.gov>
2018-11-19 11:12:48 -0600

    forgot to include lspmanager in commit

--- spyder/plugins/lspmanager.py
+++ spyder/plugins/lspmanager.py
@@ -716,6 +716,10 @@ class LSPManager(SpyderPluginWidget):
 
     @Slot()
     def reinit_all_lsp_clients(self):
+        """
+        Sends a new initialize message to each LSP server when the project
+        path has changed so they can update the respective server root paths.
+        """
         for language_client in self.clients.values():
             if language_client['status'] == self.RUNNING:
                 folder = self.get_root_path()
