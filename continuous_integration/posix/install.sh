bd458a3656f55045722e606e235b68c79312d62c
Carlos Cordoba <ccordoba12@gmail.com>
2018-12-02 15:29:34 -0500

    Testing: Fix conda installation in macOS and wrong command in Appveyor

--- continuous_integration/posix/install.sh
+++ continuous_integration/posix/install.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# Install Miniconda
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+# -- Install Miniconda
+if [ $(uname) == Linux ]; then
+
+    MINICONDA=Miniconda3-latest-Linux-x86_64.sh
+else
+    MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh
+fi
+wget https://repo.continuum.io/miniconda/$MINICONDA -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 source $HOME/miniconda/etc/profile.d/conda.sh
 
-# Make new conda environment with required Python version
+
+# -- Make new conda environment with required Python version
 conda create -y -n test python=$PYTHON_VERSION
 conda activate test
 
 
+# -- Installl dependencies
 if [ "$USE_CONDA" = "yes" ]; then
     # Install main dependencies
     conda install -q -y -c spyder-ide --file requirements/conda.txt
