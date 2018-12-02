4f8aa0593a87d945331a84a7e00370ffe049ac47
Carlos Cordoba <ccordoba12@gmail.com>
2018-12-02 15:01:25 -0500

    Testing: Simplify installation in Travis and Circle

--- continuous_integration/posix/install.sh
+++ continuous_integration/posix/install.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-if [ "$CIRCLECI" = "true" ]; then
-    export TRAVIS_OS_NAME="linux"
-fi
-
-if [ "$USE_CONDA" = "no" ]; then
-    export PIP_DEPENDENCIES_FLAGS="-q"
-    export PIP_DEPENDENCIES="coveralls"
-    export CONDA_DEPENDENCIES=""
-else
-    export CONDA_DEPENDENCIES_FLAGS="--quiet"
-    export CONDA_DEPENDENCIES="rope pyflakes sphinx pygments pylint psutil nbconvert \
-                               qtawesome cloudpickle pickleshare qtpy pyzmq chardet mock nomkl pandas \
-                               pytest pytest-cov numpydoc scipy cython pillow jedi pycodestyle sympy \
-                               keyring pexpect atomicwrites"
-    export PIP_DEPENDENCIES="coveralls pytest-qt pytest-mock flaky \
-                             python-language-server[all] qdarkstyle"
-fi
-
-
-# Download and install miniconda and conda/pip dependencies
-# with astropy helpers
-echo -e "PYTHON = $TRAVIS_PYTHON_VERSION \n============"
-git clone git://github.com/astropy/ci-helpers.git > /dev/null
-source ci-helpers/travis/setup_conda_$TRAVIS_OS_NAME.sh
+# Install Miniconda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
 source $HOME/miniconda/etc/profile.d/conda.sh
+
+# Make new conda environment with required Python version
+conda create -y -n test python=$PYTHON_VERSION
 conda activate test
 
 
-if [ "$USE_CONDA" = "no" ]; then
+if [ "$USE_CONDA" = "yes" ]; then
+    # Install main dependencies
+    conda install -q -y -c spyder-ide --file requirements/conda.txt
+
+    # Install test ones
+    conda install -q -y -c spyder-ide --file requirements/tests.txt
+
+    # Install spyder-kernels from Github with no deps
+    pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels
+else
+    # Install Spyder and its dependencies from our setup.py
+    pip install -e .[test]
+
+    # Remove pytest-xvfb because it causes hangs
+    pip uninstall -q -y pytest-xvfb
+
+    # Install qtpy from Github
+    pip install git+https://github.com/spyder-ide/qtpy.git
+
     # Install qtconsole from Github
     pip install git+https://github.com/jupyter/qtconsole.git
 
-    # Install Spyder and its dependencies
-    pip install -e .[test]
+    # Install spyder-kernels from Github
+    pip install -q git+https://github.com/spyder-ide/spyder-kernels
 fi
