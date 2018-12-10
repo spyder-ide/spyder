5e696c061165f3a61d2682957f52386a521da425
Carlos Cordoba <ccordoba12@gmail.com>
2018-12-10 09:51:31 -0500

    Testing: Run codecov in Azure and start testing on Windows/Python 3.7

--- continuous_integration/azure/install.bat
+++ continuous_integration/azure/install.bat
@@ -4,3 +4,6 @@ conda install -q -y -c spyder-ide --file requirements/tests.txt
 
 :: Install spyder-kernels from master
 pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels
+
+:: Install codecov
+pip install -q codecov
