cf4ac5193481f3d24eb9d4c4f41d693e76d54f43
Khalil Al Hooti <hooti@squ.edu.om>
2018-12-20 19:24:50 +0000

    Update indentationguides.py
    This resolves the issue resolve issue Indent guides going too far down #7339

--- spyder/plugins/editor/panels/indentationguides.py
+++ spyder/plugins/editor/panels/indentationguides.py
@@ -14,6 +14,7 @@ from qtpy.QtGui import QPainter, QColor
 from spyder.plugins.editor.utils.editor import TextBlockHelper
 from spyder.api.panel import Panel
 
+
 class IndentationGuide(Panel):
     """Indentation guides to easy identify nested blocks."""
 
@@ -28,6 +29,7 @@ class IndentationGuide(Panel):
         self.color = Qt.darkGray
         self.i_width = 4
 
+
     def paintEvent(self, event):
         """Override Qt method."""
         painter = QPainter(self)
@@ -35,15 +37,52 @@ class IndentationGuide(Panel):
         color = QColor(self.color)
         color.setAlphaF(.5)
         painter.setPen(color)
+        
+        # offset taken from :https://github.com/ninja-ide/ninja-ide/blob/
+        # master/ninja_ide/gui/editor/extensions/indentation_guides.py
+        offset = self.editor.document().documentMargin() + \
+                 self.editor.contentOffset().x()
 
-        for top, line_number, block in self.editor.visible_blocks:
+        for _, line_number, block in self.editor.visible_blocks:
+
+            indentation = TextBlockHelper.get_fold_lvl(block)
+            ref_lvl = indentation
+            block = block.next()
+            last_line = block.blockNumber()
+            lvl = TextBlockHelper.get_fold_lvl(block)
+            if ref_lvl == lvl:  # for zone set programmatically such as imports
+                # in pyqode.python
+                ref_lvl -= 1
+
+            while (block.isValid() and
+                   TextBlockHelper.get_fold_lvl(block) > ref_lvl):
+                last_line = block.blockNumber()
+                block = block.next()
+
+            end_of_sub_fold = block
+            if last_line:
+                block = block.document().findBlockByNumber(last_line)
+                while block.blockNumber() and block.text().strip() == '':
+                    block = block.previous()
+                    last_line = block.blockNumber()
+
+            block = self.editor.document().findBlockByNumber(line_number)
+            top = int(self.editor.blockBoundingGeometry(block).translated(
+                self.editor.contentOffset()).top())
             bottom = top + int(self.editor.blockBoundingRect(block).height())
-
             indentation = TextBlockHelper.get_fold_lvl(block)
 
             for i in range(1, indentation):
-                x = self.editor.fontMetrics().width(i * self.i_width * '9')
-                painter.drawLine(x, top, x, bottom)
+
+                if (line_number > last_line and
+                        TextBlockHelper.get_fold_lvl(end_of_sub_fold.next())
+                         <= i + 1):
+                    continue
+
+                else:
+                    x = self.editor.fontMetrics().width(i * self.i_width *
+                                                        '9') + offset
+                    painter.drawLine(x, top, x, bottom)
 
     # --- Other methods
     # -----------------------------------------------------------------
