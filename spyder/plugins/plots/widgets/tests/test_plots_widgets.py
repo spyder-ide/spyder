e3a4d4e6c99179082c393a6dddc6da4d4d8d23c3
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-01-05 02:08:18 -0500

    Add imports

--- spyder/plugins/plots/widgets/tests/test_plots_widgets.py
+++ spyder/plugins/plots/widgets/tests/test_plots_widgets.py
@@ -10,3 +10,18 @@
 Tests for the widgets used in the Plots plugin.
 """
 
+# Standard library imports
+import os.path as osp
+try:
+    from unittest.mock import Mock
+except ImportError:
+    from mock import Mock  # Python 2
+
+# Third party imports
+import pytest
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Local imports
+from spyder.plugins.plots.widgets.figurebrowser import FigureBrowser
+
