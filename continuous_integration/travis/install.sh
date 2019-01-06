4f8dfaf91bdb0e6759ab740552dd6cfceadf22d7
Carlos Cordoba <ccordoba12@gmail.com>
2019-01-06 17:23:47 -0500

    Testing: Backport master changes to Travis and Circle

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
 
-if [ "$CIRCLECI" = "true" ]; then
-    export TRAVIS_OS_NAME="linux"
-fi
-
-if [ "$USE_CONDA" = "no" ]; then
-    export PIP_DEPENDENCIES_FLAGS="-q"
-    export PIP_DEPENDENCIES="coveralls"
-    export CONDA_DEPENDENCIES=""
-else
-    export CONDA_DEPENDENCIES_FLAGS="--quiet"
-    export CONDA_DEPENDENCIES="rope pyflakes sphinx pygments pylint psutil nbconvert \
-                               qtawesome cloudpickle pickleshare qtpy pyzmq chardet mock nomkl pandas \
-                               pytest pytest-cov numpydoc scipy cython pillow jedi pycodestyle keyring"
-    export PIP_DEPENDENCIES="coveralls pytest-qt pytest-mock flaky"
-fi
-
-
-# Download and install miniconda and conda/pip dependencies
-# with astropy helpers
-echo -e "PYTHON = $TRAVIS_PYTHON_VERSION \n============"
-git clone git://github.com/astropy/ci-helpers.git > /dev/null
-source ci-helpers/travis/setup_conda_$TRAVIS_OS_NAME.sh
+# -- Install Miniconda
+MINICONDA=Miniconda3-latest-Linux-x86_64.sh
+wget https://repo.continuum.io/miniconda/$MINICONDA -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
 source $HOME/miniconda/etc/profile.d/conda.sh
+
+
+# -- Make new conda environment with required Python version
+conda create -y -n test python=$PYTHON_VERSION
 conda activate test
 
 
-if [ "$USE_CONDA" = "no" ]; then
+# -- Installl dependencies
+if [ "$USE_CONDA" = "yes" ]; then
+    # Install nomkl to avoid installing Intel MKL libraries
+    conda install -q -y nomkl
+
+    # Install main dependencies
+    conda install -q -y -c spyder-ide --file requirements/conda.txt
+
+    # Install test ones
+    conda install -q -y -c spyder-ide --file requirements/tests.txt
+
+    # Install coveralls
+    pip install -q coveralls
+
+    # Install spyder-kernels from Github with no deps
+    pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels@0.x
+else
+    # Install Spyder and its dependencies from our setup.py
+    pip install -e .[test]
+
+    # Remove pytest-xvfb because it causes hangs
+    pip uninstall -q -y pytest-xvfb
+
+    # Install qtpy from Github
+    pip install git+https://github.com/spyder-ide/qtpy.git
+
     # Install qtconsole from Github
     pip install git+https://github.com/jupyter/qtconsole.git
 
-    # Install Spyder and its dependencies
-    pip install -e .[test]
+    # Install spyder-kernels from Github
+    pip install -q git+https://github.com/spyder-ide/spyder-kernels@0.x
+
+    # Install coveralls
+    pip install -q coveralls
 fi
