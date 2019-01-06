99c86c91960410a418c5f91f8fa918b0951c4290
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-01-05 21:40:05 -0500

    Make filterAcceptsRow case insensitive on Windows

--- spyder/plugins/explorer/widgets.py
+++ spyder/plugins/explorer/widgets.py
@@ -970,18 +970,19 @@ class ProxyModel(QSortFilterProxyModel):
     def sort(self, column, order=Qt.AscendingOrder):
         """Reimplement Qt method"""
         self.sourceModel().sort(column, order)
-        
+
     def filterAcceptsRow(self, row, parent_index):
         """Reimplement Qt method"""
         if self.root_path is None:
             return True
         index = self.sourceModel().index(row, 0, parent_index)
-        path = osp.normpath(to_text_string(self.sourceModel().filePath(index)))
-        if self.root_path.startswith(path):
+        path = osp.normcase(osp.normpath(
+            to_text_string(self.sourceModel().filePath(index))))
+        if osp.normcase(self.root_path).startswith(path):
             # This is necessary because parent folders need to be scanned
             return True
         else:
-            for p in self.path_list:
+            for p in [osp.normcase(p) for p in self.path_list]:
                 if path == p or path.startswith(p+os.sep):
                     return True
             else:
