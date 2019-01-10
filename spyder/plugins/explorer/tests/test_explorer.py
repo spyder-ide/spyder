054df49d477f821956a80ee8c3935f3a7a7f6787
Khalil Al Hooti <hooti@squ.edu.om>
2019-01-10 14:58:41 +0000

    test for py3 only fail py2

--- spyder/plugins/explorer/tests/test_explorer.py
+++ spyder/plugins/explorer/tests/test_explorer.py
@@ -69,33 +69,31 @@ def create_folder_files(file_paths, project_dir):
     """Function to create folders and files to be used in test functions."""
     list_paths = []
     for item in file_paths:
-        #  ignore empty string
-        if item:
-            #  create folders
-            if osp.splitext(item)[1]:
-                if osp.split(item)[0]:
-                    dirs, fname = osp.split(item)
-                    os.makedirs(dirs, exist_ok=True)
-                    dirpath = osp.join(project_dir, dirs)
-                    item_path = osp.join(dirpath, fname)
-                else:
-                    item_path = osp.join(project_dir, item)
-            else:
-                dirpath = osp.join(project_dir, item)
+        if osp.splitext(item)[1]:
+            if osp.split(item)[0]:
+                dirs, fname = osp.split(item)
+                dirpath = osp.join(project_dir, dirs)
                 os.makedirs(dirpath, exist_ok=True)
-                item_path = dirpath
-            #  create files with some texts
-            if item_path.endswith('script.py'):
-                with open(item_path, 'w') as fh:
-                    fh.write('Python')
-            elif item_path.endswith('script1.py'):
-                with open(item_path, 'w') as fh:
-                    fh.write('Spyder4')
-            elif item_path.endswith('script2.py'):
-                with open(item_path, 'w') as fh:
-                    fh.write('Jan-2019')
-            #  append item path names to the list
-            list_paths.append(item_path)
+                item_path = osp.join(dirpath, fname)
+            else:
+                item_path = osp.join(project_dir, item)
+        else:
+            dirpath = osp.join(project_dir, item)
+            os.makedirs(dirpath, exist_ok=True)
+            item_path = dirpath
+        if item_path.endswith('script.py'):
+            with open(item_path, 'w') as fh:
+                fh.write('Python')
+        elif item_path.endswith('script1.py'):
+            with open(item_path, 'w') as fh:
+                fh.write('Spyder4')
+        elif item_path.endswith('script2.py'):
+            with open(item_path, 'w') as fh:
+                fh.write('Jan-2019')
+        elif item_path.endswith('text.txt'):
+            with open(item_path, 'w') as fh:
+                fh.write('Hello World')
+        list_paths.append(item_path)
     return list_paths
 
 
@@ -103,19 +101,16 @@ def create_folder_files(file_paths, project_dir):
 @pytest.mark.parametrize('file_paths',
                          [['script.py'],
                           ['script.py', 'script1.py', 'testdir/script2.py'],
-                          ['', 'testdir']])
+                          ['subdir/innerdir/text.txt', 'testdir']])
 def test_copy_path(project_explorer_with_files, path_method, file_paths):
     """Test copy absolute and relative paths."""
     project, cb = project_explorer_with_files
     project_dir = project.directory
     file_paths = create_folder_files(file_paths, project_dir)
     home_directory = project.explorer.treewidget.fsmodel.rootPath()
-    #  copy selected items [fnames]
     project.explorer.treewidget.copy_path(fnames=file_paths,
                                           method=path_method)
-    #  get the copied items from clipboard
     cb_output = cb.text(mode=cb.Clipboard)
-    #  create true path names simulating what copy_path function does
     file_paths = [_fn.replace(os.sep, '/') for _fn in file_paths]
     if len(file_paths) > 1:
         if path_method == 'absolute':
@@ -132,14 +127,13 @@ def test_copy_path(project_explorer_with_files, path_method, file_paths):
         elif path_method == 'relative':
             true_path = (osp.relpath(file_paths[0], home_directory).
                          replace(os.sep, '/'))
-    #  compare true results with clipboard data
     assert true_path == cb_output
 
 
 @pytest.mark.parametrize('file_paths',
                          [['script.py'],
                           ['script.py', 'script1.py', 'testdir/script2.py'],
-                          ['', 'testdir']])
+                          ['subdir/innerdir/text.txt', 'testdir']])
 def test_copy_file(project_explorer_with_files, file_paths):
     """Test copy file(s)/folders(s) to clipboard."""
     project, cb = project_explorer_with_files
@@ -170,40 +164,49 @@ def test_copy_file(project_explorer_with_files, file_paths):
 @pytest.mark.parametrize('file_paths',
                          [['script.py'],
                           ['script.py', 'script1.py', 'testdir/script2.py'],
-                          ['', 'testdir']])
+                          ['subdir/innerdir/text.txt', 'testdir']])
 def test_save_file(project_explorer_with_files, file_paths):
     """Test save file(s)/folders(s) from clipboard."""
     project = project_explorer_with_files[0]
     project_dir = project.directory
     file_paths = create_folder_files(file_paths, project_dir)
-    #  copy items
     project.explorer.treewidget.copy_file_clipboard(fnames=file_paths)
-    #  paste items at the selected fnames common directory
-    # project.explorer.treewidget.save_file_clipboard(fnames=file_paths)
-    # common_path = get_common_path(file_paths)
-    # if len(file_paths) == 1:
-    #     #  'script1.py' is already exists in the common path
-    #     assert osp.exists(osp.join(common_path, 'script2.py'))
-    #     with open(osp.join(common_path, 'script2.py'), 'r') as fh:
-    #         text_data = fh.read()
-    #     assert text_data == 'Python'
-#     if len(file_paths) == 3:
-#         #  'script2.py' is created before
-#         assert osp.exists(osp.join(common_path, 'script3.py'))
-#         with open(osp.join(common_path, 'script3.py'), 'r') as fh:
-#             text_data = fh.read()
-#         assert text_data == 'Python'
-#         #  script4.py is a copy of original 'script1.py'
-#         assert osp.exists(osp.join(common_path, 'script4.py'))
-#         with open(osp.join(common_path, 'script4.py'), 'r') as fh:
-#             text_data = fh.read()
-#         assert text_data == 'Spyder4'
-#         assert osp.exists(osp.join(common_path, 'script5.py'))
-#         with open(osp.join(common_path, 'script5.py'), 'r') as fh:
-#             text_data = fh.read()
-#         assert text_data == 'Jan-2019'
-#     if len(file_paths) == 2:
-#         assert osp.exists(osp.join(common_path, 'testdir1'))
+    project.explorer.treewidget.save_file_clipboard(fnames=file_paths)
+    try:
+        selected_item = osp.commonpath(file_paths)
+    except AttributeError:
+        selected_item = misc.get_common_path(file_paths)
+    if osp.isfile(selected_item):
+        parrent_path = osp.dirname(selected_item)
+    else:
+        parrent_path = osp.normpath(selected_item)
+    if len(file_paths) == 1:
+        #  there is only script.py in project_dir
+        assert osp.exists(osp.join(parrent_path, 'script1.py'))
+        with open(osp.join(parrent_path, 'script1.py'), 'r') as fh:
+            text_data = fh.read()
+        assert text_data == 'Python'
+    if len(file_paths) == 3:
+        #  'script.py', 'script1.py', 'testdir/script2.py' exists in pro dir
+        assert osp.exists(osp.join(parrent_path, 'script2.py'))
+        with open(osp.join(parrent_path, 'script2.py'), 'r') as fh:
+            text_data = fh.read()
+        assert text_data == 'Python'
+        assert osp.exists(osp.join(parrent_path, 'script3.py'))
+        with open(osp.join(parrent_path, 'script3.py'), 'r') as fh:
+            text_data = fh.read()
+        assert text_data == 'Spyder4'
+        assert osp.exists(osp.join(parrent_path, 'script4.py'))
+        with open(osp.join(parrent_path, 'script4.py'), 'r') as fh:
+            text_data = fh.read()
+        assert text_data == 'Jan-2019'
+    if len(file_paths) == 2:
+         #  'subdir/innerdir/text.txt' 'testdir' in proj dir
+        assert osp.isdir(osp.join(parrent_path, 'testdir'))
+        assert osp.isfile(osp.join(parrent_path, 'text.txt'))
+        with open(osp.join(parrent_path, 'text.txt'), 'r') as fh:
+            text_data = fh.read()
+        assert text_data == 'Hello World'
 
 
 if __name__ == "__main__":
