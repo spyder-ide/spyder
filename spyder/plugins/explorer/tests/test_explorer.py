910421c339f119653d8d32dcf1767930b0a6f6f2
Khalil Al Hooti <hooti@squ.edu.om>
2019-01-20 04:57:40 +0000

    add ability to delete long nested directories in windows

--- spyder/plugins/explorer/tests/test_explorer.py
+++ spyder/plugins/explorer/tests/test_explorer.py
@@ -8,12 +8,19 @@
 Tests for explorer.py
 """
 
-# Test library imports
+# Standard imports
+import os
+import os.path as osp
+
+# Third party imports
 import pytest
 
 # Local imports
 from spyder.plugins.explorer.widgets import (FileExplorerTest,
-                                             ProjectExplorerTest)
+                                             ProjectExplorerTest, QMessageBox)
+from spyder.py3compat import to_text_string
+from spyder.plugins.projects.widgets.explorer import ProjectExplorerTest as \
+    ProjectExplorerTest2
 
 
 @pytest.fixture
@@ -32,6 +39,48 @@ def project_explorer(qtbot):
     return widget
 
 
+@pytest.fixture(params=[['script.py', 'dir1/dir2/dir3/dir4/dir5/dir6/dir7'],
+                        ['script.py', 'script1.py', 'testdir/script2.py'],
+                        ['subdir/innerdir/dir3/text.txt',
+                         'dir1/dir2/dir3/dir4', 'dir1/dir2/dir3/file.txt',
+                         'dir1/dir2/dir3/dir4/dir5',
+                         'dir1/dir2/dir3/dir4/dir5/text.txt',
+                         'dir1/dir2/dir3/dir4/dir5/dir6/dir7/python.py']])
+def create_folders_files(tmpdir, request):
+    """A project directory with dirs and files for testing."""
+    project_dir = to_text_string(tmpdir.mkdir('project'))
+    list_paths = []
+    for item in request.param:
+        if osp.splitext(item)[1]:
+            if osp.split(item)[0]:
+                dirs, fname = osp.split(item)
+                dirpath = osp.join(project_dir, dirs)
+                if not osp.exists(dirpath):
+                    os.makedirs(dirpath)
+                    item_path = osp.join(dirpath, fname)
+            else:
+                item_path = osp.join(project_dir, item)
+        else:
+            dirpath = osp.join(project_dir, item)
+            if not osp.exists(dirpath):
+                os.makedirs(dirpath)
+                item_path = dirpath
+        if not osp.isdir(item_path):
+            with open(item_path, 'w') as fh:
+                fh.write("File Path:\n" + str(item_path) + '\n')
+        list_paths.append(item_path)
+    return list_paths, project_dir
+
+
+@pytest.fixture(params=[FileExplorerTest, ProjectExplorerTest2])
+def explorer_with_files(qtbot, create_folders_files, request):
+    """Setup Project/File Explorer widget."""
+    list_paths, project_dir = create_folders_files
+    project_explorer_orig = request.param(directory=project_dir)
+    qtbot.addWidget(project_explorer_orig)
+    return project_explorer_orig, list_paths
+
+
 def test_file_explorer(file_explorer):
     """Run FileExplorerTest."""
     file_explorer.resize(640, 480)
@@ -44,6 +93,15 @@ def test_project_explorer(project_explorer):
     project_explorer.resize(640, 480)
     project_explorer.show()
     assert project_explorer
+
+
+def test_delete_files_folders(explorer_with_files, mocker):
+    """Test delete file(s)/folders(s)."""
+    project, file_paths = explorer_with_files
+    mocker.patch.object(QMessageBox, 'warning', return_value=QMessageBox.Yes)
+    project.explorer.treewidget.delete(fnames=file_paths)
+    for item in file_paths:
+        assert not osp.exists(item)
 
 
 if __name__ == "__main__":
