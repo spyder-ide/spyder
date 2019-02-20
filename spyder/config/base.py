dc282fe112aa9fa81cc074500ffa354a7efe4d05
Carlos Cordoba <ccordoba12@gmail.com>
2019-02-20 08:45:09 +0100

    Avoid error when trying to save LANG_FILE

--- spyder/config/base.py
+++ spyder/config/base.py
@@ -345,8 +345,14 @@ def get_interface_language():
 
 def save_lang_conf(value):
     """Save language setting to language config file"""
-    with open(LANG_FILE, 'w') as f:
-        f.write(value)
+    # Needed to avoid an error when trying to save LANG_FILE
+    # but the operation fails for some reason.
+    # See issue 8807
+    try:
+        with open(LANG_FILE, 'w') as f:
+            f.write(value)
+    except EnvironmentError:
+        pass
 
 
 def load_lang_conf():
