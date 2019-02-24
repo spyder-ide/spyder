3902be086d94bc5c9e3255927fb0f819d46dfe14
C.A.M. Gerlach <widenetservices@gmail.com>
2019-02-23 23:45:02 -0600

    Add test that the correct config dir path is set with/out dev mode

--- spyder/config/tests/test_base.py
+++ spyder/config/tests/test_base.py
@@ -10,11 +10,18 @@
 Tests for the spyder.config.base module.
 """
 
+# Standard library imports
+import os.path as osp
+try:
+    from importlib import reload
+except ImportError:  # A builtin on Python 2
+    pass
+
 # Third party imports
 import pytest
 
 # Local imports
-from spyder.config.base import is_stable_version
+import spyder.config.base
 
 
 # ============================================================================
@@ -25,7 +32,22 @@ from spyder.config.base import is_stable_version
     ('3.3.2.dev0', False), ('beta', False), (('2', '0', 'alpha'), False)])
 def test_is_stable_version(version_input, expected_result):
     """Test that stable and non-stable versions are recognized correctly."""
-    assert is_stable_version(version_input) == expected_result
+    actual_result = spyder.config.base.is_stable_version(version_input)
+    assert actual_result == expected_result
+
+
+@pytest.mark.parametrize('use_dev_config_dir', [True, False])
+def test_get_conf_path(monkeypatch, use_dev_config_dir):
+    """Test that the config dir path is set under dev and release builds."""
+    monkeypatch.setenv('SPYDER_USE_DEV_CONFIG_DIR', str(use_dev_config_dir))
+    reload(spyder.config.base)
+    conf_path = spyder.config.base.get_conf_path()
+    assert conf_path
+    assert ((osp.basename(conf_path).split('-')[-1] == 'dev')
+            == use_dev_config_dir)
+    assert osp.isdir(conf_path)
+    monkeypatch.undo()
+    reload(spyder.config.base)
 
 
 if __name__ == '__main__':
