33ed48a325fbea805b11c090b15c3056c84c7399
Carlos Cordoba <ccordoba12@gmail.com>
2019-03-26 18:18:21 +0100

    Config: Make ropeFolder None to fix Rope completions in our end

--- spyder/config/main.py
+++ spyder/config/main.py
@@ -717,7 +717,7 @@ DEFAULTS = [
                                 },
                                 'rope': {
                                     'extensionModules': None,
-                                    'ropeFolder': []
+                                    'ropeFolder': None,
                                 },
                                 'rope_completion': {
                                     'enabled': False
@@ -766,7 +766,7 @@ DEFAULTS = [
 #    or if you want to *rename* options, then you need to do a MAJOR update in
 #    version, e.g. from 3.0.0 to 4.0.0
 # 3. You don't need to touch this value if you're just adding a new option
-CONF_VERSION = '47.7.0'
+CONF_VERSION = '47.8.0'
 
 
 # Main configuration instance
