5f8f9a8227e411162fca2cdd9fdb05c032e8c6f3
Carlos Cordoba <ccordoba12@gmail.com>
2019-04-09 14:30:09 +0200

    Internal Console: Make its sys.path and env dialogs follow the interface scheme

--- spyder/plugins/console/plugin.py
+++ spyder/plugins/console/plugin.py
@@ -238,12 +238,12 @@ class Console(SpyderPluginWidget):
     @Slot()
     def show_env(self):
         """Show environment variables"""
-        self.dialog_manager.show(EnvDialog())
+        self.dialog_manager.show(EnvDialog(parent=self))
     
     @Slot()
     def show_syspath(self):
         """Show sys.path"""
-        editor = CollectionsEditor()
+        editor = CollectionsEditor(parent=self)
         editor.setup(sys.path, title="sys.path", readonly=True,
                      width=600, icon=ima.icon('syspath'))
         self.dialog_manager.show(editor)
