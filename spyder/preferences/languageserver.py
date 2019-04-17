a493fcff720052328a18fbe59e06efdc685d1d91
dalthviz <d.althviz10@uniandes.edu.co>
2019-04-17 00:59:37 -0500

    Preferences: Improve docstring style options for convention

--- spyder/preferences/languageserver.py
+++ spyder/preferences/languageserver.py
@@ -11,11 +11,12 @@ Language server preferences
 # Standard library imports
 import json
 import re
+import sys
 
 # Third party imports
 from qtpy.compat import to_qvariant
 from qtpy.QtCore import (Qt, Slot, QAbstractTableModel, QModelIndex)
-from qtpy.QtWidgets import (QAbstractItemView, QButtonGroup, QCheckBox,
+from qtpy.QtWidgets import (QAbstractItemView, QCheckBox,
                             QComboBox, QDialog, QDialogButtonBox, QGroupBox,
                             QGridLayout, QHBoxLayout, QLabel, QLineEdit,
                             QPushButton, QSpinBox, QTableView, QTabWidget,
@@ -609,6 +610,7 @@ class LSPManagerConfigPage(GeneralConfigPage):
     CONF_SECTION = 'lsp-server'
     NAME = _('Completion and linting')
     ICON = ima.icon('lspserver')
+    CTRL = "Cmd" if sys.platform == 'darwin' else "Ctrl"
 
     def setup_page(self):
         newcb = self.create_checkbox
@@ -621,8 +623,8 @@ class LSPManagerConfigPage(GeneralConfigPage):
             _("Enable Go to definition"),
             'jedi_definition',
             tip=_("If this option is enabled, left-clicking on\n"
-                  "an object name while pressing the Ctrl key will go to\n"
-                  "that object's definition (if resolved)."))
+                  "an object name while pressing the {} key will go to\n"
+                  "that object's definition (if resolved).".format(self.CTRL)))
         follow_imports_box = newcb(_("Follow imports when going to a "
                                      "definition"),
                                    'jedi_definition/follow_imports')
@@ -777,23 +779,24 @@ class LSPManagerConfigPage(GeneralConfigPage):
         docstring_style_convention = self.create_combobox(
             _("Choose the convention used to lint docstrings: "),
             (("Numpy", 'numpy'),
-             ("Pep 257", 'pep257')),
+             ("PEP 257", 'pep257'),
+             ("Custom", 'custom')),
             'pydocstyle/convention')
-        docstring_style_add_select = self.create_lineedit(
+        self.docstring_style_add_select = self.create_lineedit(
             _("Display the following errors in addition to the "
               "specified convention:"),
             'pydocstyle/add_select', alignment=Qt.Horizontal, word_wrap=False,
             placeholder=_("Example codes: D100, D200"))
-        docstring_style_add_ignore = self.create_lineedit(
+        self.docstring_style_add_ignore = self.create_lineedit(
             _("Ignore the following errors in addition to "
               "the specified convention:").format(code_style_codes),
             'pydocstyle/add_ignore', alignment=Qt.Horizontal, word_wrap=False,
             placeholder=_("Example codes: D400, D405"))
-        docstring_style_select = self.create_lineedit(
+        self.docstring_style_select = self.create_lineedit(
             _("Select the following errors to show:"),
             'pydocstyle/select', alignment=Qt.Horizontal, word_wrap=False,
             placeholder=_("Example codes: D413, D414"))
-        docstring_style_ignore = self.create_lineedit(
+        self.docstring_style_ignore = self.create_lineedit(
             _("Ignore the following errors:"),
             'pydocstyle/ignore', alignment=Qt.Horizontal, word_wrap=False,
             placeholder=_("Example codes: D107, D402"))
@@ -806,6 +809,12 @@ class LSPManagerConfigPage(GeneralConfigPage):
             'pydocstyle/match_dir', alignment=Qt.Horizontal, word_wrap=False,
             placeholder=_("Skip dot directories: [^\\.].*"))
 
+        # Custom option handling
+        docstring_style_convention.combobox.currentTextChanged.connect(
+                self.setup_docstring_style_convention)
+        current_convention = docstring_style_convention.combobox.currentText()
+        self.setup_docstring_style_convention(current_convention)
+
         # Docstring style layout
         docstring_style_g_layout = QGridLayout()
         docstring_style_g_layout.addWidget(
@@ -813,21 +822,21 @@ class LSPManagerConfigPage(GeneralConfigPage):
         docstring_style_g_layout.addWidget(
             docstring_style_convention.combobox, 1, 1)
         docstring_style_g_layout.addWidget(
-            docstring_style_add_select.label, 2, 0)
+            self.docstring_style_add_select.label, 2, 0)
         docstring_style_g_layout.addWidget(
-            docstring_style_add_select.textbox, 2, 1)
+            self.docstring_style_add_select.textbox, 2, 1)
         docstring_style_g_layout.addWidget(
-            docstring_style_add_ignore.label, 3, 0)
+            self.docstring_style_add_ignore.label, 3, 0)
         docstring_style_g_layout.addWidget(
-            docstring_style_add_ignore.textbox, 3, 1)
+            self.docstring_style_add_ignore.textbox, 3, 1)
         docstring_style_g_layout.addWidget(
-            docstring_style_select.label, 4, 0)
+            self.docstring_style_select.label, 4, 0)
         docstring_style_g_layout.addWidget(
-            docstring_style_select.textbox, 4, 1)
+            self.docstring_style_select.textbox, 4, 1)
         docstring_style_g_layout.addWidget(
-            docstring_style_ignore.label, 5, 0)
+            self.docstring_style_ignore.label, 5, 0)
         docstring_style_g_layout.addWidget(
-            docstring_style_ignore.textbox, 5, 1)
+            self.docstring_style_ignore.textbox, 5, 1)
         docstring_style_g_layout.addWidget(
             self.docstring_style_match.label, 6, 0)
         docstring_style_g_layout.addWidget(
@@ -956,6 +965,34 @@ class LSPManagerConfigPage(GeneralConfigPage):
         vlayout = QVBoxLayout()
         vlayout.addWidget(tabs)
         self.setLayout(vlayout)
+
+    @Slot(str)
+    def setup_docstring_style_convention(self, text):
+        """Handle convention changes."""
+        if text == 'Custom':
+            # Show select and ignore options
+            self.docstring_style_select.label.setVisible(True)
+            self.docstring_style_select.textbox.setVisible(True)
+            self.docstring_style_ignore.label.setVisible(True)
+            self.docstring_style_ignore.textbox.setVisible(True)
+
+            # Hide add_select and add_ignore options
+            self.docstring_style_add_select.label.setVisible(False)
+            self.docstring_style_add_select.textbox.setVisible(False)
+            self.docstring_style_add_ignore.label.setVisible(False)
+            self.docstring_style_add_ignore.textbox.setVisible(False)
+        else:
+            # Hide select and ignore options
+            self.docstring_style_select.label.setVisible(False)
+            self.docstring_style_select.textbox.setVisible(False)
+            self.docstring_style_ignore.label.setVisible(False)
+            self.docstring_style_ignore.textbox.setVisible(False)
+
+            # Show add_select and add_ignore options
+            self.docstring_style_add_select.label.setVisible(True)
+            self.docstring_style_add_select.textbox.setVisible(True)
+            self.docstring_style_add_ignore.label.setVisible(True)
+            self.docstring_style_add_ignore.textbox.setVisible(True)
 
     def reset_to_default(self):
         CONF.reset_to_defaults(section='lsp-server')
