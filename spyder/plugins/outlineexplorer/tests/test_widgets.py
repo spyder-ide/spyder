2c95524723edc51f0db41eac40deec383e0e6be6
Quentin Peter <qaep2@cam.ac.uk>
2019-04-26 18:21:33 +0100

    update tests

--- spyder/plugins/outlineexplorer/tests/test_widgets.py
+++ spyder/plugins/outlineexplorer/tests/test_widgets.py
@@ -128,6 +128,8 @@ CODE = """# -*- coding: utf-8 -*-
         x = 'test'
     # %%
         test = 'text'
+    # %%
+        test = 'text'
 """
 
 
@@ -288,7 +290,9 @@ def test_code_cell_grouping(create_outlineexplorer):
         ('MGroup3', CellItem, 'test_file.py', 'test_file.py'),
         ('function6', FunctionItem, 'MGroup3', 'MGroup3', False),
         ('MGroup4', CellItem, 'MGroup3', 'test_file.py'),
-        ('Unnamed cell, line 65', CellItem, 'test_file.py',
+        ('Unnamed Cell, #1', CellItem, 'test_file.py',
+         'test_file.py')
+        ('Unnamed Cell, #2', CellItem, 'test_file.py',
          'test_file.py')
         ]
 
