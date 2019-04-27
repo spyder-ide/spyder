fd9744ccec00ad996f3ce0a4537018f3710dc508
Carlos Cordoba <ccordoba12@gmail.com>
2019-04-27 21:04:35 +0200

    Preferences: Bump CONF_VERSION and revert changes to defaults

--- spyder/config/main.py
+++ spyder/config/main.py
@@ -710,7 +710,7 @@ DEFAULTS = [
 #    or if you want to *rename* options, then you need to do a MAJOR update in
 #    version, e.g. from 3.0.0 to 4.0.0
 # 3. You don't need to touch this value if you're just adding a new option
-CONF_VERSION = '49.0.0'
+CONF_VERSION = '50.0.0'
 
 
 # Main configuration instance
