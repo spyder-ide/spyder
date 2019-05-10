a3a257c8737edbaf34b5db05540c00784e0fc8ce
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-05-10 09:59:49 -0400

    Don't add an empty wdir to cmd

--- spyder/utils/programs.py
+++ spyder/utils/programs.py
@@ -291,8 +291,10 @@ def run_python_script_in_terminal(fname, wdir, args, interact,
     p_args += get_python_args(fname, python_args, interact, debug, args)
 
     if os.name == 'nt':
-        cmd = ('start cmd.exe /K "cd %s && ' % wdir + ' '.join(p_args) +
-               '"' + ' ^&^& exit')
+        cmd = 'start cmd.exe /K "'
+        if wdir:
+            cmd += 'cd ' + wdir + ' && '
+        cmd += ' '.join(p_args) + '"' + ' ^&^& exit'
         # Command line and cwd have to be converted to the filesystem
         # encoding before passing them to subprocess, but only for
         # Python 2.
