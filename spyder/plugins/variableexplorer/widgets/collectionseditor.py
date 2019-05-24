75dacbc70f572fc14a5a34a570084f9c7d976fcd
dalthviz <d.althviz10@uniandes.edu.co>
2019-05-23 23:33:22 -0500

    Variable Explore: Change remote delegate to use sourceModel

--- spyder/plugins/variableexplorer/widgets/collectionseditor.py
+++ spyder/plugins/variableexplorer/widgets/collectionseditor.py
@@ -522,7 +522,7 @@ class CollectionsDelegate(QItemDelegate):
                                    "<i>%s</i>"
                                    ) % to_text_string(msg))
             return
-        key = index.model().get_key(index)
+        key = index.model().sourceModel().get_key(index)
         readonly = (isinstance(value, (tuple, set)) or self.parent().readonly
                     or not is_known_type(value))
         # CollectionsEditor for a list, tuple, dict, etc.
@@ -1577,13 +1577,193 @@ class RemoteCollectionsDelegate(CollectionsDelegate):
 
     def get_value(self, index):
         if index.isValid():
-            name = index.model().keys[index.row()]
+            name = index.model().sourceModel().keys[index.row()]
             return self.parent().get_value(name)
-    
+
     def set_value(self, index, value):
         if index.isValid():
-            name = index.model().keys[index.row()]
+            name = index.model().sourceModel().keys[index.row()]
             self.parent().new_value(name, value)
+
+    def show_warning(self, index):
+        """
+        Decide if showing a warning when the user is trying to view
+        a big variable associated to a Tablemodel index
+
+        This avoids getting the variables' value to know its
+        size and type, using instead those already computed by
+        the TableModel.
+
+        The problem is when a variable is too big, it can take a
+        lot of time just to get its value
+        """
+        try:
+            val_size = index.model().sourceModel().sizes[index.row()]
+            val_type = index.model().sourceModel().types[index.row()]
+        except Exception:
+            return False
+        if val_type in ['list', 'set', 'tuple', 'dict'] and \
+                int(val_size) > 1e5:
+            return True
+        else:
+            return False
+
+    def createEditor(self, parent, option, index):
+        """Overriding method createEditor"""
+        if index.column() < 3:
+            return None
+        if self.show_warning(index):
+            answer = QMessageBox.warning(
+                self.parent(), _("Warning"),
+                _("Opening this variable can be slow\n\n"
+                  "Do you want to continue anyway?"),
+                QMessageBox.Yes | QMessageBox.No)
+            if answer == QMessageBox.No:
+                return None
+        try:
+            value = self.get_value(index)
+            if value is None:
+                return None
+        except Exception as msg:
+            QMessageBox.critical(
+                self.parent(), _("Error"),
+                _("Spyder was unable to retrieve the value of "
+                  "this variable from the console.<br><br>"
+                  "The error mesage was:<br>"
+                  "<i>%s</i>") % to_text_string(msg))
+            return
+        key = index.model().sourceModel().get_key(index)
+        readonly = (isinstance(value, (tuple, set)) or self.parent().readonly
+                    or not is_known_type(value))
+        # CollectionsEditor for a list, tuple, dict, etc.
+        if isinstance(value, (list, set, tuple, dict)):
+            editor = CollectionsEditor(parent=parent)
+            editor.setup(value, key, icon=self.parent().windowIcon(),
+                         readonly=readonly)
+            self.create_dialog(editor, dict(model=index.model().sourceModel(),
+                                            editor=editor,
+                                            key=key, readonly=readonly))
+            return None
+        # ArrayEditor for a Numpy array
+        elif isinstance(value, (ndarray, MaskedArray)) \
+                and ndarray is not FakeObject:
+            editor = ArrayEditor(parent=parent)
+            if not editor.setup_and_check(value, title=key, readonly=readonly):
+                return
+            self.create_dialog(editor, dict(model=index.model().sourceModel(),
+                                            editor=editor,
+                                            key=key, readonly=readonly))
+            return None
+        # ArrayEditor for an images
+        elif isinstance(value, Image) and ndarray is not FakeObject \
+                and Image is not FakeObject:
+            arr = array(value)
+            editor = ArrayEditor(parent=parent)
+            if not editor.setup_and_check(arr, title=key, readonly=readonly):
+                return
+            conv_func = lambda arr: Image.fromarray(arr, mode=value.mode)
+            self.create_dialog(editor, dict(model=index.model().sourceModel(),
+                                            editor=editor,
+                                            key=key, readonly=readonly,
+                                            conv=conv_func))
+            return None
+        # DataFrameEditor for a pandas dataframe, series or index
+        elif isinstance(value, (DataFrame, Index, Series)) \
+                and DataFrame is not FakeObject:
+            editor = DataFrameEditor(parent=parent)
+            if not editor.setup_and_check(value, title=key):
+                return
+            editor.dataModel.set_format(
+                index.model().sourceModel().dataframe_format)
+            editor.sig_option_changed.connect(self.change_option)
+            self.create_dialog(editor, dict(model=index.model().sourceModel(),
+                                            editor=editor,
+                                            key=key, readonly=readonly))
+            return None
+        # QDateEdit and QDateTimeEdit for a dates or datetime respectively
+        elif isinstance(value, datetime.date):
+            if readonly:
+                return None
+            else:
+                if isinstance(value, datetime.datetime):
+                    editor = QDateTimeEdit(value, parent=parent)
+                else:
+                    editor = QDateEdit(value, parent=parent)
+                editor.setCalendarPopup(True)
+                editor.setFont(get_font(font_size_delta=DEFAULT_SMALL_DELTA))
+                return editor
+        # TextEditor for a long string
+        elif is_text_string(value) and len(value) > 40:
+            te = TextEditor(None, parent=parent)
+            if te.setup_and_check(value):
+                editor = TextEditor(value, key,
+                                    readonly=readonly, parent=parent)
+                self.create_dialog(editor,
+                                   dict(model=index.model().sourceModel(),
+                                        editor=editor, key=key,
+                                        readonly=readonly))
+            return None
+        # QLineEdit for an individual value (int, float, short string, etc)
+        elif is_editable_type(value):
+            if readonly:
+                return None
+            else:
+                editor = QLineEdit(parent=parent)
+                editor.setFont(get_font(font_size_delta=DEFAULT_SMALL_DELTA))
+                editor.setAlignment(Qt.AlignLeft)
+                # This is making Spyder crash because the QLineEdit that it's
+                # been modified is removed and a new one is created after
+                # evaluation. So the object on which this method is trying to
+                # act doesn't exist anymore.
+                # editor.returnPressed.connect(self.commitAndCloseEditor)
+                return editor
+        # CollectionsEditor for an arbitrary Python object
+        else:
+            editor = CollectionsEditor(parent=parent)
+            editor.setup(value, key, icon=self.parent().windowIcon(),
+                         readonly=readonly)
+            self.create_dialog(editor, dict(model=index.model().sourceModel(),
+                                            editor=editor,
+                                            key=key, readonly=readonly))
+            return None
+
+    def setModelData(self, editor, model, index):
+        """
+        Overriding method setModelData
+        Editor --> Model
+        """
+        model = model.sourceModel()
+        if not hasattr(model, "set_value"):
+            # Read-only mode
+            return
+
+        if isinstance(editor, QLineEdit):
+            value = editor.text()
+            try:
+                value = display_to_value(to_qvariant(value),
+                                         self.get_value(index),
+                                         ignore_errors=False)
+            except Exception as msg:
+                raise
+                QMessageBox.critical(editor, _("Edit item"),
+                                     _("<b>Unable to assign data to item.</b>"
+                                       "<br><br>Error message:<br>%s"
+                                       ) % str(msg))
+                return
+        elif isinstance(editor, QDateEdit):
+            qdate = editor.date()
+            value = datetime.date(qdate.year(), qdate.month(), qdate.day())
+        elif isinstance(editor, QDateTimeEdit):
+            qdatetime = editor.dateTime()
+            qdate = qdatetime.date()
+            qtime = qdatetime.time()
+            value = datetime.datetime(qdate.year(), qdate.month(),
+                                      qdate.day(), qtime.hour(),
+                                      qtime.minute(), qtime.second())
+        else:
+            # Should not happen...
+            raise RuntimeError("Unsupported editor widget")
+        self.set_value(index, value)
 
 
 class RemoteCollectionsEditorTableView(BaseTableView):
