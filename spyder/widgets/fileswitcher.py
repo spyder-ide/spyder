607e800b01c7bcdc92f057cbac7f3a116e6868a5
dalthviz <d.althviz10@uniandes.edu.co>
2019-06-06 12:37:50 -0500

    Switcher: Add validation for enter slot (only for FILE_MODE)

--- spyder/widgets/fileswitcher.py
+++ spyder/widgets/fileswitcher.py
@@ -682,17 +682,18 @@ class FileSwitcher(QDialog):
     @Slot()
     @Slot(QListWidgetItem)
     def enter(self, itemClicked=None):
-        row = self.current_row()
-        stack_index = self.paths.index(self.filtered_path[row])
-        self.plugin = self.widgets[stack_index][1]
-        plugin_index = self.plugins_instances.index(self.plugin)
-        # Count the real index in the tabWidget of the
-        # current plugin
-        real_index = self.get_stack_index(stack_index,
-                                          plugin_index)
-        self.sig_goto_file.emit(real_index,
-                                self.plugin.get_current_tab_manager())
-        self.accept()
+        if self.mode == self.FILE_MODE:
+            row = self.current_row()
+            stack_index = self.paths.index(self.filtered_path[row])
+            self.plugin = self.widgets[stack_index][1]
+            plugin_index = self.plugins_instances.index(self.plugin)
+            # Count the real index in the tabWidget of the
+            # current plugin
+            real_index = self.get_stack_index(stack_index,
+                                              plugin_index)
+            self.sig_goto_file.emit(real_index,
+                                    self.plugin.get_current_tab_manager())
+            self.accept()
 
     def setup_file_list(self, filter_text, current_path):
         """Setup list widget content for file list display."""
