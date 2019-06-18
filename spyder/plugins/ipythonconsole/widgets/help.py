8e00109a0f533cdde203b3ed0ef29841b5942733
dalthviz <d.althviz10@uniandes.edu.co>
2019-06-17 21:57:39 -0500

    IPython Console: Handle cases for signature before or after 'Docstring:'

--- spyder/plugins/ipythonconsole/widgets/help.py
+++ spyder/plugins/ipythonconsole/widgets/help.py
@@ -44,21 +44,39 @@ class HelpWidget(RichJupyterWidget):
         if text:
             text = re.compile(ANSI_PATTERN).sub('', text)
             signature = self.get_signature(content).split('(')[-1]
+
+            # Base value for the documentation
+            documentation = text.split('Docstring:')[-1].split('Type:')[0]
+
             if signature:
-                documentation = text.split(signature)
-                if len(documentation) > 1:
-                    return documentation[-1].split('Type:')[0]
-            else:
-                return text.split('Docstring:')[-1].split('Type:')[0]
+                # Check if the signature is in the Docstring
+                doc_from_signature = documentation.split(signature)
+                if len(doc_from_signature) > 1:
+                    return (doc_from_signature[-1].split('Docstring:')[-1].
+                            split('Type:')[0])
+
+            return documentation
         else:
             return ''
+
+    def _get_signature(self, name, text):
+        """Get signature from text using a given function name."""
+        signature = ''
+        argspec = getargspecfromtext(text)
+        if argspec:
+            # This covers cases like np.abs, whose docstring is
+            # the same as np.absolute and because of that a proper
+            # signature can't be obtained correctly
+            signature = name + argspec
+        else:
+            signature = getsignaturefromtext(text, name)
+        return signature
 
     def get_signature(self, content):
         """Get signature from inspect reply content"""
         data = content.get('data', {})
         text = data.get('text/plain', '')
         if text:
-            text = ANSI_OR_SPECIAL_PATTERN.sub('', text)
             self._control.current_prompt_pos = self._prompt_pos
             line = self._control.get_current_line_to_cursor()
             name = line[:-1].split('(')[-1]   # Take last token after a (
@@ -69,18 +87,25 @@ class HelpWidget(RichJupyterWidget):
             except:
                 pass
 
-            docstring_text = text.split('Docstring:')[-1]
-            argspec = getargspecfromtext(docstring_text)
-            if argspec:
-                # This covers cases like np.abs, whose docstring is
-                # the same as np.absolute and because of that a proper
-                # signature can't be obtained correctly
-                signature = name + argspec
-            else:
-                signature = getsignaturefromtext(docstring_text, name)
+            text = text.split('Docstring:')
 
-            # Remove docstring for uniformity with editor
-            signature = signature.split('Docstring:')[0]
+            # Try signature from text before 'Docstring:'
+            before_text = text[0]
+            before_signature = self._get_signature(name, before_text)
+
+            # Try signature from text after 'Docstring:'
+            after_text = text[-1]
+            after_signature = self._get_signature(name, after_text)
+
+            # Stay with the longest signature
+            if len(before_signature) > len(after_signature):
+                signature = before_signature
+            else:
+                signature = after_signature
+
+            # Prevent special characters. Applied here to ensure
+            # recognizing the signature in the logic above.
+            signature = ANSI_OR_SPECIAL_PATTERN.sub('', signature)
 
             return signature
         else:
