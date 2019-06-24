e53ed2223ac7248ad9bddfc2d68ce27a62b63994
Carlos Cordoba <ccordoba12@gmail.com>
2019-06-24 14:59:59 +0200

    Object Explorer: Move repr to be the last entry in Details

--- spyder/plugins/variableexplorer/widgets/objectexplorer/attribute_model.py
+++ spyder/plugins/variableexplorer/widgets/objectexplorer/attribute_model.py
@@ -469,13 +469,13 @@ DEFAULT_ATTR_DETAILS = (
     # ATTR_MODEL_STR, # Too similar to unicode column
     ATTR_MODEL_GET_DOC,
     ATTR_MODEL_GET_SOURCE,
+    ATTR_MODEL_GET_FILE,
     ATTR_MODEL_REPR,
     # ATTR_MODEL_DOC_STRING, # not used, too similar to ATTR_MODEL_GET_DOC
     # ATTR_MODEL_GET_MODULE, # not used, already in table
-    ATTR_MODEL_GET_FILE
     # ATTR_MODEL_GET_SOURCE_FILE,  # not used, already in table
     # ATTR_MODEL_GET_SOURCE_LINES, # not used, ATTR_MODEL_GET_SOURCE is better
-    )
+)
 
 # Sanity check for duplicates
 assert len(ALL_ATTR_MODELS) == len(set(ALL_ATTR_MODELS))
