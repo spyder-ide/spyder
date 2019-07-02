bfc031ebb908bbf97585d66d1642b8c8c5b928f8
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2019-07-02 00:12:08 -0500

    Correct LSP client tests

--- spyder/plugins/editor/widgets/tests/conftest.py
+++ spyder/plugins/editor/widgets/tests/conftest.py
@@ -21,7 +21,7 @@ from qtpy.QtGui import QFont
 # Local imports
 from spyder.plugins.editor.tests.conftest import (
     editor_plugin, editor_plugin_open_files, python_files)
-from spyder.plugins.editor.fallback.tests.conftest import fallback
+from spyder.plugins.fallback.tests.conftest import fallback
 from spyder.plugins.languageserver.tests.conftest import (
     lsp_manager, qtbot_module)
 from spyder.plugins.editor.widgets.codeeditor import CodeEditor
