e8662b9c15a25e9efee04e1365926ed505cad542
dalthviz <d.althviz10@uniandes.edu.co>
2019-07-05 12:34:39 -0500

    Variable Explorer: Change conf version

--- spyder/config/main.py
+++ spyder/config/main.py
@@ -506,7 +506,7 @@ DEFAULTS = [
 #    or if you want to *rename* options, then you need to do a MAJOR update in
 #    version, e.g. from 3.0.0 to 4.0.0
 # 3. You don't need to touch this value if you're just adding a new option
-CONF_VERSION = '51.2.0'
+CONF_VERSION = '51.0.0'
 
 
 # Main configuration instance
