f8aecc5bf7940d2a57d5e64e0bf425c3ee8923a4
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2019-07-18 12:35:26 -0500

    Add configuration option

--- spyder/plugins/editor/widgets/codeeditor.py
+++ spyder/plugins/editor/widgets/codeeditor.py
@@ -466,6 +466,9 @@ class CodeEditor(TextEditBaseWidget):
         # Intelligent backspace mode
         self.intelligent_backspace = True
 
+        # Automatic (on the fly) completions
+        self.automatic_completions = True
+
         self.close_parentheses_enabled = True
         self.close_quotes_enabled = False
         self.add_colons_enabled = True
@@ -716,6 +719,7 @@ class CodeEditor(TextEditBaseWidget):
                      tab_mode=True,
                      strip_mode=False,
                      intelligent_backspace=True,
+                     automatic_completions=True,
                      highlight_current_line=True,
                      highlight_current_cell=True,
                      occurrence_highlighting=True,
@@ -801,6 +805,9 @@ class CodeEditor(TextEditBaseWidget):
 
         # Intelligent backspace
         self.toggle_intelligent_backspace(intelligent_backspace)
+
+        # Automatic completions
+        self.toggle_automatic_completions(automatic_completions)
 
         if cloned_from is not None:
             self.set_as_clone(cloned_from)
@@ -1162,6 +1169,9 @@ class CodeEditor(TextEditBaseWidget):
 
     def toggle_intelligent_backspace(self, state):
         self.intelligent_backspace = state
+
+    def toggle_automatic_completions(self, state):
+        self.automatic_completions = state
 
     def set_close_parentheses_enabled(self, enable):
         """Enable/disable automatic parentheses insertion feature"""
@@ -3330,18 +3340,20 @@ class CodeEditor(TextEditBaseWidget):
             cursor.select(QTextCursor.WordUnderCursor)
             word_text = to_text_string(cursor.selectedText())
             # Perform completion on the fly
-            if text == '\b' or text == ' ':
-                offset = 1 + (text == ' ')
-                if len(word_text) > 0:
-                    prev_char = word_text[-1]
+            if self.automatic_completions:
+                if text == '\b' or text == ' ':
+                    offset = 1 + (text == ' ')
+                    if len(word_text) > 0:
+                        prev_char = word_text[-1]
+                    else:
+                        prev_char = self.get_character(
+                            cursor.position() - offset)
+                    if (prev_char.isalpha() or
+                            (prev_char in self.auto_completion_characters)):
+                        self.do_completion(automatic=True)
                 else:
-                    prev_char = self.get_character(cursor.position() - offset)
-                if (prev_char.isalpha() or
-                        (prev_char in self.auto_completion_characters)):
-                    self.do_completion(automatic=True)
-            else:
-                if text.isalpha():
-                    self.do_completion(automatic=True)
+                    if text.isalpha():
+                        self.do_completion(automatic=True)
         if not event.modifiers():
             # Accept event to avoid it being handled by the parent
             # Modifiers should be passed to the parent because they
