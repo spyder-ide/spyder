5fc5294734243882bf9710365795f3c32b580eba
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2019-07-19 22:57:20 -0500

    Remove separation lines on headers

--- spyder/config/main.py
+++ spyder/config/main.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-# ----------------------------------------------------------------------------
-# Copyright © Spyder Project Contributors
 #
+# # Copyright © Spyder Project Contributors
 # Licensed under the terms of the MIT License
 # (see spyder/__init__.py for details)
-# ----------------------------------------------------------------------------
 
 """
 Spyder configuration options.
