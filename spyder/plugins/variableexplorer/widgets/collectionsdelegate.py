233d0bf94d4c4ea13abef7f01704988f74514fdb
dalthviz <d.althviz10@uniandes.edu.co>
2019-08-01 16:35:37 -0500

    Variable Explorer: Enable edition only with double click

--- spyder/plugins/variableexplorer/widgets/collectionsdelegate.py
+++ spyder/plugins/variableexplorer/widgets/collectionsdelegate.py
@@ -336,6 +336,24 @@ class CollectionsDelegate(QItemDelegate):
             raise RuntimeError("Unsupported editor widget")
         self.set_value(index, value)
 
+    def updateEditorGeometry(self, editor, option, index):
+        """
+        Overriding method updateEditorGeometry
+        """
+        # Needed to set in the correct position the QLineEdit editor
+        # since option.rect doesn't have values -> QRect() and makes the editor
+        # to being invisible (has 0 as x, y, width and height)
+        # when doing double click over a cell
+        # See spyder-ide/spyder#9945
+        row = index.row()
+        column = index.column()
+        table_view = editor.parent().parent()
+        y0 = table_view.rowViewportPosition(row)
+        x0 = table_view.columnViewportPosition(column)
+        width = table_view.columnWidth(column)
+        height = table_view.rowHeight(row)
+        editor.setGeometry(x0, y0, width, height)
+
 
 class ToggleColumnDelegate(CollectionsDelegate):
     """ToggleColumn Item Delegate"""
