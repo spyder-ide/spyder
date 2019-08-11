994813dabe291d07b559fc6110840288713dd231
Quentin Peter <qaep2@cam.ac.uk>
2019-08-11 18:55:39 +0100

    Use PR 136

--- continuous_integration/azure/install.sh
+++ continuous_integration/azure/install.sh
@@ -9,7 +9,7 @@ if [ "$USE_CONDA" = "yes" ]; then
     conda install -q -y -c spyder-ide --file requirements/tests.txt
 
     # Install spyder-kernels from Github with no deps
-    pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels
+    pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels.git@refs/pull/136/head
 else
     # Update pip and setuptools
     pip install -U pip setuptools
@@ -27,5 +27,5 @@ else
     pip install git+https://github.com/jupyter/qtconsole.git
 
     # Install spyder-kernels from Github
-    pip install -q git+https://github.com/spyder-ide/spyder-kernels
+    pip install -q git+https://github.com/spyder-ide/spyder-kernels.git@refs/pull/136/head
 fi
