f8bbabe80f48e79d3587e75e771a43639a5479f7
steff456 <steff456@hotmail.com>
2019-09-19 09:43:53 -0500

    Capture the specific error generated when syncing directories fail

--- spyder/utils/encoding.py
+++ spyder/utils/encoding.py
@@ -17,6 +17,7 @@ import locale
 import re
 import os
 import sys
+import errno
 
 # Third-party imports
 from chardet.universaldetector import UniversalDetector
@@ -24,7 +25,7 @@ from atomicwrites import atomic_write
 
 # Local imports
 from spyder.py3compat import (is_string, to_text_string, is_binary_string,
-                              is_unicode, PY2)
+                              is_unicode)
 from spyder.utils.external.binaryornot.check import is_binary
 
 
@@ -237,7 +238,7 @@ def write(text, filename, encoding='utf-8', mode='wb'):
     else:
         # Based in the solution at untitaker/python-atomicwrites#42.
         # Needed to fix file permissions overwritting.
-        # See spyder-ide/spyder#9381.
+        # See untitaker/python-atomicwrites#17
         try:
             original_mode = os.stat(filename).st_mode
         except OSError:  # Change to FileNotFoundError for PY3
@@ -249,11 +250,12 @@ def write(text, filename, encoding='utf-8', mode='wb'):
             with atomic_write(filename, overwrite=True,
                               mode=mode) as textfile:
                 textfile.write(text)
-        except OSError:
+        except OSError as error:
             # Some filesystems don't support the option to sync directories
             # issue untitaker/python-atomicwrites#17
-            with open(filename, mode) as textfile:
-                textfile.write(text)
+            if error.errno != errno.EINVAL:
+                with open(filename, mode) as textfile:
+                    textfile.write(text)
         os.chmod(filename, original_mode)
     return encoding
 
