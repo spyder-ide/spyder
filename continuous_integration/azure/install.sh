f03b7530d97a3848c2610b2a72ac35ec21a6f804
Carlos Cordoba <ccordoba12@gmail.com>
2019-10-15 00:45:46 -0500

    test

--- continuous_integration/azure/install.sh
+++ continuous_integration/azure/install.sh
@@ -2,29 +2,15 @@
 
 # -- Installl dependencies
 if [ "$USE_CONDA" = "yes" ]; then
-    # Avoid problems with invalid SSL certificates
-    if [ "$PYTHON_VERSION" = "2.7" ]; then
-        conda install -q -y python=2.7.16=h97142e2_0
-    fi
-
-    # Install nomkl to avoid installing Intel MKL libraries
-    conda install -q -y nomkl
-
     # Install main dependencies
-    conda install -q -y -c spyder-ide --file requirements/conda.txt
+    conda install -q -y -c conda-forge --file requirements/conda.txt
 
     # Install test ones
-    conda install -q -y -c spyder-ide --file requirements/tests.txt
-
-    # Github backend tests are failing with 1.1.1d
-    conda install -q -y openssl=1.1.1c
+    conda install -q -y -c conda-forge --file requirements/tests.txt
 
     # Install spyder-kernels from Github with no deps
     pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels
 else
-    # Github backend tests are failing with 1.1.1d
-    conda install -q -y openssl=1.1.1c
-
     # Update pip and setuptools
     pip install -U pip setuptools
 
--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -8,28 +8,17 @@ source $HOME/miniconda/etc/profile.d/conda.sh
 
 
 # -- Make new conda environment with required Python version
-conda create -y -n test python=$PYTHON_VERSION
+conda create -y -c conda-forge -n test python=$PYTHON_VERSION
 conda activate test
 
 
 # -- Installl dependencies
 if [ "$USE_CONDA" = "yes" ]; then
-    # Avoid problems with invalid SSL certificates
-    if [ "$PYTHON_VERSION" = "2.7" ]; then
-        conda install -q -y python=2.7.16=h9bab390_0
-    fi
-
-    # Install nomkl to avoid installing Intel MKL libraries
-    conda install -q -y nomkl
-
     # Install main dependencies
-    conda install -q -y -c spyder-ide --file requirements/conda.txt
+    conda install -q -y -c conda-forge --file requirements/conda.txt
 
     # Install test ones
-    conda install -q -y -c spyder-ide --file requirements/tests.txt
-
-    # Github backend tests are failing with 1.1.1d
-    conda install -q -y openssl=1.1.1c
+    conda install -q -y -c conda-forge --file requirements/tests.txt
 
     # Install coveralls
     pip install -q coveralls
@@ -37,15 +26,6 @@ if [ "$USE_CONDA" = "yes" ]; then
     # Install spyder-kernels from Github with no deps
     pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels
 else
-    # Downgrade to Python 3.7.3 because 3.7.4 is not pulling
-    # wheels for all packages
-    if [ "$PYTHON_VERSION" = "3.7" ]; then
-        conda install -q -y python=3.7.3
-    fi
-
-    # Github backend tests are failing with 1.1.1d
-    conda install -q -y openssl=1.1.1c
-
     # Install Spyder and its dependencies from our setup.py
     pip install -e .[test]
 
