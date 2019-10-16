82fb3a578b2901cd0e24e276257aea2f57210612
dalthviz <d.althviz10@uniandes.edu.co>
2019-10-15 20:59:08 -0500

    Projects: Remove file validation for test

--- spyder/plugins/projects/projecttypes/__init__.py
+++ spyder/plugins/projects/projecttypes/__init__.py
@@ -63,13 +63,12 @@ class BaseProject(object):
         """Set a list of files opened by the project."""
         processed_recent_files = []
         for recent_file in recent_files:
-            if os.path.isfile(recent_file):
-                try:
-                    relative_recent_file = os.path.relpath(
-                        recent_file, self.root_path)
-                    processed_recent_files.append(relative_recent_file)
-                except ValueError:
-                    processed_recent_files.append(recent_file)
+            try:
+                relative_recent_file = os.path.relpath(
+                    recent_file, self.root_path)
+                processed_recent_files.append(relative_recent_file)
+            except ValueError:
+                processed_recent_files.append(recent_file)
         files = list(OrderedDict.fromkeys(processed_recent_files))
         self.config.set('main', 'recent_files', files)
 
@@ -79,11 +78,6 @@ class BaseProject(object):
         recent_files = [recent_file if os.path.isabs(recent_file)
                         else os.path.join(self.root_path, recent_file)
                         for recent_file in recent_files]
-
-        for recent_file in recent_files[:]:
-            if not os.path.isfile(recent_file):
-                recent_files.remove(recent_file)
-
         return list(OrderedDict.fromkeys(recent_files))
 
 
