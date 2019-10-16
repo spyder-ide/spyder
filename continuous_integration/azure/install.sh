25af987327ff4c6353b57265b3e347344688aec8
Carlos Cordoba <ccordoba12@gmail.com>
2019-10-16 01:09:02 -0500

    Testing: Restore installation scripts

--- continuous_integration/azure/install.sh
+++ continuous_integration/azure/install.sh
@@ -20,7 +20,7 @@ if [ "$USE_CONDA" = "yes" ]; then
     conda install -q -y openssl=1.1.1c
 
     # Install spyder-kernels from Github with no deps
-    pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels.git@refs/pull/133/head
+    pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels
 else
     # Github backend tests are failing with 1.1.1d
     conda install -q -y openssl=1.1.1c
@@ -41,5 +41,5 @@ else
     pip install git+https://github.com/jupyter/qtconsole.git
 
     # Install spyder-kernels from Github
-    pip install -q git+https://github.com/spyder-ide/spyder-kernels.git@refs/pull/133/head
+    pip install -q git+https://github.com/spyder-ide/spyder-kernels
 fi
