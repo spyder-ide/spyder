2b35fcb81a79bc29dd345213492a6b6f756bb9c8
Quentin Peter <qaep2@cam.ac.uk>
2019-10-18 13:49:00 +0100

    Print shellwidget on fail

--- spyder/app/tests/test_mainwindow.py
+++ spyder/app/tests/test_mainwindow.py
@@ -146,6 +146,18 @@ def find_desired_tab_in_window(tab_name, window):
 # =============================================================================
 # ---- Fixtures
 # =============================================================================
+@pytest.pytest_runtest_makereport(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
+    setattr(item, "rep_" + rep.when, rep)
+
+
 @pytest.fixture
 def main_window(request):
     """Main Window fixture"""
@@ -192,7 +204,12 @@ def main_window(request):
         window.close()
     request.addfinalizer(close_window)
 
-    return window
+    yield window
+    # Print shell content if failed
+    if request.node.rep_setup.passed:
+        if request.node.rep_call.failed:
+            print(main_window.ipyconsole.get_current_shellwidget(
+                )._control.toPlainText())
 
 
 # =============================================================================
