68d871d66c50562a36c9b11d28487ed20627e9cb
Quentin Peter <qaep2@cam.ac.uk>
2019-10-20 17:43:54 +0100

    use 192

--- continuous_integration/azure/install.sh
+++ continuous_integration/azure/install.sh
@@ -20,7 +20,7 @@ if [ "$USE_CONDA" = "yes" ]; then
     conda install -q -y openssl=1.1.1c
 
     # Install spyder-kernels from Github with no deps
-    pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels
+    pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels.git@refs/pull/192/head
 
     # Install python-language-server from Github with no deps
     pip install -q --no-deps git+https://github.com/palantir/python-language-server
@@ -44,7 +44,7 @@ else
     pip install git+https://github.com/jupyter/qtconsole.git
 
     # Install spyder-kernels from Github
-    pip install -q git+https://github.com/spyder-ide/spyder-kernels
+    pip install -q git+https://github.com/spyder-ide/spyder-kernels.git@refs/pull/192/head
 
     # Install python-language-server from Github
     pip install -q git+https://github.com/palantir/python-language-server
