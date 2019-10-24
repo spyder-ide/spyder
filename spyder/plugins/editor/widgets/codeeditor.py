305ca345390d847e245246f5776817eb153e547f
naman <1977419+metalogical@users.noreply.github.com>
2019-10-24 13:07:39 -0700

    better non-on-the-fly Kite completions

--- spyder/plugins/editor/widgets/codeeditor.py
+++ spyder/plugins/editor/widgets/codeeditor.py
@@ -1064,11 +1064,13 @@ class CodeEditor(TextEditBaseWidget):
             completions = params['params']
             completions = [] if completions is None else completions
 
+            replace_end = self.textCursor().position()
             under_cursor = self.get_current_word_and_position(completion=True)
             if under_cursor:
-                word, _ = under_cursor
+                word, replace_start = under_cursor
             else:
                 word = ''
+                replace_start = replace_end
             first_letter = ''
             if len(word) > 0:
                 first_letter = word[0]
@@ -1087,6 +1089,21 @@ class CodeEditor(TextEditBaseWidget):
                 return (case_mismatch, completion['sortText'])
 
             completion_list = sorted(completions, key=sort_key)
+
+            # Allow for textEdit completions to be filtered by Spyder
+            # if on-the-fly completions are disabled, only if the
+            # textEdit range matches the word under the cursor.
+            for completion in completion_list:
+                if 'textEdit' in completion:
+                    c_replace_start = completion['textEdit']['range']['start']
+                    c_replace_end = completion['textEdit']['range']['end']
+                    if (c_replace_start == replace_start
+                            and c_replace_end == replace_end):
+                        insert_text = completion['textEdit']['newText']
+                        completion['filterText'] = insert_text
+                        completion['insertText'] = insert_text
+                        del completion['textEdit']
+
             if len(completion_list) > 0:
                 self.completion_widget.show_list(
                         completion_list, position, automatic)
