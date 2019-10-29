b3a93700b334c8e9c38597c434a0a634e920a417
Quentin Peter <qaep2@cam.ac.uk>
2019-10-29 11:04:35 +0000

    Use PR 175

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -35,7 +35,7 @@ if [ "$USE_CONDA" = "yes" ]; then
     pip install -q coveralls
 
     # Install spyder-kernels from Github with no deps
-    pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels.git@refs/pull/152/head
+    pip install -q --no-deps git+https://github.com/spyder-ide/spyder-kernels.git@refs/pull/175/head
 
     # Install python-language-server from Github with no deps
     pip install -q --no-deps git+https://github.com/palantir/python-language-server
@@ -68,7 +68,7 @@ else
     pip install git+https://github.com/jupyter/qtconsole.git
 
     # Install spyder-kernels from Github
-    pip install -q git+https://github.com/spyder-ide/spyder-kernels.git@refs/pull/152/head
+    pip install -q git+https://github.com/spyder-ide/spyder-kernels.git@refs/pull/175/head
 
     # Install python-language-server from Github
     pip install -q git+https://github.com/palantir/python-language-server
