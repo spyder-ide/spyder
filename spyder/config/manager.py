32b3e56c9b9704e219637aa2a56fd35727f87c1d
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2019-10-31 23:26:01 -0500

    Moe gui options to manager and generalize shortcuts for plugins

--- spyder/config/manager.py
+++ spyder/config/manager.py
@@ -97,7 +97,7 @@ class ConfigurationManager(object):
                 raw_mode=True,
                 remove_obsolete=False,
             )
-            self._plugin_configs[conf_section] = plugin_config
+            self._plugin_configs[conf_section] = (plugin_class, plugin_config)
 
     def remove_deprecated_config_locations(self):
         """Removing old .spyder.ini location."""
@@ -107,12 +107,13 @@ class ConfigurationManager(object):
 
     def get_active_conf(self, section=None):
         """
-        Return the active user or project configurarion for plugin.
+        Return the active user or project configuration for plugin.
         """
+        # Add a check for shortcuts!
         if section is None:
             config = self._user_config
         elif section in self._plugin_configs:
-            config = self._plugin_configs[section]
+            _, config = self._plugin_configs[section]
         else:
             # TODO: implement project configuration on the following PR
             config = self._user_config
@@ -225,6 +226,73 @@ class ConfigurationManager(object):
         """Reset config to Default values."""
         config = self.get_active_conf(section)
         config.reset_to_defaults(section=section)
+
+    # Shortcut configuration management
+    # ------------------------------------------------------------------------
+    def _get_shortcut_config(self, context):
+        """Return the shortcut configuration for global or plugin configs."""
+        config = self._user_config
+        if context in self._plugin_configs:
+            plugin_class, config = self._plugin_configs[context]
+            # Check if plugin has a separate file
+            if not plugin_class.CONF_FILE:
+                config = self._user_config
+        elif context == '_':
+            config = self._user_config
+        else:
+            # Raise error?
+            pass
+
+        return config
+
+    def get_shortcut(self, context, name):
+        """
+        Get keyboard shortcut (key sequence string).
+
+        Context must be either '_' for global or the name of a plugin.
+        """
+        config = self._get_shortcut_config(context)
+        return config.get('shortcuts', context + '/' + name)
+
+    def set_shortcut(self, context, name, keystr):
+        """
+        Set keyboard shortcut (key sequence string).
+
+        Context must be either '_' for global or the name of a plugin.
+        """
+        config = self._get_shortcut_config(context)
+        config.set('shortcuts', context + '/' + name, keystr)
+
+    def config_shortcut(self, action, context, name, parent):
+        """
+        Create a Shortcut namedtuple for a widget.
+
+        The data contained in this tuple will be registered in our shortcuts
+        preferences page.
+        """
+        from spyder.config.gui import _config_shortcut
+
+        keystr = self.get_shortcut(context, name)
+        sc = _config_shortcut(action, context, name, keystr, parent)
+        return sc
+
+    def iter_shortcuts(self):
+        """Iterate over keyboard shortcuts."""
+        for context_name, keystr in self._user_config.items('shortcuts'):
+            context, name = context_name.split('/', 1)
+            yield context, name, keystr
+
+        for plugin_config in self._plugin_configs:
+            for context_name, keystr in plugin_config.items('shortcuts'):
+                context, name = context_name.split('/', 1)
+                yield context, name, keystr
+
+    def reset_shortcuts(self):
+        """Reset keyboard shortcuts to default values."""
+        self._user_config.reset_to_defaults(section='shortcuts')
+        for plugin_config in self._plugin_configs:
+            # TODO: check if the section exists?
+            plugin_config.reset_to_defaults(section='shortcuts')
 
 
 CONF = ConfigurationManager()
