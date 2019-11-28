bef86f3c03574713cafa430ffec331a281c8d3cf
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2019-11-27 19:59:06 -0500

    Remove unneeded import

--- spyder/plugins/help/plugin.py
+++ spyder/plugins/help/plugin.py
@@ -19,7 +19,6 @@ from qtpy.QtWebEngineWidgets import QWebEnginePage, WEBENGINE
 # Local imports
 from spyder.config.base import _, get_conf_path, get_module_source_path
 from spyder.config.fonts import DEFAULT_SMALL_DELTA
-from spyder.config.manager import CONF
 from spyder.api.plugins import SpyderPluginWidget
 from spyder.py3compat import get_meth_class_inst, to_text_string
 from spyder.utils import icon_manager as ima
