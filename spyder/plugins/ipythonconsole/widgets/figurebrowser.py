025ccdc4be9763f446e7325600b4f7a9cf2079b7
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-12-02 11:53:23 -0500

    Don't pass msg to rich jupyter if mute and img

--- spyder/plugins/ipythonconsole/widgets/figurebrowser.py
+++ spyder/plugins/ipythonconsole/widgets/figurebrowser.py
@@ -53,6 +53,7 @@ class FigureBrowserWidget(RichJupyterWidget):
         elif 'image/jpeg' in data and self._jpg_supported:
             fmt = 'image/jpeg'
             img = decodebytes(data['image/jpeg'].encode('ascii'))
+
         if img is not None:
             self.sig_new_inline_figure.emit(img, fmt)
             if (self.figurebrowser is not None and
@@ -67,8 +68,5 @@ class FigureBrowserWidget(RichJupyterWidget):
                           'pane options menu. \n'
                           '<hr><br>'), before_prompt=True)
                     self.sended_render_message = True
-                else:
-                    msg['content']['data']['text/plain'] = ''
-                del msg['content']['data'][fmt]
-
+                return
         return super(FigureBrowserWidget, self)._handle_display_data(msg)
