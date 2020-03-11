53235d41fd6c6d28e0626532d38d040269cf9e87
Carlos Cordoba <ccordoba12@gmail.com>
2020-03-11 08:43:37 -0500

    API: Rename i18n.py to translations.py

--- spyder/api/translations.py
+++ spyder/api/translations.py
@@ -5,7 +5,7 @@
 # (see spyder/__init__.py for details)
 
 """
-This module contains internationalization API.
+This module contains the internationalization API.
 """
 
 from spyder.config.base import get_translation
