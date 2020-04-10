937d58d142215e0d81078a17818dea12eae84bf3
Carlos Cordoba <ccordoba12@gmail.com>
2020-04-09 19:59:15 -0500

    test

--- continuous_integration/azure/runtests.bat
+++ continuous_integration/azure/runtests.bat
@@ -3,9 +3,11 @@ conda activate test
 
 :: Check manifest
 check-manifest
+if errorlevel 1 exit 1
 
 :: Run tests several times
 python runtests.py || python runtests.py || python runtests.py || python runtests.py || python runtests.py || exit 1
 
 :: Run codecov if things were successful
 codecov
+if errorlevel 1 exit 1
