edcd76c77c6b22726ec6cc6649f2a07f218f48f2
Carlos Cordoba <ccordoba12@gmail.com>
2020-04-10 01:23:27 -0500

    test

--- continuous_integration/azure/install.sh
+++ continuous_integration/azure/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -ex
 
-source activate test
-
 # -- Installl dependencies
 if [ "$USE_CONDA" = "yes" ]; then
+    # Avoid problems with invalid SSL certificates
+    if [ "$PYTHON_VERSION" = "2.7" ]; then
+        conda install -q -y python=2.7.16=h97142e2_0
+    fi
+
     # Install nomkl to avoid installing Intel MKL libraries
     conda install -q -y nomkl
 
@@ -13,19 +16,14 @@ if [ "$USE_CONDA" = "yes" ]; then
     # Install test ones
     conda install -q -y -c spyder-ide --file requirements/tests.txt
 
+    # Github backend tests are failing with 1.1.1d
+    conda install -q -y openssl=1.1.1c
+
     # Remove spyder-kernels to be sure that we use its subrepo
     conda remove -q -y --force spyder-kernels
 
     # Install python-language-server from Github with no deps
     pip install -q --no-deps git+https://github.com/palantir/python-language-server
-
-    # Create environment for Jedi environments tests
-    conda create -n jedi-test-env -q -y python=3.6 flask spyder-kernels
-    conda list -n jedi-test-env
-
-    # Create environment to test conda activation before launching a spyder kernel
-    conda create -n spytest-ž -q -y python=3.6 spyder-kernels
-    conda list -n spytest-ž
 else
     # Github backend tests are failing with 1.1.1d
     conda install -q -y openssl=1.1.1c
@@ -54,3 +52,11 @@ fi
 
 # To check our manifest
 pip install check-manifest
+
+# Create environment for Jedi environments testsTest for Jedi environments
+conda create -n jedi-test-env -q -y python=3.6 flask spyder-kernels
+conda list -n jedi-test-env
+
+# Create environment to test conda activation before launching a spyder kernel
+conda create -n spytest-ž -q -y python=3.6 spyder-kernels
+conda list -n spytest-ž
--- continuous_integration/azure/runtests.sh
+++ continuous_integration/azure/runtests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -ex
 
-source activate tests
-
 # Run tests
 python bootstrap.py -- --reset
 python runtests.py
