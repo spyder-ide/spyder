3c968453e80dc0af7e29cef11fd3bee25bfcbeae
Stephannie Jimenez <steff456@hotmail.com>
2020-04-22 23:17:06 -0500

    fix pep8 style issues

--- spyder/utils/icon_manager.py
+++ spyder/utils/icon_manager.py
@@ -336,7 +336,8 @@ _qtaargs = {
     'dependency_ok':           [('fa.check',), {'color': MAIN_FG_COLOR}],
     'dependency_warning':      [('fa.warning',), {'color': 'orange'}],
     'dependency_error':        [('fa.warning',), {'color': 'darkred'}],
-    'broken_variant':          [('mdi.image-broken-variant',), {'color': MAIN_FG_COLOR}],
+    'broken_variant':          [('mdi.image-broken-variant',),
+                                {'color': MAIN_FG_COLOR}],
     # --- Status bar --------------------------------------------------------
     'code_fork':               [('fa.code-fork',), {'color': MAIN_FG_COLOR}],
 }
