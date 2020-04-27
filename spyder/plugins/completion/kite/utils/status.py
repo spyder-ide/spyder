803f551806a3a1c1d746bcfbf0dcb9d3bad5a937
dalthviz <d.althviz10@uniandes.edu.co>
2020-04-27 17:28:01 -0500

    Kite: Add reference to the issue

--- spyder/plugins/completion/kite/utils/status.py
+++ spyder/plugins/completion/kite/utils/status.py
@@ -47,7 +47,7 @@ def check_if_kite_running():
                 running = True
                 break
     except OSError:
-        # Needed to handle a possible WinError 0
+        # Needed to handle a possible WinError 0. See spyder-ide/spyder#12510
         pass
     return running
 
