e65c5e04ae1ae84772cd4a45d4b135785bd15367
dalthviz <d.althviz10@uniandes.edu.co>
2020-06-01 10:13:47 -0500

    git subrepo clone --branch=spyder_12620_2 --force https://github.com/dalthviz/spyder-kernels.git external-deps/spyder-kernels
    subrepo:
      subdir:   "external-deps/spyder-kernels"
      merged:   "02e16ccfe"
    upstream:
      origin:   "https://github.com/dalthviz/spyder-kernels.git"
      branch:   "spyder_12620_2"
      commit:   "02e16ccfe"
    git-subrepo:
      version:  "0.4.1"
      origin:   "???"
      commit:   "???"

--- external-deps/spyder-kernels/spyder_kernels/customize/spydercustomize.py
+++ external-deps/spyder-kernels/spyder_kernels/customize/spydercustomize.py
@@ -440,7 +440,7 @@ def exec_code(code, filename, ns_globals, ns_locals=None, post_mortem=False):
             ipython_shell.kernel._pdb_obj = None
         elif post_mortem and isinstance(error, Exception):
             error_type, error, tb = sys.exc_info()
-            post_mortem_excepthook(error_type, error, tb.tb_next)
+            post_mortem_excepthook(error_type, error, tb)
         else:
             # We ignore the call to exec
             ipython_shell.showtraceback(tb_offset=1)
