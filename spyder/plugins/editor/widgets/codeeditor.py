8ce4c21f180e7ed94c68de595dcfbe53fd5517e2
Stephannie Jimenez <steff456@hotmail.com>
2020-06-24 11:46:13 -0500

    fix tests

--- spyder/plugins/editor/widgets/codeeditor.py
+++ spyder/plugins/editor/widgets/codeeditor.py
@@ -455,7 +455,6 @@ class CodeEditor(TextEditBaseWidget):
         # functions or signals for objects not created from Python" in
         # Linux Ubuntu. See spyder-ide/spyder#5215.
         self.setVerticalScrollBar(QScrollBar())
-        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
 
         # Highlights and flag colors
         self.warning_color = "#FFAD07"
