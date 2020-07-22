292e6a888c90a982dab2e5e5e9ffb2ea8490e75e
Aaron <athompson6735@gmail.com>
2020-07-22 16:20:36 -0400

    added "open in external browser" shortcut

--- spyder/plugins/editor/widgets/editor.py
+++ spyder/plugins/editor/widgets/editor.py
@@ -516,7 +516,15 @@ class EditorStack(QWidget):
         else:
            text= _("Show in external file explorer")
         external_fileexp_action = create_action(self, text,
-                                triggered=self.show_in_external_file_explorer)
+                                triggered=self.show_in_external_file_explorer,
+                                shortcut=CONF.get_shortcut(context="Editor",
+                                        name="show in external file explorer"),
+                                context=Qt.WidgetShortcut)
+        
+        parent.plugin.register_shortcut(external_fileexp_action, 
+                                        context="Editor",
+                                        name="show in external file explorer", 
+                                        add_shortcut_to_tip=True)
 
         self.menu_actions = actions + [external_fileexp_action,
                                        None, switcher_action,
@@ -836,6 +844,12 @@ class EditorStack(QWidget):
             name="close split panel",
             parent=self)
 
+        external_fileexp = CONF.config_shortcut( #XXX here
+            self.show_in_external_file_explorer,
+            context="Editor",
+            name="show in external file explorer",
+            parent=self)
+            
         # Return configurable ones
         return [inspect, set_breakpoint, set_cond_breakpoint, gotoline, tab,
                 tabshift, run_selection, new_file, open_file, save_file,
@@ -846,7 +860,7 @@ class EditorStack(QWidget):
                 go_to_next_cell, go_to_previous_cell, re_run_last_cell,
                 prev_warning, next_warning, split_vertically,
                 split_horizontally, close_split,
-                prevtab, nexttab]
+                prevtab, nexttab, external_fileexp]
 
     def get_shortcut_data(self):
         """
