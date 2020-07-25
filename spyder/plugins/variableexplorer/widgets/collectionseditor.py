dcdba86d41cae43687f5b5251976d56ba0e9cb47
Diego Prosperi Turibio <diegoprosperituribio@gmail.com>
2020-07-25 03:30:34 -0300

    FIX: resolving pep8 warnings.

--- spyder/plugins/variableexplorer/widgets/collectionseditor.py
+++ spyder/plugins/variableexplorer/widgets/collectionseditor.py
@@ -603,14 +603,14 @@ class BaseTableView(QTableView):
                                                icon=ima.icon('filesave'),
                                                triggered=self.save_array)
         self.save_array_action.setVisible(False)
-        self.insert_action_above = \
-        create_action(self, _("Insert above"),
+        self.insert_action_above = create_action(
+                      self, _("Insert above"),
                       icon=ima.icon('insert'),
                       triggered=lambda: self.insert_item(bellow=False))
-        self.insert_action_bellow = \
-             create_action(self, _("Insert bellow"),
-                           icon=ima.icon('insert'),
-                           triggered=lambda: self.insert_item(bellow=True))
+        self.insert_action_bellow = create_action(
+                      self, _("Insert bellow"),
+                      icon=ima.icon('insert'),
+                      triggered=lambda: self.insert_item(bellow=True))
         self.remove_action = create_action(self, _("Remove"),
                                            icon=ima.icon('editdelete'),
                                            triggered=self.remove_item)
