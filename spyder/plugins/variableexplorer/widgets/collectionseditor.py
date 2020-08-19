926e684b9a1f3c85705bd876878fc61dbbbcc748
skjerns <14980558+skjerns@users.noreply.github.com>
2020-08-19 22:31:15 +0200

    remove debug print

--- spyder/plugins/variableexplorer/widgets/collectionseditor.py
+++ spyder/plugins/variableexplorer/widgets/collectionseditor.py
@@ -743,7 +743,6 @@ class BaseTableView(QTableView):
 
     def set_data(self, data):
         """Set table data"""
-        print("setting", data)
         if data is not None:
             self.source_model.set_data(data, self.dictfilter)
             self.source_model.reset()
