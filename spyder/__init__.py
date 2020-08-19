ab69b4d05f077473b504632c2662387d375f2682
Quentin Peter <impact27@users.noreply.github.com>
2020-08-19 10:12:23 +0200

    Update spyder/__init__.py
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>

--- spyder/__init__.py
+++ spyder/__init__.py
@@ -74,7 +74,8 @@ def get_versions(reporev=True):
     revision = None
     if reporev:
         from spyder.utils import vcs
-        revision, branch = vcs.get_git_revision(os.path.dirname(__current_directory__))
+        revision, branch = vcs.get_git_revision(
+            os.path.dirname(__current_directory__))
 
     if not sys.platform == 'darwin':  # To avoid a crash with our Mac app
         system = platform.system()
