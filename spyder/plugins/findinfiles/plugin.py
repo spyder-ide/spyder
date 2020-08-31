396142aec2a1b9049cd9d9e5a4eb8704d5219c2e
goanpeca <goanpeca@gmail.com>
2020-08-31 17:46:04 -0500

    Update find in file UI

--- spyder/plugins/findinfiles/plugin.py
+++ spyder/plugins/findinfiles/plugin.py
@@ -9,6 +9,7 @@ Find in Files Plugin.
 
 # Third party imports
 from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QApplication
 
 # Local imports
 from spyder.api.menus import ApplicationMenus
@@ -72,7 +73,13 @@ class FindInFiles(SpyderDockablePlugin):
             working_directory.sig_current_directory_changed.connect(
                 self.refresh_search_directory)
 
-        findinfiles_action = self.get_action(FindInFilesWidgetActions.Find)
+        findinfiles_action = self.create_action(
+            FindInFilesActions.FindInFiles,
+            text=_("Find in files"),
+            tip=_("Search text in multiple files"),
+            triggered=self.find,
+            register_shortcut=True,
+        )
         menu = self.get_application_menu(ApplicationMenus.Search)
         self.add_item_to_application_menu(
             findinfiles_action,
@@ -144,9 +151,27 @@ class FindInFiles(SpyderDockablePlugin):
     def find(self):
         """
         Search text in multiple files.
+
+        Notes
+        -----
+        Find in files using the currently selected text of the focused widget.
         """
+        focus_widget = QApplication.focusWidget()
+        text = ''
+        try:
+            if focus_widget.has_selected_text():
+                text = focus_widget.get_selected_text()
+        except AttributeError:
+            # This is not a text widget deriving from TextEditBaseWidget
+            pass
+
         self.switch_to_plugin()
-        self.get_widget().find()
+        widget = self.get_widget()
+
+        if text:
+            widget.set_search_text(text)
+
+        widget.find()
 
 
 def test():
