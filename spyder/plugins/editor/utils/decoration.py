34a0b3fed7026f7404974b4fb6f3b8e080752949
Carlos Cordoba <ccordoba12@gmail.com>
2020-09-03 11:42:51 -0500

    Editor: Catch RuntimeError when updating decorations in the right place

--- spyder/plugins/editor/utils/decoration.py
+++ spyder/plugins/editor/utils/decoration.py
@@ -86,17 +86,11 @@ class TextDecorationsManager(Manager, QObject):
             return True
         except ValueError:
             return False
-        except RuntimeError:
-            # This is needed to fix spyder-ide/spyder#9173.
-            pass
 
     def clear(self):
         """Removes all text decoration from the editor."""
         self._decorations[:] = []
-        try:
-            self.update()
-        except RuntimeError:
-            pass
+        self.update()
 
     def update(self):
         """
@@ -114,25 +108,29 @@ class TextDecorationsManager(Manager, QObject):
         NOTE: Update TextDecorations to use editor font, using a different
         font family and point size could cause unwanted behaviors.
         """
-        font = self.editor.font()
+        try:
+            font = self.editor.font()
 
-        # Get the current visible block numbers
-        first, last = self.editor.get_visible_block_numbers()
+            # Get the current visible block numbers
+            first, last = self.editor.get_visible_block_numbers()
 
-        # Update visible decorations
-        visible_decorations = []
-        for decoration in self._decorations:
-            block_nb = decoration.cursor.block().blockNumber()
-            if first <= block_nb <= last:
-                visible_decorations.append(decoration)
-                try:
-                    decoration.format.setFont(
+            # Update visible decorations
+            visible_decorations = []
+            for decoration in self._decorations:
+                block_nb = decoration.cursor.block().blockNumber()
+                if first <= block_nb <= last:
+                    visible_decorations.append(decoration)
+                    try:
+                        decoration.format.setFont(
                             font, QTextCharFormat.FontPropertiesSpecifiedOnly)
-                except (TypeError, AttributeError):  # Qt < 5.3
-                    decoration.format.setFontFamily(font.family())
-                    decoration.format.setFontPointSize(font.pointSize())
+                    except (TypeError, AttributeError):  # Qt < 5.3
+                        decoration.format.setFontFamily(font.family())
+                        decoration.format.setFontPointSize(font.pointSize())
 
-        self.editor.setExtraSelections(visible_decorations)
+            self.editor.setExtraSelections(visible_decorations)
+        except RuntimeError:
+            # This is needed to fix spyder-ide/spyder#9173.
+            return
 
     def __iter__(self):
         return iter(self._decorations)
