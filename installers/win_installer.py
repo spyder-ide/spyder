22e6266d25c873e56a519173f010dc741cf76708
dalthviz <d.althviz10@uniandes.edu.co>
2020-10-06 13:32:02 -0500

    Installer: Add NOTICE update and license info of base resources used to create the script

--- installers/win_installer.py
+++ installers/win_installer.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2020 Spyder Project Contributors.
+# Licensed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+#
+# Copyright © 2018 Nicholas H.Tollervey.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Script to create a windows installer using pynsist.
 
