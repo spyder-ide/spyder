18be5c937d1c8c851fffff90e9e79653be399a65
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-10-15 17:57:19 -0500

    Remove pyls-black version pinning

--- setup.py
+++ setup.py
@@ -206,7 +206,7 @@ install_requires = [
     'pyqt5<5.13',
     'pyqtwebengine<5.13',
     'python-language-server[all]>=0.35.0,<1.0.0',
-    'pyls-black>=0.4.6',
+    'pyls-black',
     'pyxdg>=0.26;platform_system=="Linux"',
     'pyzmq>=17',
     'qdarkstyle>=2.8',
