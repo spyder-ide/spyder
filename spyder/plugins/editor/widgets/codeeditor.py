8e2f314897d468acf592ff44d9d0697a0038ba88
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-10-30 11:58:16 -0500

    Reindent completions before inserting them

--- spyder/plugins/editor/widgets/codeeditor.py
+++ spyder/plugins/editor/widgets/codeeditor.py
@@ -1276,6 +1276,14 @@ class CodeEditor(TextEditBaseWidget):
             return
         self.completion_args = None
         position, automatic = args
+
+        start_cursor = self.textCursor()
+        start_cursor.movePosition(QTextCursor.StartOfBlock)
+        line_text = self.get_text(start_cursor.position(), 'eol')
+        leading_whitespace = self.compute_whitespace(line_text)
+        indentation_whitespace = ' ' * leading_whitespace
+        eol_char = self.get_line_separator()
+
         try:
             completions = params['params']
             completions = ([] if completions is None else
@@ -1329,6 +1337,15 @@ class CodeEditor(TextEditBaseWidget):
                         completion['filterText'] = insert_text
                         completion['insertText'] = insert_text
                         del completion['textEdit']
+
+                insert_text = completion['insertText']
+                insert_text_lines = insert_text.splitlines()
+                reindented_text = [insert_text_lines[0]]
+                for insert_line in insert_text_lines[1:]:
+                    insert_line = indentation_whitespace + insert_line
+                    reindented_text.append(insert_line)
+                reindented_text = eol_char.join(reindented_text)
+                completion['insertText'] = reindented_text
 
             self.completion_widget.show_list(
                 completion_list, position, automatic)
@@ -1681,23 +1698,23 @@ class CodeEditor(TextEditBaseWidget):
             self.document_did_change()
 
     # ------------- LSP: Code folding ranges -------------------------------
-    def update_whitespace_count(self, line, column):
-        def compute_whitespace(line):
-            whitespace_regex = re.compile(r'(\s+).*')
-            whitespace_match = whitespace_regex.match(line)
-            total_whitespace = 0
-            if whitespace_match is not None:
-                whitespace_chars = whitespace_match.group(1)
-                whitespace_chars = whitespace_chars.replace(
-                    '\t', tab_size * ' ')
-                total_whitespace = len(whitespace_chars)
-            return total_whitespace
-
+    def compute_whitespace(self, line):
         tab_size = self.tab_stop_width_spaces
+        whitespace_regex = re.compile(r'(\s+).*')
+        whitespace_match = whitespace_regex.match(line)
+        total_whitespace = 0
+        if whitespace_match is not None:
+            whitespace_chars = whitespace_match.group(1)
+            whitespace_chars = whitespace_chars.replace(
+                '\t', tab_size * ' ')
+            total_whitespace = len(whitespace_chars)
+        return total_whitespace
+
+    def update_whitespace_count(self, line, column):
         self.leading_whitespaces = {}
         lines = to_text_string(self.toPlainText()).splitlines()
         for i, text in enumerate(lines):
-            total_whitespace = compute_whitespace(text)
+            total_whitespace = self.compute_whitespace(text)
             self.leading_whitespaces[i] = total_whitespace
 
     def cleanup_folding(self):
