7c51f4c36b5336d099005d1bb431b4cb6e77345b
juanis2112 <juanitagomezr2112@gmail.com>
2020-11-05 00:31:03 -0500

    Avoid command line tools dialog when starting spyder

--- spyder/utils/vcs.py
+++ spyder/utils/vcs.py
@@ -124,7 +124,7 @@ def get_git_revision(repopath):
     error
     """
     try:
-        git = programs.find_program('git')
+        git = programs.find_git()
         assert git is not None and osp.isdir(osp.join(repopath, '.git'))
         commit = programs.run_program(git, ['rev-parse', '--short', 'HEAD'],
                                       cwd=repopath).communicate()
