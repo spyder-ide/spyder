962503136039fa4c9a7d09a814c2338b67009017
Carlos Cordoba <ccordoba12@gmail.com>
2020-11-14 11:43:29 -0500

    Fix opening project from the command line

--- spyder/app/mainwindow.py
+++ spyder/app/mainwindow.py
@@ -1291,9 +1291,9 @@ class MainWindow(QMainWindow):
         if self.splash is not None:
             self.splash.hide()
 
-        if (self.open_project and
-                (running_in_mac_app() and not self.first_spyder_run)):
-            self.projects.open_project(self.open_project)
+        if self.open_project:
+            if not running_in_mac_app():
+                self.projects.open_project(self.open_project)
         else:
             # Load last project if a project was active when Spyder
             # was closed
