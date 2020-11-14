7800b59a71f08dee63b00527bc90f986b5b729fb
Quentin Peter <qaep2@cam.ac.uk>
2020-11-14 21:51:41 +0100

    fix recursive calls

--- spyder/plugins/ipythonconsole/comms/kernelcomm.py
+++ spyder/plugins/ipythonconsole/comms/kernelcomm.py
@@ -140,19 +140,22 @@ class KernelComm(CommBase, QObject):
 
         settings = call_dict['settings']
         interrupt = 'interrupt' in settings and settings['interrupt']
+        interrupt = interrupt or blocking
         # Need to make sure any blocking call is replied rapidely.
-        if not self.comm_channel_connected():
+        if interrupt and not self.comm_channel_connected():
             # Ask again for comm config
             self.remote_call()._send_comm_config()
             # Can not interrupt if comm not connected
             interrupt = False
-            logger.info("Dropping interrupt because comm is disconnected: ",
-                            str(call_dict))
+            logger.debug(
+                "Dropping interrupt because comm is disconnected: " +
+                str(call_dict)
+            )
             if blocking:
                 raise CommError("Can not block on a disconnected comm")
         try:
             with self.comm_channel_manager(
-                    comm_id, queue_message=not (interrupt or blocking)):
+                    comm_id, queue_message=not interrupt):
                 return super(KernelComm, self)._get_call_return_value(
                     call_dict, call_data, comm_id)
         except RuntimeError:
