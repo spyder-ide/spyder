8c27059e3a54c7bd0cb21b3aa1b10e689d545b6a
Quentin Peter <impact27@users.noreply.github.com>
2021-02-08 13:30:05 +0100

    Update test_mainwindow.py

--- spyder/app/tests/test_mainwindow.py
+++ spyder/app/tests/test_mainwindow.py
@@ -3853,13 +3853,10 @@ def test_print_comms(main_window, qtbot):
     with qtbot.waitSignal(shell.executed):
         shell.execute(code)
 
-    assert nsb.editor.source_model.rowCount() == 0
     with qtbot.waitSignal(shell.executed):
         shell.execute("a = Test()")
 
-    # Make sure the warning is printed and that the variable
-    # is in the variable explorer
-    assert nsb.editor.source_model.rowCount() == 1
+    # Make sure the warning is printed
     assert ("Output from spyder call 'get_namespace_view':"
             in control.toPlainText())
 
