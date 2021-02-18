c5787630af48db3cdeb660c89c37b066d843f5b4
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-02-17 21:28:43 -0500

    Fix test_warnings

--- spyder/plugins/editor/widgets/tests/test_warnings.py
+++ spyder/plugins/editor/widgets/tests/test_warnings.py
@@ -28,25 +28,63 @@ TEXT = ("def some_function():\n"  # D100, D103: Missing docstring
         "undefined_function()")  # Undefined name 'undefined_function'
 
 
+@pytest.fixture
+def completions_codeeditor_linting(request, qtbot, completions_codeeditor):
+    editor, completion_plugin = completions_codeeditor
+    CONF.set('completions',
+             ('provider_configuration', 'lsp', 'values', 'pydocstyle'),
+             True)
+
+    CONF.set('completions',
+             ('provider_configuration', 'lsp', 'values','pycodestyle'),
+             True)
+
+    # After this call the manager needs to be reinitialized
+    completion_plugin.after_configuration_update([])
+    qtbot.wait(2000)
+
+    def teardown():
+        CONF.set('completions',
+                 ('provider_configuration', 'lsp', 'values', 'pydocstyle'),
+                 False)
+
+        CONF.set('completions',
+                 ('provider_configuration', 'lsp', 'values','pycodestyle'),
+                 False)
+
+        # After this call the manager needs to be reinitialized
+        completion_plugin.after_configuration_update([])
+        qtbot.wait(2000)
+
+    request.addfinalizer(teardown)
+
+    return editor, completion_plugin
+
+
 @pytest.mark.slow
 @pytest.mark.second
-@flaky(max_runs=5)
-def test_ignore_warnings(qtbot, lsp_codeeditor):
+# @flaky(max_runs=5)
+def test_ignore_warnings(qtbot, completions_codeeditor_linting):
     """Test that the editor is ignoring some warnings."""
-    editor, manager = lsp_codeeditor
+    editor, completion_plugin = completions_codeeditor_linting
 
     # Set text in editor
     editor.set_text(TEXT)
 
-    CONF.set('lsp-server', 'pydocstyle/ignore', 'D100')
-    CONF.set('lsp-server', 'pycodestyle/ignore', 'E261')
+    CONF.set('completions',
+             ('provider_configuration', 'lsp', 'values', 'pydocstyle/ignore'),
+             'D100')
+
+    CONF.set('completions',
+             ('provider_configuration', 'lsp', 'values','pycodestyle/ignore'),
+             'E261')
 
     # After this call the manager needs to be reinitialized
-    manager.update_configuration()
+    completion_plugin.after_configuration_update([])
     qtbot.wait(2000)
 
     # Notify changes
-    with qtbot.waitSignal(editor.lsp_response_signal, timeout=30000):
+    with qtbot.waitSignal(editor.completions_response_signal, timeout=30000):
         editor.document_did_change()
 
     # Get current warnings
@@ -61,9 +99,15 @@ def test_ignore_warnings(qtbot, lsp_codeeditor):
                 ["""E305 expected 2 blank lines after class or """
                  """function definition, found 0""", 7]]
 
-    CONF.set('lsp-server', 'pydocstyle/ignore', '')
-    CONF.set('lsp-server', 'pycodestyle/ignore', '')
-    manager.update_configuration()
+    CONF.set('completions',
+             ('provider_configuration', 'lsp', 'values', 'pydocstyle/ignore'),
+             '')
+
+    CONF.set('completions',
+             ('provider_configuration', 'lsp', 'values','pycodestyle/ignore'),
+             '')
+
+    completion_plugin.after_configuration_update([])
     qtbot.wait(2000)
 
     assert warnings == expected
@@ -72,15 +116,15 @@ def test_ignore_warnings(qtbot, lsp_codeeditor):
 @pytest.mark.slow
 @pytest.mark.second
 @flaky(max_runs=5)
-def test_adding_warnings(qtbot, lsp_codeeditor):
+def test_adding_warnings(qtbot, completions_codeeditor_linting):
     """Test that warnings are saved in the editor blocks."""
-    editor, _ = lsp_codeeditor
+    editor, _ = completions_codeeditor_linting
 
     # Set text in editor
     editor.set_text(TEXT)
 
     # Notify changes
-    with qtbot.waitSignal(editor.lsp_response_signal, timeout=30000):
+    with qtbot.waitSignal(editor.completions_response_signal, timeout=30000):
         editor.document_did_change()
 
     qtbot.wait(2000)
@@ -107,15 +151,15 @@ def test_adding_warnings(qtbot, lsp_codeeditor):
 @pytest.mark.slow
 @pytest.mark.second
 @flaky(max_runs=5)
-def test_move_warnings(qtbot, lsp_codeeditor):
+def test_move_warnings(qtbot, completions_codeeditor_linting):
     """Test that moving to next/previous warnings is working."""
-    editor, _ = lsp_codeeditor
+    editor, _ = completions_codeeditor_linting
 
     # Set text in editor
     editor.set_text(TEXT)
 
     # Notify changes
-    with qtbot.waitSignal(editor.lsp_response_signal, timeout=30000):
+    with qtbot.waitSignal(editor.completions_response_signal, timeout=30000):
         editor.document_did_change()
 
     # Wait for linting info to arrive
@@ -143,15 +187,15 @@ def test_move_warnings(qtbot, lsp_codeeditor):
 @pytest.mark.slow
 @pytest.mark.second
 @flaky(max_runs=5)
-def test_get_warnings(qtbot, lsp_codeeditor):
+def test_get_warnings(qtbot, completions_codeeditor_linting):
     """Test that the editor is returning the right list of warnings."""
-    editor, _ = lsp_codeeditor
+    editor, _ = completions_codeeditor_linting
 
     # Set text in editor
     editor.set_text(TEXT)
 
     # Notify changes
-    with qtbot.waitSignal(editor.lsp_response_signal, timeout=30000):
+    with qtbot.waitSignal(editor.completions_response_signal, timeout=30000):
         editor.document_did_change()
 
     # Wait for linting info to arrive
@@ -176,18 +220,18 @@ def test_get_warnings(qtbot, lsp_codeeditor):
 @pytest.mark.slow
 @pytest.mark.second
 @flaky(max_runs=5)
-def test_update_warnings_after_delete_line(qtbot, lsp_codeeditor):
+def test_update_warnings_after_delete_line(qtbot, completions_codeeditor_linting):
     """
     Test that code style warnings are correctly updated after deleting a line
     in the Editor.
 
     Regression test for spyder-ide/spyder#9299.
     """
-    editor, _ = lsp_codeeditor
+    editor, _ = completions_codeeditor_linting
     editor.set_text(TEXT)
 
     # Notify changes.
-    with qtbot.waitSignal(editor.lsp_response_signal, timeout=30000):
+    with qtbot.waitSignal(editor.completions_response_signal, timeout=30000):
         editor.document_did_change()
 
     # Wait for linting info to arrive
@@ -198,7 +242,7 @@ def test_update_warnings_after_delete_line(qtbot, lsp_codeeditor):
     editor.delete_line()
 
     # Wait for the lsp_response_signal.
-    qtbot.waitSignal(editor.lsp_response_signal, timeout=30000)
+    qtbot.waitSignal(editor.completions_response_signal, timeout=30000)
 
     # Assert that the W293 warning is gone.
     expected = [['D100: Missing docstring in public module', 1],
@@ -215,20 +259,20 @@ def test_update_warnings_after_delete_line(qtbot, lsp_codeeditor):
 @pytest.mark.slow
 @pytest.mark.second
 @flaky(max_runs=5)
-def test_update_warnings_after_closequotes(qtbot, lsp_codeeditor):
+def test_update_warnings_after_closequotes(qtbot, completions_codeeditor_linting):
     """
     Test that code errors are correctly updated after activating closequotes
     in the Editor.
 
     Regression test for spyder-ide/spyder#9323.
     """
-    editor, _ = lsp_codeeditor
+    editor, _ = completions_codeeditor_linting
     editor.textCursor().insertText("print('test)\n")
 
     expected = [['EOL while scanning string literal', 1]]
 
     # Notify changes.
-    with qtbot.waitSignal(editor.lsp_response_signal, timeout=30000):
+    with qtbot.waitSignal(editor.completions_response_signal, timeout=30000):
         editor.document_did_change()
 
     # Wait for linting info to arrive
@@ -237,7 +281,7 @@ def test_update_warnings_after_closequotes(qtbot, lsp_codeeditor):
     assert editor.get_current_warnings() == expected
 
     # Wait for the lsp_response_signal.
-    with qtbot.waitSignal(editor.lsp_response_signal, timeout=30000):
+    with qtbot.waitSignal(editor.completions_response_signal, timeout=30000):
         # Add a single quote to fix the error
         editor.move_cursor(-2)
         qtbot.keyPress(editor, Qt.Key_Apostrophe)
@@ -252,21 +296,21 @@ def test_update_warnings_after_closequotes(qtbot, lsp_codeeditor):
 @pytest.mark.slow
 @pytest.mark.second
 @flaky(max_runs=5)
-def test_update_warnings_after_closebrackets(qtbot, lsp_codeeditor):
+def test_update_warnings_after_closebrackets(qtbot, completions_codeeditor_linting):
     """
     Test that code errors are correctly updated after activating closebrackets
     in the Editor.
 
     Regression test for spyder-ide/spyder#9323.
     """
-    editor, _ = lsp_codeeditor
+    editor, _ = completions_codeeditor_linting
     editor.textCursor().insertText("print('test'\n")
 
     expected = [['unexpected EOF while parsing', 1],
                 ['E901 TokenError: EOF in multi-line statement', 2]]
 
     # Notify changes.
-    with qtbot.waitSignal(editor.lsp_response_signal, timeout=30000):
+    with qtbot.waitSignal(editor.completions_response_signal, timeout=30000):
         editor.document_did_change()
 
     # Wait for linting info to arrive
@@ -275,7 +319,7 @@ def test_update_warnings_after_closebrackets(qtbot, lsp_codeeditor):
     assert editor.get_current_warnings() == expected
 
     # Wait for the lsp_response_signal.
-    with qtbot.waitSignal(editor.lsp_response_signal, timeout=30000):
+    with qtbot.waitSignal(editor.completions_response_signal, timeout=30000):
         # Add a bracket to fix the error
         editor.move_cursor(-1)
         qtbot.keyPress(editor, Qt.Key_ParenRight)
