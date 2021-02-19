bafb33d4608314abc553f492f162ef6489530571
Carlos Cordoba <ccordoba12@gmail.com>
2021-02-19 01:54:17 -0500

    Fix style issues

--- spyder/api/utils.py
+++ spyder/api/utils.py
@@ -9,9 +9,13 @@
 API utilities.
 """
 
+
 def get_class_values(cls):
     """
     Get the attribute values for the class enumerations used in our
     API.
+
+    Idea from:
+    https://stackoverflow.com/a/17249228/438386
     """
-    return [v for (k,v) in cls.__dict__.items() if k[:1] != '_']
+    return [v for (k, v) in cls.__dict__.items() if k[:1] != '_']
