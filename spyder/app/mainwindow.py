924d9dd19e9294b48a3570c2b4591a8d7d8251a6
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-02-23 11:19:51 -0500

    Remove Kite menu

--- spyder/app/mainwindow.py
+++ spyder/app/mainwindow.py
@@ -1059,18 +1059,6 @@ class MainWindow(QMainWindow):
                 menu_id=ApplicationMenus.Tools,
                 section=ToolsMenuSections.Tools,
                 before=winenv_action)
-        from spyder.plugins.completion.kite.utils.install import (
-            check_if_kite_installed)
-        is_kite_installed, kite_path = check_if_kite_installed()
-        if not is_kite_installed:
-            install_kite_action = create_action(
-                self, _("Install Kite completion engine"),
-                icon=get_icon('kite', adjust_for_interface=True),
-                triggered=self.show_kite_installation)
-            self.mainmenu.add_item_to_application_menu(
-                install_kite_action,
-                menu_id=ApplicationMenus.Tools,
-                section=ToolsMenuSections.Tools)
         self.mainmenu.add_item_to_application_menu(
                 reset_spyder_action,
                 menu_id=ApplicationMenus.Tools)
