27bab415f752352b1c13357f6e99f606b11d6066
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-03-01 20:46:39 -0500

    Prevent IndexError when updating folding

--- spyder/plugins/editor/panels/utils.py
+++ spyder/plugins/editor/panels/utils.py
@@ -54,7 +54,10 @@ class FoldingRegion:
         for child in self.children[node.index + 1:]:
             child.index -= 1
 
-        self.children.pop(node.index)
+        try:
+            self.children.pop(node.index)
+        except IndexError:
+            pass
         for idx, next_idx in zip(self.children, self.children[1:]):
             assert idx.index < next_idx.index
 
