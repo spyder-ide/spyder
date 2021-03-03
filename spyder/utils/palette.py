505cac28663a686063df5e8babeb9aad69004425
juanis2112 <juanitagomezr2112@gmail.com>
2021-03-03 02:32:33 -0500

    Add light palette temporarily to handle qdarkstyle colors in light mode

--- spyder/utils/palette.py
+++ spyder/utils/palette.py
@@ -7,6 +7,7 @@
 # Local imports
 from spyder.utils.color_system import Green, Red, Orange, GroupDark, GroupLight
 from qdarkstyle.colorsystem import Blue, Gray
+from qdarkstyle.darkpalette import DarkPalette
 from spyder.config.gui import is_dark_interface
 
 
@@ -91,8 +92,49 @@ class SpyderPaletteLight:
     GROUP_LIGHT_11 = GroupLight.B110
     GROUP_LIGHT_12 = GroupLight.B120
 
+class LightPalette(object):
+    """Theme variables."""
+
+    # Color
+    COLOR_BACKGROUND_1 = Gray.B140
+    COLOR_BACKGROUND_2 = Gray.B130
+    COLOR_BACKGROUND_3 = Gray.B120
+    COLOR_BACKGROUND_4 = Gray.B110
+    COLOR_BACKGROUND_5 = Gray.B100
+    COLOR_BACKGROUND_6 = Gray.B90
+
+    COLOR_TEXT_1 = Gray.B10
+    COLOR_TEXT_2 = Gray.B30
+    COLOR_TEXT_3 = Gray.B50
+    COLOR_TEXT_4 = Gray.B70
+
+    COLOR_ACCENT_1 = Blue.B130
+    COLOR_ACCENT_2 = Blue.B100
+    COLOR_ACCENT_3 = Blue.B90
+    COLOR_ACCENT_4 = Blue.B70
+    COLOR_ACCENT_5 = Blue.B50
+
+    OPACITY_TOOLTIP = 230
+
+    # Size
+    SIZE_BORDER_RADIUS = '4px'
+
+    # Borders
+    BORDER_1 = '1px solid $COLOR_BACKGROUND_1'
+    BORDER_2 = '1px solid $COLOR_BACKGROUND_4'
+    BORDER_3 = '1px solid $COLOR_BACKGROUND_6'
+
+    BORDER_SELECTION_3 = '1px solid $COLOR_ACCENT_3'
+    BORDER_SELECTION_2 = '1px solid $COLOR_ACCENT_2'
+    BORDER_SELECTION_1 = '1px solid $COLOR_ACCENT_1'
+
+
+    # Example of additional widget specific variables
+    W_STATUS_BAR_BACKGROUND_COLOR = COLOR_ACCENT_1
 
 if is_dark_interface():
     SpyderPalette = SpyderPaletteDark
+    QDarkPalette = DarkPalette
 else:
     SpyderPalete = SpyderPaletteLight
+    QDarkPalette = LightPalette
