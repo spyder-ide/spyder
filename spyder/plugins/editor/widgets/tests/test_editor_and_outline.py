987ac6d9275148dcb6be147f860a28c7690ace81
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-03-24 13:28:40 -0500

    Fix test_editor_and_outline.py

--- spyder/plugins/editor/widgets/tests/test_editor_and_outline.py
+++ spyder/plugins/editor/widgets/tests/test_editor_and_outline.py
@@ -87,10 +87,14 @@ def test_files(tmpdir_factory):
 @pytest.fixture
 def outlineexplorer(qtbot):
     """Set up an OutlineExplorerWidget."""
-    outlineexplorer = OutlineExplorerWidget(
-        show_fullpath=False, show_all_files=True, group_cells=True,
-        show_comments=True, sort_files_alphabetically=False,
-        display_variables=True)
+    outlineexplorer = OutlineExplorerWidget(None, None, None)
+    outlineexplorer.set_conf('show_fullpath', False)
+    outlineexplorer.set_conf('show_all_files', True)
+    outlineexplorer.set_conf('group_cells', True)
+    outlineexplorer.set_conf('show_comments', True)
+    outlineexplorer.set_conf('sort_files_alphabetically', False)
+    outlineexplorer.set_conf('display_variables', True)
+
     # Fix the size of the outline explorer to prevent an
     # 'Unable to set geometry ' warning if the test fails.
     outlineexplorer.setFixedSize(400, 350)
