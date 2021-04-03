b84f2221b253567dfc1f011dee0eda89ae3c36d6
Carlos Cordoba <ccordoba12@gmail.com>
2021-04-02 21:19:39 -0500

    API: Start to version it

---
+++ spyder/api/_version.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © Spyder Project Contributors
+# Licensed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+
+"""Spyder API Version."""
+
+VERSION_INFO = (0, 1, 0)
+__version__ = '.'.join(map(str, VERSION_INFO))
