e485fac3a3f2e2bf25491c6d97eb25af89b72c05
Stephannie Jimenez <steff456@hotmail.com>
2021-04-19 23:25:01 -0500

    fix layout issues

--- spyder/api/widgets/main_widget.py
+++ spyder/api/widgets/main_widget.py
@@ -759,8 +759,6 @@ class PluginMainWidget(QWidget, SpyderWidgetMixin, SpyderToolbarMixin):
         Dock widget visibility has changed.
         """
         is_visible = not self.is_visible
-        self.setVisible(enable)
-        self.is_visible = enable
 
         if self.dockwidget is None:
             return
@@ -784,6 +782,8 @@ class PluginMainWidget(QWidget, SpyderWidgetMixin, SpyderToolbarMixin):
                 focus_widget.setFocus()
         elif force_focus is False:
             pass
+
+        self.is_visible = enable
 
         # TODO: Pending on plugin migration that uses this
         # if getattr(self, 'DISABLE_ACTIONS_WHEN_HIDDEN', None):
