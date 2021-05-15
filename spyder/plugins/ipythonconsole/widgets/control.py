f2b8b496eab4391b90d397b2135413e6937e83cd
Carlos Cordoba <ccordoba12@gmail.com>
2021-05-15 13:49:43 -0500

    Fix a style issue

--- spyder/plugins/ipythonconsole/widgets/control.py
+++ spyder/plugins/ipythonconsole/widgets/control.py
@@ -61,8 +61,8 @@ class ControlWidget(TracebackLinksMixin, GetHelpMixin,
     def keyPressEvent(self, event):
         """Reimplement Qt Method - Basic keypress event handler"""
         event, text, key, ctrl, shift = restore_keyevent(event)
-        if key == Qt.Key_ParenLeft and not self.has_selected_text() \
-          and self.help_enabled and not self.parent()._reading:
+        if (key == Qt.Key_ParenLeft and not self.has_selected_text()
+                and self.help_enabled and not self.parent()._reading):
             self._key_paren_left(text)
         else:
             # Let the parent widget handle the key press event
