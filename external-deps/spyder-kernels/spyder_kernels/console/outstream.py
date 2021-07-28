839d46123c907b000df62e2c5ee781a322be2a0a
Carlos Cordoba <ccordoba12@gmail.com>
2021-07-28 10:07:53 -0500

    git subrepo clone --branch=patch-terminal-size --force https://github.com/ccordoba12/spyder-kernels.git external-deps/spyder-kernels
    subrepo:
      subdir:   "external-deps/spyder-kernels"
      merged:   "c262584b3"
    upstream:
      origin:   "https://github.com/ccordoba12/spyder-kernels.git"
      branch:   "patch-terminal-size"
      commit:   "c262584b3"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

--- external-deps/spyder-kernels/spyder_kernels/console/outstream.py
+++ external-deps/spyder-kernels/spyder_kernels/console/outstream.py
@@ -16,6 +16,7 @@ from ipykernel.iostream import OutStream
 class TTYOutStream(OutStream):
     """Subclass of OutStream that represents a TTY."""
 
-    def __init__(self, session, pub_thread, name, pipe=None, echo=None, *, watchfd=True):
+    def __init__(self, session, pub_thread, name, pipe=None, echo=None, *,
+                 watchfd=True):
         super().__init__(session, pub_thread, name, pipe,
                          echo=echo, watchfd=watchfd, isatty=True)
