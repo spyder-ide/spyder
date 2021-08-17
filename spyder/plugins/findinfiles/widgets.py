07f07143a6fe4bc49c75dd068a3abe4a433e5d0e
Carlos Cordoba <ccordoba12@gmail.com>
2021-08-17 12:14:45 -0500

    Find: Fix style issue

--- spyder/plugins/findinfiles/widgets.py
+++ spyder/plugins/findinfiles/widgets.py
@@ -1061,7 +1061,7 @@ class FindInFilesWidget(PluginMainWidget):
         # Toolbar
         toolbar = self.get_main_toolbar()
         for item in [self.search_text_edit, self.find_action,
-                     self.search_regexp_action,self.case_action,
+                     self.search_regexp_action, self.case_action,
                      self.more_options_action]:
             self.add_item_to_toolbar(
                 item,
