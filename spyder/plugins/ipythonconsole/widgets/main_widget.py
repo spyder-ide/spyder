65e0e1334b76a15522af56f1dd542c366acee0e0
dalthviz <d.althviz10@uniandes.edu.co>
2021-11-24 15:28:58 -0500

    Update core dependencies versions

--- spyder/plugins/ipythonconsole/widgets/main_widget.py
+++ spyder/plugins/ipythonconsole/widgets/main_widget.py
@@ -1505,12 +1505,12 @@ class IPythonConsoleWidget(PluginMainWidget):
             has_spyder_kernels = programs.is_module_installed(
                 'spyder_kernels',
                 interpreter=pyexec,
-                version='>=2.0.1;<2.2.0')
+                version='>=2.2.0;<2.3.0')
             if not has_spyder_kernels and not running_under_pytest():
                 client.show_kernel_error(
                     _("Your Python environment or installation doesn't have "
                       "the <tt>spyder-kernels</tt> module or the right "
-                      "version of it installed (>= 2.0.1 and < 2.2.0). "
+                      "version of it installed (>= 2.2.0 and < 2.3.0). "
                       "Without this module is not possible for Spyder to "
                       "create a console for you.<br><br>"
                       "You can install it by running in a system terminal:"
