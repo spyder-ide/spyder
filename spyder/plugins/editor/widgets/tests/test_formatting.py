0c9e015973f2e82d636504972bf772902b4c7e0f
Carlos Cordoba <ccordoba12@gmail.com>
2022-03-29 11:14:34 -0500

    Testing: Check that the max_line_length is working when formating code

--- spyder/plugins/editor/widgets/tests/test_formatting.py
+++ spyder/plugins/editor/widgets/tests/test_formatting.py
@@ -12,18 +12,18 @@ import os.path as osp
 
 # Third party imports
 import pytest
-import yapf
-
-# Qt imports
 from qtpy.QtGui import QTextCursor
+import yapf
 
 # Local imports
 from spyder.config.manager import CONF
 from spyder.utils.programs import is_module_installed
 
 
+# ---- Auxiliary constants and functions
 HERE = osp.dirname(osp.abspath(__file__))
 ASSETS = osp.join(HERE, 'assets')
+
 
 autopep8 = pytest.param(
     'autopep8',
@@ -50,8 +50,14 @@ black = pytest.param(
 )
 
 
-def get_formatter_values(formatter, newline, range_fmt=False):
-    suffix = 'range' if range_fmt else 'result'
+def get_formatter_values(formatter, newline, range_fmt=False, max_line=False):
+    if range_fmt:
+        suffix = 'range'
+    elif max_line:
+        suffix = 'max_line'
+    else:
+        suffix = 'result'
+
     original_file = osp.join(ASSETS, 'original_file.py')
     formatted_file = osp.join(ASSETS, '{0}_{1}.py'.format(formatter, suffix))
 
@@ -66,6 +72,7 @@ def get_formatter_values(formatter, newline, range_fmt=False):
     return text, result
 
 
+# ---- Tests
 @pytest.mark.slow
 @pytest.mark.order(1)
 @pytest.mark.parametrize('formatter', [autopep8, yapf, black])
@@ -76,10 +83,13 @@ def test_document_formatting(formatter, newline, completions_codeeditor,
     code_editor, completion_plugin = completions_codeeditor
     text, expected = get_formatter_values(formatter, newline)
 
-    # After this call the manager needs to be reinitialized
-    CONF.set('completions',
-             ('provider_configuration', 'lsp', 'values','formatting'),
-             formatter)
+    # Set formatter
+    CONF.set(
+        'completions',
+        ('provider_configuration', 'lsp', 'values', 'formatting'),
+        formatter
+    )
+    completion_plugin.after_configuration_update([])
     qtbot.wait(2000)
 
     # Set text in editor
@@ -114,10 +124,12 @@ def test_document_range_formatting(formatter, newline, completions_codeeditor,
     code_editor, completion_plugin = completions_codeeditor
     text, expected = get_formatter_values(formatter, newline, range_fmt=True)
 
-    # After this call the manager needs to be reinitialized
-    CONF.set('completions',
-             ('provider_configuration', 'lsp', 'values','formatting'),
-             formatter)
+    # Set formatter
+    CONF.set(
+        'completions',
+        ('provider_configuration', 'lsp', 'values', 'formatting'),
+        formatter
+    )
     completion_plugin.after_configuration_update([])
     qtbot.wait(2000)
 
@@ -150,4 +162,48 @@ def test_document_range_formatting(formatter, newline, completions_codeeditor,
 
     # Wait to text to be formatted
     qtbot.wait(2000)
+    assert code_editor.get_text_with_eol() == expected
+
+
+@pytest.mark.slow
+@pytest.mark.order(1)
+@pytest.mark.parametrize('formatter', [autopep8, black])
+def test_max_line_length(formatter, completions_codeeditor, qtbot):
+    """Validate autoformatting with a different value of max_line_length."""
+    code_editor, completion_plugin = completions_codeeditor
+    text, expected = get_formatter_values(
+        formatter, newline='\n', max_line=True)
+    max_line_length = 20
+
+    # Set formatter and max line length options
+    CONF.set(
+        'completions',
+        ('provider_configuration', 'lsp', 'values', 'formatting'),
+        formatter
+    )
+    CONF.set(
+        'completions',
+        ('provider_configuration', 'lsp', 'values',
+         'pycodestyle/max_line_length'),
+        max_line_length
+    )
+    completion_plugin.after_configuration_update([])
+    qtbot.wait(2000)
+
+    # Set text in editor
+    code_editor.set_text(text)
+
+    # Notify changes
+    with qtbot.waitSignal(
+            code_editor.completions_response_signal, timeout=30000):
+        code_editor.document_did_change()
+
+    # Perform formatting
+    with qtbot.waitSignal(
+            code_editor.completions_response_signal, timeout=30000):
+        code_editor.format_document()
+
+    # Wait for text to be formatted
+    qtbot.wait(2000)
+
     assert code_editor.get_text_with_eol() == expected
