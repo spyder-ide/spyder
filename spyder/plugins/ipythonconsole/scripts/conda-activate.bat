978869814b87f77b7695a74b75f81c9908a33a08
Ryan Clary <mrclary@gmail.com>
2022-04-27 13:41:31 -0700

    Update conda-actiavate.(sh|bat) to activate environment using micromamba if the activation script is a micromamba executable

--- spyder/plugins/ipythonconsole/scripts/conda-activate.bat
+++ spyder/plugins/ipythonconsole/scripts/conda-activate.bat
@@ -10,8 +10,13 @@ set SPYDER_KERNEL_SPEC=%4
 :: Enforce encoding
 chcp 65001>nul
 
-:: Activate kernel environment
-call %CONDA_ACTIVATE_SCRIPT% %CONDA_ENV_PATH%
+echo %CONDA_ACTIVATE_SCRIPT%| findstr /e "micromamba.exe">Nul && (
+    for /f %%i in ('%CONDA_ACTIVATE_SCRIPT% shell activate %CONDA_ENV_PATH%') do set SCRIPT=%%i
+    call %SCRIPT%
+) || (
+    :: Activate kernel environment
+    call %CONDA_ACTIVATE_SCRIPT% %CONDA_ENV_PATH%
+)
 
 :: Start kernel
 %CONDA_ENV_PYTHON% -m spyder_kernels.console -f %SPYDER_KERNEL_SPEC%
