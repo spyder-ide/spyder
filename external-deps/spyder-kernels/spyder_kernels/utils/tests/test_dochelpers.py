0a98d6e2ce212743e09dce5d6ed3ce2eee459e07
Ryan Clary <mrclary@gmail.com>
2022-06-21 13:49:53 -0700

    git subrepo clone (merge) --branch=2.x --force https://github.com/spyder-ide/spyder-kernels.git external-deps/spyder-kernels
    subrepo:
      subdir:   "external-deps/spyder-kernels"
      merged:   "4302062d8"
    upstream:
      origin:   "https://github.com/spyder-ide/spyder-kernels.git"
      branch:   "2.x"
      commit:   "4302062d8"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

--- external-deps/spyder-kernels/spyder_kernels/utils/tests/test_dochelpers.py
+++ external-deps/spyder-kernels/spyder_kernels/utils/tests/test_dochelpers.py
@@ -9,9 +9,9 @@
 """
 Tests for dochelpers.py
 """
+
 # Standard library imports
 import os
-import sys
 
 # Test library imports
 import pytest
@@ -27,57 +27,29 @@ class Test(object):
         pass
 
 
-@pytest.mark.skipif(not PY2, reason="It fails in Python 3")
+@pytest.mark.skipif(PY2 or os.name == 'nt',
+                    reason="Only works on Linux and Mac")
 def test_dochelpers():
     """Test dochelpers."""
+    assert getargtxt(Test.method) == ['x, ', 'y=2']
     assert not getargtxt(Test.__init__)
-    if PY2:                    
-        assert getargtxt(Test.method) == ['x, ', 'y=2']
-        assert getdoc(sorted) == {'note': 'Function of __builtin__ module',
-                                  'argspec': u'(iterable, cmp=None, key=None, '
-                                              'reverse=False)',
-                                  'docstring': u'sorted(iterable, cmp=None, '
-                                                'key=None, reverse=False) --> '
-                                                'new sorted list',
-                                  'name': 'sorted'}
-        assert getargtxt(sorted) == ['iterable, ', ' cmp=None, ',
-                                     ' key=None, ', ' reverse=False']
-    else:
-        assert not getargtxt(Test.method)
-        if os.name == 'nt':
-            assert getdoc(sorted) == {'note': 'Function of builtins module',
-                                      'argspec': '(...)',
-                                      'docstring': 'Return a new list '
-                                                   'containing '
-                                                   'all items from the '
-                                                   'iterable in ascending '
-                                                   'order.\n\nA custom '
-                                                   'key function can be '
-                                                   'supplied to customise the '
-                                                   'sort order, and '
-                                                   'the\nreverse flag can be '
-                                                   'set to request the result '
-                                                   'in descending order.',
-                                      'name': 'sorted'}
-        else:
-            assert getdoc(sorted) == {'note': 'Function of builtins module',
-                                      'argspec': '(...)',
-                                      'docstring': 'Return a new list '
-                                                   'containing '
-                                                   'all items from the '
-                                                   'iterable in ascending '
-                                                   'order.\n\nA custom '
-                                                   'key function can be '
-                                                   'supplied to customize the '
-                                                   'sort order, and '
-                                                   'the\nreverse flag can be '
-                                                   'set to request the result '
-                                                   'in descending order.',
-                                      'name': 'sorted'}
-        assert not getargtxt(sorted)
+
+    assert getdoc(sorted) == {
+        'note': 'Function of builtins module',
+        'argspec': '(...)',
+        'docstring': 'Return a new list containing all items from the '
+                     'iterable in ascending order.\n\nA custom key function '
+                     'can be supplied to customize the sort order, and the\n'
+                     'reverse flag can be set to request the result in '
+                     'descending order.',
+        'name': 'sorted'
+    }
+    assert not getargtxt(sorted)
+
     assert isdefined('numpy.take', force_import=True)
     assert isdefined('__import__')
-    assert not isdefined('.keys', force_import=True)
+    assert not isdefined('zzz', force_import=True)
+
     assert getobj('globals') == 'globals'
     assert not getobj('globals().keys')
     assert getobj('+scipy.signal.') == 'scipy.signal'
