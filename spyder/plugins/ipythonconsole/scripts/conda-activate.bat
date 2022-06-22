a93f2927b452402ab4b6afdb9ba10a89389b6e66
Ryan Clary <mrclary@gmail.com>
2022-06-21 18:06:21 -0700

    Clean up the script.

--- spyder/plugins/ipythonconsole/scripts/conda-activate.bat
+++ spyder/plugins/ipythonconsole/scripts/conda-activate.bat
@@ -11,17 +11,16 @@ set SPYDER_KERNEL_SPEC=%4
 chcp 65001>nul
 
 :: Activate kernel environment
-echo %CONDA_ACTIVATE_SCRIPT%| findstr /e "micromamba.exe">Nul && (
-    for /f %%i in ('%CONDA_ACTIVATE_SCRIPT% shell activate %CONDA_ENV_PATH%') do set SCRIPT=%%i
-    goto micromamba
-)
-
-:: Activate using conda
-call %CONDA_ACTIVATE_SCRIPT% %CONDA_ENV_PATH%
-goto start
+echo %CONDA_ACTIVATE_SCRIPT%| findstr /e "micromamba.exe">Nul && goto micromamba || goto conda
 
 :micromamba Activate using micromamba
+for /f %%i in ('%CONDA_ACTIVATE_SCRIPT% shell activate %CONDA_ENV_PATH%') do set SCRIPT=%%i
 call %SCRIPT%
+goto start
+
+:conda Activate using conda
+call %CONDA_ACTIVATE_SCRIPT% %CONDA_ENV_PATH%
+goto start
 
 :start Start kernel
 %CONDA_ENV_PYTHON% -m spyder_kernels.console -f %SPYDER_KERNEL_SPEC%
