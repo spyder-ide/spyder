ecf74a0a4a122b98cace1094d5da94b35e925ca1
Ryan Clary <mrclary@gmail.com>
2022-06-28 17:36:16 -0700

    Copy liblzma.5.dylib again from environment PIL package to resolve signing error.
    On CI, this dylib is first copied from from /usr/.../xz to Frameworks, then again from PIL to Frameworks. This is causing metadata issues for signing. Local builds only copy from PIL to Frameworks.

--- installers/macOS/codesign.sh
+++ installers/macOS/codesign.sh
@@ -4,7 +4,7 @@ set -e
 CERTFILE=certificate.p12
 KEY_PASS=keypass
 KEYCHAIN=build.keychain
-KEYCHAINFILE=${HOME}/Library/Keychains/${KEYCHAIN}-db
+KEYCHAINFILE=$HOME/Library/Keychains/$KEYCHAIN-db
 
 # certificate common name
 CNAME="Developer ID Application: Ryan Clary (6D7ZTH6B38)"
@@ -30,31 +30,31 @@ EOF
 
 cleanup () {
     security default-keychain -s login.keychain  # restore default keychain
-    rm -f ${CERTFILE}  # remove cert file
+    rm -f $CERTFILE  # remove cert file
 
     # remove temporary keychain
-    if security delete-keychain ${KEYCHAIN} 2> /dev/null; then
-        echo "clean up: ${KEYCHAIN} deleted."
+    if security delete-keychain $KEYCHAIN 2> /dev/null; then
+        echo "clean up: $KEYCHAIN deleted."
     fi
 
     # make sure temporary keychain file is gone
-    if [[ -e ${KEYCHAINFILE} ]]; then
-        rm -f ${KEYCHAINFILE}
-        echo "clean up: ${KEYCHAINFILE} file deleted"
+    if [[ -e $KEYCHAINFILE ]]; then
+        rm -f $KEYCHAINFILE
+        echo "clean up: $KEYCHAINFILE file deleted"
     fi
 }
 
 unset CERT PASS DEEP ELEM APP
 
 while getopts "hc:p:de" option; do
-    case ${option} in
+    case $option in
         h)
             help
             exit;;
         c)
-            CERT="${OPTARG}";;
+            CERT="$OPTARG";;
         p)
-            PASS="${OPTARG}";;
+            PASS="$OPTARG";;
         d)
             DEEP="--deep"
             unset ELEM;;
@@ -63,11 +63,11 @@ while getopts "hc:p:de" option; do
             unset DEEP;;
     esac
 done
-shift $((${OPTIND} - 1))
+shift $(($OPTIND - 1))
 
-test -z "${CERT}" && error "Error: Certificate must be provided. ${CERT}"
+test -z "$CERT" && error "Error: Certificate must be provided. $CERT"
 
-test -z "${PASS}" && error "Error: Password must be provided. ${PASS}"
+test -z "$PASS" && error "Error: Password must be provided. $PASS"
 
 test -n "$1" && APP="$1" || error "Error: Application must be provided."
 
@@ -79,39 +79,39 @@ cleanup  # make sure keychain and file don't exist
 # --- Prepare the certificate
 # decode certificate-as-Github-secret back to p12 for import into keychain
 echo "Decoding Certificate..."
-echo $CERT | base64 --decode > ${CERTFILE}
+echo $CERT | base64 --decode > $CERTFILE
 
 # --- Create keychain
 echo "Creating keychain..."
-security create-keychain -p ${KEY_PASS} ${KEYCHAIN}
+security create-keychain -p $KEY_PASS $KEYCHAIN
 
 # Set keychain to default and unlock it so that we can add the certificate
 # without GUI prompt
 echo "Importing certificate..."
-security default-keychain -s ${KEYCHAIN}
-security unlock-keychain -p ${KEY_PASS} ${KEYCHAIN}
-security import ${CERTFILE} -k ${KEYCHAIN} -P ${PASS} -T /usr/bin/codesign
+security default-keychain -s $KEYCHAIN
+security unlock-keychain -p $KEY_PASS $KEYCHAIN
+security import $CERTFILE -k $KEYCHAIN -P $PASS -T /usr/bin/codesign
 
 # Ensure that codesign can access the cert without GUI prompt
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k ${KEY_PASS} ${KEYCHAIN}
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $KEY_PASS $KEYCHAIN
 
 # verify import
 echo "Verifying identity..."
-security find-identity -p codesigning -v ${KEYCHAIN}
+security find-identity -p codesigning -v $KEYCHAIN
 
 # ---- Sign app
-if [[ -n ${ELEM} ]]; then
+if [[ -n $ELEM ]]; then
     echo "Signing individual elements..."
     # sign frameworks first
-    for framework in ${APP}/Contents/Frameworks/*; do
-        /usr/bin/codesign -f -v -s "${CNAME}" "${framework}"
+    for framework in $APP/Contents/Frameworks/*; do
+        codesign -f -v -s "$CNAME" "$framework"
     done
 
     # sign extra binary next
     echo "Signing extra binary..."
-    /usr/bin/codesign -f -v -s "${CNAME}" "${APP}/Contents/MacOS/python"
+    codesign -f -v -s "$CNAME" "$APP/Contents/MacOS/python"
 fi
 
 # sign app bundle
 echo "Signing application..."
-/usr/bin/codesign -f -v ${DEEP} -s "${CNAME}" "${APP}"
+codesign -f -v $DEEP -s "$CNAME" "$APP"
