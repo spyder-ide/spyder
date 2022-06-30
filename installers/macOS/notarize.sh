784299b78cc48d410a00a84671efefe435cf8fc0
Ryan Clary <mrclary@gmail.com>
2022-06-30 12:39:54 -0700

    Use altool rather than notarytool.
    notarytool is not available until Xcode >13. For Xcode <13 (CI Xcode = 12), altool must be used.

--- installers/macOS/notarize.sh
+++ installers/macOS/notarize.sh
@@ -11,14 +11,20 @@ Required:
 Options:
   -h          Display this help
 
+  -i INTERVAL Interval in seconds at which notarization status is polled.
+              Default is 30s.
+
   -p PWD      Developer application-specific password
 
 EOF
 }
 
-while getopts "hp:" option; do
+INTERVAL=30
+
+while getopts "hi:p:" option; do
     case $option in
         (h) help; exit ;;
+        (i) INTERVAL=$OPTARG ;;
         (p) PWD=$OPTARG ;;
     esac
 done
@@ -33,6 +39,9 @@ log(){
 [[ -z $PWD ]] && log "Application-specific password not provided" && exit 1
 [[ $# = 0 ]] && log "File not provided" && exit 1
 
+APPLEID="mrclary@me.com"
+BUNDLEID="com.spyder-ide.Spyder"
+
 DMG=$(cd $(dirname $1) && pwd -P)/$(basename $1)  # Resolve full path
 
 # --- Get certificate id
@@ -41,18 +50,29 @@ log "Certificate ID: $CNAME"
 
 # --- Notarize
 log "Notarizing..."
-xcrun notarytool submit $DMG --wait --team-id $CNAME --apple-id mrclary@me.com --password "$PWD" | tee temp.txt
+auth_args=("--username" "$APPLEID" "--password" "$PWD")
 
-submitid=$(pcregrep -o1 "^\s*id: ([0-9a-z-]+)" temp.txt | head -1)
-status=$(pcregrep -o1 "^\s*status: (\w+$)" temp.txt)
-rm temp.txt
+xcrun altool --notarize-app  --file $DMG --primary-bundle-id $BUNDLEID ${auth_args[@]} | tee result.txt
+requuid=$(pcregrep -o1 "^\s*RequestUUID = ([0-9a-z-]+)$" result.txt)
+
+status="in progress"
+while [[ "$status" = "in progress" ]]; do
+    sleep $INTERVAL
+    xcrun altool --notarization-info $requuid ${auth_args[@]} > result.txt
+    status=$(pcregrep -o1 "^\s*Status: ([\w\s]+)$" result.txt)
+    log "Status: $status"
+done
 
 log "Notary log:"
-xcrun notarytool log $submitid --team-id $CNAME --apple-id mrclary@me.com --password "$PWD"
+logurl=$(pcregrep -o1 "^\s*LogFileURL: (.*)$" result.txt)
+curl $logurl
 
-if [[ "$status" = "Accepted" ]]; then
+rm result.txt
+
+if [[ $status = "success" ]]; then
     log "Stapling notary ticket..."
     xcrun stapler staple -v "$DMG"
+else
+    log "Notarization unsuccessful"
+    exit 1
 fi
-
-# spctl -a -t exec -vv $DMG
