21617d411d7a038d8e37c94e2ad98321c6caef19
jsbautista <42411448+jsbautista@users.noreply.github.com>
2022-07-05 17:21:07 -0500

    Apply suggestions from code review
    Co-authored-by: Daniel Althviz Moré <d.althviz10@uniandes.edu.co>

--- installers/Windows/installer.py
+++ installers/Windows/installer.py
@@ -374,9 +374,12 @@ def run(python_version, bitness, repo_root, entrypoint, package, icon_path,
                 "installers/Windows/assets/nsist/plugins/x86-unicode/")
             for element in contents:
                 shutil.copy(
-                    "installers/Windows/assets/nsist/plugins/x86-unicode/" +
-                    element,
-                    "C:/Program Files (x86)/NSIS/Plugins/x86-unicode/"+element)
+                    os.path.join(
+                        "installers/Windows/assets/nsist/plugins/x86-unicode/",
+                        element),
+                    os.path.join(
+                        "C:/Program Files (x86)/NSIS/Plugins/x86-unicode/",
+                        element))
 
             if template:
                 print("Copying template into discoverable path for Pynsist")
