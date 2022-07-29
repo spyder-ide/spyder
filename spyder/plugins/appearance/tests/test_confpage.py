9fabf8f57fc412972500151c216f72116df04410
Carlos Cordoba <ccordoba12@gmail.com>
2022-07-29 17:03:43 -0500

    Testing: Fix test_change_ui_theme_and_color_scheme

--- spyder/plugins/appearance/tests/test_confpage.py
+++ spyder/plugins/appearance/tests/test_confpage.py
@@ -52,7 +52,7 @@ def test_change_ui_theme_and_color_scheme(config_dialog, mocker, qtbot):
     widget.schemes_combobox.setCurrentIndex(names.index('pydev'))
     dlg.apply_btn.clicked.emit()
     assert SpyderConfigPage.prompt_restart_required.call_count == 1
-    assert CONF.disable_notifications.call_count == 1
+    assert CONF.disable_notifications.call_count == 2
 
     # Change to the 'dark' ui theme
     widget.ui_combobox.setCurrentIndex(2)
@@ -64,22 +64,22 @@ def test_change_ui_theme_and_color_scheme(config_dialog, mocker, qtbot):
     widget.ui_combobox.setCurrentIndex(0)
     dlg.apply_btn.clicked.emit()
     assert SpyderConfigPage.prompt_restart_required.call_count == 2
-    assert CONF.disable_notifications.call_count == 2
+    assert CONF.disable_notifications.call_count == 4
 
     # Change to the 'light' ui theme
     widget.ui_combobox.setCurrentIndex(1)
     dlg.apply_btn.clicked.emit()
     assert SpyderConfigPage.prompt_restart_required.call_count == 3
-    assert CONF.disable_notifications.call_count == 3
+    assert CONF.disable_notifications.call_count == 6
 
     # Change to another dark color scheme
     widget.schemes_combobox.setCurrentIndex(names.index('solarized/dark'))
     dlg.apply_btn.clicked.emit()
     assert SpyderConfigPage.prompt_restart_required.call_count == 4
-    assert CONF.disable_notifications.call_count == 4
+    assert CONF.disable_notifications.call_count == 8
 
     # Change to the 'automatic' ui theme again
     widget.ui_combobox.setCurrentIndex(0)
     dlg.apply_btn.clicked.emit()
     assert SpyderConfigPage.prompt_restart_required.call_count == 4
-    assert CONF.disable_notifications.call_count == 4
+    assert CONF.disable_notifications.call_count == 8
