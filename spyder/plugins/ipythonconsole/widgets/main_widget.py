34cb1d07050116f7d28ee5ac89b463c4e95e38e0
Quentin Peter <impact27@users.noreply.github.com>
2022-08-30 22:49:25 +0200

    Update spyder/plugins/ipythonconsole/widgets/main_widget.py
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>

--- spyder/plugins/ipythonconsole/widgets/main_widget.py
+++ spyder/plugins/ipythonconsole/widgets/main_widget.py
@@ -2319,13 +2319,11 @@ class IPythonConsoleWidget(PluginMainWidget):
                 if args:
                     line += " %s" % norm(args)
             else:
-                QMessageBox.warning(
-                    self,
-                    _('Warning'),
-                    _("The client is not a spyder-kernel "
-                      "to run <b>{}</b>.<br><br>Please Use a spyder-kernel."
-                      ).format(method),
-                    QMessageBox.Ok
+                client.shellwidget.append_html_message(
+                    _("The console is not running a Spyder-kernel, so it "
+                      "can't execute <b>{}</b>.<br><br>"
+                      "Please use a Spyder-kernel for this.").format(method),
+                    before_prompt=True
                 )
                 return
 
