052d03e06bc6c4171c77878fb17a31bd41db1dac
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2022-10-03 13:11:49 -0700

    Fix PermissionError - Put feedstock build.sh patch into separate file and apply only for posix platforms. - Build executable in build.sh patch - Allow user's shell to be determined at runtime instead of install time. - Set SPYDER_APP to runtime location of app bundle - Compile application executable for macOS at conda package build time. Requires shc - Post install script moves compiled executable to correct location and patches Info.plist
    Note that Spyder.sh must be compiled before or at conda-build; referenced install-time dependent variables must be included in LSEnvironment.

--- installers-conda/build_conda_pkgs.py
+++ installers-conda/build_conda_pkgs.py
@@ -152,21 +152,12 @@ class SpyderCondaPkg(BuildCondaPkg):
         self.yaml['source']['patches'] = patches
 
     def _patch_build(self):
-        file = self.fdstk_path / "recipe" / "build.sh"
-        text = file.read_text()
-        text += dedent(
-            """
-            mkdir -p "${PREFIX}/Menu"
-            sed "s/__PKG_VERSION__/${PKG_VERSION}/" """
-            """"${SRC_DIR}/installers-conda/resources/spyder-menu.json" """
-            """> "${PREFIX}/Menu/spyder-menu.json"
-            cp "${SRC_DIR}/img_src/spyder.png" "${PREFIX}/Menu/spyder.png"
-            cp "${SRC_DIR}/img_src/spyder.icns" "${PREFIX}/Menu/spyder.icns"
-            cp "${SRC_DIR}/img_src/spyder.ico" "${PREFIX}/Menu/spyder.ico"
-
-            """
-        )
-        file.write_text(text)
+        if os.name == 'posix':
+            file = self.fdstk_path / "recipe" / "build.sh"
+            build_patch = RESOURCES / "build-patch.sh"
+            text = file.read_text()
+            text += build_patch.read_text()
+            file.write_text(text)
 
 
 class PylspCondaPkg(BuildCondaPkg):
@@ -237,7 +228,7 @@ if __name__ == "__main__":
             """
         ),
         usage="python build_conda_pkgs.py "
-              "[--build subrepo [subrepo] ...] [--debug]",
+              "[--build BUILD [BUILD] ...] [--debug]",
     )
     p.add_argument(
         '--debug', action='store_true', default=False,
