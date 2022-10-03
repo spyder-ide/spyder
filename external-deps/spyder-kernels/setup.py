3a35065598deebbd434d24558259a81306257e5f
Carlos Cordoba <ccordoba12@gmail.com>
2022-10-03 10:59:08 -0300

    git subrepo pull (merge) external-deps/spyder-kernels
    subrepo:
      subdir:   "external-deps/spyder-kernels"
      merged:   "9697d7532"
    upstream:
      origin:   "https://github.com/spyder-ide/spyder-kernels.git"
      branch:   "master"
      commit:   "9697d7532"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

--- external-deps/spyder-kernels/setup.py
+++ external-deps/spyder-kernels/setup.py
@@ -37,7 +37,7 @@ def get_version(module='spyder_kernels'):
 
 REQUIREMENTS = [
     'cloudpickle',
-    'ipykernel>=6.9.2,<7',
+    'ipykernel>=6.15.3,<7',
     'ipython>=7.31.1,<8',
     'jupyter-client>=7.3.4,<8',
     'packaging',
@@ -59,6 +59,7 @@ TEST_REQUIREMENTS = [
     'scipy',
     'xarray',
     'pillow',
+    'django',
 ]
 
 setup(
