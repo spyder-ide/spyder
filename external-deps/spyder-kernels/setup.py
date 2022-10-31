c40ee4badb52b7b3fff882658f5589a3b685da9c
dalthviz <d.althviz10@uniandes.edu.co>
2022-10-31 11:06:32 -0500

    git subrepo clone (merge) --branch=2.x --force https://github.com/spyder-ide/spyder-kernels.git external-deps/spyder-kernels
    subrepo:
      subdir:   "external-deps/spyder-kernels"
      merged:   "6fa55207f"
    upstream:
      origin:   "https://github.com/spyder-ide/spyder-kernels.git"
      branch:   "2.x"
      commit:   "6fa55207f"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

--- external-deps/spyder-kernels/setup.py
+++ external-deps/spyder-kernels/setup.py
@@ -40,7 +40,7 @@ REQUIREMENTS = [
     'backports.functools-lru-cache; python_version<"3"',
     'cloudpickle',
     'ipykernel<5; python_version<"3"',
-    'ipykernel>=6.15.3,<7; python_version>="3"',
+    'ipykernel>=6.16.1,<7; python_version>="3"',
     'ipython<6; python_version<"3"',
     'ipython>=7.31.1,<8; python_version>="3"',
     'jupyter-client>=5.3.4,<6; python_version<"3"',
