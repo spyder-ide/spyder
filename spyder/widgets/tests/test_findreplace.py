a4920455fe2b74c25b399572a6788183d8754eea
Carlos Cordoba <ccordoba12@gmail.com>
2022-11-09 20:43:20 -0500

    Testing: Check functionality of the new messages_label in FindReplace
    Also check that the replace button is checked when showing the replace
    row directly.

--- spyder/widgets/tests/test_findreplace.py
+++ spyder/widgets/tests/test_findreplace.py
@@ -99,5 +99,55 @@ def test_replace_selection(findreplace_editor, qtbot):
     assert len(editor.get_selected_text()) == len(expected)
 
 
+def test_messages_label(findreplace_editor, qtbot):
+    """
+    Test that we set the right icons and tooltips on messages_label.
+    """
+    editor = findreplace_editor.editor
+    findreplace = findreplace_editor.findreplace
+    editor.set_text('Spyder as great!')
+
+    # Search for missing text
+    edit = findreplace.search_text.lineEdit()
+    edit.clear()
+    qtbot.keyClicks(edit, 'foo')
+    assert not findreplace.number_matches_text.isVisible()
+    assert findreplace.messages_label.pixmap().toImage() == \
+           findreplace.warning_icon.toImage()
+    assert findreplace.messages_label.toolTip() == \
+           findreplace.TOOLTIP['no_matches']
+
+    # Search with wrong regexp
+    msg = ': nothing to repeat at position 0'
+    edit.clear()
+    findreplace.re_button.setChecked(True)
+    qtbot.keyClicks(edit, '?')
+    assert not findreplace.number_matches_text.isVisible()
+    assert findreplace.messages_label.pixmap().toImage() == \
+           findreplace.error_icon.toImage()
+    assert findreplace.messages_label.toolTip() == \
+           findreplace.TOOLTIP['regexp_error'] + msg
+
+    # Search for available text
+    edit.clear()
+    qtbot.keyClicks(edit, 'great')
+    qtbot.wait(500)
+    assert not findreplace.messages_label.isVisible()
+    assert findreplace.number_matches_text.isVisible()
+    assert findreplace.number_matches_text.text() == '1 of 1'
+
+
+def test_replace_button(findreplace_editor, qtbot):
+    """
+    Test that the replace button is checked when showing the replace row
+    directly.
+    """
+    findreplace = findreplace_editor.findreplace
+    findreplace.hide()
+    findreplace.show(hide_replace=False)
+    qtbot.wait(500)
+    assert findreplace.replace_text_button.isChecked()
+
+
 if __name__ == "__main__":
     pytest.main([os.path.basename(__file__)])
