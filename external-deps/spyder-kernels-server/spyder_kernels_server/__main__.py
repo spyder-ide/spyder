d02be3f014c1c7f4c45165fdf30f1c68cbd39625
Quentin Peter <quentin.peter@bluewin.ch>
2022-12-05 22:27:36 +0100

    Revert "QApplication"
    This reverts commit 9913876c76dab3f1063c545c70f7602554c29b91.

--- external-deps/spyder-kernels-server/spyder_kernels_server/__main__.py
+++ external-deps/spyder-kernels-server/spyder_kernels_server/__main__.py
@@ -10,63 +10,50 @@ import zmq
 import json
 from spyder_kernels_server.kernel_server import KernelServer
 from zmq.ssh import tunnel as zmqtunnel
-from qtpy.QtWidgets import QApplication
-from qtpy.QtCore import QSocketNotifier, QObject, QCoreApplication
 
 
-class Server(QObject):
+def main():
+    if len(sys.argv) > 1:
+        port = sys.argv[1]
+    else:
+        port = str(zmqtunnel.select_random_ports(1)[0])
 
-    def __init__(self):
-        super().__init__()
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:%s" % port)
+    print(f"Server running on port {port}")
+    kernel_server = KernelServer()
+    shutdown = False
 
-        if len(sys.argv) > 1:
-            port = sys.argv[1]
-        else:
-            port = str(zmqtunnel.select_random_ports(1)[0])
-
-        context = zmq.Context()
-        self.socket = context.socket(zmq.REP)
-        self.socket.bind("tcp://*:%s" % port)
-        print(f"Server running on port {port}")
-        self.kernel_server = KernelServer()
-
-        self._notifier = QSocketNotifier(self.socket.getsockopt(zmq.FD),
-                                         QSocketNotifier.Read, self)
-        self._notifier.activated.connect(self._socket_activity)
-
-    def _socket_activity(self):
-        self._notifier.setEnabled(False)
+    while not shutdown:
         #  Wait for next request from client
-        message = self.socket.recv_pyobj()
+        message = socket.recv_pyobj()
         print(message)
         cmd = message[0]
         if cmd == "shutdown":
-            self.socket.send_pyobj(["shutting_down"])
-            self.kernel_server.shutdown()
+            socket.send_pyobj(["shutting_down"])
+            shutdown = True
+            kernel_server.shutdown()
 
         elif cmd == "open_kernel":
             try:
-                cf = self.kernel_server.open_kernel(message[1])
+                cf = kernel_server.open_kernel(message[1])
                 print(cf)
                 with open(cf, "br") as f:
                     cf = (cf, json.load(f))
 
             except Exception as e:
                 cf = ("error", e)
-            self.socket.send_pyobj(["new_kernel", *cf])
+            socket.send_pyobj(["new_kernel", *cf])
 
         elif cmd == "close_kernel":
-            self.socket.send_pyobj(["closing_kernel"])
+            socket.send_pyobj(["closing_kernel"])
             try:
-                self.kernel_server.close_kernel(message[1])
+                kernel_server.close_kernel(message[1])
             except Exception:
                 print("Nope")
                 pass
-        self._notifier.setEnabled(True)
 
 
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    w = Server()
-    sys.exit(app.exec_())
+if __name__ == "__main__":
+    main()
