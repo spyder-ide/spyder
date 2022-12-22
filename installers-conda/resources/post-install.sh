77525105d80be9d88e22348442156bd35930acc4
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2022-12-22 12:33:52 -0800

    Args are no longer provided to the post-install script. Install location is now in DSTROOT, e.g. /Library or ~/Library. Previously, DSTROOT was / or ~/.

--- installers-conda/resources/post-install.sh
+++ installers-conda/resources/post-install.sh
@@ -4,39 +4,31 @@ set -e
 echo "*** Starting post install script for __NAME__.app"
 
 cat <<EOF
-__PKG_NAME_LOWER__
-__NAME__
-__VERSION__
-__CHANNELS__
-__WRITE_CONDARC__
-__SHORTCUTS__
-__DEFAULT_PREFIX__
-__LICENSE__
-__FIRST_PAYLOAD_SIZE__
-__SECOND_PAYLOAD_SIZE__
-__MD5__
-__INSTALL_COMMANDS__
-__PLAT__
-__NAME_LOWER__
+* __PKG_NAME_LOWER__
+* __NAME__
+* __VERSION__
+* __CHANNELS__
+* __WRITE_CONDARC__
+* __SHORTCUTS__
+* __DEFAULT_PREFIX__
+* __LICENSE__
+* __FIRST_PAYLOAD_SIZE__
+* __SECOND_PAYLOAD_SIZE__
+* __MD5__
+* __INSTALL_COMMANDS__
+* __PLAT__
+* __NAME_LOWER__
 EOF
-
-ROOT_PREFIX=$(cd "$2/Library/__NAME_LOWER__"; pwd)
-PREFIX=$(cd "$ROOT_PREFIX/envs/__NAME_LOWER__"; pwd)
-
-# Installed for all users
-app_path="/Applications/__NAME__.app"
-
-if [[ "$PREFIX" == "$HOME"* ]]; then
-    # Installed for user
-    app_path="$HOME$app_path"
-fi
 
 echo "Args = $@"
 echo "$(declare -p)"
 
+ENV_PREFIX=$(cd "${PREFIX}/envs/__NAME_LOWER__"; pwd)
+app_path="$(dirname ${DSTROOT})/Applications/__NAME__.app"
+
 if [[ -e "$app_path" ]]; then
     echo "Creating python symbolic link..."
-    ln -sf "$PREFIX/bin/python" "$app_path/Contents/MacOS/python"
+    ln -sf "${ENV_PREFIX}/bin/python" "$app_path/Contents/MacOS/python"
 
     # Remove this block when LSEnvironment is fixed in menuinst
     if [[ -e "/usr/libexec/PlistBuddy" ]]; then
