b338bf7d1fd60be867d5ec28cf4e7d8f124b53bb
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2022-12-27 14:07:26 -0800

    Use the logomark background png for Linux

--- installers-conda/resources/build-patch.sh
+++ installers-conda/resources/build-patch.sh
@@ -6,9 +6,11 @@ mkdir -p "${PREFIX}/Menu"
 sed "s/__PKG_VERSION__/${PKG_VERSION}/" "${SRC_DIR}/installers-conda/resources/spyder-menu.json" > "${PREFIX}/Menu/spyder-menu.json"
 
 # Copy application icons
-icon_ext="ico"
 case $OSTYPE in
-    ("darwin"*) icon_ext="icns";;
-    ("linux"*) icon_ext="png";;
+    ("darwin"*)
+        cp "${SRC_DIR}/img_src/spyder.icns" "${PREFIX}/Menu/spyder.icns";;
+    ("linux"*)
+        cp "${SRC_DIR}/branding/logo/logomark/spyder-logomark-background.png" "${PREFIX}/Menu/spyder.png";;
+    (*)
+        cp "${SRC_DIR}/img_src/spyder.ico" "${PREFIX}/Menu/spyder.ico"
 esac
-cp "${SRC_DIR}/img_src/spyder.${icon_ext}" "${PREFIX}/Menu/spyder.${icon_ext}"
