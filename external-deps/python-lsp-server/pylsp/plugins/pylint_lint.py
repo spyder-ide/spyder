e6cb35cba962fd7026bdafbfac2617b7b0279d4f
Carlos Cordoba <ccordoba12@gmail.com>
2023-01-17 15:06:09 -0500

    git subrepo pull --remote=https://github.com/python-lsp/python-lsp-server.git --branch=develop --update --force external-deps/python-lsp-server
    subrepo:
      subdir:   "external-deps/python-lsp-server"
      merged:   "11b54415d"
    upstream:
      origin:   "https://github.com/python-lsp/python-lsp-server.git"
      branch:   "develop"
      commit:   "11b54415d"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

--- external-deps/python-lsp-server/pylsp/plugins/pylint_lint.py
+++ external-deps/python-lsp-server/pylsp/plugins/pylint_lint.py
@@ -9,6 +9,7 @@ import sys
 import re
 from subprocess import Popen, PIPE
 import os
+import shlex
 
 from pylsp import hookimpl, lsp
 
@@ -91,7 +92,7 @@ class PylintLinter:
             '-f',
             'json',
             document.path
-        ] + (str(flags).split(' ') if flags else [])
+        ] + (shlex.split(str(flags)) if flags else [])
         log.debug("Calling pylint with '%s'", ' '.join(cmd))
 
         with Popen(cmd, stdout=PIPE, stderr=PIPE,
@@ -126,6 +127,7 @@ class PylintLinter:
         # The type can be any of:
         #
         #  * convention
+        #  * information
         #  * error
         #  * fatal
         #  * refactor
@@ -150,6 +152,8 @@ class PylintLinter:
             }
 
             if diag['type'] == 'convention':
+                severity = lsp.DiagnosticSeverity.Information
+            elif diag['type'] == 'information':
                 severity = lsp.DiagnosticSeverity.Information
             elif diag['type'] == 'error':
                 severity = lsp.DiagnosticSeverity.Error
