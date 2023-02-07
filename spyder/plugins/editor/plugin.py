42d879a5e962d6b5c52f52fdd626601bb24a0b11
Quentin Peter <quentin.peter@bluewin.ch>
2023-02-07 06:10:14 +0100

    remove unnecessary abspath

--- spyder/plugins/editor/plugin.py
+++ spyder/plugins/editor/plugin.py
@@ -2450,7 +2450,7 @@ class Editor(SpyderPluginWidget, SpyderConfigurationObserver):
             self.switch_to_plugin()
 
         def _convert(fname):
-            fname = osp.abspath(encoding.to_unicode_from_fs(fname))
+            fname = encoding.to_unicode_from_fs(fname)
             fname = Path(fname).resolve()
             if os.name == 'nt' and len(fname) >= 2 and fname[1] == ':':
                 fname = fname[0].upper()+fname[1:]
