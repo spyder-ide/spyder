8d64dd265a78bcddb14bd3ad5337217517edfe73
Quentin Peter <quentin.peter@bluewin.ch>
2023-04-01 08:17:50 +0200

    fix test

--- spyder/app/tests/test_mainwindow.py
+++ spyder/app/tests/test_mainwindow.py
@@ -6253,7 +6253,7 @@ def test_recursive_debug_exception(main_window, qtbot):
 
     # Debug line
     with qtbot.waitSignal(shell.executed):
-        shell.execute("%debugfile")
+        shell.execute("%debugfile " + str(main_window.editor.get_current_filename()))
 
     assert shell.is_debugging()
     assert '----> 1 print("res", 1 + 2)' in control.toPlainText()
