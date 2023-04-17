fc3afdfb2cc0ec9d4d825b12ab2d2114752595fc
dalthviz <d.althviz10@uniandes.edu.co>
2023-04-17 14:11:08 -0500

    Fix IPython console test

--- spyder/plugins/ipythonconsole/tests/test_ipythonconsole.py
+++ spyder/plugins/ipythonconsole/tests/test_ipythonconsole.py
@@ -916,9 +916,7 @@ def test_load_kernel_file(ipyconsole, qtbot, tmpdir):
     with qtbot.waitSignal(new_shell.executed):
         new_shell.execute('a = 10')
 
-    assert new_client.id_ == dict(
-        int_id='1', str_id='B', str_env_name=''
-    )
+    assert new_client.id_ == dict(int_id='1', str_id='B')
     assert shell.get_value('a') == new_shell.get_value('a')
 
 
