c46f3c3c9e253deabc4ea000eddee278961bed0c
Carlos Cordoba <ccordoba12@gmail.com>
2023-05-02 21:03:37 -0500

    Preferences: Use ElementsTable in the Plugins page

--- spyder/api/plugin_registration/_confpage.py
+++ spyder/api/plugin_registration/_confpage.py
@@ -7,33 +7,32 @@
 """Plugin registry configuration page."""
 
 # Third party imports
-from qtpy.QtWidgets import (QGroupBox, QVBoxLayout, QCheckBox,
-                            QGridLayout, QLabel)
+from qtpy.QtWidgets import QVBoxLayout, QLabel
 
 # Local imports
-from spyder.api.plugins import SpyderPlugin
 from spyder.api.preferences import PluginConfigPage
 from spyder.config.base import _
 from spyder.config.manager import CONF
+from spyder.widgets.elementstable import ElementsTable
 
 
 class PluginsConfigPage(PluginConfigPage):
+
     def setup_page(self):
         newcb = self.create_checkbox
         self.plugins_checkboxes = {}
 
         header_label = QLabel(
-            _("Here you can turn on/off any internal or external Spyder plugin "
-              "to disable functionality that is not desired or to have a lighter "
-              "experience. Unchecked plugins in this page will be unloaded "
-              "immediately and will not be loaded the next time Spyder starts."))
+            _("Here you can turn on/off any internal or external Spyder "
+              "plugin to disable functionality that is not desired or to have "
+              "a lighter experience.")
+        )
         header_label.setWordWrap(True)
 
-        # ------------------ Internal plugin status group ---------------------
-        internal_layout = QGridLayout()
-        self.internal_plugins_group = QGroupBox(_("Internal plugins"))
+        # To save the plugin elements attributes
+        plugin_elements = []
 
-        i = 0
+        # ------------------ Internal plugins ---------------------------------
         for plugin_name in self.plugin.all_internal_plugins:
             (conf_section_name,
              PluginClass) = self.plugin.all_internal_plugins[plugin_name]
@@ -42,58 +41,60 @@ class PluginsConfigPage(PluginConfigPage):
                 # Do not list core plugins that can not be disabled
                 continue
 
-            plugin_loc_name = None
-            if hasattr(PluginClass, 'get_name'):
-                plugin_loc_name = PluginClass.get_name()
-            elif hasattr(PluginClass, 'get_plugin_title'):
-                plugin_loc_name = PluginClass.get_plugin_title()
-
             plugin_state = CONF.get(conf_section_name, 'enable', True)
-            cb = newcb(plugin_loc_name, 'enable', default=True,
-                       section=conf_section_name, restart=True)
-            internal_layout.addWidget(cb, i // 2, i % 2)
+            cb = newcb('', 'enable', default=True, section=conf_section_name,
+                       restart=True)
+
+            plugin_elements.append(
+                dict(
+                    title=PluginClass.get_name(),
+                    description=PluginClass.get_description(),
+                    icon=PluginClass.get_icon(),
+                    widget=cb
+                )
+            )
+
             self.plugins_checkboxes[plugin_name] = (cb, plugin_state)
-            i += 1
 
-        self.internal_plugins_group.setLayout(internal_layout)
-
-        # ------------------ External plugin status group ---------------------
-        external_layout = QGridLayout()
-        self.external_plugins_group = QGroupBox(_("External plugins"))
-
-        i = 0
+        # ------------------ External plugins ---------------------------------
         # Temporal fix to avoid disabling external plugins.
-        # for more info see spyder#17464
-        show_external_plugins_group = False
-        for i, plugin_name in enumerate(self.plugin.all_external_plugins):
-            (conf_section_name,
-             PluginClass) = self.plugin.all_external_plugins[plugin_name]
+        # For more info see spyder-ide/spyder#17464
+        show_external_plugins = False
 
-            if not getattr(PluginClass, 'CAN_BE_DISABLED', True):
-                # Do not list external plugins that can not be disabled
-                continue
+        if show_external_plugins:
+            for plugin_name in self.plugin.all_external_plugins:
+                (conf_section_name,
+                 PluginClass) = self.plugin.all_external_plugins[plugin_name]
 
-            plugin_loc_name = None
-            if hasattr(PluginClass, 'get_name'):
-                plugin_loc_name = PluginClass.get_name()
-            elif hasattr(PluginClass, 'get_plugin_title'):
-                plugin_loc_name = PluginClass.get_plugin_title()
+                if not getattr(PluginClass, 'CAN_BE_DISABLED', True):
+                    # Do not list external plugins that can not be disabled
+                    continue
 
-            cb = newcb(plugin_loc_name, 'enable', default=True,
-                       section=conf_section_name, restart=True)
-            external_layout.addWidget(cb, i // 2, i % 2)
-            plugin_state = CONF.get(conf_section_name, 'enable', True)
-            self.plugins_checkboxes[plugin_name] = (cb, plugin_state)
-            i += 1
+                plugin_state = CONF.get(conf_section_name, 'enable', True)
+                cb = newcb('', 'enable', default=True,
+                           section=conf_section_name, restart=True)
 
-        self.external_plugins_group.setLayout(external_layout)
+                plugin_elements.append(
+                    dict(
+                        title=PluginClass.get_name(),
+                        description=PluginClass.get_description(),
+                        icon=PluginClass.get_icon(),
+                        widget=cb
+                    )
+                )
+
+                self.plugins_checkboxes[plugin_name] = (cb, plugin_state)
+
+        # Build plugins table
+        plugins_table = ElementsTable(
+            self, plugin_elements, with_icons=True, with_widgets=True
+        )
 
         layout = QVBoxLayout()
         layout.addWidget(header_label)
-        layout.addWidget(self.internal_plugins_group)
-        if show_external_plugins_group:
-            layout.addWidget(self.external_plugins_group)
-        layout.addStretch(1)
+        layout.addSpacing(15)
+        layout.addWidget(plugins_table)
+        layout.addSpacing(15)
         self.setLayout(layout)
 
     def apply_settings(self):
@@ -109,7 +110,7 @@ class PluginsConfigPage(PluginConfigPage):
                 elif plugin_name in self.plugin.all_external_plugins:
                     (__,
                      PluginClass) = self.plugin.all_external_plugins[plugin_name]
-                    external = True
+                    external = True  # noqa
 
                 # TODO: Once we can test that all plugins can be restarted
                 # without problems during runtime, we can enable the
