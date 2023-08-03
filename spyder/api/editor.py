ae03eb6bcd488feaf16c0daa2122ef103f3ab9b8
dalthviz <d.althviz10@uniandes.edu.co>
2023-08-03 17:50:12 -0500

    API: Restore Editor API access from the Spyder general API

---
+++ spyder/api/editor.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © Spyder Project Contributors
+# Licensed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+
+"""
+This module contains the code editor API.
+"""
+
+from spyder.plugins.editor.api.editorextension import EditorExtension
+from spyder.plugins.editor.api.panel import Panel
