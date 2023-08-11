13e04f46a85cc9f2cc27ec9b6a14fe020fccbd24
Carlos Cordoba <ccordoba12@gmail.com>
2023-08-11 11:05:22 -0500

    Pythonpath: Expand pattern to detect possible site-packages directories

--- spyder/plugins/pythonpath/utils.py
+++ spyder/plugins/pythonpath/utils.py
@@ -18,12 +18,16 @@ from spyder.utils.environ import get_user_env
 def check_path(path):
     """Check that `path` is not a [site|dist]-packages folder."""
     if os.name == 'nt':
-        pat = re.compile(r'.*lib/(?:site|dist)-packages.*')
+        pattern = re.compile(r'.*(?:l|L)ib/(?:site|dist)-packages.*')
     else:
-        pat = re.compile(r'.*lib/python.../(?:site|dist)-packages.*')
+        pattern = re.compile(
+            r'.*(:?lib|lib64)/'
+            '(?:python|python(\d+)|python(\d+)\.(\d+)|python(\d+)\.(\d+)(\d+))'
+            '/(?:site|dist)-packages.*'
+        )
 
     path_norm = path.replace('\\', '/')
-    return pat.match(path_norm) is None
+    return pattern.match(path_norm) is None
 
 
 def get_system_pythonpath():
