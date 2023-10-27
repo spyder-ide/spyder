ec72828c1bee592a839db4420c3531658268db35
Carlos Cordoba <ccordoba12@gmail.com>
2023-10-27 13:53:58 -0500

    Utils: Fix functions that need to run conda and parse its output
    - This problem only happens on Windows.
    - Also, fix test test_get_list_conda_envs on Windows.

--- spyder/utils/tests/test_conda.py
+++ spyder/utils/tests/test_conda.py
@@ -71,13 +71,10 @@ def test_find_conda():
 @pytest.mark.skipif(not running_in_ci(), reason="Only meant for CIs")
 def test_get_list_conda_envs():
     output = get_list_conda_envs()
-    expected_envs = ['base', 'jedi-test-env', 'spytest-ž']
 
-    # Conda can't detect the test env on Windows, don't know why.
-    if os.name != 'nt':
-        expected_envs.append('test')
-
+    expected_envs = ['base', 'jedi-test-env', 'spytest-ž', 'test']
     expected_envs = ['conda: ' + env for env in expected_envs]
+
     assert set(expected_envs) == set(output.keys())
 
 
