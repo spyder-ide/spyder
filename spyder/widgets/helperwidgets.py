de7e1a1b58cc7d8a76ef78d2518119f98b7ff09d
dalthviz <16781833+dalthviz@users.noreply.github.com>
2023-11-15 17:28:21 -0500

    Widgets: Cast to int parameters used inside ItemDelegate.sizeHint return value

--- spyder/widgets/helperwidgets.py
+++ spyder/widgets/helperwidgets.py
@@ -253,7 +253,7 @@ class ItemDelegate(QStyledItemDelegate):
         doc.setHtml(options.text)
         doc.setTextWidth(options.rect.width())
 
-        return QSize(doc.idealWidth(), doc.size().height())
+        return QSize(int(doc.idealWidth()), int(doc.size().height()))
 
 
 class IconLineEdit(QLineEdit):
