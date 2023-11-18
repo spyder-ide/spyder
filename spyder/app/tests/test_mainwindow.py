6eb55ce9437e134e2374c67fe31c35f4cda56292
Quentin Peter <quentin.peter@bluewin.ch>
2023-11-18 07:03:39 +0100

    remonve no banner test

--- spyder/app/tests/test_mainwindow.py
+++ spyder/app/tests/test_mainwindow.py
@@ -821,10 +821,10 @@ def test_dedicated_consoles(main_window, qtbot):
     qtbot.waitUntil(lambda: nsb.editor.source_model.rowCount() == 4)
     assert nsb.editor.source_model.rowCount() == 4
 
-    # --- Assert only runfile text is present and there's no banner text ---
+    # --- Assert only runfile text is present ---
     # See spyder-ide/spyder#5301.
     text = control.toPlainText()
-    assert ('runfile' in text) and not ('Python' in text or 'IPython' in text)
+    assert ('runfile' in text)
 
     # --- Check namespace retention after re-execution ---
     with qtbot.waitSignal(shell.executed):
