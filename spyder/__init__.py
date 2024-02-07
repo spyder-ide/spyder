237175ba959a1aaa103f443099604b49e57c9bbe
Carlos Cordoba <ccordoba12@gmail.com>
2024-02-06 21:09:14 -0500

    Release 5.5.1

--- spyder/__init__.py
+++ spyder/__init__.py
@@ -29,7 +29,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 """
 
-version_info = (5, 5, "1rc1")
+version_info = (5, 5, 1)
 
 __version__ = '.'.join(map(str, version_info))
 __installer_version__ = __version__
