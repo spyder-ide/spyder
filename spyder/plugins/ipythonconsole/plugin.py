d0c3d77c15e4b844f4398a1f7b0d9894192ce8c1
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-02-29 11:55:00 -0800

    Propagate changes to sig_pythonpath_changed to ipythonconsole plugin. Note that spyder-kernels must be updated to accommodate.

--- spyder/plugins/ipythonconsole/plugin.py
+++ spyder/plugins/ipythonconsole/plugin.py
@@ -956,7 +956,7 @@ class IPythonConsole(SpyderDockablePlugin, RunExecutor):
         """
         self.get_widget().save_working_directory(dirname)
 
-    def update_path(self, old_path, new_path, prioritize):
+    def update_path(self, new_path, prioritize):
         """
         Update path on consoles.
 
@@ -965,8 +965,6 @@ class IPythonConsole(SpyderDockablePlugin, RunExecutor):
 
         Parameters
         ----------
-        old_path : list of str
-            Corresponds to the previous state of the PYTHONPATH.
         new_path : list of str
             Corresponds to the new state of the PYTHONPATH.
         prioritize : bool
@@ -976,7 +974,7 @@ class IPythonConsole(SpyderDockablePlugin, RunExecutor):
         -------
         None.
         """
-        self.get_widget().update_path(old_path, new_path, prioritize)
+        self.get_widget().update_path(new_path, prioritize)
 
     def restart(self):
         """
