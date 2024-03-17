760accf8460bbf6947a154d8cb921ec207ca5241
Quentin Peter <quentin.peter@bluewin.ch>
2024-03-17 08:43:16 +0100

    add set_

--- spyder/plugins/ipythonconsole/widgets/shell.py
+++ spyder/plugins/ipythonconsole/widgets/shell.py
@@ -412,7 +412,7 @@ class ShellWidget(NamepaceBrowserWidget, HelpWidget, DebuggingWidget,
         self.set_autocall(self.get_conf('autocall'))
 
         # Set greedy completer
-        self.greedy_completer(self.get_conf('greedy_completer'))
+        self.set_greedy_completer(self.get_conf('greedy_completer'))
 
         # Set Jedi Completer
         self.set_jedi_completer(self.get_conf('jedi_completer'))
