1414e5e4fb202010398fef2969db34c1a0891d3f
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2024-04-10 01:05:50 -0300

    ehn: add async dispatcher to run coroutines

--- spyder/plugins/remoteclient/plugin.py
+++ spyder/plugins/remoteclient/plugin.py
@@ -20,7 +20,7 @@ from spyder.api.translations import _
 from spyder.api.plugins import Plugins, SpyderPluginV2
 from spyder.plugins.remoteclient.api.protocol import KernelConnectionInfo, DeleteKernel, KernelInfo, KernelsList, SSHClientOptions
 from spyder.plugins.remoteclient.api.client import SpyderRemoteClient
-from spyder.api.utils import AsSync
+from spyder.api.asyncdispatcher import AsyncDispatcher
 
 _logger = logging.getLogger(__name__)
 
@@ -76,7 +76,7 @@ class RemoteClient(SpyderPluginV2):
     def on_close(self, cancellable=True):
         """Stops remote server and close any opened connection."""
         for client in self._remote_clients.values():
-            AsSync(client.close)()
+            AsyncDispatcher(client.close, early_return=False)()
 
     # ---- Public API
     # -------------------------------------------------------------------------
@@ -86,33 +86,33 @@ class RemoteClient(SpyderPluginV2):
         if config_id in self._remote_clients:
             return self._remote_clients[config_id]
 
-    @AsSync
-    async def install_remote_server(self, config_id):
+    @AsyncDispatcher.dispatch()
+    async def _install_remote_server(self, config_id):
         """Install remote server."""
         if config_id in self._remote_clients:
             client = self._remote_clients[config_id]
-            return await client.connect_and_install_remote_server()
+            await client.connect_and_install_remote_server()
 
-    @AsSync
+    @AsyncDispatcher.dispatch()
     async def start_remote_server(self, config_id):
         """Start remote server."""
         if config_id in self._remote_clients:
             server = self._remote_clients[config_id]
-            return await server.connect_and_start_server()
+            await server.connect_and_start_server()
 
-    @AsSync
+    @AsyncDispatcher.dispatch()
     async def stop_remote_server(self, config_id):
         """Stop remote server."""
         if config_id in self._remote_clients:
             client = self._remote_clients[config_id]
             await client.stop_remote_server()
 
-    @AsSync
+    @AsyncDispatcher.dispatch()
     async def ensure_remote_server(self, config_id):
         """Ensure remote server is running and installed."""
         if config_id in self._remote_clients:
             client = self._remote_clients[config_id]
-            return await client.connect_and_ensure_server()
+            await client.connect_and_ensure_server()
 
     def restart_remote_server(self, config_id):
         """Restart remote server."""
@@ -123,7 +123,7 @@ class RemoteClient(SpyderPluginV2):
     def load_client_from_id(self, config_id):
         """Load remote server from configuration id."""
         options = self.load_conf(config_id)
-        self.load_client(config_id, options)    
+        self.load_client(config_id, options)
 
     def load_client(self, config_id: str, options: SSHClientOptions):
         """Load remote server."""
@@ -148,41 +148,37 @@ class RemoteClient(SpyderPluginV2):
     # -------------------------------------------------------------------------
     # --- Remote Server Kernel Methods
     @Slot(str)
-    @AsSync
+    @AsyncDispatcher.dispatch()
     async def get_kernels(self, config_id):
         """Get opened kernels."""
         if config_id in self._remote_clients:
             client = self._remote_clients[config_id]
             kernels_list = await client.get_kernels()
             self.sig_kernel_list.emit(kernels_list)
-            return kernels_list
 
     @Slot(str)
-    @AsSync
+    @AsyncDispatcher.dispatch()
     async def get_kernel_info(self, config_id, kernel_key):
         """Get kernel info."""
         if config_id in self._remote_clients:
             client = self._remote_clients[config_id]
             kernel_info = await client.get_kernel_info(kernel_key)
             self.sig_kernel_info.emit(kernel_info or {})
-            return kernel_info
 
     @Slot(str)
-    @AsSync
+    @AsyncDispatcher.dispatch()
     async def terminate_kernel(self, config_id, kernel_key):
         """Terminate opened kernel."""
         if config_id in self._remote_clients:
             client = self._remote_clients[config_id]
             delete_kernel = await client.terminate_kernel(kernel_key)
             self.sig_kernel_terminated.emit(delete_kernel or {})
-            return delete_kernel
 
     @Slot(str)
-    @AsSync
+    @AsyncDispatcher.dispatch()
     async def start_new_kernel(self, config_id):
         """Start new kernel."""
         if config_id in self._remote_clients:
             client = self._remote_clients[config_id]
             kernel_connection_info = await client.start_new_kernel_ensure_server()
-            self.sig_kernel_started.emit(kernel_connection_info)
-            return kernel_connection_info
+            self.sig_kernel_started.emit(kernel_connection_info or {})
