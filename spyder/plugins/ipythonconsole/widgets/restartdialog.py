da3e23b6a1d504bccf3e3bcab812f36078219a25
Carlos Cordoba <ccordoba12@gmail.com>
2024-04-27 13:18:11 -0500

    IPython console: Improve UI/UX of ConsoleRestartDialog a bit
    - Use radio buttons instead of checkboxes because they are the right
    graphical elements for it.
    - Add a new radio button to keep the existing kernels.
    - Indent radio buttons a bit to make them stand out from the text.

--- spyder/plugins/ipythonconsole/widgets/restartdialog.py
+++ spyder/plugins/ipythonconsole/widgets/restartdialog.py
@@ -9,12 +9,20 @@ IPython Console restart dialog for preferences.
 """
 
 # Third party imports
+import qstylizer.style
 from qtpy.QtCore import Qt
-from qtpy.QtWidgets import (QButtonGroup, QCheckBox, QDialog, QLabel,
-                            QPushButton, QVBoxLayout)
+from qtpy.QtWidgets import (
+    QButtonGroup,
+    QDialog,
+    QLabel,
+    QPushButton,
+    QRadioButton,
+    QVBoxLayout,
+)
 
 # Local imports
 from spyder.config.base import _
+from spyder.utils.stylesheet import AppStyle
 
 
 class ConsoleRestartDialog(QDialog):
@@ -28,14 +36,16 @@ class ConsoleRestartDialog(QDialog):
     RESTART_ALL = 3
 
     def __init__(self, parent):
-        super(ConsoleRestartDialog, self).__init__(parent)
+        super().__init__(parent)
         self.setWindowFlags(Qt.Dialog | Qt.WindowTitleHint)
         self._parent = parent
+        self.setWindowTitle(_("Restart kernels"))
+
         self._action = self.NO_RESTART
         self._action_string = {
-            self.NO_RESTART: _("Keep Existing Kernels"),
-            self.RESTART_CURRENT: _("Restart Current Kernel"),
-            self.RESTART_ALL: _("Restart All Kernels")
+            self.NO_RESTART: _("Keep existing kernels"),
+            self.RESTART_CURRENT: _("Restart current kernel"),
+            self.RESTART_ALL: _("Restart all kernels")
             }
         # Dialog widgets
         # Text
@@ -49,52 +59,70 @@ class ConsoleRestartDialog(QDialog):
         self._text_label.setWordWrap(True)
         self._text_label.setFixedWidth(450)
 
-        # Checkboxes
-        self._restart_current = QCheckBox(
-            _("Apply to current console and restart kernel"), self)
-        self._restart_all = QCheckBox(
-            _("Apply to all existing consoles and restart all kernels"), self)
-        self._checkbox_group = QButtonGroup(self)
-        self._checkbox_group.setExclusive(False)
-        self._checkbox_group.addButton(
-            self._restart_current, id=self.RESTART_CURRENT)
-        self._checkbox_group.addButton(
-            self._restart_all, id=self.RESTART_ALL)
+        # Radio buttons
+        self._no_restart = QRadioButton(
+            _("Don't apply changes to existing consoles"), self
+        )
+        self._restart_current = QRadioButton(
+            _("Apply to current console and restart kernel"), self
+        )
+        self._restart_all = QRadioButton(
+            _("Apply to all existing consoles and restart all kernels"), self
+        )
+
+        self._radio_group = QButtonGroup(self)
+        self._radio_group.setExclusive(True)
+        self._radio_group.addButton(
+            self._no_restart, id=self.NO_RESTART
+        )
+        self._radio_group.addButton(
+            self._restart_current, id=self.RESTART_CURRENT
+        )
+        self._radio_group.addButton(
+            self._restart_all, id=self.RESTART_ALL
+        )
 
         self._action_button = QPushButton(
-            self._action_string[self.NO_RESTART], parent=self)
+            self._action_string[self.NO_RESTART], parent=self
+        )
 
         # Dialog Layout
         layout = QVBoxLayout(self)
         layout.addWidget(self._text_label)
-        layout.addSpacing(5)
+        layout.addSpacing(2 * AppStyle.MarginSize)
+        layout.addWidget(self._no_restart)
         layout.addWidget(self._restart_current)
         layout.addWidget(self._restart_all)
-        layout.addSpacing(10)
+        layout.addSpacing(3 * AppStyle.MarginSize)
         layout.addWidget(self._action_button, 0, Qt.AlignRight)
-        layout.setContentsMargins(20, 20, 20, 20)
+        layout.setContentsMargins(*((7 * AppStyle.MarginSize,) * 4))
         self.setLayout(layout)
 
         # Signals
-        self._checkbox_group.buttonToggled.connect(
+        self._no_restart.setChecked(True)
+        self._radio_group.buttonToggled.connect(
             self.update_action_button_text)
         self._action_button.clicked.connect(self.accept)
 
-    def update_action_button_text(self, checkbox, is_checked):
+        # Stylesheet
+        self._set_stylesheet()
+
+    def update_action_button_text(self, radiobutton, is_checked):
         """
         Update action button text.
 
-        Takes into account the given checkbox to update the text.
+        Takes into account the given radio button to update the text.
         """
-        checkbox_id = self._checkbox_group.id(checkbox)
+        radiobutton_id = self._radio_group.id(radiobutton)
         if is_checked:
-            text = self._action_string[checkbox_id]
-            self._checkbox_group.buttonToggled.disconnect(
+            text = self._action_string[radiobutton_id]
+            self._radio_group.buttonToggled.disconnect(
                 self.update_action_button_text)
+            self._no_restart.setChecked(False)
             self._restart_current.setChecked(False)
             self._restart_all.setChecked(False)
-            checkbox.setChecked(True)
-            self._checkbox_group.buttonToggled.connect(
+            radiobutton.setChecked(True)
+            self._radio_group.buttonToggled.connect(
                 self.update_action_button_text)
         else:
             text = self._action_string[self.NO_RESTART]
@@ -108,3 +136,15 @@ class ConsoleRestartDialog(QDialog):
         restart_all = self._restart_all.isChecked()
         no_restart = not any([restart_all, restart_current])
         return restart_all, restart_current, no_restart
+
+    def _set_stylesheet(self):
+        radiobuttons_css = qstylizer.style.StyleSheet()
+        radiobuttons_css.setValues(
+            marginLeft=f"{5 * AppStyle.MarginSize}px"
+        )
+        for label in [
+            self._no_restart,
+            self._restart_current,
+            self._restart_all,
+        ]:
+            label.setStyleSheet(radiobuttons_css.toString())
