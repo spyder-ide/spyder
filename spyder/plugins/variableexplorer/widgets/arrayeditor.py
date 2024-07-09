e1633966246bf2b3c8f94b914b219e889a0bffc0
dalthviz <16781833+dalthviz@users.noreply.github.com>
2024-07-09 18:04:29 -0500

    Fix PEP8 error

--- spyder/plugins/variableexplorer/widgets/arrayeditor.py
+++ spyder/plugins/variableexplorer/widgets/arrayeditor.py
@@ -847,8 +847,8 @@ class ArrayEditor(BaseDialog, SpyderWidgetMixin):
             _('<u>Warning</u>: Changes are applied separately')
         )
         self.masked_label.setToolTip(
-            _("For performance reasons, changes applied to masked arrays won't "
-              "be reflected in array's data (and vice-versa).")
+            _("For performance reasons, changes applied to masked arrays "
+              "won't be reflected in array's data (and vice-versa).")
           )
         self.btn_layout.addWidget(self.masked_label)
 
