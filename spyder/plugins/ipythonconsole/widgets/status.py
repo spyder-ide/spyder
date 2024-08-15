7db3f41e850aaede040b2d54b3fc99fb51d50ae0
Carlos Cordoba <ccordoba12@gmail.com>
2024-08-14 19:36:18 -0500

    API: Add a new statusbar widget to shellconnect module
    Also, use that widget to simplify the Matplotlib status one from the
    IPython console.

--- spyder/plugins/ipythonconsole/widgets/status.py
+++ spyder/plugins/ipythonconsole/widgets/status.py
@@ -15,13 +15,12 @@ import textwrap
 from spyder_kernels.comms.frontendcomm import CommError
 
 # Local imports
-from spyder.api.shellconnect.mixins import ShellConnectMixin
+from spyder.api.shellconnect.status import ShellConnectStatusBarWidget
 from spyder.api.translations import _
-from spyder.api.widgets.status import StatusBarWidget
 from spyder.config.base import running_in_ci
 
 
-class MatplotlibStatus(StatusBarWidget, ShellConnectMixin):
+class MatplotlibStatus(ShellConnectStatusBarWidget):
     """Status bar widget for current Matplotlib backend."""
 
     ID = "matplotlib_status"
@@ -30,10 +29,9 @@ class MatplotlibStatus(StatusBarWidget, ShellConnectMixin):
 
     def __init__(self, parent):
         super().__init__(parent)
+
         self._gui = None
         self._interactive_gui = None
-        self._shellwidget_dict = {}
-        self._current_id = None
 
         # Signals
         self.sig_clicked.connect(self.toggle_matplotlib)
@@ -41,7 +39,7 @@ class MatplotlibStatus(StatusBarWidget, ShellConnectMixin):
     # ---- StatusBarWidget API
     # -------------------------------------------------------------------------
     def get_tooltip(self):
-        """Return localized tool tip for widget."""
+        """Return localized tooltip for widget."""
         msg = _(
             "Click to toggle between inline and interactive Matplotlib "
             "plotting"
@@ -56,7 +54,7 @@ class MatplotlibStatus(StatusBarWidget, ShellConnectMixin):
     # -------------------------------------------------------------------------
     def toggle_matplotlib(self):
         """Toggle matplotlib interactive backend."""
-        if self._current_id is None or self._gui == 'failed':
+        if self.current_shellwidget is None or self._gui is None:
             return
 
         if self._gui != "inline":
@@ -72,26 +70,27 @@ class MatplotlibStatus(StatusBarWidget, ShellConnectMixin):
                 # Always use the interactive backend otherwise
                 backend = self._interactive_gui
 
-        sw = self._shellwidget_dict[self._current_id]["widget"]
+        sw = self.current_shellwidget
         sw.execute("%matplotlib " + backend)
-        is_spyder_kernel = self._shellwidget_dict[self._current_id][
-            "widget"].is_spyder_kernel
+        is_spyder_kernel = sw.is_spyder_kernel
 
         if not is_spyder_kernel:
             self.update_matplotlib_gui(backend)
 
-    def update_matplotlib_gui(self, gui, shellwidget_id=None):
+    def update_matplotlib_gui(self, gui, shellwidget=None):
         """Update matplotlib interactive."""
-        if shellwidget_id is None:
-            shellwidget_id = self._current_id
-            if shellwidget_id is None:
+        if shellwidget is None:
+            shellwidget = self.current_shellwidget
+            if shellwidget is None:
                 return
 
-        if shellwidget_id in self._shellwidget_dict:
-            self._shellwidget_dict[shellwidget_id]["gui"] = gui
-            if shellwidget_id == self._current_id:
+        if shellwidget in self.shellwidget_to_status:
+            self.shellwidget_to_status[shellwidget] = gui
+            if shellwidget == self.current_shellwidget:
                 self.update_status(gui)
 
+    # ---- ShellConnectStatusBarWidget API
+    # -------------------------------------------------------------------------
     def update_status(self, gui):
         """Update interactive state."""
         if self._interactive_gui is None and gui != "inline":
@@ -113,7 +112,7 @@ class MatplotlibStatus(StatusBarWidget, ShellConnectMixin):
         shellwidget.kernel_handler.kernel_comm.register_call_handler(
             "update_matplotlib_gui",
             functools.partial(
-                self.update_matplotlib_gui, shellwidget_id=id(shellwidget)
+                self.update_matplotlib_gui, shellwidget=shellwidget
             )
         )
         shellwidget.set_kernel_configuration("update_gui", True)
@@ -136,68 +135,25 @@ class MatplotlibStatus(StatusBarWidget, ShellConnectMixin):
             except CommError:
                 mpl_backend = None
 
-        # Hide widget if Matplotlib is not available or failed to import
+        # Associate detected backend to shellwidget
+        self.shellwidget_to_status[shellwidget] = mpl_backend
+
+        # Hide widget if Matplotlib is not available or failed to import in the
+        # kernel
         if mpl_backend is None:
-            gui = "failed"
-            self._shellwidget_dict[id(shellwidget)]['gui'] = gui
             self.hide()
         else:
+            self.set_shellwidget(shellwidget)
             self.show()
 
         # Ask the kernel to update the current backend, in case it has changed
         shellwidget.set_kernel_configuration("update_gui", True)
 
-    # ---- ShellConnectMixin API
-    # -------------------------------------------------------------------------
-    def set_shellwidget(self, shellwidget):
-        """Set current shellwidget."""
-        self._current_id = None
-        shellwidget_id = id(shellwidget)
-        if shellwidget_id in self._shellwidget_dict:
-            gui = self._shellwidget_dict[shellwidget_id]["gui"]
-            if gui == "failed":
-                # This means the console failed to start or Matplotlib is not
-                # installed in the kernel. So, we don't need to show this
-                # widget in those cases.
-                self.hide()
-            else:
-                self.show()
-                self.update_status(gui)
-                self._current_id = shellwidget_id
-
-    def add_shellwidget(self, shellwidget):
-        """Add shellwidget."""
-        shellwidget.sig_config_spyder_kernel.connect(
-            functools.partial(self.config_spyder_kernel, shellwidget)
-        )
-        shellwidget.kernel_handler.sig_kernel_is_ready.connect(
-            # We can't use functools.partial here because it gives memory leaks
-            # in Python versions older than 3.10
-            lambda sw=shellwidget: self.on_kernel_start(sw)
-        )
-
-        backend = self.get_conf('pylab/backend')
-        swid = id(shellwidget)
-        self._shellwidget_dict[swid] = {
-            "gui": backend,
-            "widget": shellwidget,
-        }
-        self.set_shellwidget(shellwidget)
-
     def remove_shellwidget(self, shellwidget):
-        """Remove shellwidget."""
+        """
+        Overridden method to remove the call handler registered by this widget.
+        """
         shellwidget.kernel_handler.kernel_comm.unregister_call_handler(
             "update_matplotlib_gui"
         )
-        shellwidget_id = id(shellwidget)
-        if shellwidget_id in self._shellwidget_dict:
-            del self._shellwidget_dict[shellwidget_id]
-
-    def add_errored_shellwidget(self, shellwidget):
-        """Add errored shellwidget."""
-        swid = id(shellwidget)
-        self._shellwidget_dict[swid] = {
-            "gui": 'failed',
-            "widget": shellwidget,
-        }
-        self.set_shellwidget(shellwidget)
+        super().remove_shellwidget(shellwidget)
