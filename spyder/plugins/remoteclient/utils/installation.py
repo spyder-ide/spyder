103189417961ba516c11cf076b118f72d2f84d56
Carlos Cordoba <ccordoba12@gmail.com>
2024-08-21 23:57:59 -0500

    Remote client: Use spyder-kernels version from the IPython console
    - That's for consistency with other places we use it.
    - Also, fix some small style issues.

--- spyder/plugins/remoteclient/utils/installation.py
+++ spyder/plugins/remoteclient/utils/installation.py
@@ -3,7 +3,8 @@
 # Copyright © Spyder Project Contributors
 # Licensed under the terms of the MIT License
 # (see spyder/__init__.py for details)
-from spyder.dependencies import SPYDER_KERNELS_REQVER
+
+from spyder.plugins.ipythonconsole import SPYDER_KERNELS_VERSION
 
 
 SERVER_ENTRY_POINT = "spyder-server"
@@ -13,10 +14,15 @@ PACKAGE_VERSION = "0.1.3"
 
 ENCODING = "utf-8"
 
-SCRIPT_URL = f"https://raw.githubusercontent.com/spyder-ide/{PACKAGE_NAME}/master/scripts"
+SCRIPT_URL = (
+    f"https://raw.githubusercontent.com/spyder-ide/{PACKAGE_NAME}/master/scripts"
+)
 
 def get_installer_command(platform: str) -> str:
     if platform == "win":
         raise NotImplementedError("Windows is not supported yet")
 
-    return f'"${{SHELL}}" <(curl -L {SCRIPT_URL}/installer.sh) "{PACKAGE_VERSION}" "{SPYDER_KERNELS_REQVER}"'
+    return (
+        f'"${{SHELL}}" <(curl -L {SCRIPT_URL}/installer.sh) '
+        f'"{PACKAGE_VERSION}" "{SPYDER_KERNELS_VERSION}"'
+    )
