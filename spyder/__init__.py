cc7434f722f5908617117f8dfd91fd6a966908ab
dalthviz <16781833+dalthviz@users.noreply.github.com>
2024-09-02 21:51:28 -0500

    Release 6.0.0

--- spyder/__init__.py
+++ spyder/__init__.py
@@ -31,7 +31,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 from packaging.version import parse
 
-version_info = (6, 0, "0rc3")
+version_info = (6, 0, 0)
 
 __version__ = str(parse('.'.join(map(str, version_info))))
 __installer_version__ = __version__
