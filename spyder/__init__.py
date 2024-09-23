8f4bf7fb07810a2c59e1f606202c28c1ab09cc88
Daniel Althviz Moré <16781833+dalthviz@users.noreply.github.com>
2024-09-23 15:50:46 -0500

    Release 6.0.1rc0 [ci skip]

--- spyder/__init__.py
+++ spyder/__init__.py
@@ -31,7 +31,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 from packaging.version import parse
 
-version_info = (6, 0, 1, "a1", "dev0")
+version_info = (6, 0, 1, "rc0")
 
 __version__ = str(parse('.'.join(map(str, version_info))))
 __installer_version__ = __version__
