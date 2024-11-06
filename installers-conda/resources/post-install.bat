e6082f9eddd563101b8098342b965a30832d21df
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-11-06 08:16:25 -0800

    Do not launch Spyder if installing in CI or batch/silent mode. With conda/constructor#885, batch/silent mode installs will automatically set INSTALLER_UNATTENDED environment variable accessible by user scripts. Until the next constructor version (>3.9.3) users may set either CI=1 or INSTALLER_UNATTENDED=1 at the commandline.

--- installers-conda/resources/post-install.bat
+++ installers-conda/resources/post-install.bat
@@ -1,17 +1,24 @@
-rem This script launches Spyder after install
+@rem  This script launches Spyder after install
 @echo off
+
+if "%CI%"=="1" set no_launch_spyder=true
+if "%INSTALLER_UNATTENDED%"=="1" set no_launch_spyder=true
+if defined no_launch_spyder (
+    @echo Installing in CI or silent mode, do not launch Spyder
+    exit /b %errorlevel%
+)
 
 set mode=system
 if exist "%PREFIX%\.nonadmin" set mode=user
 
-rem Get shortcut path
+@rem  Get shortcut path
 for /F "tokens=*" %%i in (
     '%PREFIX%\python %PREFIX%\Scripts\menuinst_cli.py shortcut --mode=%mode%'
 ) do (
     set shortcut=%%~fi
 )
 
-rem Launch Spyder
+@rem  Launch Spyder
 set tmpdir=%TMP%\spyder
 set launch_script=%tmpdir%\launch_script.bat
 
@@ -21,6 +28,7 @@ echo @echo off
 echo :loop
 echo tasklist /fi "ImageName eq Spyder-*" /fo csv 2^>NUL ^| findstr /r "Spyder-.*Windows-x86_64.exe"^>NUL
 echo if "%%errorlevel%%"=="0" ^(
+echo     @rem  Installer is still running
 echo     timeout /t 1 /nobreak ^> nul
 echo     goto loop
 echo ^) else ^(
