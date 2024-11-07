07e8f52d134b3f6abf91324fae1f071687a13293
Carlos Cordoba <ccordoba12@gmail.com>
2024-11-07 14:26:21 -0500

    Backport PR #22876 on branch 6.x (PR: Do not launch Spyder if installing in CI or batch/silent mode (Installers)) (#22893)

--- installers-conda/resources/post-install.bat
+++ installers-conda/resources/post-install.bat
@@ -1,17 +1,36 @@
-rem This script launches Spyder after install
+@rem  This script launches Spyder after install
 @echo off
+
+call :redirect 2>&1 >> %PREFIX%\install.log
+
+:redirect
+@echo Environment Variables:
+set
+
+if defined CI set no_launch_spyder=true
+if "%INSTALLER_UNATTENDED%"=="1" set no_launch_spyder=true
+@echo CI = %CI%
+@echo INSTALLER_UNATTENDED = %INSTALLER_UNATTENDED%
+@echo no_launch_spyder = %no_launch_spyder%
+if defined no_launch_spyder (
+    @echo Installing in silent mode, do not launch Spyder
+    exit /b %errorlevel%
+) else (
+    @echo Launching Spyder after install completed.
+)
 
 set mode=system
 if exist "%PREFIX%\.nonadmin" set mode=user
 
-rem Get shortcut path
+@rem  Get shortcut path
 for /F "tokens=*" %%i in (
     '%PREFIX%\python %PREFIX%\Scripts\menuinst_cli.py shortcut --mode=%mode%'
 ) do (
     set shortcut=%%~fi
 )
+@echo shortcut = %shortcut%
 
-rem Launch Spyder
+@rem  Launch Spyder
 set tmpdir=%TMP%\spyder
 set launch_script=%tmpdir%\launch_script.bat
 
@@ -21,6 +40,7 @@ echo @echo off
 echo :loop
 echo tasklist /fi "ImageName eq Spyder-*" /fo csv 2^>NUL ^| findstr /r "Spyder-.*Windows-x86_64.exe"^>NUL
 echo if "%%errorlevel%%"=="0" ^(
+echo     @rem  Installer is still running
 echo     timeout /t 1 /nobreak ^> nul
 echo     goto loop
 echo ^) else ^(
