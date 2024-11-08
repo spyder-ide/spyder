c20cbbfedda9e747d879e4aead2efa82a0f9bf8d
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-11-07 17:39:04 -0800

    Move common items to utils.py

---
+++ installers-conda/utils.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © Spyder Project Contributors
+# Licensed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+
+from logging import Formatter, StreamHandler, getLogger
+from pathlib import Path
+
+fmt = Formatter('%(asctime)s [%(levelname)s] [%(name)s] -> %(message)s')
+h = StreamHandler()
+h.setFormatter(fmt)
+logger = getLogger('Installer')
+logger.addHandler(h)
+logger.setLevel('INFO')
+
+HERE = Path(__file__).parent
+SPYREPO = HERE.parent
+RESOURCES = HERE / "resources"
+BUILD = HERE / "build"
+DIST = HERE / "dist"
