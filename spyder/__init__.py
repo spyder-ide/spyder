7ed73b0768b92c1b0a303a843d82da54070522e0
Daniel Althviz Moré <16781833+dalthviz@users.noreply.github.com>
2024-12-06 21:47:04 -0500

    Back to work [ci skip]

--- spyder/__init__.py
+++ spyder/__init__.py
@@ -31,7 +31,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 from packaging.version import parse
 
-version_info = (6, 0, 3, "rc1")
+version_info = (6, 0, 3, "rc2", "dev0")
 
 __version__ = str(parse('.'.join(map(str, version_info))))
 __installer_version__ = __version__
