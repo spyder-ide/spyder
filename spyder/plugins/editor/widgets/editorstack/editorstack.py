ef9ad0630b1e13434168a96f82fe061cecea200d
Daniel Althviz Moré <16781833+dalthviz@users.noreply.github.com>
2024-12-30 15:19:12 -0500

    Backport PR #23369: PR: Fix shortcut stop debugger

--- spyder/plugins/editor/widgets/editorstack/editorstack.py
+++ spyder/plugins/editor/widgets/editorstack/editorstack.py
@@ -508,6 +508,7 @@ class EditorStack(QWidget, SpyderWidgetMixin):
             "continue",
             "step",
             "return",
+            "stop",
             "toggle breakpoint",
             "toggle conditional breakpoint",
         ]:
