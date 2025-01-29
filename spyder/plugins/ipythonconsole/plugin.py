0c86203888f7c61c1633d31f63269fdbab1db262
Carlos Cordoba <ccordoba12@gmail.com>
2025-01-29 17:59:36 -0500

    Implement simpler solution to connect to a kernel at startup when a project is loaded

--- spyder/plugins/ipythonconsole/plugin.py
+++ spyder/plugins/ipythonconsole/plugin.py
@@ -565,19 +565,17 @@ class IPythonConsole(SpyderDockablePlugin, RunExecutor):
         """
         cli_options = self.get_command_line_options()
         connection_file = cli_options.connection_file
+
         if connection_file is not None:
-            self._handle_connect_to_kernel_cli(connection_file)
+            self.create_client_for_kernel(
+                self.get_widget().find_connection_file(connection_file),
+                give_focus=False,
+            )
         else:
             self.create_new_client(give_focus=False)
 
     # ---- Private methods
     # -------------------------------------------------------------------------
-    def _handle_connect_to_kernel_cli(self, connection_file):
-        self.create_client_for_kernel(
-            self.get_widget().find_connection_file(connection_file),
-            give_focus=False,
-        )
-
     def _on_project_loaded(self, path):
         self.get_widget().update_active_project_path(path)
 
