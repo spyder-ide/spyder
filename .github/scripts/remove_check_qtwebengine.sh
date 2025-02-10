5376d0fd55522c55071e57af14810a8639058330
Eric Larson <larson.eric.d@gmail.com>
2025-02-10 16:33:59 -0500

    Fix bug with qtwebengine not being optional

---
+++ .github/scripts/remove_check_qtwebengine.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+
+echo "which spyder: $(which spyder)"
+pip show qtwebengine
+TO=10s
+for i in 1 2; do
+    if [[ "$i" == "2" ]]; then
+        pip remove -y qtwebengine
+    fi
+    echo "Running Spyder with a timeout of $TO:"
+    timeout $TO spyder
+    RESULT=$?
+    if [[ $RESULT -eq 124 ]]; then
+        echo "Spyder succeeded with timeout"
+        echo
+    else
+        echo "Spyder failed with error code $RESULT (should be 124 for timeout)"
+        exit 1
+    fi
+done
