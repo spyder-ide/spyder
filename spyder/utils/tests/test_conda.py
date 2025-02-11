f467c7997226bc47efdd98fa94eabc6bc43cd6ad
jsbautista <js.bautista@uniandes.edu.co>
2025-02-10 20:44:15 -0500

    fix test

--- spyder/utils/tests/test_conda.py
+++ spyder/utils/tests/test_conda.py
@@ -70,8 +70,12 @@ def test_find_conda():
 def test_get_list_conda_envs():
     output = get_list_conda_envs()
 
-    expected_envs = ['base', 'jedi-test-env', 'spytest-ž',
-                     'micromamba/envs/test', 'miniconda3/envs/test']
+    if sys.platform == 'darwin':
+        expected_envs = ['base', 'jedi-test-env', 'spytest-ž',
+                         'micromamba/envs/test', 'miniconda3/envs/test']
+    else:
+        expected_envs = ['base', 'jedi-test-env', 'spytest-ž', 'test']
+
     expected_envs = ['Conda: ' + env for env in expected_envs]
 
     assert set(expected_envs) == set(output.keys())
