7738df5a0b7ae002c240f327e67af9df68ca3285
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2025-05-07 13:26:24 -0300

    fix: update timeouts to apply only for functions and set global session timeout

--- runtests.py
+++ runtests.py
@@ -35,8 +35,7 @@ def run_pytest(run_slow=False, extra_args=None, remoteclient=False):
     """Run pytest tests for Spyder."""
     # Be sure to ignore subrepos and remoteclient plugin
     pytest_args = ['-vv', '-rw', '--durations=10', '--ignore=./external-deps',
-                   '-W ignore::UserWarning', '--timeout=120',
-                   '--timeout_method=thread']
+                   '-W ignore::UserWarning']
 
     if CI:
         # Show coverage
