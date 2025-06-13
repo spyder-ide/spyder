5954f50632cc29cc789a932363edfa99057afb5f
jsbautista <42411448+jsbautista@users.noreply.github.com>
2025-06-13 15:57:02 -0500

    PR: Add option to use Flake8 for linting (Completions) (#24532)
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>

--- spyder/plugins/completion/providers/languageserver/conftabs/linting.py
+++ spyder/plugins/completion/providers/languageserver/conftabs/linting.py
@@ -9,11 +9,12 @@ Language Server Protocol linting configuration tab.
 """
 
 # Third party imports
-from qtpy.QtWidgets import QLabel, QVBoxLayout
+from qtpy.QtWidgets import QButtonGroup, QGroupBox, QLabel, QVBoxLayout
 
 # Local imports
 from spyder.api.preferences import SpyderPreferencesTab
-from spyder.config.base import _
+from spyder.api.translations import _
+from spyder.utils.stylesheet import AppStyle
 
 
 class LintingConfigTab(SpyderPreferencesTab):
@@ -25,14 +26,45 @@ class LintingConfigTab(SpyderPreferencesTab):
         super().__init__(parent)
         newcb = self.create_checkbox
 
-        linting_label = QLabel(_("Spyder can optionally highlight syntax "
-                                 "errors and possible problems with your "
-                                 "code in the editor."))
+        linting_label = QLabel(
+            _(
+                "Spyder can highlight syntax errors and possible problems "
+                "with your code in the editor by using one of the providers "
+                "below"
+            )
+        )
         linting_label.setOpenExternalLinks(True)
         linting_label.setWordWrap(True)
-        linting_check = self.create_checkbox(
-            _("Enable basic linting"),
-            'pyflakes')
+
+        linting_select_group = QGroupBox(_("Provider"))
+
+        linting_bg = QButtonGroup(linting_select_group)
+        basic_linting_radio = self.create_radiobutton(
+            _("Pyflakes (basic)"),
+            'pyflakes',
+            button_group=linting_bg
+        )
+        flake_linting_radio = self.create_radiobutton(
+            _("Flake8 (intermediate)"),
+            'flake8',
+            button_group=linting_bg
+        )
+        
+        disable_linting_radio = self.create_radiobutton(
+            _("Disable linting"),
+            'no_linting',
+            button_group=linting_bg
+        )
+        
+        linting_select_layout = QVBoxLayout()
+        linting_select_layout.addSpacing(3 * AppStyle.MarginSize)
+        linting_select_layout.addWidget(basic_linting_radio)
+        linting_select_layout.addWidget(flake_linting_radio)
+        linting_select_layout.addWidget(disable_linting_radio)
+        linting_select_group.setLayout(linting_select_layout)
+
+        additional_options_group = QGroupBox(_("Additional options"))
+
         underline_errors_box = newcb(
             _("Underline errors and warnings"),
             'underline_errors',
@@ -41,11 +73,15 @@ class LintingConfigTab(SpyderPreferencesTab):
             _("Enable complexity linting with the Mccabe package"),
             'mccabe')
 
+        additional_options_layout = QVBoxLayout()
+        additional_options_layout.addWidget(underline_errors_box)
+        additional_options_layout.addWidget(linting_complexity_box)
+        additional_options_group.setLayout(additional_options_layout)
+
+
         # Linting layout
         linting_layout = QVBoxLayout()
         linting_layout.addWidget(linting_label)
-        linting_layout.addWidget(linting_check)
-        linting_layout.addWidget(underline_errors_box)
-        linting_layout.addWidget(linting_complexity_box)
+        linting_layout.addWidget(linting_select_group)
+        linting_layout.addWidget(additional_options_group)
         self.setLayout(linting_layout)
-        linting_check.checkbox.toggled.connect(underline_errors_box.setEnabled)
