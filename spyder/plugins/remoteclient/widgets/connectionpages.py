684ba02f1de2327d1a15710c21c3e3a3040ee7f6
Carlos Cordoba <ccordoba12@gmail.com>
2025-07-04 12:17:04 -0500

    Remote client: Remove repeated code in connection{dialog/pages} modules

--- spyder/plugins/remoteclient/widgets/connectionpages.py
+++ spyder/plugins/remoteclient/widgets/connectionpages.py
@@ -4,7 +4,7 @@
 # Licensed under the terms of the MIT License
 # (see spyder/__init__.py for details)
 
-"""Dialog to handle remote connections."""
+"""Pages for the dialog that handles remote connections."""
 
 # Standard library imports
 from __future__ import annotations
@@ -14,14 +14,11 @@ from typing import TypedDict
 import uuid
 
 # Third party imports
-from qtpy.QtCore import Qt, Signal
+from qtpy.QtCore import Qt
 from qtpy.QtWidgets import (
-    QDialogButtonBox,
     QGridLayout,
     QHBoxLayout,
     QLabel,
-    QMessageBox,
-    QPushButton,
     QStackedWidget,
     QStackedLayout,
     QVBoxLayout,
@@ -32,7 +29,6 @@ from qtpy.QtWidgets import (
 from spyder.api.fonts import SpyderFontType, SpyderFontsMixin
 from spyder.api.translations import _
 from spyder.api.utils import get_class_values
-from spyder.api.widgets.dialogs import SpyderDialogButtonBox
 from spyder.plugins.remoteclient.api.protocol import (
     ConnectionInfo,
     ConnectionStatus,
@@ -46,10 +42,9 @@ from spyder.plugins.remoteclient.widgets.connectionstatus import (
     ConnectionStatusWidget,
 )
 from spyder.utils.icon_manager import ima
-from spyder.utils.stylesheet import AppStyle, MAC, WIN
+from spyder.utils.stylesheet import AppStyle, MAC
 from spyder.widgets.config import SpyderConfigPage
 from spyder.widgets.helperwidgets import MessageLabel, TipWidget
-from spyder.widgets.sidebardialog import SidebarDialog
 
 
 # =============================================================================
@@ -893,301 +888,3 @@ class ConnectionPage(BaseConnectionPage):
         else:
             self.new_name = None
             return False
-
-
-# =============================================================================
-# ---- Dialog
-# =============================================================================
-class ConnectionDialog(SidebarDialog):
-    """
-    Dialog to handle and display remote connection information for different
-    machines.
-    """
-
-    TITLE = _("Remote connections")
-    MIN_WIDTH = 900 if MAC else (830 if WIN else 880)
-    MIN_HEIGHT = 700 if MAC else (635 if WIN else 650)
-    PAGE_CLASSES = [NewConnectionPage]
-
-    sig_start_server_requested = Signal(str)
-    sig_stop_server_requested = Signal(str)
-    sig_server_renamed = Signal(str)
-    sig_connections_changed = Signal()
-
-    def __init__(self, parent=None):
-        self.ICON = ima.icon('remote_server')
-        super().__init__(parent)
-        self._container = parent
-
-        # -- Setup
-        self._add_saved_connection_pages()
-
-        # If there's more than one page, give focus to the first server because
-        # users will probably want to interact with servers here rather than
-        # create new connections.
-        if self.number_of_pages() > 1:
-            # Index 1 is the separator added after the new connection page
-            self.set_current_index(2)
-
-        # -- Signals
-        if self._container is not None:
-            self._container.sig_connection_status_changed.connect(
-                self._update_connection_buttons_state
-            )
-
-    # ---- SidebarDialog API
-    # -------------------------------------------------------------------------
-    def create_buttons(self):
-        bbox = SpyderDialogButtonBox(QDialogButtonBox.Cancel)
-
-        self._button_save_connection = QPushButton(_("Save connection"))
-        self._button_save_connection.clicked.connect(
-            self._save_connection_info
-        )
-        bbox.addButton(
-            self._button_save_connection, QDialogButtonBox.ResetRole
-        )
-
-        self._button_remove_connection = QPushButton(_("Remove connection"))
-        self._button_remove_connection.clicked.connect(
-            self._remove_connection_info
-        )
-        bbox.addButton(
-            self._button_remove_connection, QDialogButtonBox.ResetRole
-        )
-
-        self._button_clear_settings = QPushButton(_("Clear settings"))
-        self._button_clear_settings.clicked.connect(self._clear_settings)
-        bbox.addButton(
-            self._button_clear_settings, QDialogButtonBox.ActionRole
-        )
-
-        self._button_connect = QPushButton(_("Connect"))
-        self._button_connect.clicked.connect(self._start_server)
-        bbox.addButton(self._button_connect, QDialogButtonBox.ActionRole)
-
-        self._button_stop = QPushButton(_("Stop"))
-        self._button_stop.clicked.connect(self._stop_server)
-        bbox.addButton(self._button_stop, QDialogButtonBox.ActionRole)
-
-        layout = QHBoxLayout()
-        layout.addWidget(bbox)
-
-        return bbox, layout
-
-    def current_page_changed(self, index):
-        """Update the state of buttons when moving from page to page."""
-        page = self.get_page(index)
-        if page.NEW_CONNECTION:
-            self._button_save_connection.setEnabled(True)
-            self._button_clear_settings.setHidden(False)
-            self._button_remove_connection.setHidden(True)
-            self._button_stop.setHidden(True)
-        else:
-            if page.is_modified:
-                self._button_save_connection.setEnabled(True)
-            else:
-                self._button_save_connection.setEnabled(False)
-
-            self._button_clear_settings.setHidden(True)
-            self._button_remove_connection.setHidden(False)
-            self._button_stop.setHidden(False)
-
-        if page.status in [
-            ConnectionStatus.Inactive,
-            ConnectionStatus.Error,
-        ]:
-            self._button_connect.setEnabled(True)
-        else:
-            self._button_connect.setEnabled(False)
-
-        # TODO: Check if it's possible to stop a connection while it's
-        # connecting
-        if page.status == ConnectionStatus.Active:
-            self._button_stop.setEnabled(True)
-        else:
-            self._button_stop.setEnabled(False)
-
-    # ---- Private API
-    # -------------------------------------------------------------------------
-    def _save_connection_info(self):
-        """Save the connection info stored in a page."""
-        page = self.get_page()
-
-        # Validate info
-        if not page.validate_page():
-            return
-
-        if page.NEW_CONNECTION:
-            # Save info provided by users
-            page.save_to_conf()
-
-            # Add separator if needed
-            if self.number_of_pages() == 1:
-                self.add_separator()
-
-            # Add connection page to the dialog with the new info
-            self._add_connection_page(host_id=page.host_id, new=True)
-
-            # Give focus to the new page
-            self.set_current_index(self.number_of_pages() - 1)
-
-            # Reset page in case users want to introduce another connection
-            page.reset_page()
-
-            # Inform container that a change in connections took place
-            self.sig_connections_changed.emit()
-        else:
-            # Update name in the dialog if it was changed by users. This needs
-            # to be done before calling save_to_conf so that we can compare the
-            # saved name with the current one.
-            if page.has_new_name():
-                self.get_item().setText(page.new_name)
-
-            # Update connection info
-            page.save_to_conf()
-
-            # After saving to our config system, we can inform the container
-            # that a change in connections took place.
-            if page.new_name is not None:
-                self.sig_connections_changed.emit()
-                self.sig_server_renamed.emit(page.host_id)
-                page.new_name = None
-
-            # Mark page as not modified and disable save button
-            page.set_modified(False)
-            self._button_save_connection.setEnabled(False)
-
-    def _remove_connection_info(self):
-        """
-        Remove the connection info stored in a given page and hide it as well.
-        """
-        page = self.get_page()
-        if not page.NEW_CONNECTION:
-            reply = QMessageBox.question(
-                self,
-                _("Remove connection"),
-                _(
-                    "Do you want to remove the connection called <b>{}</b>?"
-                ).format(page.get_name()),
-                QMessageBox.Yes,
-                QMessageBox.No,
-            )
-
-            if reply == QMessageBox.Yes:
-                self.hide_page()
-                page.remove_config_options()
-
-                # Inform container that a change in connections took place
-                self.sig_connections_changed.emit()
-
-    def _clear_settings(self):
-        """Clear the setting introduced in the new connection page."""
-        page = self.get_page()
-        if page.NEW_CONNECTION:
-            page.reset_page(clear=True)
-
-    def _start_server(self):
-        """Start the server corresponding to a given page."""
-        page = self.get_page()
-
-        # Validate info
-        if not page.validate_page():
-            return
-
-        # This uses the current host_id in case users want to start a
-        # connection directly from the new connection page (
-        # _save_connection_info generates a new id fo that page at the end).
-        host_id = page.host_id
-
-        if page.NEW_CONNECTION or page.is_modified:
-            # Save connection info if necessary
-            self._save_connection_info()
-
-            # TODO: Handle the case when the connection info is active and
-            # users change its info.
-
-        self.sig_start_server_requested.emit(host_id)
-
-    def _stop_server(self):
-        """Stop the server corresponding to a given page."""
-        page = self.get_page()
-
-        # The stop button is not visible in the new connection page
-        if not page.NEW_CONNECTION:
-            self._button_stop.setEnabled(False)
-            self.sig_stop_server_requested.emit(page.host_id)
-
-    def _add_connection_page(self, host_id: str, new: bool):
-        """Add a new connection page to the dialog."""
-        page = ConnectionPage(self, host_id=host_id)
-
-        # This is necessary to make button_save_connection enabled when there
-        # are config changes in the page
-        page.apply_button_enabled.connect(
-            self._update_button_save_connection_state
-        )
-
-        if new:
-            page.save_server_info()
-
-        self.add_page(page)
-
-        # Add saved logs to the page
-        if self._container is not None:
-            page.add_logs(self._container.client_logs.get(host_id, []))
-
-            # This updates the info shown in the "Connection info" tab of pages
-            self._container.sig_connection_status_changed.connect(
-                page.update_status
-            )
-            self._container.sig_client_message_logged.connect(page.add_log)
-
-    def _add_saved_connection_pages(self):
-        """Add a connection page for each server saved in our config system."""
-        page = self.get_page(index=0)
-        servers = page.get_option("servers", default={})
-
-        if servers:
-            self.add_separator()
-
-            for id_ in servers.keys():
-                self._add_connection_page(host_id=id_, new=False)
-
-    def _update_button_save_connection_state(self, state: bool):
-        """Update the state of the 'Save connection' button."""
-        self._button_save_connection.setEnabled(state)
-
-    def _update_connection_buttons_state(self, info: ConnectionInfo):
-        """Update the state of the 'Connect' button."""
-        page = self.get_page()
-        if page.host_id == info["id"]:
-            if info["status"] in [
-                ConnectionStatus.Inactive,
-                ConnectionStatus.Error,
-            ]:
-                self._button_connect.setEnabled(True)
-            else:
-                self._button_connect.setEnabled(False)
-
-            # TODO: Check if it's possible to stop a connection while it's
-            # connecting
-            if info["status"] == ConnectionStatus.Active:
-                self._button_stop.setEnabled(True)
-            else:
-                self._button_stop.setEnabled(False)
-
-
-def test():
-    from spyder.utils.qthelpers import qapplication
-    from spyder.utils.stylesheet import APP_STYLESHEET
-
-    app = qapplication()  # noqa
-    app.setStyleSheet(str(APP_STYLESHEET))
-
-    dialog = ConnectionDialog()
-    dialog.exec_()
-
-
-if __name__ == "__main__":
-    test()
