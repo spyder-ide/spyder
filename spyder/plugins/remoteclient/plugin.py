347e5af7762cc0cba2a2265dd8a537b374b84d11
Carlos Cordoba <ccordoba12@gmail.com>
2025-07-09 13:12:56 -0500

    Remote client: Allow Envs Manager to set the default kernelspec
    [ci skip]

--- spyder/plugins/remoteclient/plugin.py
+++ spyder/plugins/remoteclient/plugin.py
@@ -23,7 +23,7 @@ from spyder.api.plugin_registration.decorators import (
     on_plugin_available,
     on_plugin_teardown,
 )
-from spyder.api.plugins import Plugins, SpyderPluginV2
+from spyder.api.plugins import OptionalPlugins, Plugins, SpyderPluginV2
 from spyder.api.translations import _
 from spyder.plugins.mainmenu.api import (
     ApplicationMenus,
@@ -71,7 +71,7 @@ class RemoteClient(SpyderPluginV2):
     """
 
     NAME = "remoteclient"
-    OPTIONAL = [Plugins.IPythonConsole, Plugins.MainMenu]
+    OPTIONAL = [Plugins.MainMenu, OptionalPlugins.EnvsManager]
     CONF_SECTION = NAME
     CONTAINER_CLASS = RemoteClientContainer
     CONF_FILE = False
@@ -177,6 +177,20 @@ class RemoteClient(SpyderPluginV2):
         mainmenu.remove_item_from_application_menu(
             RemoteClientActions.ManageConnections,
             menu_id=ApplicationMenus.Tools,
+        )
+
+    @on_plugin_available(plugin=OptionalPlugins.EnvsManager)
+    def on_envsmanager_available(self):
+        envsmanager = self.get_plugin(OptionalPlugins.EnvsManager)
+        envsmanager.sig_set_default_kernel_spec_requested.connect(
+            self.set_default_kernel_spec
+        )
+
+    @on_plugin_teardown(plugin=OptionalPlugins.EnvsManager)
+    def on_envsmanager_teardown(self):
+        envsmanager = self.get_plugin(OptionalPlugins.EnvsManager)
+        envsmanager.sig_set_default_kernel_spec_requested.disconnect(
+            self.set_default_kernel_spec
         )
 
     # ---- Public API
