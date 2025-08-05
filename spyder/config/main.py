3856bd180a1960557777c6cd4551b5f59415893d
Carlos Cordoba <ccordoba12@gmail.com>
2025-08-05 15:27:54 -0500

    Config: Add Profile toolbar to the list of visible toolbars

--- spyder/config/main.py
+++ spyder/config/main.py
@@ -100,6 +100,7 @@ DEFAULTS = [
                   ApplicationToolbars.File,
                   ApplicationToolbars.Run,
                   ApplicationToolbars.Debug,
+                  ApplicationToolbars.Profile,
                   ApplicationToolbars.Main,
                   ApplicationToolbars.WorkingDirectory,
               ],
@@ -709,4 +710,4 @@ NAME_MAP = {
 #    or if you want to *rename* options, then you need to do a MAJOR update in
 #    version, e.g. from 3.0.0 to 4.0.0
 # 3. You don't need to touch this value if you're just adding a new option
-CONF_VERSION = '87.3.0'
+CONF_VERSION = '87.4.0'
