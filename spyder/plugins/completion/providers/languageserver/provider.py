08c71ae099959b693e27be384fa08fe9017a6e16
Daniel Althviz Moré <16781833+dalthviz@users.noreply.github.com>
2025-09-05 18:07:45 -0500

    Apply suggestions from code review
    Co-authored-by: Carlos Cordoba <ccordoba12@gmail.com>

--- spyder/plugins/completion/providers/languageserver/provider.py
+++ spyder/plugins/completion/providers/languageserver/provider.py
@@ -72,7 +72,7 @@ class LanguageServerProvider(SpyderCompletionProvider):
         ('flake8/max_line_length', 79),
         ('ruff/exclude', ''),
         ('ruff/extendSelect', ''),
-        ('ruff/extendIgnore', 'D,DOC,E,W,C90'),
+        ('ruff/extendIgnore', 'E'),
         ('pydocstyle', False),
         ('pydocstyle/convention', 'numpy'),
         ('pydocstyle/select', ''),
