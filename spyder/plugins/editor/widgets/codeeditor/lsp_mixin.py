65eee5eb407ae4337942647256c4da6b833925b5
jsbautista <js.bautista@uniandes.edu.co>
2025-09-13 18:33:32 -0500

    add variable to identify the first update

--- spyder/plugins/editor/widgets/codeeditor/lsp_mixin.py
+++ spyder/plugins/editor/widgets/codeeditor/lsp_mixin.py
@@ -203,6 +203,7 @@ class LSPMixin:
         self.formatting_in_progress = False
         self.symbols_in_sync = False
         self.folding_in_sync = False
+        self._update_first_time = False
 
     # ---- Helper private methods
     # -------------------------------------------------------------------------
@@ -355,9 +356,11 @@ class LSPMixin:
         # It is an error if this happens because as per LSP specification
         # `didOpen` “must not be sent more than once without a corresponding
         # close notification send before”.
-        self._timer_sync_symbols_and_folding.timeout.connect(
-            self.sync_symbols_and_folding, Qt.UniqueConnection
-        )
+        if not self.oe_proxy.update_on_save:
+            self._timer_sync_symbols_and_folding.timeout.connect(
+                self.sync_symbols_and_folding, Qt.UniqueConnection
+            )
+        self._update_first_time = True
 
         cursor = self.textCursor()
         text = self.get_text_with_eol()
@@ -1319,6 +1322,8 @@ class LSPMixin:
         self.folding_in_sync = True
 
     def update_outline_on_save(self, state):
+        if not self._update_first_time:
+            return
         if state:
             try:
                 self._timer_sync_symbols_and_folding.timeout.disconnect()
