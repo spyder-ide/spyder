776c48c9c8a31ca40fdf6f18bac8bb58e4c7b095
Carlos Cordoba <ccordoba12@gmail.com>
2025-09-28 13:33:11 -0700

    Backport PR #25067: PR: Some improvements to the Update manager statusbar widget

--- spyder/utils/icon_manager.py
+++ spyder/utils/icon_manager.py
@@ -298,6 +298,7 @@ class IconManager():
             'close_pane':              [('mdi.window-close',), {'color': self.MAIN_FG_COLOR}],
             'toolbar_ext_button':      [('mdi.dots-horizontal',), {'color': self.MAIN_FG_COLOR}],
             'inapp_appeal':            [('mdi6.heart',), {'color': SpyderPalette.COLOR_HEART}],
+            'update':                  [('mdi6.tray-arrow-down',), {'color': self.MAIN_FG_COLOR}],
             # --- Autocompletion/document symbol type icons --------------
             'completions':             [('mdi.code-tags-check',), {'color': self.MAIN_FG_COLOR}],
             'keyword':                 [('mdi.alpha-k-box',), {'color': SpyderPalette.GROUP_9, 'scale_factor': self.BIG_ATTR_FACTOR}],
