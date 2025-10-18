20ad6b0836cf13c008ef7358f322da621f760b8e
Carlos Cordoba <ccordoba12@gmail.com>
2025-10-17 21:43:00 -0500

    Variable Explorer: Don't show object explorer for short bytes
    Also, stop spinner if TextEditor setup fails.

--- spyder/plugins/variableexplorer/widgets/collectionsdelegate.py
+++ spyder/plugins/variableexplorer/widgets/collectionsdelegate.py
@@ -340,6 +340,9 @@ class CollectionsDelegate(QItemDelegate, SpyderFontsMixin):
                         ),
                     ),
                 )
+            else:
+                self.sig_editor_shown.emit()
+
             return None
         # QLineEdit for an individual value (int, float, short string, etc)
         elif is_editable_type(value) and not object_explorer:
@@ -373,8 +376,16 @@ class CollectionsDelegate(QItemDelegate, SpyderFontsMixin):
                 return editor
         # ObjectExplorer for an arbitrary Python object
         else:
-            from spyder.plugins.variableexplorer.widgets.objectexplorer \
-                import ObjectExplorer
+            # Don't show the object explorer for short bytes because it's not
+            # really necessary
+            if isinstance(value, bytes):
+                self.sig_editor_shown.emit()
+                return None
+
+            from spyder.plugins.variableexplorer.widgets.objectexplorer import (
+                ObjectExplorer,
+            )
+
             editor = ObjectExplorer(
                 value,
                 name=key,
@@ -498,11 +509,6 @@ class CollectionsDelegate(QItemDelegate, SpyderFontsMixin):
         """
         value = self.get_value(index)
         if isinstance(editor, QLineEdit):
-            if isinstance(value, bytes):
-                try:
-                    value = str(value, 'utf8')
-                except Exception:
-                    pass
             if not isinstance(value, str):
                 value = repr(value)
             editor.setText(value)
