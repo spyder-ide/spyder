0779d35f63d1f5ba9df0ff05c8c8e9cad332251d
jsbautista <js.bautista@uniandes.edu.co>
2025-11-06 23:47:00 -0500

    fix regular expression

--- spyder/plugins/ipythonconsole/widgets/client.py
+++ spyder/plugins/ipythonconsole/widgets/client.py
@@ -610,8 +610,9 @@ class ClientWidget(QWidget, SaveHistoryMixin, SpyderWidgetMixin):  # noqa: PLR09
     def is_warning_message(self, error):
         """Decide if a message contains a warning in order to filter it."""
         warning_pattern = re.compile(
-            r"(?:^|\s)(?:[A-Za-z]*Warning:|WARNING:?)(?=\s|$)"
-            )
+            r"(?:^|\s)(?:[A-Za-z]*Warning:|"
+            r"(?<=\s)WARNING(?=\s))(?:\:)?(?=\s|$)"
+        )
         return warning_pattern.search(error)
 
     def get_name(self):
