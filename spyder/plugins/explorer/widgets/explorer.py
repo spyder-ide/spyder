a02397a28f28a77dc511a47193dff5996d7c02aa
jsbautista <js.bautista@uniandes.edu.co>
2025-11-27 23:21:28 -0500

    Fix right click on DirView

--- spyder/plugins/explorer/widgets/explorer.py
+++ spyder/plugins/explorer/widgets/explorer.py
@@ -919,11 +919,13 @@ class DirView(QTreeView, SpyderWidgetMixin):
 
         Taken from https://stackoverflow.com/a/13142586/438386
         """
+        if event.button() == Qt.RightButton:
+            return
         clicked_index = self.indexAt(event.pos())
         if clicked_index.isValid():
             vrect = self.visualRect(clicked_index)
             item_identation = vrect.x() - self.visualRect(self.rootIndex()).x()
-            if event.pos().x() < item_identation and event.button() == Qt.LeftButton:
+            if event.pos().x() < item_identation:
                 self.expanded_or_colapsed_by_mouse = True
             else:
                 self.expanded_or_colapsed_by_mouse = False
@@ -938,8 +940,6 @@ class DirView(QTreeView, SpyderWidgetMixin):
 
     def mouseReleaseEvent(self, event):
         """Handle single clicks."""
-        if event.button() != Qt.LeftButton:
-            return
         super().mouseReleaseEvent(event)
         if self.get_conf('single_click_to_open'):
             self.clicked(index=self.indexAt(event.pos()))
