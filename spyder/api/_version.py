ec04526f3a73f02d6c14996c8f387da6d60db52e
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2025-11-29 18:13:43 -0600

    Add and revise __init__.py docstrings in spyder.api

--- spyder/api/_version.py
+++ spyder/api/_version.py
@@ -5,22 +5,30 @@
 # (see spyder/__init__.py for details)
 
 """
-Spyder API Version.
+Spyder API version; minor bumped for additions/deprecations, major for breaks.
 
 The API version should be modified according to the following rules:
 
-1. If a module/class/method/function/signal is added, then the minor version
-   must be increased.
+* If a module/class/method/function/signal is added, then the minor version
+  must be incremented.
 
-2. If a module/class/method/function/signal is removed, renamed or changes its
-   signature, then the major version must be increased.
+* If a module/class/method/function/signal is removed, renamed or changes its
+  signature, then the major version must be incremented.
 
-3. Whenever possible, deprecation marks and alerts should be employed in
-   order to inform developers of breaking-compatibility changes in a
-   future API release. In such case, the minor version should be increased
-   and once the deprecated APIs disappear then the major version should be
-   updated.
+* Whenever possible, deprecation warnings and alerts should be employed to
+  inform developers of breaking-compatibility changes in a future API release
+  In such cases, the minor version should be increased and once the deprecated
+  APIs disappear then the major version should be updated.
 """
 
 VERSION_INFO = (1, 4, 0)
+
 __version__ = '.'.join(map(str, VERSION_INFO))
+"""
+Spyder API version; minor bumped for additions/deprecations, major for breaks.
+
+The minor version will be incremented when adding or deprecating an API,
+(for example, a module, class, method, function, signal, constant, or similar)
+while the major version will be incremented for all breaking API changes,
+such as removing, renaming or changing the signature of an API.
+"""
