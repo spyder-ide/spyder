edd03efcc398e7b14d19460caccbaf20e9a13317
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2025-12-03 18:16:03 -0600

    Inline API __version__ into top-level spyder.api __init__

--- spyder/api/__init__.py
+++ spyder/api/__init__.py
@@ -8,9 +8,30 @@
 Base classes, mixins and widgets for creating plugins to extend Spyder.
 
 This API should be considered production-ready as of Spyder 6.0.
+The API version is modified according to the following rules:
+
+* If a module/class/method/function/signal is added, then the minor version
+  will be incremented.
+
+* If a module/class/method/function/signal is removed, renamed or changes its
+  signature, then the major version will be incremented.
+
+* Whenever possible, deprecation warnings and alerts will be employed to
+  inform developers of breaking-compatibility changes in a future API release
+  In such cases, the minor version will be increased and once the deprecated
+  APIs are removed then the major version will be updated.
 """
 
-from ._version import __version__
+from packaging.version import parse
 
+version_info = (1, 4, 0)
 
-__all__ = ["__version__"]
+__version__: str = str(parse('.'.join(map(str, version_info))))
+"""
+Spyder API version; minor bumped for additions/deprecations, major for breaks.
+
+The minor version will be incremented when adding or deprecating an API,
+(for example, a module, class, method, function, signal, constant, or similar)
+while the major version will be incremented for all breaking API changes,
+such as removing, renaming or changing the signature of an API.
+"""
