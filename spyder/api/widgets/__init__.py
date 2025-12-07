76d81e45b7b5fbb87277fb1fd3c97989ff60d3f2
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2025-12-07 00:08:09 -0600

    Add missing types to spyder.api.widgets pseudo-enum attributes

--- spyder/api/widgets/__init__.py
+++ spyder/api/widgets/__init__.py
@@ -11,32 +11,32 @@ Widgets to extend Spyder through its API.
 
 
 class PluginMainWidgetWidgets:
-    """Basic widgets that any :class:`~spyder.api.plugins.SpyderDockablePlugin` has."""
+    """Basic widgets any :class:`~spyder.api.plugins.SpyderDockablePlugin` has."""
 
-    CornerWidget = 'corner_widget'
+    CornerWidget: str = 'corner_widget'
     """Right-corner pane toolbar buttons, just left of the options button."""
 
-    MainToolbar = 'main_toolbar_widget'
+    MainToolbar: str = 'main_toolbar_widget'
     """The primary pane toolbar, left-aligned."""
 
-    OptionsToolButton = 'options_button_widget'
+    OptionsToolButton: str = 'options_button_widget'
     """The pane's "hamburger menu", on the very right of the toolbar area."""
 
-    Spinner = 'spinner_widget'
+    Spinner: str = 'spinner_widget'
     """An optional progress spinner widget in the toolbar."""
 
 
 class PluginMainWidgetActions:
-    """Common menu actions for all :class:`~spyder.api.plugins.SpyderDockablePlugin`\\s."""
+    """Common menu actions for :class:`~spyder.api.plugins.SpyderDockablePlugin`\\s."""
 
-    ClosePane = 'close_pane'
+    ClosePane: str = 'close_pane'
     """Close the plugin's pane."""
 
-    DockPane = 'dock_pane'
+    DockPane: str = 'dock_pane'
     """Re-dock a popped-out pane to the Spyder main window."""
 
-    UndockPane = 'undock_pane'
+    UndockPane: str = 'undock_pane'
     """Pop out a plugin's pane into a separate floating window."""
 
-    LockUnlockPosition = 'lock_unlock_position'
+    LockUnlockPosition: str = 'lock_unlock_position'
     """Toggle whether a pane can be freely moved around the Spyder window."""
