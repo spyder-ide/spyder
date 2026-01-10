2a3434b2afb653ae6e9554cc6a43b7b979483e58
athompson673 <athompson6735@gmail.com>
2026-01-10 01:05:15 -0500

    Implemented copy to html

---
+++ spyder/plugins/editor/utils/html_export.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © Spyder Project Contributors
+# Licensed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+
+"""
+Editor widget html export helper functions
+"""
+
+# Standard library imports
+import html
+from inspect import cleandoc
+from io import StringIO
+
+# Third party imports
+from qtpy.QtGui import QTextCharFormat, QTextCursor
+
+
+HTML_TEMPLATE = cleandoc("""
+     <!DOCTYPE html>
+     <html>
+     <head></head>
+     <body>
+     <pre>{}</pre>
+     </body>
+     </html>
+""")
+
+
+def format_to_style(char_format: QTextCharFormat) -> str:
+    """
+    Gather the foreground color, font-style, and font-weight from a
+    QTextCharFormat, and generate the contents of a 'style' tag for a <span>
+    html element.
+    """
+    color = char_format.foreground().color().name()
+    font = char_format.font()
+    style = "italic" if font.italic() else "normal"
+    weight = "bold" if font.bold() else "normal"
+    return f"color:{color};font-style: {style};font-weight: {weight}"
+
+
+def selection_to_html(cursor: QTextCursor) -> str:
+    """
+    Create an html document from a QTextCursor selection
+    to capture syntax highlighting.
+    """
+    if not cursor.hasSelection():
+        return
+
+    sio = StringIO()
+    selection_start = cursor.selectionStart()
+    selection_end = cursor.selectionEnd()
+    document = cursor.document()
+    first_block = True
+    block = document.findBlock(selection_start)
+    while block.isValid():
+        if not first_block:
+                sio.write("\n")
+        else:
+            first_block = False
+        rel_start = selection_start - block.position()
+        rel_end = selection_end - block.position()
+        block_text = block.text()
+        for f_range in block.layout().additionalFormats():
+            range_end = f_range.start + f_range.length
+            if rel_start > range_end:
+                continue  # skip ranges until start of selection
+            s_start = max(f_range.start, rel_start)
+            s_end = min(range_end, rel_end)
+            s_text = block_text[s_start:s_end]
+            style = format_to_style(f_range.format)
+            sio.write(f"<span style=\"{style}\">{html.escape(s_text)}</span>")
+            if s_end == rel_end: #last span
+                return HTML_TEMPLATE.format(sio.getvalue())
+        block = block.next()
