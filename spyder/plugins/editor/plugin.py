2f2d2580040ac81f3f1173c888cda0ec7e0976fa
hprodh <hugo.prodhomme@gmx.fr>
2026-04-06 21:18:58 +0200

    Custom annotations in editor preferences dialog

--- spyder/plugins/editor/plugin.py
+++ spyder/plugins/editor/plugin.py
@@ -74,6 +74,8 @@ class Editor(SpyderDockablePlugin):
     CONF_SECTION = NAME
     CONF_WIDGET_CLASS = EditorConfigPage
     CONF_FILE = False
+    CONF_DEFAULTS = [(CONF_SECTION, {'todo_list': True,
+                                     'custom_annotations': 'NOTE, note'})]
 
     # ---- Signals
     # ------------------------------------------------------------------------
