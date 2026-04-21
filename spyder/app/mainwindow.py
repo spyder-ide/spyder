3e8f0b521fd74f670ffd0044d57af94a179435fd
Carlos Cordoba <ccordoba12@gmail.com>
2026-04-21 12:16:16 -0500

    Main window: Declare some actions required on plugin re-registration
    - Register shortcuts and tabify dockable plugins.
    - Also, fix bug when re-registering "switch to" shortcut.

--- spyder/app/mainwindow.py
+++ spyder/app/mainwindow.py
@@ -471,7 +471,7 @@ class MainWindow(QMainWindow, SpyderMainWindowMixin, SpyderShortcutsMixin):
             plugin._switch_to_shortcut = sc
 
             if Plugins.Shortcuts in PLUGIN_REGISTRY:
-                self.shortcuts.register_shortcut(sc, context, action_name)
+                self.shortcuts.register_shortcut(sc, context, name)
                 self.shortcuts.register_shortcut(
                     plugin.toggle_view_action, context, name
                 )
@@ -480,6 +480,15 @@ class MainWindow(QMainWindow, SpyderMainWindowMixin, SpyderShortcutsMixin):
                 self.shortcut_queue.append(
                     (plugin.toggle_view_action, context, name)
                 )
+
+        # Actions to perform when the plugin is re-registered
+        if not self.is_setting_up:
+            # This is necessary to register plugin shortcuts again
+            self.shortcuts.apply_shortcuts()
+
+            # Tabify plugin
+            if isinstance(plugin, SpyderDockablePlugin):
+                self.layouts.tabify_plugin(plugin)
 
     def unregister_plugin(self, plugin_name):
         """Unregister a plugin from the main window."""
