a2c89d5a852b6d4122a93fd1791e941ea432878c
Carlos Cordoba <ccordoba12@gmail.com>
2026-05-05 11:52:24 -0500

    Editor: Add API to remove panels and extensions

--- spyder/plugins/editor/widgets/editorstack/editorstack.py
+++ spyder/plugins/editor/widgets/editorstack/editorstack.py
@@ -807,6 +807,7 @@ class EditorStack(QWidget, SpyderWidgetMixin):
         self.send_to_help(name, help_text, force=True)
 
     # ---- Editor Widget Settings
+    # -------------------------------------------------------------------------
     @on_conf_change(
         option=("provider_configuration", "lsp", "values", "pyflakes"),
         section='completions',
@@ -1191,6 +1192,7 @@ class EditorStack(QWidget, SpyderWidgetMixin):
             finfo.editor.set_current_project_path(root_path)
 
     # ---- Stacked widget management
+    # -------------------------------------------------------------------------
     def get_stack_index(self):
         return self.tabs.currentIndex()
 
@@ -1349,6 +1351,7 @@ class EditorStack(QWidget, SpyderWidgetMixin):
         self.tabs.setTabToolTip(index, tab_tip)
 
     # ---- Context menu
+    # -------------------------------------------------------------------------
     def __setup_menu(self):
         """Setup tab context menu before showing it"""
         self.menu.clear_actions()
@@ -1421,6 +1424,7 @@ class EditorStack(QWidget, SpyderWidgetMixin):
         self.menu.render()
 
     # ---- Hor/Ver splitting actions
+    # -------------------------------------------------------------------------
     def __get_split_actions(self):
         self.versplit_action = self.create_action(
             EditorStackActions.SplitVertically,
@@ -1462,6 +1466,7 @@ class EditorStack(QWidget, SpyderWidgetMixin):
         return actions
 
     # ---- Window actions
+    # -------------------------------------------------------------------------
     def __get_window_actions(self):
         actions = []
         if self.new_window_action:
@@ -1482,6 +1487,7 @@ class EditorStack(QWidget, SpyderWidgetMixin):
         return actions
 
     # ---- New window and close/docking/undocking actions
+    # -------------------------------------------------------------------------
     def __get_main_widget_actions(self):
         actions = []
         if self.parent() is not None:
@@ -1636,6 +1642,7 @@ class EditorStack(QWidget, SpyderWidgetMixin):
         self.refresh()
 
     # ---- Close file, tabwidget...
+    # -------------------------------------------------------------------------
     def close_file(self, index=None, force=False):
         """Close file (index=None -> close current file)
         Keep current file index unchanged (if current file
@@ -1805,6 +1812,7 @@ class EditorStack(QWidget, SpyderWidgetMixin):
         self.last_closed_files = fnames
 
     # ---- Save
+    # -------------------------------------------------------------------------
     def save_if_changed(self, cancelable=False, index=None):
         """Ask user to save file if modified.
 
@@ -2571,6 +2579,7 @@ class EditorStack(QWidget, SpyderWidgetMixin):
         self.sig_refresh_eol_chars.emit(os_name)
 
     # ---- Load, reload
+    # -------------------------------------------------------------------------
     def reload(self, index):
         """Reload file from disk."""
         finfo = self.data[index]
@@ -3005,6 +3014,7 @@ class EditorStack(QWidget, SpyderWidgetMixin):
                 )
 
     # ---- Run
+    # -------------------------------------------------------------------------
     def _get_lines_cursor(self, direction):
         """ Select and return all lines from cursor in given direction"""
         editor = self.get_current_editor()
@@ -3122,7 +3132,36 @@ class EditorStack(QWidget, SpyderWidgetMixin):
 
         return text, off_pos, col_pos, cell_name, encoding
 
+    # ---- For panels, extensions and shortcuts
+    # -------------------------------------------------------------------------
+    def add_extension(self, extension: type[EditorExtension]):
+        """Add an extension to all CodeEditors."""
+        for finfo in self.data:
+            finfo.editor.editor_extensions.add(extension())
+
+    def remove_extension(self, extension: type[EditorExtension]):
+        """Remove an extension from all CodeEditors."""
+        for finfo in self.data:
+            finfo.editor.editor_extensions.remove(extension)
+
+    def add_panel(
+        self, panel: type[Panel], position: PanelPosition = PanelPosition.LEFT
+    ):
+        """Add a panel to all CodeEditors."""
+        for finfo in self.data:
+            panel_instance = finfo.editor.panels.register(panel(), position)
+            panel_instance.show()
+            finfo.editor.panels.refresh()
+
+    def remove_panel(
+        self, panel: type[Panel], position: PanelPosition = PanelPosition.LEFT
+    ):
+        """Remove a panel from all CodeEditors."""
+        for finfo in self.data:
+            finfo.editor.panels.remove(panel, position)
+
     # ---- Drag and drop
+    # -------------------------------------------------------------------------
     def dragEnterEvent(self, event):
         """
         Reimplemented Qt method.
