f0380d6d3b1e63334fb2ff60c34e9ae77cd9e7ea
Carlos Cordoba <ccordoba12@gmail.com>
2026-05-06 17:24:31 -0500

    Profiler: Fix shortcuts for its run actions
    - Make "profile file" a global shortcut (i.e. the same as the for "debug
    file"), and "profile cell/selection" work only in Editor.
    - Also, fix context of "run file in pylint" shortcut, which is global.

--- spyder/config/main.py
+++ spyder/config/main.py
@@ -562,11 +562,11 @@ DEFAULTS = [
               'internal_console/clear shell': "Ctrl+L",
               'internal_console/clear line': "Shift+Escape",
               # -- Pylint --
-              'pylint/run file in pylint': "F8",
+              '_/run file in pylint': "F8",
               # -- Profiler --
-              'profiler/run file in profiler': "F10",
-              'profiler/run cell in profiler': "Alt+F10",
-              'profiler/run selection in profiler': "",
+              '_/run file in profiler': "F10",
+              'editor/run cell in profiler': "Alt+F10",
+              'editor/run selection in profiler': "",
               'profiler/find_action': "Ctrl+F",
               # -- Switcher --
               '_/file switcher': 'Ctrl+P',
