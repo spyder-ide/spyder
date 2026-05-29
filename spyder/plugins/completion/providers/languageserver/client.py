5c8aa2143f6dc9d12e23232700a8d5ed0c248b8a
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2026-05-28 21:08:51 -0300

    feat: specify position encoding used by spyder for lsp

--- spyder/plugins/completion/providers/languageserver/client.py
+++ spyder/plugins/completion/providers/languageserver/client.py
@@ -659,4 +659,10 @@ class LSPClient(LSPMethodProviderMixIn, SpyderConfigurationAccessor, QObject):
                     dynamic_registration=True,
                 ),
             ),
+
+            general=lsp.GeneralClientCapabilities(
+                position_encodings=[
+                    lsp.PositionEncodingKind.Utf16,
+                ],
+            ),
         )
