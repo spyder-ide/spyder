75151c4647ba2a672b70da8c49bc5eb98d524f17
Aaron <athompson6735@gmail.com>
2026-08-03 12:28:46 -0400

    potentially fix linux pyqt6 test by forcing enum to int

--- spyder/utils/stylesheet.py
+++ spyder/utils/stylesheet.py
@@ -443,13 +443,17 @@ class CloseButtonTabBarStyle(QCommonStyle, SpyderConfigurationAccessor):
             section="main"
         )
         if position == "left":
-            self.close_btn_side = QTabBar.LeftSide
+            self.close_btn_side = QTabBar.ButtonPosition.LeftSide
         elif position == "right":
-            self.close_btn_side = QTabBar.RightSide
+            self.close_btn_side = QTabBar.ButtonPosition.RightSide
         else:
             self.close_btn_side = (
-                QTabBar.LeftSide if MAC else QTabBar.RightSide
+                QTabBar.ButtonPosition.LeftSide
+                if MAC
+                else QTabBar.ButtonPosition.RightSide
             )
+        if isinstance(self.close_btn_side, QTabBar.ButtonPosition):
+            self.close_btn_side = self.close_btn_side.value
         
     def styleHint(self, hint, options=None, widget=None, returnData=None):
         if hint == QStyle.SH_TabBar_CloseButtonPosition:
