95fde6fd3f8789c1590d2d171009789f08529d3b
Carlos Cordoba <ccordoba12@gmail.com>
2026-08-07 13:10:54 -0500

    Editor: Fix re-highlighting files that use the Pygments highlighter

--- spyder/plugins/editor/widgets/codeeditor/codeeditor.py
+++ spyder/plugins/editor/widgets/codeeditor/codeeditor.py
@@ -450,7 +450,8 @@ class CodeEditor(
         self.timer_syntax_highlight = QTimer(self)
         self.timer_syntax_highlight.setSingleShot(True)
         self.timer_syntax_highlight.timeout.connect(
-            self.run_pygments_highlighter)
+            self.run_pygments_highlighter
+        )
 
         # Mark occurrences timer
         self.occurrence_highlighting = None
@@ -3987,7 +3988,7 @@ class CodeEditor(
     def run_pygments_highlighter(self):
         """Run pygments highlighter."""
         if isinstance(self.highlighter, sh.PygmentsSH):
-            self.highlighter.make_charlist()
+            self.highlighter.rehighlight()
 
     def get_pattern_at(self, coordinates):
         """
