c9c4ac1cafbb139de1d90004926e9bb6927ec589
Carlos Cordoba <ccordoba12@gmail.com>
2026-08-11 22:46:14 -0500

    Fix error on finishing of remote client tests

--- spyder/api/plugins/tests.py
+++ spyder/api/plugins/tests.py
@@ -77,6 +77,7 @@ class MainWindowMock(QMainWindow):
         super().__init__()
         PLUGIN_REGISTRY.main = self
         self.is_setting_up = True
+        self.is_closing = False
 
     def register_plugin(self, plugin_class: type[SpyderPluginClass]):
         plugin = PLUGIN_REGISTRY.register_plugin(self, plugin_class)
@@ -86,9 +87,7 @@ class MainWindowMock(QMainWindow):
     @staticmethod
     def unregister_plugin(plugin: SpyderPluginClass):
         try:
-            PLUGIN_REGISTRY.delete_plugin(
-                plugin.NAME
-            )
+            PLUGIN_REGISTRY.delete_plugin(plugin.NAME)
         except KeyError:
             pass
 
