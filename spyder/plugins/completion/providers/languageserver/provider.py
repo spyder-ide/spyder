c91ce3a5523f5e5a4cdc7f48278b465d68457064
Carlos Cordoba <ccordoba12@gmail.com>
2026-08-14 19:46:25 -0700

    Backport PR #26227 on branch 6.x: (PR: Use proper option key to enable/disable only ruff formatter (Completions/Linting)) (#26263)

--- spyder/plugins/completion/providers/languageserver/provider.py
+++ spyder/plugins/completion/providers/languageserver/provider.py
@@ -812,12 +812,16 @@ class LanguageServerProvider(SpyderCompletionProvider):
 
         # Enabling/disabling formatters
         formatters = ['autopep8', 'yapf', 'black', 'ruff']
-        formatter_options = {
-            fmt: {
-                'enabled': fmt == formatter
-            }
-            for fmt in formatters
-        }
+        formatter_options = {}
+        for fmt in formatters:
+            # Needed to handle a specific key for ruff (`formatEnabled`)
+            # See spyder-ide/spyder#26138
+            format_key = 'formatEnabled' if fmt == 'ruff' else 'enabled'
+            formatter_options.update({
+                fmt: {
+                    format_key: fmt == formatter
+                }
+            })
 
         # Setting max line length for formatters.
         # Notes:
