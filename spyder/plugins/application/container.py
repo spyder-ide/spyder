e568b605387078b1804bf63a52e5b464b709f773
Daniel Althviz Moré <16781833+dalthviz@users.noreply.github.com>
2026-08-19 16:27:16 -0500

    Update Changelog [ci skip]

--- spyder/plugins/application/container.py
+++ spyder/plugins/application/container.py
@@ -66,7 +66,7 @@ from spyder.widgets.helperwidgets import MessageCheckBox
 DONATIONS_URL = "https://www.spyder-ide.org/donate"
 CHANGELOG_URL = (
     "https://github.com/spyder-ide/spyder/blob/6.x/changelogs/"
-    "Spyder-6.md#version-616-2026-07-28"
+    "Spyder-6.md#version-620a2-2026-08-19"
 )
 
 
