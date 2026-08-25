82f5ca6ae0b8911647cfbeb2c148bb42fbd2502b
Juan Sebastian Bautista <js.bautista@uniandes.edu.co>
2026-08-24 21:41:49 -0500

    Add Tests to get_size and test pandas return

--- spyder/plugins/variableexplorer/widgets/tests/test_collectionsdelegate.py
+++ spyder/plugins/variableexplorer/widgets/tests/test_collectionsdelegate.py
@@ -6,10 +6,14 @@
 
 """Tests for collectionsdelegate.py."""
 
+import numpy as np
+import pandas as pd
 # Local imports
 from spyder.plugins.variableexplorer.widgets.collectionsdelegate import (
     CollectionsDelegate,
 )
+from spyder.plugins.variableexplorer.widgets import collectionsdelegate
+from spyder_kernels.utils.nsview import get_size
 
 
 class MockEditor:
@@ -36,3 +40,35 @@ def test_close_all_editors():
     assert editor_1.rejected
     assert editor_2.rejected
     assert not delegate._editors
+
+class Cell:
+    def __init__(self, value):
+        self.value = value
+
+    def data(self):
+        return self.value
+
+
+class FakeIndex:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def row(self):
+        return 0
+
+    def sibling(self, row, column):
+        if column == 1:
+            return Cell("DataFrame")
+        elif column == 2:
+            return Cell(str(get_size(self.obj)))
+
+
+def test_show_warning_dataframe(monkeypatch):
+    """Test warning for large DataFrames."""
+    monkeypatch.setattr(collectionsdelegate, "LARGE_ARRAY", 10)
+
+    df = pd.DataFrame(np.random.rand(2, 6))
+
+    delegate = CollectionsDelegate()
+
+    assert delegate.show_warning(FakeIndex(df))
